fd2363bed06e6fffc1299e893752014bba40c7f3
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-08 13:44:39 +0300

    Use Sync API Umbrella Users method.

--- server/startup.js
+++ server/startup.js
@@ -1,29 +1,13 @@
 Meteor.startup(function () {
   // Check if API Umbrella settings are available
   SyncedCron.add({
-    name: 'Testing cron task',
+    name: 'Sync API Umbrella Users',
     schedule: function(parser) {
       // parser is a later.parse object
-      return parser.text('every 1 second');
+      return parser.text('every 1 day');
     },
     job: function() {
-      console.log("here..");
-      if (Meteor.settings.api_umbrella) {
-        // Get users from API Umbrella instance
-        var response = apiUmbrellaWeb.adminApi.v1.apiUsers.getUsers();
-
-        // Add each user to collection if not already there
-        _.each(response.data.data, function (item) {
-          // Get existing user
-          var existingUser = ApiUmbrellaUsers.findOne({'id': item.id});
-
-          // If user doesn't exist in collection, insert into collection
-          if (! existingUser ) {
-            ApiUmbrellaUsers.insert(item);
-          };
-        });
-      };
-      return false;
+      Meteor.call("syncApiUmbrellaUsers");
     }
   });
 });
