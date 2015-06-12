761db3b785cf976f5c4472159c3eacf553147521
nizyev <nzv@protonmail.ch>
2015-06-12 11:01:59 +0300

    cleaned comments

--- server/publish/apiBackends.js
+++ server/publish/apiBackends.js
@@ -1,12 +1,3 @@
-Meteor.publish('apibackends', function () {
-//   var self = this;
-//   var response = apiUmbrellaWeb.getUsers();
-//
-//   _.each(response.data, function (item) {
-//     self.added('apiUmbrellaUsers', Ramdom.id(), item);
-//   });
-//
-//   self.ready();
-
+Meteor.publish('apiBackends', function () {
   return ApiBackends.find();
 });
