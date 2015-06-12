ba4ccfe9d2274f58e6ac9da92a1b78736a4d37de
nizyev <nzv@protonmail.ch>
2015-06-12 11:02:30 +0300

    sync method added to syncedcron

--- server/startup.js
+++ server/startup.js
@@ -1,36 +1,18 @@
-//Meteor.startup(function () {
-//  // Check if API Umbrella settings are available
-//  SyncedCron.add({
-//    name: 'Sync API Umbrella Users',
-//    schedule: function(parser) {
-//      // parser is a later.parse object
-//      return parser.text('every 1 day');
-//    },
-//    job: function() {
-//      Meteor.call("syncApiUmbrellaUsers");
-//    }
-//  });
-//});
-//
-//SyncedCron.start();
-
-
 Meteor.startup(function () {
   // Check if API Umbrella settings are available
-  if (Meteor.settings.api_umbrella) {
-    // Get users from API Umbrella instance
-    var response = apiUmbrellaWeb.adminApi.v1.apiBackends.getApiBackends();
-    console.log(response.data);
-
-    // Add each user to collection if not already there
-    _.each(response.data, function (item) {
-      // Get existing user
-      var existingApi = ApiBackends.findOne({'id': item.id});
-
-      // If user doesn't exist in collection, insert into collection
-      if (! existingApi ) {
-          ApiBackends.insert(item);
-      };
-    });
-  };
+  SyncedCron.add({
+    name: 'Sync API Umbrella Users and API Backends',
+    schedule: function(parser) {
+      // parser is a later.parse object
+      return parser.text('every 1 day');
+    },
+    job: function() {
+      Meteor.call("syncApiUmbrellaUsers");
+      Meteor.call("syncApiBackends");
+    }
+  });
+  Meteor.call("syncApiUmbrellaUsers");
+  Meteor.call("syncApiBackends");
 });
+
+SyncedCron.start();
