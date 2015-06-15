9b66ead20d98112f7eba64e442565072c3bd656f
nizyev <nzv@protonmail.ch>
2015-06-15 15:07:20 +0300

    Created AdminUsers collection, Created AdminUsers schema

---
+++ collections/admin.js
@@ -0,0 +1,38 @@
+ApiUmbrellaAdmins = new Mongo.Collection('apiUmbrellaAdmins');
+
+ApiUmbrellaAdminsSchema = new SimpleSchema({
+  _id: {
+    type: String,
+    optional: true
+  },
+  username: {
+    type: String,
+    optional: true
+  },
+  email: {
+    type: String,
+    optional: true
+  },
+  name: {
+    type:String,
+    optional: true
+  },
+  notes:{
+    type:String,
+    optional: true
+  },
+  superuser:{
+    type:Boolean,
+    optional: true
+  },
+  authentication_token:{
+    type:String,
+    optional: true
+    },
+  last_sign_in_provider:{
+    type:String,
+    optional: true
+  }
+});
+
+ApiUmbrellaAdmins.attachSchema(ApiUmbrellaAdminsSchema);
