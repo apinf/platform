b112ebe255f5ca437ddc256c03a5c803907d0f68
nizyev <nzv@protonmail.ch>
2015-06-16 15:17:03 +0300

    changed 'item' to 'backend' for better readability

--- server/methods/apiBackends.js
+++ server/methods/apiBackends.js
@@ -6,13 +6,13 @@ Meteor.methods({
       var response = apiUmbrellaWeb.adminApi.v1.apiBackends.getApiBackends();
       var apiBackends = response.data.data;
 
-      _.each(apiBackends, function (item) {
+      _.each(apiBackends, function (backend) {
         // Get existing API Backend
-        var existingApiBackend = ApiBackends.findOne({'id': item.id});
+        var existingApiBackend = ApiBackends.findOne({'id': backend.id});
 
         // If API Backend doesn't exist in collection, insert into collection
         if (! existingApiBackend ) {
-          ApiBackends.insert(item);
+          ApiBackends.insert(backend);
         };
       });
     };
