b58e345f364b7571f6b4ed890f3d4ba62b116201
nizyev <nzv@protonmail.ch>
2015-06-22 14:20:35 +0300

    Initialize collapse button, show/hide sidebar

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,12 +1,7 @@
 Template.sidebar.rendered = function() {
 
   // Initialize collapse button
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({'edge': 'left'});
   // Initialize collapsible
   $('.collapsible').collapsible();
-
-  // Show sideNav
-  $('.button-collapse').sideNav('show');
-  // Hide sideNav
-  $('.button-collapse').sideNav('hide');
 }
