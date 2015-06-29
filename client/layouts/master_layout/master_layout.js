2b0592177346008326b1044df54420f484d2f0f4
nizyev <nzv@protonmail.ch>
2015-06-29 11:00:53 +0300

    Closes side-nav on <a> clicks

--- client/layouts/master_layout/master_layout.js
+++ client/layouts/master_layout/master_layout.js
@@ -1,6 +1,6 @@
 Template.masterLayout.rendered = function() {
   // Initialize collapse button
-  $('.button-collapse').sideNav({'edge': 'left'});
+  $('.button-collapse').sideNav({'edge': 'left', 'closeOnClick': true});
   // Initialize collapsible
   $('.collapsible').collapsible();
 }
