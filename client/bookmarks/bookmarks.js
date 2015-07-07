6c12891bf29a101def392c0893cff7142513c5a6
JuusoV <juuso.vallius@cs.tamk.fi>
2015-07-07 15:14:06 +0300

    Added userbookmarks helper

---
+++ client/bookmarks/bookmarks.js
@@ -0,0 +1,10 @@
+Template.bookmarks.helpers({
+  userBookmarks : function() {
+    /*
+  1. Populate the currentUser with the id of the current user.
+  2. Get all bookmarkedApis from the collection for current user
+  */
+    var currentUserId = Meteor.userId();
+    return ApiBookmarks.find({user: currentUserId});
+  }
+});
