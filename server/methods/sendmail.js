0fe67ad28fac29161e7c733e97299a00abc0f361
JuusoV <juuso.vallius@cs.tamk.fi>
2015-07-09 15:17:54 +0300

    Created method sendmail

---
+++ server/methods/sendmail.js
@@ -0,0 +1,15 @@
+Meteor.methods({
+
+  "sendmail": function(to) {
+    var to = to,
+    from = "register@apinf.com";
+
+    Email.send({
+      from: from,
+      to: to,
+      subject: "Registration complete ",
+      text: "Welcome to Apinf. Enjoy your stay in the world of APIs. The third round"
+    });
+  }
+
+});
