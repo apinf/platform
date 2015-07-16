4dc9cae2c4ce8ce5580d293be7fb86019d1848d5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-07-16 16:10:22 +0300

    Added isServer check; additional comments TODO

--- both/collections/backendConfigurations.js
+++ both/collections/backendConfigurations.js
@@ -1,3 +1,14 @@
 ApiBackendConfigurations = new FS.Collection("apiBackendConfigurations", {
+  // TODO: Determine how to upload files within the project directory without hardcoding FS path
   stores: [new FS.Store.FileSystem("apiBackendConfigurations", {path: "~/apiConfigurations"})]
 });
+
+if (Meteor.isServer) {
+  ApiBackendConfigurations.allow({
+    insert: function (userId, doc) {
+      // allow insert by default
+      // TODO: determine proper upload/insert permisison
+      return true;
+    }
+  });
+}
