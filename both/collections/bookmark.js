27ff9636496b13c7ab03e99e1cedcd3e9120fdaa
el nzv <nzv@protonmail.ch>
2015-07-20 11:39:04 +0300

    fix: cleaning up changes form another branch

--- both/collections/bookmark.js
+++ both/collections/bookmark.js
@@ -2,16 +2,7 @@ ApiBookmarks = new Mongo.Collection('apiBookmarks');
 
 ApiBookmarks.helpers({
   apis : function() {
-  return ApiBackends.find({_id :{$in: this.apiIds}})
-}
-
-});
-
-ApiBookmarks.allow({
-  update: function () {
-    return true;
-  },
-  remove: function () {
-    return true;
+    return ApiBackends.find({_id :{$in: this.apiIds}})
   }
+
 });
