317b43e94a938b170581fe966c42e84ef7c64b72
JuusoV <juuso.vallius@cs.tamk.fi>
2015-07-21 15:49:38 +0300

    To test the functionality with dummy data

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,20 +1,38 @@
 // Creates variable that can be used in both .rendered and .created
 Template.map.rendered = function() {
-  var input = {
+  /*  var input = {
     index : "api-umbrella-logs-v1-2015-07",
     type  : "log",
     limit : 100000,
     query : {
       match_all: {}
     }
-  };
+  };*/
+
+  var input = [
+    [-37.8839, 175.3745188667, "571"],
+    [-37.8869090667, 175.3657417333, "486"],
+    [-37.8894207167, 175.4015351167, "807"],
+    [-37.8927369333, 175.4087452333, "899"],
+    [-37.90585105, 175.4453463833, "1273"],
+    [-37.9064188833, 175.4441556833, "1258"],
+    [-37.90584715, 175.4463564333, "1279"],
+    [-37.9033391333, 175.4244005667, "1078"],
+    [-37.9061991333, 175.4492620333, "1309"],
+    [-37.9058955167, 175.4445613167, "1261"],
+    [-37.88888045, 175.39146475, "734"],
+    [-37.8950811333, 175.41079175, "928"]
+  ]
+
+
+
   drawMap(input);
 }
 Template.map.created = function() {
   drawMap = function (input) {
 
     // Creates the map with the view coordinates of 61.5, 23.7667 and the zoom of 6
-    var map = L.map('map').setView([61.5, 23.7667], 6);
+    var map = L.map('map').setView([-37.8839, 175.3745188667], 6);
 
     // adds tilelayer
     var tiles = L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
@@ -29,16 +47,18 @@ Template.map.created = function() {
     var density = 1000;
     var blur = 5;
 
+/*
     // Gets data from the ElasticSearch
     Meteor.call("getChartData", input, function(err, data) {
       var items = data.hits.hits;
       //loops throught the array of objects
       items.forEach(function(item) {
-            addressPoints.push([item._source.request_ip_location.lat, item._source.request_ip_location.lon, density, blur])
+        addressPoints.push([item._source.request_ip_location.lat, item._source.request_ip_location.lon, density, blur])
       });
     });
+*/
 
     // adds the heatpoints to the map
-    var heat = L.heatLayer(addressPoints).addTo(map);
+    var heat = L.heatLayer(input).addTo(map);
   }
 }
