251ae4639d0f1a5b620637b8d30b04ecd0ee080f
JuusoV <juuso.vallius@cs.tamk.fi>
2015-07-21 11:33:08 +0300

    Cleanup

--- server/methods/elasticSearch.js
+++
@@ -1,8 +0,0 @@
-Meteor.methods({
-  "getChartData": function (data) {
-
-    var newSearch = new ElasticRest(data.index, data.type, data.limit, data.query);
-
-    return newSearch.doSearch();
-  }
-});
