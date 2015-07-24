ae1fa69f3cb50e0001bd265a0b93f0fae5f40edd
el nzv <nzv@protonmail.ch>
2015-07-24 09:11:05 +0300

    cleaned up

--- client/shared/navbar.js
+++
@@ -1,8 +0,0 @@
-Template.navbar.helpers({
-  profileImageUrl: function() {
-    // get a object with profile image url
-    var profilePicture = ProfilePictures.findOne({});
-    // return that url
-    return profilePicture.url();
-  }
-})
