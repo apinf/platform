40c6be84ab972bb75db69f004ffd549e1473d56d
Damir Mustafin <frenchthebread@gmail.com>
2015-07-27 18:42:19 +0300

    added "fields" argument to ElasticRest class

--- server/methods/elasticSearch.js
+++ server/methods/elasticSearch.js
@@ -1,7 +1,19 @@
 Meteor.methods({
   "getChartData": function (data) {
 
-    var newSearch = new ElasticRest(data.index, data.type, data.limit, data.query);
+    var newSearch = new ElasticRest(
+      data.index,
+      data.type,
+      data.limit,
+      data.query,
+      [
+        'request_at',
+        'request_ip_country',
+        'request_ip',
+        'response_time',
+        'request_path'
+      ]
+    );
 
     return newSearch.doSearch();
   }
