6593913c78bb807bb17bfa7dc4eac0da6cea9190
el nzv <nzv@protonmail.ch>
2015-07-28 11:31:46 +0300

    schema for contact form

---
+++ both/contactForm.js
@@ -0,0 +1,31 @@
+Schema = {};
+Schema.contact = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Your name",
+    max: 50,
+    optional: false,
+    autoform: {
+      placeholder: 'Your name'
+    }
+  },
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+    label: "E-mail address",
+    optional: false,
+    autoform: {
+      placeholder: 'Your email'
+    }
+  },
+  message: {
+    type: String,
+    label: "Message",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 3,
+      placeholder: 'Your message'
+    }
+  }
+});
