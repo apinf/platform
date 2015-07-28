f573e8ca078532e2ce708b8ef6fee162b554e976
el nzv <nzv@protonmail.ch>
2015-07-28 16:51:54 +0300

    added a basic filter for swagger upload

--- both/collections/swaggerConfigurations.js
+++ both/collections/swaggerConfigurations.js
@@ -1,6 +1,20 @@
 SwaggerBackendConfigurations = new FS.Collection("swaggerBackendConfigurations", {
   // TODO: Determine how to upload files within the project directory without hardcoding FS path
-  stores: [new FS.Store.FileSystem("swaggerBackendConfigurations", {path: "~/apinf/swaggerConfigurations"})]
+  stores: [new FS.Store.FileSystem("swaggerBackendConfigurations", {path: "~/apinf/swaggerConfigurations"})],
+  filter: {
+    allow: {
+      // ~ 10Mbs.
+      maxSize: 10048567,
+      extensions: ['json']
+    }
+  },
+  onInvalid: function (message) {
+    if (Meteor.isClient) {
+      alert(message);
+    } else {
+      console.log(message);
+    }
+  }
 });
 
 if (Meteor.isServer) {
