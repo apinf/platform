2fe41bfd00b28b6f19013bf51d66b5df6802fab8
el nzv <nzv@protonmail.ch>
2015-07-28 14:21:28 +0300

    added autoform hooks to disable fields while submitting

--- client/views/home/contact_form/contactForm.js
+++ client/views/home/contact_form/contactForm.js
@@ -3,3 +3,16 @@ Template.contactForm.helpers({
     return Schema.contact;
   }
 });
+
+AutoForm.hooks({
+  contactForm: {
+    beginSubmit: function () {
+      // disable form elements while submitting form
+      $('[data-schema-key],button').attr("disabled", "disabled");
+    },
+    endSubmit: function () {
+      // enable form elements after form submission
+      $('[data-schema-key],button').removeAttr("disabled");
+    }
+  }
+});
