564e57fd060ef4f7ac6add26b5e86256060c2529
el nzv <nzv@protonmail.ch>
2015-07-30 15:01:26 +0300

    toggle icons on events

--- client/views/api_backends/apiBackends.js
+++ client/views/api_backends/apiBackends.js
@@ -1,6 +1,15 @@
 Template.apiBackends.rendered = function () {
+
   // Hides blocks on template load
   $('#global-request-block, #sub-url-block, #advanced-block').collapse({
     hide: true
+  });
+
+  // Toggles icon on hide and show events
+  $('#global-request-block, #sub-url-block, #advanced-block').on('shown.bs.collapse', function () {
+    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+  });
+  $('#global-request-block, #sub-url-block, #advanced-block').on('hidden.bs.collapse', function () {
+    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
   });
 }
