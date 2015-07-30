d4153be23900f0abd5d61a4d8aae56423eb756b6
JuusoV <juuso.vallius@cs.tamk.fi>
2015-07-30 13:11:11 +0300

    Updating branch

--- both/collections/backendConfigurations.js
+++ both/collections/backendConfigurations.js
@@ -3,6 +3,18 @@ ApiBackendConfigurations = new FS.Collection("apiBackendConfigurations", {
   stores: [new FS.Store.FileSystem("apiBackendConfigurations", {path: "~/apinf/apiConfigurations"})]
 });
 
+ApiBackendConfigurations.filters({
+  allow: {
+    // ~ 10Mbs.
+    maxSize: 10048567,
+    extensions: ['json', 'yaml', 'txt']
+  },
+  onInvalid: function (message) {
+    // Show error message if it doesn't pass filter settings
+    FlashMessages.sendError(message);
+  }
+});
+
 if (Meteor.isServer) {
   ApiBackendConfigurations.allow({
     insert: function (userId, doc) {
--- both/collections/swaggerConfigurations.js
+++ both/collections/swaggerConfigurations.js
@@ -1,6 +1,18 @@
 SwaggerBackendConfigurations = new FS.Collection("swaggerBackendConfigurations", {
   // TODO: Determine how to upload files within the project directory without hardcoding FS path
-  stores: [new FS.Store.FileSystem("swaggerBackendConfigurations", {path: "~/apinf/swaggerConfigurations"})]
+  stores: [new FS.Store.FileSystem("swaggerBackendConfigurations", {path: "~/apinf/swaggerConfigurations"})],
+});
+
+SwaggerBackendConfigurations.filters({
+  allow: {
+    // ~ 10Mbs.
+    maxSize: 10048567,
+    extensions: ['json', 'yaml', 'txt', 'swagger']
+  },
+  onInvalid: function (message) {
+    // Show error message if it doesn't pass filter settings
+    FlashMessages.sendError(message);
+  }
 });
 
 if (Meteor.isServer) {
