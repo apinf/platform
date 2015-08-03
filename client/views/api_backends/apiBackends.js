aca60fc2588b81cd9ffb7d43d62ebb3e12eaa074
elnzv <illya.nizyev@gmail.com>
2015-08-03 08:34:04 +0300

    new icons ; new block options

--- client/views/api_backends/apiBackends.js
+++ client/views/api_backends/apiBackends.js
@@ -1,15 +1,15 @@
 Template.apiBackends.rendered = function () {
 
   // Hides blocks on template load
-  $('#global-request-block, #sub-url-block, #advanced-block').collapse({
+  $('#global-request-block, #sub-url-block, #advanced-block, #advanced-rewriting-block').collapse({
     hide: true
   });
 
   // Toggles icon on hide and show events
-  $('#global-request-block, #sub-url-block, #advanced-block').on('shown.bs.collapse', function () {
-    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-right").addClass("glyphicon-chevron-down");
+  $('#global-request-block, #sub-url-block, #advanced-block, #advanced-rewriting-block').on('shown.bs.collapse', function () {
+    $(this).prev().find(".fa").removeClass("fa-chevron-right").addClass("fa-chevron-down");
   });
-  $('#global-request-block, #sub-url-block, #advanced-block').on('hidden.bs.collapse', function () {
-    $(this).prev().find(".glyphicon").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-right");
+  $('#global-request-block, #sub-url-block, #advanced-block, #advanced-rewriting-block').on('hidden.bs.collapse', function () {
+    $(this).prev().find(".fa").removeClass("fa-chevron-down").addClass("fa-chevron-right");
   });
 }
