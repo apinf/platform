78cb381742d745de75ad962df9de0e8da2caf6bd
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-04 15:03:33 +0300

    Removed profile photo allow rules.

--- server/allow.js
+++ server/allow.js
@@ -1,15 +1,3 @@
-ProfilePictures.allow({
-  insert: function(userId, doc) {
-    return true;
-  },
-  update: function(userId, doc, fieldNames, modifier) {
-    return true;
-  },
-  download: function(userId) {
-    return true;
-  }
-});
-
 Meteor.users.allow({
   update: function(userId, doc, fieldNames, modifier) {
     if (userId === doc._id && !doc.username && fieldNames.length === 1 && fieldNames[0] === 'username') {
