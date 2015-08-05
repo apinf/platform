0d05f85aa244395c2e092fce5a461fd57591f38a
Damir Mustafin <frenchthebread@gmail.com>
2015-08-05 14:08:13 +0300

    checking if user is admin

--- server/methods/elasticSearch.js
+++ server/methods/elasticSearch.js
@@ -1,6 +1,29 @@
 Meteor.methods({
   "getChartData": function (data) {
 
+    // get userId
+
+    // get user object
+
+    // get user's api_key
+
+    // get user role
+
+    // check user role
+
+    // construct query depending on user role
+
+    // if admin - match_all
+
+    // else - match api_key: api_key
+
+    var loggedInUser = Meteor.user();
+
+    if (Roles.userIsInRole(loggedInUser, ['admin'])) {
+      // NOTE: This example assumes the user is not using groups.
+      console.log("User is admin");
+    }
+
     var newSearch = new ElasticRest(
       data.index,
       data.type,
