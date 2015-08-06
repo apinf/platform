620c3509e1efa8da46a7343a53957fecc16ff8bc
Damir Mustafin <frenchthebread@gmail.com>
2015-08-06 16:26:03 +0300

    populating form with data from api config file

--- client/views/api_backends/import_config/import.js
+++ client/views/api_backends/import_config/import.js
@@ -12,12 +12,35 @@ Template.importApiConfiguration.events({
   },
   'submit #apiConfigurationUploadForm': function (event, template) {
     Meteor.call("convertYamlToJson", template.reactiveFile.get().name, function (err, file) {
+
       if (err) console.log(err);
-      console.log(file);
-      $('#apiBackends').attr({
-        'doc'   : JSON.stringify(file),
-        'type'  : "update"
-      });
+
+      for (var key in  file) {
+        if (file.hasOwnProperty(key)) {
+
+          switch (key){
+            case "backend_protocol":
+                      $("select[data-schema-key='"+key+"'] option[value='"+file[key]+"']").attr('selected', 'selected');
+                  break;
+            case "url_matches":
+                    for(var i = 0; i<file[key].length; i++){
+                      $("input[data-schema-key='matching."+i+".frontend_prefix']").val(file[key][i].frontend_prefix);
+                      $("input[data-schema-key='matching."+i+".backend_prefix']").val(file[key][i].backend_prefix);
+                    }
+                  break;
+            case "servers":
+                    for(var i = 0; i<file[key].length; i++){
+                      $("input[data-schema-key='server."+i+".backend_host']").val(file[key][i].host);
+                      $("input[data-schema-key='server."+i+".backend_port']").val(file[key][i].port);
+                    }
+                  break;
+            default :
+                    $("input[data-schema-key='"+key+"']").val(file[key]);
+                  break;
+
+          }
+        }
+      }
     });
 
     return false;
