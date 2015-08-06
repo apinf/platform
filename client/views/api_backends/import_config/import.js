38e6c13acb9bc4de063268ca6a465ce147c1e3e5
Damir Mustafin <frenchthebread@gmail.com>
2015-08-06 22:44:46 +0300

    comments added

--- client/views/api_backends/import_config/import.js
+++ client/views/api_backends/import_config/import.js
@@ -11,31 +11,35 @@ Template.importApiConfiguration.events({
 
   },
   'submit #apiConfigurationUploadForm': function (event, template) {
-    Meteor.call("convertYamlToJson", template.reactiveFile.get().name, function (err, file) {
+
+
+    Meteor.call("convertYamlToJson", template.reactiveFile.get().name, function (err, jsonFile) {
 
       if (err) console.log(err);
 
-      for (var key in  file) {
-        if (file.hasOwnProperty(key)) {
+      // iterating through JSON object keys
+      for (var key in jsonFile) {
+        if (jsonFile.hasOwnProperty(key)) {
 
+          // checking if key field contains object or an array, iterating through it manually as well as populating data
           switch (key){
             case "backend_protocol":
-                      $("select[data-schema-key='"+key+"'] option[value='"+file[key]+"']").attr('selected', 'selected');
+                      $("select[data-schema-key='"+key+"'] option[value='"+jsonFile[key]+"']").attr('selected', 'selected');
                   break;
             case "url_matches":
-                    for(var i = 0; i<file[key].length; i++){
-                      $("input[data-schema-key='matching."+i+".frontend_prefix']").val(file[key][i].frontend_prefix);
-                      $("input[data-schema-key='matching."+i+".backend_prefix']").val(file[key][i].backend_prefix);
+                    for(var i = 0; i<jsonFile[key].length; i++){
+                      $("input[data-schema-key='matching."+i+".frontend_prefix']").val(jsonFile[key][i].frontend_prefix);
+                      $("input[data-schema-key='matching."+i+".backend_prefix']").val(jsonFile[key][i].backend_prefix);
                     }
                   break;
             case "servers":
-                    for(var i = 0; i<file[key].length; i++){
-                      $("input[data-schema-key='server."+i+".backend_host']").val(file[key][i].host);
-                      $("input[data-schema-key='server."+i+".backend_port']").val(file[key][i].port);
+                    for(var i = 0; i<jsonFile[key].length; i++){
+                      $("input[data-schema-key='server."+i+".backend_host']").val(jsonFile[key][i].host);
+                      $("input[data-schema-key='server."+i+".backend_port']").val(jsonFile[key][i].port);
                     }
                   break;
             default :
-                    $("input[data-schema-key='"+key+"']").val(file[key]);
+                    $("input[data-schema-key='"+key+"']").val(jsonFile[key]);
                   break;
 
           }
