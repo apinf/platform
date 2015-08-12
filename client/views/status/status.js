da44c8a654919ca67eb71ef904d08dca6da77177
Damir Mustafin <frenchthebread@gmail.com>
2015-08-12 16:21:16 +0300

    added inline comments

--- client/views/status/status.js
+++ client/views/status/status.js
@@ -1,43 +1,65 @@
 Template.statusCheck.rendered = function () {
+
+  // calls statusCheck method
   Meteor.call("statusCheck", function (err, status) {
 
+    // initialising DIV tags
     var apinfState       = $('#apinfState');
     var esState          = $('#esState');
     var apiUmbrellaState = $('#apiUmbrellaState');
     var fullState        = $('#fullState');
 
-    //apinf
+    // checks if apinf is operating
     if (status.apinf.operational){
+
+      // if yes, colors text to green color
       apinfState.addClass('text-success');
     }else{
+
+      // if not, colors text to red
       apinfState.addClass('text-danger');
     }
 
+    // adds status message to a template
     apinfState.html(status.apinf.message);
 
-    //apiUmbrella
+    // checks if API Umbrella is operating
     if (status.apiUmbrella.operational){
+
+      // if yes, colors text to green color
       apiUmbrellaState.addClass('text-success');
     }else{
+
+      // if not, colors text to red
       apiUmbrellaState.addClass('text-danger');
     }
 
+    // adds status message to a template
     apiUmbrellaState.html(status.apiUmbrella.message);
 
     //elasticsearch
     if (status.elasticsearch.operational){
+
+      // if yes, colors text to green color
       esState.addClass('text-success');
     }else{
+
+      // if not, colors text to red
       esState.addClass('text-danger');
     }
 
+    // adds status message to a template
     esState.html(status.elasticsearch.message);
 
-    //all
+    // checks if all the services are operational
     if (status.apinf.operational && status.apiUmbrella.operational && status.elasticsearch.operational){
+
+      // if all the systems are operational, adds alert-success class and success message to an element
       fullState.addClass('alert-success');
       fullState.html('All systems operational.')
     }else{
+
+      // if not, adds alert-danger class and message to an element
       fullState.addClass('alert-danger');
       fullState.html('Something is wrong.');
     }
