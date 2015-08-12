85b7547e6614fe9701dceecba2dcdb4682d29e47
Damir Mustafin <frenchthebread@gmail.com>
2015-08-12 15:45:12 +0300

    added apiUmbrella & elastic rest statuses

--- client/views/status/status.js
+++ client/views/status/status.js
@@ -4,16 +4,40 @@ Template.statusCheck.rendered = function () {
     var apinfState       = $('#apinfState');
     var esState          = $('#esState');
     var apiUmbrellaState = $('#apiUmbrellaState');
+    var fullState        = $('#fullState');
 
     //apinf
     if (status.apinf.operational){
-      apinfState.addClass('alert-success');
-      apinfState.html(status.apinf.message);
+      apinfState.addClass('text-success');
+    }else{
+      apinfState.addClass('text-danger');
     }
 
-    //elasticsearch
+    apinfState.html(status.apinf.message);
 
-    //apiumbrell
+    //apiUmbrella
+    if (status.apiUmbrella.operational){
+      apiUmbrellaState.addClass('text-success');
+    }else{
+      apiUmbrellaState.addClass('text-danger');
+    }
+
+    apiUmbrellaState.html(status.apiUmbrella.message);
+
+    //elasticsearch
+    if (status.elasticsearch.operational){
+      esState.addClass('text-success');
+    }else{
+      esState.addClass('text-danger');
+    }
+
+    esState.html(status.elasticsearch.message);
+
+    //all
+    if (status.apinf.operational && status.apiUmbrella.operational && status.elasticsearch.operational){
+      fullState.addClass('alert-success');
+      fullState.html('All systems operational.')
+    }
 
   })
 };
