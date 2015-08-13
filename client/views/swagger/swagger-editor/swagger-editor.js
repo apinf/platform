a686379364ebd3f13ba6231f81d2bc405c2c157c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-13 15:38:36 +0300

    Change variable name to apiDocumentationEditorHost

--- client/views/swagger/swagger-editor/swagger-editor.js
+++ client/views/swagger/swagger-editor/swagger-editor.js
@@ -1,7 +1,9 @@
 Template.editor.helpers({
   // Creates a variable from the
   editorUrl: function () {
-    var editorUrl = Meteor.settings.public.editorCongfiguration.host;
-    return editorUrl;
+    // Get the URL of the API Documentation Editor from public settings
+    var apiDocumentationEditorUrl = Meteor.settings.public.apiDocumentationEditor.host;
+    
+    return apiDocumentationEditorHost;
   }
 });
