80743174b308c14a891abb189246e3f2d52d70c6
JuusoV <juuso.vallius@cs.tamk.fi>
2015-08-13 15:32:26 +0300

    Refactoring the code to fetch the url from settings.json

--- client/views/swagger/swagger-editor/swagger-editor.js
+++ client/views/swagger/swagger-editor/swagger-editor.js
@@ -1,6 +1,7 @@
 Template.editor.helpers({
-  returnUrl: function () {
-    var returnedUrl = "http://editor.apinf.io:49160";
-    return returnedUrl;
+  // Creates a variable from the
+  editorUrl: function () {
+    var editorUrl = Meteor.settings.public.editorCongfiguration.host;
+    return editorUrl;
   }
 });
