372cd6cf93373261400c63909678415f9c2b1e66
JuusoV <juuso.vallius@cs.tamk.fi>
2015-08-13 15:47:55 +0300

    Fixed typo

--- client/views/swagger/swagger-editor/swagger-editor.js
+++ client/views/swagger/swagger-editor/swagger-editor.js
@@ -1,9 +1,9 @@
-Template.editor.helpers({
+Template.apiDocumentationEditor.helpers({
   // Creates a variable from the
   editorUrl: function () {
     // Get the URL of the API Documentation Editor from public settings
-    var apiDocumentationEditorUrl = Meteor.settings.public.apiDocumentationEditor.host;
-    
+    var apiDocumentationEditorHost = Meteor.settings.public.apiDocumentationEditor.host;
+
     return apiDocumentationEditorHost;
   }
 });
