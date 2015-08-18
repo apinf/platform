fa77c26159a02136863578ab3f172e76aeb20749
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-18 15:54:24 +0300

    Add addFirstUserToAdminRole method

--- server/methods/roles.js
+++ server/methods/roles.js
@@ -2,7 +2,7 @@ Meteor.methods({
   "createAdminRoleIfNotDefined": function (adminRole) {
     // TODO: Refactor this process to allow user to specify  admin role as parameter
     // E.g. a config page where the user can input the desired string
-    
+
     // Define the admin role
     var adminRole = 'admin';
 
@@ -26,5 +26,30 @@ Meteor.methods({
       console.log('Defining "' + adminRole + '" role.');
       Roles.createRole(adminRole);
     }
+  },
+  'addFirstUserToAdminRole': function (userId) {
+    // Look for existing users and set initial user as admin if none exist
+    Meteor.call('countUsers', function(error, userCount) {
+      // If there is only one user
+      if (userCount === 1) {
+        // Get the user's roles
+        var userRoles = Roles.getRolesForUser(userId);
+
+        // Define the admin role
+        var adminRole = 'admin';
+
+        // Check if current user is admin
+        var userIsAdmin = _.contains(userRoles, adminRole);
+
+        // If user is not admin
+        if (!userIsAdmin) {
+          // Add user to admin role
+          Roles.addUsersToRoles(userId, ['admin']);
+          console.log("Added initial user as admin:", userId);
+        } else {
+          console.log("User already has admin role.")
+        }
+      }
+    });
   }
 });
