6ef7964aad3880ded75cecdee02839133f5497f9
JuusoV <juuso.vallius@cs.tamk.fi>
2015-08-19 16:21:29 +0300

    Made suggested changes

--- client/views/api_backends/import_documentation/import.js
+++ client/views/api_backends/import_documentation/import.js
@@ -1,10 +1,9 @@
 Template.importdocumentationConfiguration.events({
-  'change #documentationCofigurationFile': function (event, template) {
+  'change #apiDocumentationFile': function (event, template) {
     // Get the submitted file
     var file = event.target.files[0];
 
     // Insert into filesystem collection
-    var insertedFile = documentationBackendConfigurations.insert(file);
-    console.log(insertedFile);
+    var insertedFile = apiDocumentation.insert(file);
   }
 })
