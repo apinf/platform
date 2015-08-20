71cc97a6225edea5f5e7960d3f1a0c1a41edd0db
Damir Mustafin <frenchthebread@gmail.com>
2015-08-20 15:32:20 +0300

    apiIsValid - function added

--- server/methods/converYamlToJson.js
+++ server/methods/converYamlToJson.js
@@ -1,37 +1,98 @@
 Meteor.methods({
-  'convertYamlToJson': function(fileId){
-    //var fs = Npm.require('fs');
+  'importApiConfigs': function(jsonObj){
 
-    var item = ApiBackendConfigurations.findOne(fileId);
+    var status = {
+      isSuccessful: false,
+      message: ""
+    };
 
-    if (item) {
+    if (jsonObj) {
 
-      var Id = item.copies.apiConfigs.key;
+      // parse json object
 
-      console.log(Id)
+      var parsedJson = apiIsValid(jsonObj);
 
-      //var file = configFiles.findOne({ "files_id['_str']": Id });
+      if (parsedJson.isValid) {
 
-      //console.log(file)
+        try{
 
-      //var path = "/uploads/apiBackendConfigs/" + fileFullName;
-      //
-      //var jsonFile;
-      //
-      //try {
-      //  jsonFile = YAML.safeLoad(fs.readFileSync(path, 'utf8'));
-      //
-      //  jsonFile.apis.forEach(function (api) {
-      //    ApiBackends.insert(api);
-      //  });
-      //
-      //} catch (e) {
-      //  console.log(e);
-      //}
+          ApiBackends.insert(jsonObj);
 
-      return "Done";
+          status.isSuccessful = true;
+          status.message      = "API config has been successfully imported."
+
+        }catch(e){
+
+          status.message = JSON.stringify(e);
+
+        }
+
+      }else{
+        status.message = parsedJson.message;
+      }
+
+      return status;
+
     }else{
-      return "Ooops";
+
+      status.message = "Config is not found.";
+
+      return status;
     }
   }
 });
+
+function apiIsValid (jsonObj) {
+
+  var status = {
+    isValid : false,
+    message : ""
+  };
+
+  if (jsonObj.hasOwnProperty("_id")){
+
+    status.message += "'_id' field is not allowed to import."
+
+  }else{
+
+    if (jsonObj.hasOwnProperty("name")){
+
+      if (jsonObj.hasOwnProperty("backend_host")){
+
+        if (jsonObj.hasOwnProperty("backend_protocol")){
+
+          if (jsonObj.hasOwnProperty("frontend_host")){
+
+            if (jsonObj.hasOwnProperty("balance_algorithm")){
+
+              status.isValid = true;
+
+              return status;
+
+            }else{
+              status.message += "'balance_algorithm'";
+            }
+
+          }else{
+            status.message += "'frontend_host'";
+          }
+
+        }else{
+          status.message += "'backend_protocol'";
+        }
+
+      }else{
+        status.message += "'backend_host'";
+      }
+
+    }else{
+      status.message += "'name'";
+    }
+
+    status.message += " field is required.";
+
+  }
+
+
+  return status;
+}
