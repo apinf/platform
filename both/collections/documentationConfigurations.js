f885868a55a01a1515c3d24a803327d7bf0a1ae1
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-20 11:18:58 +0300

    apiDocumentationFiles on filesystem

--- both/collections/documentationConfigurations.js
+++ both/collections/documentationConfigurations.js
@@ -1,6 +1,6 @@
 apiDocumentation = new FS.Collection("apiDocumentation", {
   // TODO: Determine how to upload files within the project directory without hardcoding FS path
-  stores: [new FS.Store.FileSystem("apiDocumentation", {path: "~/apinf/documentationConfigurations"})],
+  stores: [new FS.Store.FileSystem("apiDocumentation", {path: "~/apinf/apiDocumentationFiles"})],
 });
 
 apiDocumentation.filters({
