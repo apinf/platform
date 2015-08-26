9578d3aeed1a7d1e5ad5daf6b2a36bdecde73dd4
Damir Mustafin <frenchthebread@gmail.com>
2015-08-26 13:54:39 +0300

    comments

--- client/views/api_backends/view_api_backend/view_api_backend.js
+++ client/views/api_backends/view_api_backend/view_api_backend.js
@@ -1,4 +1,5 @@
 Template.viewApiBackend.created = function() {
+
   // Create reference to instance
   var instance = this;
 
@@ -7,12 +8,15 @@ Template.viewApiBackend.created = function() {
 
   // Subscribe to a single API Backend, by ID
   instance.subscribe("apiBackend", backendId);
+  
 };
 
 Template.viewApiBackend.rendered = function () {
 
+  // fetches current apiBackend
   var apiBackend = ApiBackends.find().fetch();
 
+  // sets up request url based on protocol and host
   var url = apiBackend[0].backend_protocol + "://" + apiBackend[0].backend_host;
 
   Meteor.call("checkApi", url, function (err, status) {
@@ -21,12 +25,14 @@ Template.viewApiBackend.rendered = function () {
 
       console.log("UP!");
 
+      // updates layout with success status
       $('#apiState').addClass('alert-success').html("API is operating normally.");
 
     }else{
 
       console.log("DOWN!");
 
+      // updates layout with success status
       $('#apiState').addClass('alert-danger').html("API backend is down for some reason. Please contact support.");
 
     }
