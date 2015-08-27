54c6764cb3ed35b84fd4da7c44f9704a20be9620
Damir Mustafin <frenchthebread@gmail.com>
2015-08-27 12:37:07 +0300

    comments

--- client/views/api_backends/view_api_backend/view_api_backend.js
+++ client/views/api_backends/view_api_backend/view_api_backend.js
@@ -12,23 +12,30 @@ Template.viewApiBackend.created = function() {
 Template.viewApiBackend.events({
   'click #exportJSONConfig' : function () {
 
+    // fetches API object
     var apiBackend = ApiBackends.find().fetch();
 
+    // converts JSON object to JSON string and adds indentation
     var jsonObj = JSON.stringify(apiBackend, null, '\t');
 
+    // creates file object with content type of JSON
     var file = new Blob([jsonObj], {type: "application/json;charset=utf-8"});
 
+    // forces "save As" function allow user download file
     saveAs(file, "apiConfig.json");
   },
   'click #exportYAMLConfig' : function () {
 
+    // fetches API object
     var apiBackend = ApiBackends.find().fetch();
 
-
+    // converts from json to yaml
     var yml = jsyaml.safeDump(apiBackend);
 
+    // creates file object with content type of YAML
     var file = new Blob([yml], {type: "application/x-yaml;charset=utf-8"});
 
+    // forces "save As" function allow user download file
     saveAs(file, "apiConfig.yaml");
   }
 });
