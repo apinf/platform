0d0c1ca9e2df33b8891f66eb1b545a96eda9705f
Damir Mustafin <frenchthebread@gmail.com>
2015-08-27 12:34:08 +0300

    added template events

--- client/views/api_backends/view_api_backend/view_api_backend.js
+++ client/views/api_backends/view_api_backend/view_api_backend.js
@@ -1,10 +1,34 @@
 Template.viewApiBackend.created = function() {
   // Create reference to instance
   var instance = this;
-  
+
   // Get the API Backend ID from the route
   var backendId = Router.current().params.apiBackendId;
-  
+
   // Subscribe to a single API Backend, by ID
   instance.subscribe("apiBackend", backendId);
-}
+};
+
+Template.viewApiBackend.events({
+  'click #exportJSONConfig' : function () {
+
+    var apiBackend = ApiBackends.find().fetch();
+
+    var jsonObj = JSON.stringify(apiBackend, null, '\t');
+
+    var file = new Blob([jsonObj], {type: "application/json;charset=utf-8"});
+
+    saveAs(file, "apiConfig.json");
+  },
+  'click #exportYAMLConfig' : function () {
+
+    var apiBackend = ApiBackends.find().fetch();
+
+
+    var yml = jsyaml.safeDump(apiBackend);
+
+    var file = new Blob([yml], {type: "application/x-yaml;charset=utf-8"});
+
+    saveAs(file, "apiConfig.yaml");
+  }
+});
