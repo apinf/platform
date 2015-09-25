17c119dafa30b87e2451a1c1fe0f8d8f298d4e26
elnzv <illya.nizyev@gmail.com>
2015-09-25 04:18:16 +0300

    each field basic hint text

--- client/views/api_backends/form_hints/form_hints.js
+++ client/views/api_backends/form_hints/form_hints.js
@@ -10,6 +10,102 @@ var helpData = {
     options: {
       placement: 'right'
     }
+  },
+  'servers': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'backend_host': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'frontend_host': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'url_matches': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'documentation_link': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'append_query_string': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'set_headers': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'http_basic_auth': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'require_https': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'api_key_verification_level': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'required_roles': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'pass_api_key_header': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'pass_api_key_query_param': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'rewrite': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'balance_algorithm': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'error_templates': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
   }
 }
 InlineHelp.initHelp(helpData);
