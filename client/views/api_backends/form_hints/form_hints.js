7eb98cd937b236daf0bc0ac9491c020c309cf73e
elnzv <illya.nizyev@gmail.com>
2015-09-25 08:40:48 +0300

    cleaned up

--- client/views/api_backends/form_hints/form_hints.js
+++ client/views/api_backends/form_hints/form_hints.js
@@ -1,108 +1,108 @@
 var helpData = {
   'api_name': {
-    message: "Name your API", //supports Markdown
+    message: "Name your API",
     options: {
       placement: 'right'
     }
   },
   'backend_protocol': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'servers': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'backend_host': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'frontend_host': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'url_matches': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'documentation_link': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'append_query_string': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'set_headers': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'http_basic_auth': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'require_https': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'api_key_verification_level': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'required_roles': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'pass_api_key_header': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'pass_api_key_query_param': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'rewrite': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'balance_algorithm': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
   },
   'error_templates': {
-    message: "Choose backend server protocol", //supports Markdown
+    message: "Choose backend server protocol",
     options: {
       placement: 'right'
     }
