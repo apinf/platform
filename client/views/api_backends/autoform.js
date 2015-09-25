e5c0ba3f1329a0bc6b147a26c8d5eabcd96864a4
elnzv <illya.nizyev@gmail.com>
2015-09-25 04:30:35 +0300

    sAlert config

--- client/views/api_backends/autoform.js
+++ client/views/api_backends/autoform.js
@@ -3,6 +3,21 @@ AutoForm.hooks({
     onSuccess: function (formType, backendId) {
       // Send the API Backend to API Umbrella
       Meteor.call('createApiBackendOnApiUmbrella', backendId);
+      sAlert.success('New API was successfully submitted!');
     }
   }
+});
+
+Meteor.startup(function () {
+
+    sAlert.config({
+        effect: '',
+        position: 'top',
+        timeout: 5000,
+        html: false,
+        onRouteClose: true,
+        stack: true,
+        beep: false
+    });
+
 });
