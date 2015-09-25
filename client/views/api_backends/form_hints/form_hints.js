35428842da64335b819d492fe77f8006580b1a09
elnzv <illya.nizyev@gmail.com>
2015-09-25 08:58:55 +0300

    popup hint to the left

--- client/views/api_backends/form_hints/form_hints.js
+++ client/views/api_backends/form_hints/form_hints.js
@@ -2,115 +2,115 @@ var helpData = {
   'api_name': {
     message: "Name your API",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'backend_protocol': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'servers': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'backend_host': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'frontend_host': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'url_matches': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'documentation_link': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'importApiDocumentation': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'append_query_string': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'set_headers': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'http_basic_auth': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'require_https': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'api_key_verification_level': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'required_roles': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'pass_api_key_header': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'pass_api_key_query_param': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'rewrite': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'balance_algorithm': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   },
   'error_templates': {
     message: "Choose backend server protocol",
     options: {
-      placement: 'right'
+      placement: 'left'
     }
   }
 }
