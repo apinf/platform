812d873c5f198a0babaf14c8b4ad586b4bae1c54
elnzv <illya.nizyev@gmail.com>
2015-09-25 04:47:40 +0300

    added a basic redirect to manage apis page

--- client/views/api_backends/autoform.js
+++ client/views/api_backends/autoform.js
@@ -4,20 +4,21 @@ AutoForm.hooks({
       // Send the API Backend to API Umbrella
       Meteor.call('createApiBackendOnApiUmbrella', backendId);
       sAlert.success('New API was successfully submitted!');
+      Router.go('manageApiBackends');
     }
   }
 });
 
 Meteor.startup(function () {
 
-    sAlert.config({
-        effect: '',
-        position: 'top',
-        timeout: 5000,
-        html: false,
-        onRouteClose: true,
-        stack: true,
-        beep: false
-    });
+  sAlert.config({
+    effect: '',
+    position: 'top',
+    timeout: 5000,
+    html: false,
+    onRouteClose: true,
+    stack: true,
+    beep: false
+  });
 
 });
