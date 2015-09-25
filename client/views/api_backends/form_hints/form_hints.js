0a35d314a529636bfda0eccba696311870f2faef
elnzv <illya.nizyev@gmail.com>
2015-09-25 03:55:44 +0300

    added first fields hints

---
+++ client/views/api_backends/form_hints/form_hints.js
@@ -0,0 +1,15 @@
+var helpData = {
+  'api_name': {
+    message: "Name your API", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  },
+  'backend_protocol': {
+    message: "Choose backend server protocol", //supports Markdown
+    options: {
+      placement: 'right'
+    }
+  }
+}
+InlineHelp.initHelp(helpData);
