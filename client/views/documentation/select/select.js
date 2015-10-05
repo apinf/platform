e76085d1dc4958d2d31857dc4555ddbecc5c7fdd
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-10-05 13:31:10 +0300

    Add template subscription

---
+++ client/views/documentation/select/select.js
@@ -0,0 +1,7 @@
+Template.apiDocumentationSelect.created = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Subscribe to All API Documentation
+  instance.subscribe('allApiDocumentation');
+};
