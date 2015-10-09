e1643ac1fd0af3bbc0fc9963bb462ef563af785f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-10-09 14:16:04 +0300

    Add apiUmbrellaBaseUrl reactive variable

--- client/views/api_backends/view_api_backend/usageInstructions/usageInstructions.js
+++ client/views/api_backends/view_api_backend/usageInstructions/usageInstructions.js
@@ -1,6 +1,9 @@
 Template.apiBackendUsageInstructions.created = function () {
   // Get reference to template instance
   var instance = this;
+
+  // Create variable to hold API Umbrella base URL
+  instance.apiUmbrellaBaseUrl = new ReactiveVar();
 };
 
 Template.apiBackendUsageInstructions.helpers({
