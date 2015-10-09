0d8e54d1e75e526704443f888ae2f2eb384b74a8
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-10-09 13:24:33 +0300

    Add View Details column

--- both/catalogue.js
+++ both/catalogue.js
@@ -9,6 +9,11 @@ TabularTables.ApiTable = new Tabular.Table({
   name: "ApiTable",
   collection: ApiBackends,
   columns: [
+    {
+      data: "_id",
+      title: "View Details",
+      tmpl: Meteor.isClient && Template.viewApiBackendButton
+    },
     {data: "name", title: "Name"},
     {data: "backend_host", title: "Backend host"},
     {tmpl: Meteor.isClient && Template.favourite, title: "Bookmark"}
