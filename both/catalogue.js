1c38cf3d4db7eec89dea1e70fc45211c7c6a5fce
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-10-09 13:28:15 +0300

    Move "View Details" column

--- both/catalogue.js
+++ both/catalogue.js
@@ -9,13 +9,13 @@ TabularTables.ApiTable = new Tabular.Table({
   name: "ApiTable",
   collection: ApiBackends,
   columns: [
+    {data: "name", title: "Name"},
+    {data: "backend_host", title: "Backend host"},
     {
       data: "_id",
       title: "View Details",
       tmpl: Meteor.isClient && Template.viewApiBackendButton
     },
-    {data: "name", title: "Name"},
-    {data: "backend_host", title: "Backend host"},
     {tmpl: Meteor.isClient && Template.favourite, title: "Bookmark"}
   ]
 });
