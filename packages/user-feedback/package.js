ea204eee223ba334fd8e06ee0f8914989041ee4a
elnzv <illya.nizyev@gmail.com>
2015-10-15 15:49:51 +0300

    cleaned package from chat; general clean up

--- packages/user-feedback/package.js
+++ packages/user-feedback/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'user-feedback',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'A self-contained user feedback module for Meteor - with Chat',
+  summary: 'User feedback package for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/vivekvrao/user-feedback.git',
   // By default, Meteor will default to using README.md for documentation.
@@ -15,18 +15,9 @@ Package.onUse(function(api) {
   api.use('mongo', ['server','client']);
   api.use('templating');
   api.use('accounts-base');
-  api.addFiles('chat.png','client');
   api.addFiles('feedback.png','client');
   api.addFiles('user-feedback.html','client');
   api.addFiles('user-feedback.js','client');
-  api.addFiles('user-feedback-chat.html','client');
-  api.addFiles('user-feedback-chat.js','client');
   api.addFiles('user-feedback.css','client');
   api.addFiles('user-feedback-server.js','server');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('viloma:user-feedback');
-  api.addFiles('user-feedback-tests.js');
 });
--- packages/user-feedback/user-feedback.js
+++ packages/user-feedback/user-feedback.js
@@ -4,24 +4,24 @@ UI.registerHelper('ufbmatches', function(foo, bar1, bar2, bar3, bar4) {
 });
 
 UI.registerHelper('ufbsort', function(array, field, ord) {
-	console.log('sort called '+array+' '+field +" ord "+ ord);
-if(ord === "desc")
-	return _.sortBy(array, function(item){ return -item[field]; });
-return _.sortBy(array, function(item){ return item[field]; });
+  console.log('sort called '+array+' '+field +" ord "+ ord);
+  if(ord === "desc")
+    return _.sortBy(array, function(item){ return -item[field]; });
+  return _.sortBy(array, function(item){ return item[field]; });
 });
 
 function updateTopic(topicId, type, comment){
-    Meteor.call("updateTopic", topicId, type, comment, function(err, res){
-		if(!err)
-  			Session.set('currTopic', res);
-  		else
-  			alert(err);
-  	});
+  Meteor.call("updateTopic", topicId, type, comment, function(err, res){
+    if(!err)
+      Session.set('currTopic', res);
+    else
+      alert(err);
+  });
 }
 
 ufbFormatDate = function(date) {
-	if(!date)
-		return "-";
+  if(!date)
+    return "-";
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'pm' : 'am';
@@ -32,193 +32,177 @@ ufbFormatDate = function(date) {
   return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
 }
 UI.registerHelper('ufbFormatDate',function(date){
-    return ufbFormatDate(date);
+  return ufbFormatDate(date);
 });
 
 
 Template.userfblink.helpers({
-	showFb: function(){
-		return Session.get('showFb');
-	},
-	showFbChat: function(){
-		return Session.get('showFbChat');
-	},
-	enableFbChat: function(){
-		if(Meteor.settings && Meteor.settings.public &&
-			Meteor.settings.public.userfeedback &&
-			Meteor.settings.public.userfeedback.enableChat)
-			return Meteor.settings.public.userfeedback.enableChat;
-		// show by default
-		return true;
-	}
+  showFb: function(){
+    return Session.get('showFb');
+  }
 });
 
 Template.userfblink.events({
-	"click .ufb-button": function (event) {
-		//$('.ufb-page').css('display','block');
-		Session.set('showFb', true);
-    },
-    "click .ufb-chat": function (event) {
-		//$('.ufb-page').css('display','block');
-		Session.set('showFbChat', true);
-    }
-
+  "click .ufb-button": function (event) {
+    //$('.ufb-page').css('display','block');
+    Session.set('showFb', true);
+  }
 });
 Template.userfeedback.rendered = function(){
-	if (!this.rendered){
-		this.rendered = true;
-		// initialize user feedback - get starting list, Stats and isModerator
-		Meteor.call("initUFB", function (err, asyncValue) {
-			if (err)
-				console.log(err);
-			else {
-				Session.set('ufb-list', asyncValue.topics);
-				Session.set('isModerator', asyncValue.isModerator);
-				var newCount = 0;
-				if(asyncValue.New)
-					newCount = asyncValue.New;
-				var solvedCount = 0;
-				if(asyncValue.Solved)
-					solvedCount = asyncValue.Solved;
-				Session.set('ufb-stats', "New: "+newCount + " / Solved: "+solvedCount);
-			}
-		});
+  if (!this.rendered){
+    this.rendered = true;
+    // initialize user feedback - get starting list, Stats and isModerator
+    Meteor.call("initUFB", function (err, asyncValue) {
+      if (err)
+        console.log(err);
+      else {
+        Session.set('ufb-list', asyncValue.topics);
+        Session.set('isModerator', asyncValue.isModerator);
+        var newCount = 0;
+        if(asyncValue.New)
+          newCount = asyncValue.New;
+        var solvedCount = 0;
+        if(asyncValue.Solved)
+          solvedCount = asyncValue.Solved;
+        Session.set('ufb-stats', "New: "+newCount + " / Solved: "+solvedCount);
+      }
+    });
 
-  	}
+  }
 };
 
 Template.userfeedback.helpers({
-	topicCount: function(){
-		return Session.get('ufb-stats');
-	},
-	categories: function(){
-		// if topic types overridden in config use those
-		if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.userfeedback && Meteor.settings.public.userfeedback.categories){
-			return Meteor.settings.public.userfeedback.categories;
-		}
-		else
-			return [{desc: "Feature Ideas", id:"Idea"},
-					{desc:"Technical Issues", id:"Issue"},
-					{desc:"General Feedback", id:"General"}];
-	},
-	topicList: function(){
-		return Session.get('ufb-list');
-	},
-	currTopic: function(){
-		return Session.get('currTopic');
-	},
-	readonly: function(){
-		var currTopic = Session.get('currTopic');
-		var isModerator = Session.get('isModerator');
-		// if you own the current topic or are the moderator then not readonly
-		if (currTopic.owner === Meteor.userId() ||
-			isModerator === true)
-				return null;
-		return "readonly";
-	}
+  topicCount: function(){
+    return Session.get('ufb-stats');
+  },
+  categories: function(){
+    // if topic types overridden in config use those
+    if(Meteor.settings && Meteor.settings.public && Meteor.settings.public.userfeedback && Meteor.settings.public.userfeedback.categories){
+      return Meteor.settings.public.userfeedback.categories;
+    }
+    else
+      return [{desc: "Feature Ideas", id:"Idea"},
+              {desc:"Technical Issues", id:"Issue"},
+              {desc:"General Feedback", id:"General"}];
+  },
+  topicList: function(){
+    return Session.get('ufb-list');
+  },
+  currTopic: function(){
+    return Session.get('currTopic');
+  },
+  readonly: function(){
+    var currTopic = Session.get('currTopic');
+    var isModerator = Session.get('isModerator');
+    // if you own the current topic or are the moderator then not readonly
+    if (currTopic.owner === Meteor.userId() ||
+        isModerator === true)
+      return null;
+    return "readonly";
+  }
 });
 Template.userfeedback.events({
-	"click .ufb-close": function (event) {
-		Session.set('showFb',false);
-    },
-	"click .ufb-topic-close": function (event) {
-		Session.set('showFb',false);
-    },
-    "submit .ufb-search-form": function (event) {
-      // search box - find result of topic
-      var text = $('.ufb-search').val();
-      Session.set('currTopic', null);
-  		Meteor.call("findTopic", text, function (err, asyncValue) {
-			if (err)
-				console.log(err);
-			else {
-				Session.set('ufb-list', asyncValue);
-			}
-		});
-      // Prevent default form submit
-      return false;
-    },
-    "click .ufb-search-button":function(event){
-    	Session.set('currTopic', null);
-    },
-    "click .ufb-new": function (event) {
-      // create a new topic
-      Session.set('currTopic', {owner: Meteor.userId(), desc:""});
-      return false;
-    },
-    "click .ufb-accept-answer": function(event){
-	    var currTopic = Session.get("currTopic");
-	    var cmtId = event.target.id.substring(3);
-	    updateTopic(currTopic._id, "accept-answer", cmtId);
-    },
-    "click .ufb-remove-comment": function(event){
-	    var currTopic = Session.get("currTopic");
-	    var cmtId = event.target.id.substring(3);
-	    updateTopic(currTopic._id, "remove-comment", cmtId);
-    },
-    "click .ufb-save-button": function(event){
-	    var currTopic = Session.get("currTopic");
-	    var currTopicId;
-	    // if it not new topic - then pass the curr topic id
-	    if(currTopic)
-	    	currTopicId = currTopic._id;
-      var head =$('.ufb-textbox').val();
-      var typ =$('.ufb-type-input').val();
-      var desc =$('.ufb-textdesc').val();
-  		Meteor.call("setTopic", head, typ, desc, currTopicId, function (err, res) {
-  			if(!err){
-	  			Session.set('currTopic', res);
-	  		}
-	  		else
-	  			alert(err);
-  		});
-    },
-    "click .ufb-topic": function(e){
-		var k = e.target.id;
-		// get the fill topic
-    	Meteor.call("getTopicDetails", k, function(err, res){
-  			if(!err)
-	  			Session.set('currTopic', res);
-	  		else
-	  			alert(err);
-    	});
-    	return false;
-    },
-    "click .ufb-comment-save" : function(e){
-	    var text =$('.ufb-comment-input').val();
-	    $('.ufb-comment-input').val("");
-	    var currTopic = Session.get("currTopic");
-	    updateTopic(currTopic._id, "comment", text);
-    	return false;
-    },
-    "click .ufb-unlike-button" : function(e){
-	    var currTopic = Session.get("currTopic");
-	    updateTopic(currTopic._id, "unlikes", "");
-    	return false;
-    },
-    "click .ufb-like-button" : function(e){
-	    var currTopic = Session.get("currTopic");
-	    updateTopic(currTopic._id, "likes", "");
-    	return false;
-    },
-    "click .ufb-rating-up" : function(e){
-	    var currTopic = Session.get("currTopic");
-	    var cmtId = e.target.id.substring(3);
-	    updateTopic(currTopic._id, "clikes", cmtId);
-    	return false;
-    },
-    "click .ufb-rating-down" : function(e){
-	    var currTopic = Session.get("currTopic");
-	    var cmtId = e.target.id.substring(3);
-	    updateTopic(currTopic._id, "cunlikes", cmtId);
-    	return false;
-    },
-    "change .ufb-status-box" : function(e){
-    	var val =  $('#'+e.target.id).val();
-		if($('#'+e.target.id).attr('orig') !== val){
-		    var currTopic = Session.get("currTopic");
-			console.log('combobox '+e.target.id+'='+val);
-		    updateTopic(currTopic._id, "status", val);
-		}
+  "click .ufb-close": function (event) {
+    Session.set('showFb',false);
+  },
+  "click .ufb-topic-close": function (event) {
+    Session.set('showFb',false);
+  },
+  "submit .ufb-search-form": function (event) {
+    // search box - find result of topic
+    var text = $('.ufb-search').val();
+    Session.set('currTopic', null);
+    Meteor.call("findTopic", text, function (err, asyncValue) {
+      if (err)
+        console.log(err);
+      else {
+        Session.set('ufb-list', asyncValue);
+      }
+    });
+    // Prevent default form submit
+    return false;
+  },
+  "click .ufb-search-button":function(event){
+    Session.set('currTopic', null);
+  },
+  "click .ufb-new": function (event) {
+    // create a new topic
+    Session.set('currTopic', {owner: Meteor.userId(), desc:""});
+    return false;
+  },
+  "click .ufb-accept-answer": function(event){
+    var currTopic = Session.get("currTopic");
+    var cmtId = event.target.id.substring(3);
+    updateTopic(currTopic._id, "accept-answer", cmtId);
+  },
+  "click .ufb-remove-comment": function(event){
+    var currTopic = Session.get("currTopic");
+    var cmtId = event.target.id.substring(3);
+    updateTopic(currTopic._id, "remove-comment", cmtId);
+  },
+  "click .ufb-save-button": function(event){
+    var currTopic = Session.get("currTopic");
+    var currTopicId;
+    // if it not new topic - then pass the curr topic id
+    if(currTopic)
+      currTopicId = currTopic._id;
+    var head =$('.ufb-textbox').val();
+    var typ =$('.ufb-type-input').val();
+    var desc =$('.ufb-textdesc').val();
+    Meteor.call("setTopic", head, typ, desc, currTopicId, function (err, res) {
+      if(!err){
+        Session.set('currTopic', res);
+      }
+      else
+        alert(err);
+    });
+  },
+  "click .ufb-topic": function(e){
+    var k = e.target.id;
+    // get the fill topic
+    Meteor.call("getTopicDetails", k, function(err, res){
+      if(!err)
+        Session.set('currTopic', res);
+      else
+        alert(err);
+    });
+    return false;
+  },
+  "click .ufb-comment-save" : function(e){
+    var text =$('.ufb-comment-input').val();
+    $('.ufb-comment-input').val("");
+    var currTopic = Session.get("currTopic");
+    updateTopic(currTopic._id, "comment", text);
+    return false;
+  },
+  "click .ufb-unlike-button" : function(e){
+    var currTopic = Session.get("currTopic");
+    updateTopic(currTopic._id, "unlikes", "");
+    return false;
+  },
+  "click .ufb-like-button" : function(e){
+    var currTopic = Session.get("currTopic");
+    updateTopic(currTopic._id, "likes", "");
+    return false;
+  },
+  "click .ufb-rating-up" : function(e){
+    var currTopic = Session.get("currTopic");
+    var cmtId = e.target.id.substring(3);
+    updateTopic(currTopic._id, "clikes", cmtId);
+    return false;
+  },
+  "click .ufb-rating-down" : function(e){
+    var currTopic = Session.get("currTopic");
+    var cmtId = e.target.id.substring(3);
+    updateTopic(currTopic._id, "cunlikes", cmtId);
+    return false;
+  },
+  "change .ufb-status-box" : function(e){
+    var val =  $('#'+e.target.id).val();
+    if($('#'+e.target.id).attr('orig') !== val){
+      var currTopic = Session.get("currTopic");
+      console.log('combobox '+e.target.id+'='+val);
+      updateTopic(currTopic._id, "status", val);
     }
+  }
 });
