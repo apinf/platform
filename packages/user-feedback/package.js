5eaf34b4705f355fdcf56c71ca2a8b7e9d6d0792
elnzv <illya.nizyev@gmail.com>
2015-10-15 15:00:13 +0300

    changed package name

--- packages/user-feedback/package.js
+++ packages/user-feedback/package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'viloma:user-feedback',
-  version: '0.5.4',
+  name: 'user-feedback',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'A self-contained user feedback module for Meteor - with Chat',
   // URL to the Git repository containing the source code for this package.
