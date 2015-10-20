97335445348c570416a1d9f632356823b25599e4
elnzv <illya.nizyev@gmail.com>
2015-10-21 02:18:50 +0300

    feedback schema

---
+++ both/collections/feedback.js
@@ -0,0 +1,31 @@
+Feedback = new Mongo.Collection('feedback');
+
+Schemas.feedbackSchema = new SimpleSchema({
+  topic: {
+    type: String,
+    label: "Topic",
+    max: 50,
+    optional: false,
+    autoform: {
+      placeholder: 'Feedback topic'
+    }
+  },
+  message: {
+    type: String,
+    label: "Your Message",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Your message'
+    }
+  }
+});
+
+Feedback.attachSchema(Schemas.FeedbackSchema);
+
+Feedback.allow({
+  insert: function () {
+    return true;
+  }
+});
