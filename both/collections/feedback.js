dd0915d66581100752d13b7e105c7c016eec4c16
elnzv <illya.nizyev@gmail.com>
2015-10-21 04:37:39 +0300

    cleaned schema

--- both/collections/feedback.js
+++ both/collections/feedback.js
@@ -19,34 +19,6 @@ Schemas.FeedbackSchema = new SimpleSchema({
       rows: 5,
       placeholder: 'Your message'
     }
-  },
-  _id: {
-    type: String,
-    optional: true,
-    autoform: {
-      omit: true
-    }
-  },
-  userId: {
-    type: String,
-    optional: true,
-    autoform: {
-      omit: true
-    }
-  },
-  author: {
-    type: String,
-    optional: true,
-    autoform: {
-      omit: true
-    }
-  },
-  createdAt: {
-    type: Date,
-    optional: true,
-    autoform: {
-      omit: true
-    }
   }
 });
 
