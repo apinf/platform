b4095bdfef16900827f5aa823a9f12f1b32b1e6c
elnzv <illya.nizyev@gmail.com>
2015-10-21 04:37:54 +0300

    added subscription

--- client/views/feedback/feedback.js
+++ client/views/feedback/feedback.js
@@ -1,3 +1,9 @@
 Template.feedbackList.created = function () {
   this.subscribe('feedback');
 };
+
+Template.feedbackList.helpers({
+  userFeedback : function() {
+    return Feedback.find({});
+  }
+});
