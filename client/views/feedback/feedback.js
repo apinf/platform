b1dc57b471c9dd1e885bd2f6abaa5e5c0f1cbf4d
elnzv <illya.nizyev@gmail.com>
2015-10-22 23:22:29 +0300

    subscribe, events, heplers

--- client/views/feedback/feedback.js
+++ client/views/feedback/feedback.js
@@ -2,8 +2,30 @@ Template.feedbackList.created = function () {
   this.subscribe('feedback');
 };
 
+Template.feedbackPage.created = function () {
+  this.subscribe('feedback');
+};
+
 Template.feedbackList.helpers({
   userFeedback : function() {
-    return Feedback.find({});
+    return Feedback.find().fetch();
+  }
+});
+
+Template.feedbackPage.helpers({
+  'userFeedback' : function () {
+    // Get the current user
+    var userId = Meteor.user()._id;
+
+    var userFeedback = Feedback.find({author: userId});
+
+    return userFeedback;
+  }
+});
+
+Template.feedbackList.events({
+  // delete feedback
+  'click .delete-feedback': function (event) {
+    Meteor.call('deleteFeedback', this._id);
   }
 });
