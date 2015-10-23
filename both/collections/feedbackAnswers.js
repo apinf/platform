3e8d87e5e371572581cad030f00274d2fb194784
elnzv <illya.nizyev@gmail.com>
2015-10-23 03:10:03 +0300

    answers collection

---
+++ both/collections/feedbackAnswers.js
@@ -0,0 +1,40 @@
+FeedbackAnswers = new Mongo.Collection('feedbackAnswers');
+
+Schemas.FeedbackAnswersSchema = new SimpleSchema({
+  message: {
+    type: String,
+    label: "Your Message",
+    max: 1000,
+    optional: false,
+    autoform: {
+      rows: 5,
+      placeholder: 'Your message'
+    }
+  },
+  author: {
+    type: String,
+    autoValue: function () {
+      return Meteor.userId()
+    }
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date()};
+      } else {
+        this.unset();
+      }
+    }
+  }
+});
+
+FeedbackAnswers.attachSchema(Schemas.FeedbackAnswersSchema);
+
+FeedbackAnswers.allow({
+  insert: function () {
+    return true;
+  }
+});
