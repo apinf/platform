af499bf2c8826ac8135326e214e2087d9079ea39
elnzv <illya.nizyev@gmail.com>
2015-10-26 11:22:56 +0200

    added comments

--- server/publish/feedback.js
+++ server/publish/feedback.js
@@ -1,7 +1,9 @@
 Meteor.publish('feedback', function(){
+  // If user is admin - show all feedback
   if (Roles.userIsInRole(this.userId, ['admin'])){
     return Feedback.find({});
   } else {
+    // else - show all user's feedback
     return Feedback.find({author: this.userId});
   }
 });
