17beb0aa50d6bfe4792db7024158fff117be8c99
elnzv <illya.nizyev@gmail.com>
2015-10-26 12:51:18 +0200

    changed publication

--- server/publish/feedback.js
+++ server/publish/feedback.js
@@ -1,9 +1,5 @@
-Meteor.publish('feedback', function(){
+Meteor.publish('apiBackendFeedback', function(apiBackendId){
   // If user is admin - show all feedback
-  if (Roles.userIsInRole(this.userId, ['admin'])){
-    return Feedback.find({});
-  } else {
-    // else - show all user's feedback
-    return Feedback.find({author: this.userId});
-  }
+  console.log(apiBackendId);
+  return Feedback.find({apiBackendId: apiBackendId});
 });
