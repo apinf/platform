61254765750132060609ab62ec8440b9b627931c
elnzv <illya.nizyev@gmail.com>
2015-10-26 21:34:35 +0200

    moved code to own files

--- client/views/feedback/feedback_form/feedbackForm.js
+++ client/views/feedback/feedback_form/feedbackForm.js
@@ -1,32 +1,3 @@
-Template.feedbackList.created = function () {
-  // Subscription to feedback collection
-  var currentRouter = Router.current().params._id;
-  this.subscribe('apiBackendFeedback', currentRouter);
-};
-
-Template.feedbackItem.created = function () {
-  // Subscription to feedback collection
-  this.subscribe('feedback');
-};
-
-Template.feedbackList.helpers({
-  'userFeedback' : function() {
-    return Feedback.find();
-  },
-  'haveFeedback' : function() {
-    // Count user's feedback in feedback collection
-    var feedbackCount  = Feedback.find().count();
-    return feedbackCount > 0;
-  }
-});
-
-Template.feedbackList.events({
-  // Delete feedback
-  'click .delete-feedback': function (event) {
-    Meteor.call('deleteFeedback', this._id);
-  }
-});
-
 AutoForm.hooks({
   feedback: {
     before: {
