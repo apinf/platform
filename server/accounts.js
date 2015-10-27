36bc62235bd32b1c07c2f26f060a5f3f059836b1
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2015-10-27 11:56:16 +0200

    Use Meteor accounts email verification instead of manual sendmail function.

--- server/accounts.js
+++ server/accounts.js
@@ -36,7 +36,8 @@ Accounts.onCreateUser(function(options, user) {
   user.apiUmbrellaUserId = response.data.user.id;
   user.profile.apiKey = response.data.user.api_key;
   ApiUmbrellaUsers.insert(response.data.user);
-  Meteor.call("sendmail", user.emails[0].address);
+  // Using Meteor Accounts email verification, so we don't need to say this below.
+  //Meteor.call("sendmail", user.emails[0].address);
 
   return user;
 });
