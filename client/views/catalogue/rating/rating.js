55c678a0d84b4af5d42b0871c7a938eebba4a705
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-10-27 12:07:32 +0200

    Use updated div Id with API Backend ID

--- client/views/catalogue/rating/rating.js
+++ client/views/catalogue/rating/rating.js
@@ -1,4 +1,10 @@
 Template.apiBackendRating.rendered = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Get API Backend ID from template instance
+  var apiBackendId = instance.data._id;
+
   // Add the jQuery RateIt widget
-  $('.rateit').rateit();
+  $("#rating-" + apiBackendId).rateit({max: 4});
 };
