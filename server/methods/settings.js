46b75e6ad3fee3428d61dd310711f763113cf264
elnzv <illya.nizyev@gmail.com>
2015-10-30 02:16:21 +0200

    cleaned up

--- server/methods/settings.js
+++ server/methods/settings.js
@@ -1,7 +1,7 @@
 Meteor.methods({
   'getApiUmbrellaBaseUrl': function () {
     // Get base url from settings file
-    var apiUmbrellaBaseUrl = Settings.findOne({name: "api_umbrella_base_url"});
+    var apiUmbrellaBaseUrl = Meteor.settings.api_umbrella.base_url;
 
     return apiUmbrellaBaseUrl;
   }
