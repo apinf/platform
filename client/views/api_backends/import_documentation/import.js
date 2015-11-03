c15279691c582a2f5a158d0cf8660da2ae0af281
Damir Mustafin <frenchthebread@gmail.com>
2015-11-04 02:08:13 +0900

    Inline comments added

--- client/views/api_backends/import_documentation/import.js
+++ client/views/api_backends/import_documentation/import.js
@@ -4,17 +4,22 @@ Template.importApiDocumentation.events({
     var customProxy = "umbrella.apinf.io";
     var customBasePath = "/api-umbrella";
 
-    // Get the submitted file
+    // Gets selected file from the form
     var file = event.target.files[0];
 
+    // Initialises new reader instance
     var reader = new FileReader();
 
+    // Reads file
     reader.readAsText(file, "UTF-8");
 
+    // When ready ..
     reader.onload = function (event) {
 
+      // Gets file data from reader object
       var fileData = event.target.result;
 
+      // Parses file data to JSON object, to be able to modify it
       var dataObj = JSON.parse(fileData);
 
       dataObj.host = customProxy;
@@ -23,11 +28,15 @@ Template.importApiDocumentation.events({
 
       var documentationFile = JSON.stringify(dataObj);
 
+      // Creates blob object with content type of JSON
       var blobData = new Blob([documentationFile], {type: "application/json"});
 
+      // Creates file object from Blob object
       var updatedFile = new File([blobData], file.name, {type: "application/json"});
 
+      // Insert into filesystem collection
       ApiDocumentation.insert(updatedFile);
+
     };
   }
 });
