877aa8bc2c6b32a78d5e9a6eb0b7f7e403ca717c
Damir Mustafin <frenchthebread@gmail.com>
2015-11-04 01:59:50 +0900

    Custom variables

--- client/views/api_backends/import_documentation/import.js
+++ client/views/api_backends/import_documentation/import.js
@@ -1,5 +1,9 @@
 Template.importApiDocumentation.events({
   'change #apiDocumentationFile': function (event/*, template*/) {
+
+    var customProxy = "umbrella.apinf.io";
+    var customBasePath = "/api-umbrella";
+
     // Get the submitted file
     var file = event.target.files[0];
 
