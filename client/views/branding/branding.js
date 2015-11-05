c470eaeda49cab17552c163233d3610be220a449
elnzv <illya.nizyev@gmail.com>
2015-11-05 03:58:44 +0200

    sort and show only last img

--- client/views/branding/branding.js
+++ client/views/branding/branding.js
@@ -8,3 +8,11 @@ Template.branding.helpers({
     return Branding.findOne();
   }
 });
+
+
+Template.branding.helpers({
+  projectLogo: function () {
+    return ProjectLogo.find({}, {sort: {uploadedAt: -1}}).fetch()[0];
+  }
+});
+
