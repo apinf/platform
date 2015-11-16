0bb69f4971676cb8adbfb0a8ad1ae03ab70f18d7
Damir Mustafin <frenchthebread@gmail.com>
2015-11-16 15:05:07 +0900

    SwaggerUi init

--- client/views/documentation/view/documentation.js
+++ client/views/documentation/view/documentation.js
@@ -7,19 +7,15 @@ Template.documentation.created = function () {
 };
 
 Template.documentation.rendered = function () {
-  $(function() {
-    // Pass window url parameters into documentation IFRAME
-    // http://stackoverflow.com/a/12944835/1191545
 
-    // Get search parameter(s) from window location
-    var search = window.location.search;
+  console.log("Hello");
 
-    // Create new iframe src by appending search parameter(s)
-    var newSrc = $("#documentation").attr("src") + search;
-
-    // Update iframe src attribute with new source
-    $("#documentation").attr("src", newSrc);
+  var swaggerUi = new SwaggerUi({
+    url:"http://petstore.swagger.io/v2/swagger.json",
+    dom_id:"swagger-ui-container"
   });
+  swaggerUi.load();
+  console.log("Hello");
 };
 
 Template.documentation.events({
