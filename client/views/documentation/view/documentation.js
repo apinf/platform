0f4f64ff1d03f110b5618cd9752d4822e6e5af91
Damir Mustafin <frenchthebread@gmail.com>
2015-11-16 15:04:03 +0900

    Cleaning up last two commits..

--- client/views/documentation/view/documentation.js
+++ client/views/documentation/view/documentation.js
@@ -7,15 +7,19 @@ Template.documentation.created = function () {
 };
 
 Template.documentation.rendered = function () {
+  $(function() {
+    // Pass window url parameters into documentation IFRAME
+    // http://stackoverflow.com/a/12944835/1191545
 
-  console.log("Hello");
+    // Get search parameter(s) from window location
+    var search = window.location.search;
 
-  var swaggerUi = new SwaggerUi({
-    url:"http://petstore.swagger.io/v2/swagger.json",
-    dom_id:"swagger-ui-container"
+    // Create new iframe src by appending search parameter(s)
+    var newSrc = $("#documentation").attr("src") + search;
+
+    // Update iframe src attribute with new source
+    $("#documentation").attr("src", newSrc);
   });
-  swaggerUi.load();
-  console.log("Hello");
 };
 
 Template.documentation.events({
