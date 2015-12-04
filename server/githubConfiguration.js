3f75b181369cb1ae0359b97b00089d13dcd92a9f
elnzv <illya.nizyev@gmail.com>
2015-12-04 06:37:35 +0200

    githubConfiguration options to find its values

--- server/githubConfiguration.js
+++ server/githubConfiguration.js
@@ -3,13 +3,41 @@ ServiceConfiguration.configurations.remove({
   service: 'github'
 });
 
-ServiceConfiguration.configurations.insert({
-  /*extend settings.json with Client ID and Client Secret:
+// If settings are already in Settings collection
+if ( Settings.findOne() ) {
+
+  ServiceConfiguration.configurations.insert({
+    service: 'github',
+    clientId: Settings.findOne().githubConfiguration.clientId,
+    secret: Settings.findOne().githubConfiguration.secret
+  });
+
+  // If settings are available in Meteor.settings.private
+} else if (Meteor.settings.private) {
+  ServiceConfiguration.configurations.insert({
+    /*extend settings.json with Client ID and Client Secret:
   "githubConfiguration": {
     "clientId" : "xxxx",
     "secret" : "xxxx"
   }*/
-  service: 'github',
-  clientId: Meteor.settings.private.githubConfiguration.clientId,
-  secret: Meteor.settings.private.githubConfiguration.secret
-});
+    service: 'github',
+    clientId: Meteor.settings.private.githubConfiguration.clientId,
+    secret: Meteor.settings.private.githubConfiguration.secret
+  });
+
+} try {
+
+  ServiceConfiguration.configurations.insert({
+    /*extend settings.json with Client ID and Client Secret:
+  "githubConfiguration": {
+    "clientId" : "xxxx",
+    "secret" : "xxxx"
+  }*/
+    service: 'github',
+    clientId: Meteor.settings.private.githubConfiguration.clientId,
+    secret: Meteor.settings.private.githubConfiguration.secret
+  });
+  // otherwise show an error
+} catch (_error) {
+  e = _error;
+}
