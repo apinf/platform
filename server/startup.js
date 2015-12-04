0d0bebda82dfbd16f5e9e2ac69b740dfd7763ab4
elnzv <illya.nizyev@gmail.com>
2015-12-04 06:40:27 +0200

    check for Meteor settings object

--- server/startup.js
+++ server/startup.js
@@ -9,50 +9,54 @@ Meteor.startup(function () {
   //    }
   //  });
 
-  // Store settings object
-  var settings = Meteor.settings.private;
-  // Check if something is already in collection
-  if ( ! Settings.findOne() ) {
-    // if not insert settings object
-    Settings.insert(settings);
-  }
+  if (Meteor.settings.private) {
 
-  // Check if something is on Settings collection
-  if ( Settings.findOne() ) {
-    // Create config object for API Umbrella Web interface from Settings collection
-    var config = {
-      baseUrl: Settings.findOne().apiUmbrella.baseUrl,
-      apiKey: Settings.findOne().apiUmbrella.apiKey,
-      authToken: Settings.findOne().apiUmbrella.authToken
-    };
-  } else {
-    // Create config object for API Umbrella Web interface from Meteor.settings.private
-    var config = {
-      baseUrl: Meteor.settings.private.apiUmbrella.baseUrl,
-      apiKey: Meteor.settings.private.apiUmbrella.apiKey,
-      authToken: Meteor.settings.private.apiUmbrella.authToken
-    };
-  }
-
-  // Create API Umbrella Web object for REST calls
-  apiUmbrellaWeb = new ApiUmbrellaWeb(config);
-
-  // Check if API Umbrella settings are available
-  SyncedCron.add({
-    name: 'Sync API Umbrella Users and API Backends',
-    schedule: function(parser) {
-      // parser is a later.parse object
-      return parser.text('every 1 day');
-    },
-    job: function() {
-      Meteor.call("syncApiUmbrellaUsers");
-      Meteor.call("syncApiUmbrellaAdmins");
-      Meteor.call("syncApiBackends");
+    // Store settings object
+    var settings = Meteor.settings.private;
+    // Check if something is already in collection
+    if ( ! Settings.findOne() ) {
+      // if not insert settings object
+      Settings.insert(settings);
     }
-  });
-  Meteor.call("syncApiUmbrellaUsers");
-  Meteor.call("syncApiBackends");
+
+    // Check if something is on Settings collection
+    if ( Settings.findOne() ) {
+      // Create config object for API Umbrella Web interface from Settings collection
+      var config = {
+        baseUrl: Settings.findOne().apiUmbrella.baseUrl,
+        apiKey: Settings.findOne().apiUmbrella.apiKey,
+        authToken: Settings.findOne().apiUmbrella.authToken
+      };
+    } else {
+      // Create config object for API Umbrella Web interface from Meteor.settings.private
+      var config = {
+        baseUrl: Meteor.settings.private.apiUmbrella.baseUrl,
+        apiKey: Meteor.settings.private.apiUmbrella.apiKey,
+        authToken: Meteor.settings.private.apiUmbrella.authToken
+      };
+    }
+
+    // Create API Umbrella Web object for REST calls
+    apiUmbrellaWeb = new ApiUmbrellaWeb(config);
+
+    // Check if API Umbrella settings are available
+    SyncedCron.add({
+      name: 'Sync API Umbrella Users and API Backends',
+      schedule: function(parser) {
+        // parser is a later.parse object
+        return parser.text('every 1 day');
+      },
+      job: function() {
+        Meteor.call("syncApiUmbrellaUsers");
+        Meteor.call("syncApiUmbrellaAdmins");
+        Meteor.call("syncApiBackends");
+      }
+    });
+    Meteor.call("syncApiUmbrellaUsers");
+    Meteor.call("syncApiBackends");
+  }
 
 });
+
 
 SyncedCron.start();
