c1f7998e3f969b534b5c449e5dc65d665508ef92
elnzv <illya.nizyev@gmail.com>
2015-12-04 06:39:58 +0200

    mail settings options to search for its values

--- server/smtp.js
+++ server/smtp.js
@@ -4,8 +4,24 @@ Meteor.startup(function() {
     "username" : "xxxx",
       "password" : "xxxx"
   }*/
-  var username = Meteor.settings.private.mail.username;
-  var password = Meteor.settings.private.mail.password;
+  // If settings are already in Settings collection
+  if ( Settings.findOne() ) {
+    var username = Settings.findOne().mail.username;
+    var password = Settings.findOne().mail.password;
+
+    // If settings are available in Meteor.settings.private
+  } else if ( Meteor.settings.private ){
+    var username = Meteor.settings.private.mail.username;
+    var password = Meteor.settings.private.mail.password;
+
+  } try {
+    var username = Meteor.settings.private.mail.username;
+    var password = Meteor.settings.private.mail.password;
+
+    // otherwise show an error
+  } catch (_error) {
+    e = _error;
+  }
   var server = "smtp.mailgun.org";
   var port = "587"
 
