3b88677beea0e6c55676ece9aeb05cf81786fff5
elnzv <illya.nizyev@gmail.com>
2015-12-07 10:22:30 +0200

    on startup insert into db and Meteor Settings

--- server/startup.js
+++ server/startup.js
@@ -9,6 +9,21 @@ Meteor.startup(function () {
   //    }
   //  });
 
+  // Get the settings
+  var settings = Meteor.settings.private;
+
+  // check is settings are exist
+  var dbSettings = Settings.find().fetch();
+
+  if ( dbSettings.length === 0 ) {
+    // insert Meteor.settings into Settings Collection
+    Settings.insert(settings);
+    // TODO: figure out how to update Settings collection from settings.json
+  }
+
+  // Updating Meteor.settings from Settings collection
+  Meteor.call('updateMeteorSettings');
+
   if (Meteor.settings.private) {
 
     // Store settings object
