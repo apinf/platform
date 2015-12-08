279b2f521470d5bd5344fcea6159add0a4fbea14
Damir Mustafin <frenchthebread@gmail.com>
2015-12-08 21:30:13 +0900

    Commented lines

--- server/routes/jsonRoutes.js
+++ server/routes/jsonRoutes.js
@@ -6,9 +6,11 @@ JsonRoutes.add("get", "api/:id/swagger.json", function (request, response, next)
   // Fetch API Document from mongo collection
   var apiDoc = ApiDocs.findOne(id);
 
+  
+
   // Updates values to custom ones
-  apiDoc.host = baseURL.hostname();
-  apiDoc.basePath = baseURL.path();
+  //apiDoc.host = baseURL.hostname();
+  //apiDoc.basePath = baseURL.path();
 
   // Sends result back to client
   JsonRoutes.sendResult(response, 200, apiDoc);
