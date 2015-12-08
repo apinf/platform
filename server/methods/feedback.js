4a79e83cb8f6046f48cd9640820171a9b3397c81
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2015-12-08 15:39:51 +0200

    Added server method submitVote and pulication for feedbackVotes.

--- server/methods/feedback.js
+++ server/methods/feedback.js
@@ -2,5 +2,27 @@ Meteor.methods({
   'deleteFeedback': function(feedbackId) {
     // Removing feedback from collection
     Feedback.remove(feedbackId);
+  },
+  'submitVote': function(feedbackId, vote) {
+    //check whether user has already voted
+    var fvotesForId = FeedbackVotes.find(feedbackId).fetch();
+    for(fvote in fvotesForId) {
+      if(fvote.userId === Meteor.userId()) {
+        // User already voted -> Update vote
+        FeedbackVotes.update({
+          feedbackId: feedbackId,
+          userId: Meteor.userId()
+        }, {
+          $set: {vote: vote}
+        });
+        return;
+      }
+    }
+    //User has not voted -> add new user vote
+    FeedbackVotes.insert({
+      feedbackId: feedbackId,
+      userId: Meteor.userId(),
+      vote: vote
+    });
   }
 });
