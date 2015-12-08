4cb924b7e2159c3a59dbfd00fc76955b925417f9
Damir Mustafin <frenchthebread@gmail.com>
2015-12-08 23:01:26 +0900

    Replace lines for consistency

--- server/routes/jsonRoutes.js
+++ server/routes/jsonRoutes.js
@@ -3,16 +3,24 @@ JsonRoutes.add("get", "api/:id/swagger.json", function (request, response, next)
   // Get current id
   var id = request.params.id;
 
+  // Get basePath for apiUmbrella from Meteor settings file
+  var apiUmbrellaBaseUrl = Meteor.settings.apiUmbrella.baseUrl;
+
+  // Parse basePath string to URI obj
+  var baseURL = new URI(apiUmbrellaBaseUrl);
+
   // Fetch API Document from mongo collection
   var apiDoc = ApiDocs.findOne(id);
 
-  var apiBackend = ApiBackends.findOne(apiDoc.apiBackendId);
+  var apiBackendId = apiDoc.apiBackendId;
 
-  var basePath = apiBackend.url_matches[0].frontend_prefix;
+  var apiBackend = ApiBackends.findOne(apiBackendId);
+
+  var urlPrefix = apiBackend.url_matches[0].frontend_prefix;
 
   // Updates values to custom ones
-  //apiDoc.host = baseURL.hostname();
-  //apiDoc.basePath = baseURL.path();
+  apiDoc.host = baseURL.hostname();
+  apiDoc.basePath = urlPrefix;
 
   // Sends result back to client
   JsonRoutes.sendResult(response, 200, apiDoc);
