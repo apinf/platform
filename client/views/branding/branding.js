4b372b548a8dc3033469f08ac4be14953bb6dac2
elnzv <illya.nizyev@gmail.com>
2015-12-10 10:26:19 +0200

    added default skin

--- client/views/branding/branding.js
+++ client/views/branding/branding.js
@@ -24,7 +24,7 @@ Template.AdminLTE.helpers({
   skin: function () {
     // Get color theme from branding collection
     var adminLTESkin = Branding.findOne().color_theme;
-    // Set chosen AdminLTE skin
-    return adminLTESkin;
+    // Set chosen AdminLTE skin or use default
+    return adminLTESkin || 'blue-light';
   }
 });
