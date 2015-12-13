9c0450a0feab93fe4cc3a48276abecf3f5153d10
Damir Mustafin <frenchthebread@gmail.com>
2015-12-14 03:22:45 +0900

    Comments

--- server/methods/searchApiBackends.js
+++ server/methods/searchApiBackends.js
@@ -1,7 +1,13 @@
 Meteor.methods({
   'searchApiBackends': function (searchQuery) {
 
- 
+    // searchApiBackends methods takes 1 parameter - search query string
+
+    // check if searchQuery is ok
+
+    // parse searchQuery
+
+    // fetch apiBackends
 
   }
 });
