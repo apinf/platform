8a65e9e66bce1d5773351a6948e1c7715503a1fa
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2015-12-14 14:23:43 +0200

    Using template helpers to make color changes persist.

--- client/views/feedback/single_feedback/singleFeedback.js
+++ client/views/feedback/single_feedback/singleFeedback.js
@@ -5,17 +5,36 @@ Template.singleFeedback.created = function () {
   this.subscribe('getAllVotesForSingleFeedback', feedbackId);
 };
 
+Template.singleFeedback.helpers({
+  'userUpvote': function() {
+    var feedbackId = this._id;
+    var userId = Meteor.userId();
+    var query = {feedbackId: feedbackId, userId: userId};
+    var userVote = FeedbackVotes.findOne(query);
+    if(userVote && userVote.vote === 1) {
+      return 'userVote';
+    }
+  },
+  'userDownvote': function() {
+    var feedbackId = this._id;
+    var userId = Meteor.userId();
+    var query = {feedbackId: feedbackId, userId: userId};
+    var userVote = FeedbackVotes.findOne(query);
+    if(userVote && userVote.vote === -1) {
+      return 'userVote';
+    }
+  }
+});
+
 Template.singleFeedback.events({
   'click .upvote': function (event, template) {
     var feedback = this;
     var feedbackId = feedback._id;
-    template.$('#upvote-'+feedbackId).toggleClass('userVote');
     Meteor.call('submitVote', feedbackId, 1);
   },
   'click .downvote': function (event, template) {
     var feedback = this;
     var feedbackId = feedback._id;
-    template.$('#downvote-'+feedbackId).toggleClass('userVote');
     Meteor.call('submitVote', feedbackId, -1);
   }
 });
