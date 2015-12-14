28da4adde309ac08fc7b31347924b0166eeeeb79
elnzv <illya.nizyev@gmail.com>
2015-12-14 11:51:22 +0200

    changed order of the check

--- server/githubConfiguration.js
+++ server/githubConfiguration.js
@@ -1,43 +1,39 @@
-ServiceConfiguration.configurations.remove({
-  // removing existing configurations
-  service: 'github'
-});
+// If settings are already in Meteor.settings
+if ( Meteor.settings ) {
 
-// If settings are already in Settings collection
-if ( Settings.findOne() ) {
+  ServiceConfiguration.configurations.remove({
+    // removing existing configurations
+    service: 'github'
+  });
+
+  ServiceConfiguration.configurations.insert({
+    /*extend settings.json with Client ID and Client Secret:
+  "githubConfiguration": {
+    "clientId" : "xxxx",
+    "secret" : "xxxx"
+  }*/
+    service: 'github',
+    clientId: Meteor.settings.githubConfiguration.clientId,
+    secret: Meteor.settings.githubConfiguration.secret
+
+  });
+
+}
+
+// If settings are available in Settings collection
+if else ( Settings.findOne() ) {
+
+  ServiceConfiguration.configurations.remove({
+    // removing existing configurations
+    service: 'github'
+  });
 
   ServiceConfiguration.configurations.insert({
     service: 'github',
     clientId: Settings.findOne().githubConfiguration.clientId,
     secret: Settings.findOne().githubConfiguration.secret
+
   });
 
-  // If settings are available in Meteor.settings
-} else if (Meteor.settings) {
-  ServiceConfiguration.configurations.insert({
-    /*extend settings.json with Client ID and Client Secret:
-  "githubConfiguration": {
-    "clientId" : "xxxx",
-    "secret" : "xxxx"
-  }*/
-    service: 'github',
-    clientId: Meteor.settings.githubConfiguration.clientId,
-    secret: Meteor.settings.githubConfiguration.secret
-  });
 
-} try {
-
-  ServiceConfiguration.configurations.insert({
-    /*extend settings.json with Client ID and Client Secret:
-  "githubConfiguration": {
-    "clientId" : "xxxx",
-    "secret" : "xxxx"
-  }*/
-    service: 'github',
-    clientId: Meteor.settings.githubConfiguration.clientId,
-    secret: Meteor.settings.githubConfiguration.secret
-  });
-  // otherwise show an error
-} catch (_error) {
-  e = _error;
 }
