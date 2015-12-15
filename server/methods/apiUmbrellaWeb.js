574ddbfc4aaf16871b50a67cd04101dfaf3922d6
elnzv <illya.nizyev@gmail.com>
2015-12-15 13:17:34 +0200

    method for creating apiUmbrellaWeb object

--- server/methods/apiUmbrellaWeb.js
+++ server/methods/apiUmbrellaWeb.js
@@ -15,5 +15,29 @@ Meteor.methods({
         }
       });
     };
+  },
+  "createApiUmbrellaWeb": function () {
+    // Check if something is on Settings collection
+    if ( Settings.findOne() ) {
+      // Create config object for API Umbrella Web interface from Settings collection
+      var config = {
+        baseUrl: Settings.findOne().apiUmbrella.baseUrl,
+        apiKey: Settings.findOne().apiUmbrella.apiKey,
+        authToken: Settings.findOne().apiUmbrella.authToken
+      };
+    } else {
+      // Create config object for API Umbrella Web interface from Meteor.settings
+      var config = {
+        baseUrl: Meteor.settings.apiUmbrella.baseUrl,
+        apiKey: Meteor.settings.apiUmbrella.apiKey,
+        authToken: Meteor.settings.apiUmbrella.authToken
+      };
+    }
+    try {
+      // Create API Umbrella Web object for REST calls
+      apiUmbrellaWeb = new ApiUmbrellaWeb(config);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
