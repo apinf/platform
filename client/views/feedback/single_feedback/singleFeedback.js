0a8df7553a123e2724158a1f50ddffa9e415b0f0
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-16 13:15:32 +0200

    Comments and whitespace

--- client/views/feedback/single_feedback/singleFeedback.js
+++ client/views/feedback/single_feedback/singleFeedback.js
@@ -8,19 +8,37 @@ Template.singleFeedback.created = function () {
 
 Template.singleFeedback.helpers({
   'userUpvote': function() {
+    // Get current Feedback ID
     var feedbackId = this._id;
+    
+    // Get current User ID
     var userId = Meteor.userId();
+    
+    // Build a query for to get current user's feedback
     var query = {feedbackId: feedbackId, userId: userId};
+    
+    // Get user vote using query
     var userVote = FeedbackVotes.findOne(query);
+    
+    // If user vote is plus one, it is an upvote
     if(userVote && userVote.vote === 1) {
       return 'userVote';
     }
   },
   'userDownvote': function() {
+    // Get current Feedback ID
     var feedbackId = this._id;
+    
+    // Get current User ID
     var userId = Meteor.userId();
+    
+    // Build a query for to get current user's feedback
     var query = {feedbackId: feedbackId, userId: userId};
+    
+    // Get user vote using query
     var userVote = FeedbackVotes.findOne(query);
+    
+    // If user vote is minus one, it is a downvote
     if(userVote && userVote.vote === -1) {
       return 'userVote';
     }
@@ -29,16 +47,22 @@ Template.singleFeedback.helpers({
 
 Template.singleFeedback.events({
   'click .upvote': function (event, template) {
+    // Get reference to current feedback
     var feedback = this;
+    
     // Get ID of current feedback object
     var feedbackId = feedback._id;
+    
     // Submit upvote (+1) for current feedback
     Meteor.call('submitVote', feedbackId, 1);
   },
   'click .downvote': function (event, template) {
+    // Get reference to current feedback
     var feedback = this;
+    
     // Get ID of current feedback object
     var feedbackId = feedback._id;
+    
     // Submit downvote (-1) for current feedback
     Meteor.call('submitVote', feedbackId, -1);
   }
