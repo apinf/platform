3283a7d8e514cd711f812aad9c582721fb5cce26
Damir Mustafin <frenchthebread@gmail.com>
2015-12-17 16:14:53 +0900

    Check if User is authorised

--- server/methods/elasticSearch.js
+++ server/methods/elasticSearch.js
@@ -4,57 +4,64 @@ Meteor.methods({
     // Initialise variables
     var loggedInUser, apiKey, query, searchResults;
 
-    // Get user object
-    loggedInUser = Meteor.user();
+    if (Meteor.user()) {
 
-    // Get user role & check user role
-    if (Roles.userIsInRole(loggedInUser, ['admin'])) {
+      // Get user object
+      loggedInUser = Meteor.user();
 
-      // If current user is Admin
-      // Construct query with no filters (See elasticsearch docs - https://www.elastic.co/guide/index.html)
-      query = {
-        match_all: {}
-      }
+      // Get user role & check user role
+      if (Roles.userIsInRole(loggedInUser, ['admin'])) {
 
-    } else{
+        // If current user is Admin
+        // Construct query with no filters (See elasticsearch docs - https://www.elastic.co/guide/index.html)
+        query = {
+          match_all: {}
+        }
 
-      // Get user's api_key
-      apiKey  = loggedInUser.profile.apiKey;
+      } else{
 
-      // Construct query based on user's api key
-      query   = {
-        "match": {
-          "api_key": apiKey
+        // Get user's api_key
+        apiKey  = loggedInUser.profile.apiKey;
+
+        // Construct query based on user's api key
+        query   = {
+          "match": {
+            "api_key": apiKey
+          }
         }
       }
+
+      var config = {
+        host: Meteor.settings.elasticsearch.host
+      };
+
+      var options = {
+        index: data.index,
+        type: data.type,
+        size: data.limit,
+        query: query,
+        fields: data.fields
+      };
+
+      // Create a new ElasticRest instance
+      var es = new ElasticRest(config, options);
+
+      // Try catch - if search fails, returns user friendly error
+      try{
+
+        searchResults = es.doSearch();
+
+      }catch(err){
+
+        // Throw a 500 error explaining that the data was not found
+        throw new Meteor.Error(500, 'Analytics data is not found.');
+      }
+
+      return searchResults;
+
+    } else {
+      
+
     }
-
-    var config = {
-      host: Meteor.settings.elasticsearch.host
-    };
-
-    var options = {
-      index: data.index,
-      type: data.type,
-      size: data.limit,
-      query: query,
-      fields: data.fields
-    };
-
-    // Create a new ElasticRest instance
-    var es = new ElasticRest(config, options);
-
-    // Try catch - if search fails, returns user friendly error
-    try{
-
-      searchResults = es.doSearch();
-
-    }catch(err){
-
-      // Throw a 500 error explaining that the data was not found
-      throw new Meteor.Error(500, 'Analytics data is not found.');
-    }
-
-    return searchResults;
   }
 });
