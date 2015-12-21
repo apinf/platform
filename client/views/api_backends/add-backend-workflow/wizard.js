69807db91087219fc9dcaf6ea1e5af3b7ab0af1f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-21 16:35:21 +0200

    Success adding API Backend to Umbrella & Apinf

--- client/views/api_backends/add-backend-workflow/wizard.js
+++ client/views/api_backends/add-backend-workflow/wizard.js
@@ -81,12 +81,48 @@ Template.addApiBackendWizard.helpers({
       formId: 'prefixes-information-form',
       schema: prefixesInformation,
       onSubmit: function (data, wizard) {
+        // Keep the context to use inside the callback function
+        var context = this;
+
         // Get API Backend details from form steps
         var apiBackend = _.extend(wizard.mergedData(), data);
+
+        // Restructure object to match API Umbrella API requirements
+        apiBackend.servers = [{
+          host: apiBackend.backend_host,
+          port: apiBackend.backend_port
+        }];
+
+        apiBackend.url_matches = [apiBackend.url_matches];
+
+        apiBackend.balance_algorithm = "least_conn";
 
         // Delete unneeded properties: insert/update related
         delete apiBackend.insertDoc;
         delete apiBackend.updateDoc;
+        delete apiBackend.backend_port;
+
+        Meteor.call('createApiBackendOnApiUmbrella', apiBackend, function(error, apiUmbrellaWebResponse) {
+          if (apiUmbrellaWebResponse.http_status === 200) {
+            // Insert the API Backend
+            ApiBackends.insert(apiBackend);
+
+            // Submit form on meteor:api-umbrella success
+            context.done();
+          } else {
+            var errors = _.values(apiUmbrellaWebResponse.errors);
+
+            // Flatten all error descriptions to show using sAlert
+            errors = _.flatten(errors);
+            _.each(errors, function(error) {
+              //Display error to the user, keep the sAlert box visible.
+              sAlert.error(error, {timeout: 'none'});
+            });
+
+            //Cancel form submission on error, so user see the sAlert.error message and edit the incorrect fields
+            context.result(false);
+          }
+        });
       }
     }];
 
