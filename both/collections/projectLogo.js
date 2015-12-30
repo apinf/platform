a38f660386415ddd4dc3492ad65ad080659713da
Damir Mustafin <frenchthebread@gmail.com>
2015-12-30 14:58:58 +0300

    Added extension check & maxSize filter

--- both/collections/projectLogo.js
+++ both/collections/projectLogo.js
@@ -4,6 +4,17 @@ ProjectLogo = new FS.Collection("projectLogo", {
   ]
 });
 
+ProjectLogo.filters({
+  allow: {
+    maxSize: 10048567, // ~ 10Mbs.
+    extensions: ['jpg', 'jpeg', 'png', 'gif']
+  },
+  onInvalid: function (message) {
+    // Show error message if it doesn't pass filter settings
+    FlashMessages.sendError(message);
+  }
+});
+
 if (Meteor.isServer) {
   ProjectLogo.allow({
     insert: function(userId, doc) {
