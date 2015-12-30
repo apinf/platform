2ca5d0e40136654dfe8607bcbe4604e2b9d744ce
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-30 16:29:37 +0200

    Refactor to use instance query; cleanup

--- client/views/search/search.js
+++ client/views/search/search.js
@@ -1,89 +1,94 @@
 Template.search.created = function () {
-
   // Get reference to Template instance
   var instance = this;
 
-  // Init reactive var for search value
-  instance.searchValue = new ReactiveVar();
+  // Init reactive var for search value with empty string for all results
+  instance.searchValue = new ReactiveVar('');
 
-  // Init reactive var for search results count
-  instance.searchResultsCount = new ReactiveVar(0);
+  // Init the query reactive variable
+  instance.query = new ReactiveVar();
+
+  // Check if search parameter is set
+  if (Router.current().params.query.q) {
+    // Get the query string parameter
+    var searchValue = Router.current().params.query.q;
+
+    // Assign current search value to the reactive variable
+    instance.searchValue.set(searchValue);
+  }
 
   instance.autorun(function () {
+    //var instance = this;
 
-    // Check if search parameter is set
-    if (Router.current().params.query.q) {
+    var searchValue = instance.searchValue.get();
 
-      var searchValue = Router.current().params.query.q;
-
-      // Update search field with current search value
-      $('#search-text').val(searchValue);
-
-      // Assign current search value to the reactive variable
-      instance.searchValue.set(searchValue);
-
-    }
-
+    // Construct query using regex using search value
+    instance.query.set({
+      $or: [
+        {
+          name: {
+            $regex: searchValue,
+            $options: 'i' // case-insensitive option
+          }
+        },
+        {
+          backend_host: {
+            $regex: searchValue,
+            $options: 'i' // case-insensitive option
+          }
+        }
+      ]
+    });
   });
 
+  instance.getSearchResults = function () {
+    var query = instance.query.get();
+    return ApiBackends.find(query).fetch();
+  };
 };
 
 Template.search.rendered = function () {
-
   // Get reference to Template
   var instance = this;
+
+  // Update search field with current search value
+  $('#search-text').val(instance.searchValue.get());
 
   // Check if search parameter is set
   if (instance.searchValue.get()) {
 
     // Update search field with search value provided in the URL
     $('#search-text').val(instance.searchValue.get());
-
   }
 
   // Put focus of search field on a page
   $('#search-text').focus();
-
 };
 
 Template.search.helpers({
   searchResults: function() {
-
     // Get reference to Template instance
     var instance = Template.instance();
 
-    // Get query parameter & assign it to searchValue
-    var searchValue = instance.searchValue.get();
+    // Init search results variable
+    var searchResults = instance.getSearchResults();
 
-    // Subscribe to a "search" publication
-    Meteor.subscribe("searchApiBackends", searchValue);
+    if (searchResults !== undefined) {
+      // Iterate through all documents
+      _.each(searchResults, function (result) {
 
-    // Fetch ApiBackend documents
-    var searchResults = ApiBackends.find().fetch();
-
-    // Count the amount of api backends found
-    var searchResultsCount = searchResults.length;
-
-    // Assign amount of search results to a reactive variable
-    instance.searchResultsCount.set(searchResultsCount);
-
-    // Iterate through all documents
-    _.each(searchResults, function (result) {
-
-      // Return to user human-readable timestamp
-      result.relative_created_at = moment(result.created_at).fromNow();
-    });
-
+        // Return to user human-readable timestamp
+        result.relative_created_at = moment(result.created_at).fromNow();
+      });
+    }
     return searchResults;
-
   },
   searchResultsCount: function () {
-
     // Get reference to Template instance
     var instance = Template.instance();
 
-    // Get the amount of api backends found from reactive var
-    var searchResultsCount = instance.searchResultsCount.get();
+        // Get the amount of api backends found from reactive var
+    var searchResultsCount = instance.getSearchResults().length;
 
     return searchResultsCount;
   }
@@ -104,18 +109,6 @@ Template.search.events({
 
     // Set query parameter to value of search text
     UniUtils.url.setQuery("q", searchValue);
-
-    return false;
-  },
-  "submit #search-form": function (event, template) {
-
-    event.preventDefault();
-
-    // Get search text from a text field.
-    var searchValue = $('#search-text').val();
-
-    // Update current page URL with updated search value
-    Router.go('search', {}, {query: 'q='+searchValue});
 
     return false;
   }
