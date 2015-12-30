db236c3c0d1cbd556b8293b4746be965359ec047
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-12-30 20:46:43 +0200

    Hide navbar search form on search page

--- client/layouts/master_layout/navbar/navbar.js
+++ client/layouts/master_layout/navbar/navbar.js
@@ -4,6 +4,16 @@ Template.navbar.helpers({
     var profilePicture = ProfilePictures.findOne({});
     // return that url
     return profilePicture.url();
+  },
+  "isSearchRoute": function () {
+    // Get name of current route from Router
+    var routeName = Router.current().route.getName();
+
+    if (routeName === "search") {
+      return true;
+    } else {
+      return false;
+    }
   }
 });
 
