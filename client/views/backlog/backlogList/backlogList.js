b53d138c181e67acf4a7d9e2bc7bc44fb31a43a7
Damir Mustafin <frenchthebread@gmail.com>
2016-01-07 23:17:42 +0200

    Remove backlog item functionality

--- client/views/backlog/backlogList/backlogList.js
+++ client/views/backlog/backlogList/backlogList.js
@@ -60,3 +60,12 @@ Template.apiBacklogList.helpers({
     return ApiBacklog.find().count() > 0;
   }
 });
+
+
+Template.apiBacklogList.events({
+  // Delete backlog item
+  'click .delete-backlog': function (event) {
+    //Meteor.call('deleteBacklogItem', this._id);
+    ApiBacklog.remove(this._id);
+  }
+});
