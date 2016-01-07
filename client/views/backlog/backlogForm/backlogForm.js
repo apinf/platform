05a5ee1c3e926cbc3e20929ad0f64b890ea3306b
Damir Mustafin <frenchthebread@gmail.com>
2016-01-07 22:21:39 +0200

    Autoform hooks

--- client/views/backlog/backlogForm/backlogForm.js
+++ client/views/backlog/backlogForm/backlogForm.js
@@ -1,7 +1,27 @@
-Template.apiBacklogForm.created = function () {
+AutoForm.hooks({
+  apiBacklog: {
+    beginSubmit: function () {
+      // Disable form elements while submitting form
+      $('#apiBacklogFormSubmit').attr("disabled", "disabled");
+    },
+    endSubmit: function () {
+      // Enable form elements after form submission
+      $('#apiBacklogFormSubmit').removeAttr("disabled");
+    }
+  }
+});
 
-};
+AutoForm.addHooks(['apiBacklog'], {
+  // Success message
+  onSuccess: function () {
+    $('#apiBacklogFormModal').css('display', 'none');
+    FlashMessages.sendSuccess('Thank you! Your backlog has been successfully published.');
+  }
+});
 
-Template.apiBacklogForm.rendered = function () {
-  
-};
+FlashMessages.configure({
+  // Configuration for FlashMessages
+  autoHide: true,
+  hideDelay: 5000,
+  autoScroll: false
+});
