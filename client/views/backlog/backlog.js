fe5de1ff10661df5b4ded0051c710d394da16d4e
Damir Mustafin <frenchthebread@gmail.com>
2016-01-11 22:10:56 +0200

    Inline comments & cleanup

--- client/views/backlog/backlog.js
+++ client/views/backlog/backlog.js
@@ -1,36 +1,39 @@
 Template.apiBacklog.created = function () {
 
+  // Get reference to template instance
   var instance = this;
 
+  // Get current API Backend Id from URL - (URL should look like "/api/:_id/backlog")
   instance.apiBackendId = Router.current().params._id;
 };
-
-
-Template.apiBacklog.rendered = function() {
-
-};
-
 
 Template.apiBacklog.helpers({
   currentUserIsApiManager: function () {
 
+    // Get reference to a template instance
     var instance = Template.instance();
 
+    // Fetch current user's id
     var userId = Meteor.userId();
 
     var apiBackendId = instance.apiBackendId;
 
+    // Find related API Backend that contains "managerIds" field
     var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
 
+    // Try - Catch wrapper here because Mongodb call above can return zero matches
     try {
 
+      // Get managerIds array from API Backend document
       var managerId = apiBackend.managerIds;
 
     } catch (err) {
 
+      // If no related document found return false - API Backend does not have any managers listed
       return false;
     }
 
+    // Check if an array of managerIds contain user id passed
     var isManager = _.contains(managerId, userId);
 
     return isManager;
