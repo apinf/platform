2a937cdada8ba7b7ed146be578bf46a3be980a19
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-12 15:27:30 +0200

    Add organization field; attach schema

--- both/collections/apiMetadata.js
+++ both/collections/apiMetadata.js
@@ -4,5 +4,16 @@ ApiMetadata.schema = new SimpleSchema({
   "apiBackendId": {
     type: String,
     regEx: SimpleSchema.RegEx.Id
+  },
+  "organization": {
+    type: Object
+  },
+  "organization.name": {
+    type: String
+  },
+  "organization.description": {
+    type: String
   }
 });
+
+ApiMetadata.attachSchema(ApiMetadata.schema);
