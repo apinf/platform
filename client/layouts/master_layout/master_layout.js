d13a454116cb69882ec80d1a30f0a8ad12877136
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-01-12 16:51:05 +0200

    Removed unused helpers.

--- client/layouts/master_layout/master_layout.js
+++ client/layouts/master_layout/master_layout.js
@@ -1,24 +1,17 @@
-Template.masterLayout.helpers({
-  branding: function() {
-    var theme = Branding.find();
-    if (theme) {
-      return theme
-    }
-  },
-  projectLogo: function () {
-
-    // Get last uploaded image from collection
-    var lastUploadedLogo = ProjectLogo.findOne({}, {sort: {uploadedAt: -1}});
-    
-    if (lastUploadedLogo) {
-      return lastUploadedLogo
-    }
-  }
-});
-
 Template.masterLayout.created = function () {
   // Subscription to branding collection
   this.subscribe('branding');
   // Subscription to projectLogo collection
   this.subscribe('projectLogo');
 };
+
+Template.masterLayout.helpers({
+  projectLogo: function () {
+    // Get last uploaded image from collection
+    var lastUploadedLogo = ProjectLogo.findOne({}, {sort: {uploadedAt: -1}});
+
+    if (lastUploadedLogo) {
+      return lastUploadedLogo
+    }
+  }
+});
