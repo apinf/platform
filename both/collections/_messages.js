59d8ce27c33f6804dca1794f5b2f2ec70e5e878e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-15 10:29:58 +0200

    Use i18n string

--- both/collections/_messages.js
+++ both/collections/_messages.js
@@ -1,2 +1,5 @@
-// var usernameIsTaken = TAPi18n.__("profile-usernameIsTaken");
-SimpleSchema.messages({usernameTaken: "User name already exists. Enter another user name."});
+// Fetch the profile username is taken message
+var usernameIsTaken = TAPi18n.__("profile-usernameIsTaken");
+
+// Configure SimpleSchema with username is taken message
+SimpleSchema.messages({usernameTaken: usernameIsTaken});
