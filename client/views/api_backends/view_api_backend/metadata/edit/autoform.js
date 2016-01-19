0255f6c70f911a46228f4ca863fa215cdadc9c71
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-19 13:15:10 +0200

    Add AutoForm hook

---
+++ client/views/api_backends/view_api_backend/metadata/edit/autoform.js
@@ -0,0 +1,16 @@
+AutoForm.addHooks("editApiMetadataForm", {
+  before: {
+    "insert": function (metadata) {
+      // Get reference to Router
+      let router = Router.current();
+
+      // Get API Backend ID, from Router
+      let apiBackendId = router.params._id;
+
+      // Set the API Backend ID property of the metadata document
+      metadata.apiBackendId = apiBackendId;
+
+      return metadata;
+    }
+  }
+});
