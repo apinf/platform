3eb31d3f7e09db5bcfc0472fda1cd1d94309b9e9
Damir Mustafin <frenchthebread@gmail.com>
2016-01-19 02:16:00 +0200

    "Search form present" - test case

---
+++ tests/jasmine/client/integration/sample/spec/hasSearchBox.js
@@ -0,0 +1,14 @@
+describe('Search form present', function () {
+  beforeEach(function (done) {
+    Router.go('/search');
+    Tracker.afterFlush(done);
+  });
+
+  beforeEach(waitForRouter);
+
+  it('should not contain search form at "/search" page', function () {
+
+    expect($('form#header-search-form').length).toEqual(0);
+
+  });
+});
