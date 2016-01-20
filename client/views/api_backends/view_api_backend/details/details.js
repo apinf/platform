44636c55a47495d59a32b8613a44568553d117b6
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 14:12:43 +0200

    Add currentUserCanEditApiBackend helper

---
+++ client/views/api_backends/view_api_backend/details/details.js
@@ -0,0 +1,12 @@
+Template.viewApiBackendDetails.helpers({
+  currentUserCanEditApiBackend: function() {
+    // Get current API backend ID
+    var apiBackendId = this.apiBackend._id;
+
+    // Find related API Backend that contains "managerIds" field
+    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
+
+    // Check if current user can edit API Backend
+    return apiBackend.currentUserCanEdit();
+  }
+});
