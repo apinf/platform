c3b6a4fe2f118db73132e02f0878b09331c0d014
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 16:00:03 +0200

    Switch to currentUserCanEditBacklog helper

--- client/views/api_backends/view_api_backend/backlog/backlogList/backlogList.js
+++ client/views/api_backends/view_api_backend/backlog/backlogList/backlogList.js
@@ -59,34 +59,19 @@ Template.apiBacklogList.helpers({
     return priorityClass;
 
   },
-  currentUserIsApiManager: function () {
+  currentUserCanEditBacklog: function() {
+    /*
+     API Backlog shares permissions with the API Backend
+     Make sure user can edit API Backend before allowing Metadata permissions
+    */
 
-    // Get reference to a template instance
-    var instance = Template.instance();
-
-    // Ger current user's id
-    var userId = Meteor.userId();
-
-    var apiBackendId = instance.apiBackendId;
+    // Get current API backend ID
+    var apiBackendId = this.apiBackend._id;
 
     // Find related API Backend that contains "managerIds" field
     var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
 
-    // Try - Catch wrapper here because Mongodb call above can return zero matches
-    try {
-
-      // Get managerIds array from API Backend document
-      var managerIds = apiBackend.managerIds;
-
-    } catch (err) {
-
-      // If no related document found return false - API Backend does not have any managers listed
-      return false;
-    }
-
-    // Check if an array of managerIds contain user id passed
-    var isManager = _.contains(managerIds, userId);
-
-    return isManager;
+    // Check if current user can edit API Backend
+    return apiBackend.currentUserCanEdit();
   }
 });
