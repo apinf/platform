af7ac54a2ddf396d1d04eeba08074de47cacb90b
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 13:07:43 +0200

    Use data context and collection helper

--- client/views/backlog/backlog.js
+++ client/views/backlog/backlog.js
@@ -1,55 +1,19 @@
 Template.apiBacklog.created = function () {
-
   // Get reference to template instance
-  var instance = this;
+  const instance = this;
 
-  // Get current API Backend Id from URL - (URL should look like "/api/:_id/backlog")
-  instance.apiBackendId = Router.current().params._id;
+  // Get API Backend ID from data context
+  const apiBackendId = instance.data.apiBackend._id;
 
   // Subscribe to API Backlog items for this API Backend
-  instance.subscribe("apiBacklogItems", instance.apiBackendId);
-
-  // TODO: probably remove this sub later
-  // Subscribe to API Backends managed by current user
-  instance.subscribe("myManagedApis");
+  instance.subscribe("apiBacklogItems", apiBackendId);
 };
 
 Template.apiBacklog.helpers({
-  apiBackendId: function () {
-
-    // Get reference to a template instance
-    var instance = Template.instance();
-
-    return instance.apiBackendId;
-  },
   currentUserIsApiManager: function () {
+    var apiBackend = this.apiBackend;
 
-    // Get reference to a template instance
-    var instance = Template.instance();
-
-    // Fetch current user's id
-    var userId = Meteor.userId();
-
-    var apiBackendId = instance.apiBackendId;
-
-    // Find related API Backend that contains "managerIds" field
-    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
-
-    // Try - Catch wrapper here because Mongodb call above can return zero matches
-    try {
-
-      // Get managerIds array from API Backend document
-      var managerIds = apiBackend.managerIds;
-
-    } catch (err) {
-
-      // If no related document found return false - API Backend does not have any managers listed
-      return false;
-    }
-
-    // Check if an array of managerIds contain user id passed
-    var isManager = _.contains(managerIds, userId);
-
-    return isManager;
+    // Make sure current user is API Backend manager
+    return apiBackend.currentUserIsManager();
   }
 });
