d431557c8257966bcac68c05b64c39b09b91fe0c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 12:20:32 +0200

    Use apiBackend.currentUserCanEdit in helper

--- client/views/api_backends/view_api_backend/metadata/metadata.js
+++ client/views/api_backends/view_api_backend/metadata/metadata.js
@@ -1,38 +1,12 @@
 Template.viewApiBackendMetadata.helpers({
   canEditMetadata: function() {
-    // Get current userId
-    var userId = Meteor.userId();
+    // Get current API backend ID
+    var apiBackendId = this.apiBackend._id;
 
-    // Check that user is logged in
-    if( userId ) {
-      // Get current API backend ID
-      var apiBackendId = this.apiBackend._id;
+    // Find related API Backend that contains "managerIds" field
+    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
 
-      // Find related API Backend that contains "managerIds" field
-      var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
-
-      // Try - Catch wrapper here because Mongodb call above can return zero matches
-      try {
-        // Get managerIds array from API Backend document
-        var managerIds = apiBackend.managerIds;
-      } catch (err) {
-        // If no related document found return false - API Backend does not have any managers listed
-        return false;
-      }
-
-      // Check if an array of managerIds contain user id passed
-      var isManager = _.contains(managerIds, userId);
-      // Check if user is administrator
-      var isAdmin = Roles.userIsInRole(userId, ['admin']);
-
-      // Check that user is either API manager OR Admin
-      if(isManager || isAdmin) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
+    // Check if current user can edit API Backend
+    return apiBackend.currentUserCanEdit();
   }
 });
