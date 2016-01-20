3207006321080226614a65242863a513c5cc79cb
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 15:49:23 +0200

    Add currentUserCanEditBacklog helper

--- client/views/api_backends/view_api_backend/backlog/backlog.js
+++ client/views/api_backends/view_api_backend/backlog/backlog.js
@@ -10,10 +10,19 @@ Template.apiBacklog.created = function () {
 };
 
 Template.apiBacklog.helpers({
-  currentUserIsApiManager: function () {
-    var apiBackend = this.apiBackend;
+  currentUserCanEditBacklog: function() {
+    /*
+     API Metadata shares permissions with the API Backend
+     Make sure user can edit API Backend before allowing Metadata permissions
+    */
 
-    // Make sure current user is API Backend manager
-    return apiBackend.currentUserIsManager();
+    // Get current API backend ID
+    var apiBackendId = this.apiBackend._id;
+
+    // Find related API Backend that contains "managerIds" field
+    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
+
+    // Check if current user can edit API Backend
+    return apiBackend.currentUserCanEdit();
   }
 });
