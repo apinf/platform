681290aa6e441a53c24cdff4369fe3ca784d63da
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-01-20 10:56:19 +0200

    Add database level permissions

--- both/collections/apiMetadata.js
+++ both/collections/apiMetadata.js
@@ -62,16 +62,61 @@ ApiMetadata.schema = new SimpleSchema({
 ApiMetadata.attachSchema(ApiMetadata.schema);
 
 ApiMetadata.allow({
-  "insert": function () {
-    // TODO: add the following checks
-    // Count existing API Metadata documents for API Backend ID
+  "insert": function (userId, doc) {
+    var apiBackendId = doc.apiBackendId;
     // Make sure there is only one document per API Backend ID
-    // Make sure user is API Backend manager or administrator
-    return true;
+    if(ApiMetadata.find({apiBackendId}).count() !== 0) {
+      return false;
+    } else {
+      // Find related API Backend that contains "managerIds" field
+      var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
+
+      // Try - Catch wrapper here because Mongodb call above can return zero matches
+      try {
+        // Get managerIds array from API Backend document
+        var managerIds = apiBackend.managerIds;
+      } catch (err) {
+        // If no related document found return false - API Backend does not have any managers listed
+        return false;
+      }
+
+      // Check if an array of managerIds contain user id passed
+      var isManager = _.contains(managerIds, userId);
+      // Check if user is administrator
+      var isAdmin = Roles.userIsInRole(userId, ['admin']);
+
+      // Check that user is either API manager OR Admin
+      if(isManager || isAdmin) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   },
-  "update": function () {
-    // TODO: Add the following checks
-    // Make sure user is manager for API Backend, or administrator
-    return true;
+  "update": function (userId, doc) {
+    var apiBackendId = doc.apiBackendId;
+    // Find related API Backend that contains "managerIds" field
+    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
+
+    // Try - Catch wrapper here because Mongodb call above can return zero matches
+    try {
+      // Get managerIds array from API Backend document
+      var managerIds = apiBackend.managerIds;
+    } catch (err) {
+      // If no related document found return false - API Backend does not have any managers listed
+      return false;
+    }
+
+    // Check if an array of managerIds contain user id passed
+    var isManager = _.contains(managerIds, userId);
+    // Check if user is administrator
+    var isAdmin = Roles.userIsInRole(userId, ['admin']);
+
+    // Check that user is either API manager OR Admin
+    if(isManager || isAdmin) {
+      return true;
+    } else {
+      return false;
+    }
   }
 });
