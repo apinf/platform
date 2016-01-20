e156bbb2698466e83a153fd3765a6c314d0ff7cb
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-20 16:46:48 +0200

    Cleanup

--- client/views/api_backends/view_api_backend/backlog/backlog.js
+++ client/views/api_backends/view_api_backend/backlog/backlog.js
@@ -1,14 +1,3 @@
-Template.apiBacklog.created = function () {
-  // Get reference to template instance
-  const instance = this;
-
-  // Get API Backend ID from data context
-  const apiBackendId = instance.data.apiBackend._id;
-
-  // Subscribe to API Backlog items for this API Backend
-  instance.subscribe("apiBacklogItems", apiBackendId);
-};
-
 Template.apiBacklog.helpers({
   currentUserCanEditBacklog: function() {
     /*
