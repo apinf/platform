c3bce7fee46d586a8ed4f7b162aa0ba382bf2191
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-22 11:34:02 +0200

    Remove unused file

--- client/views/api_backends/view_api_backend/details/details.js
+++
@@ -1,9 +0,0 @@
-Template.viewApiBackendDetails.helpers({
-  currentUserCanEditApiBackend: function() {
-    // Get current API backend ID
-    var apiBackend = this.apiBackend;
-
-    // Check if current user can edit API Backend
-    return apiBackend.currentUserCanEdit();
-  }
-});
