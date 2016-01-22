681fc67f7825d02ea790d188c3ec6a5a3b027c1f
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-01-22 12:06:18 +0200

    Add helper for languages

--- client/views/home/header/language/language.js
+++ client/views/home/header/language/language.js
@@ -1,3 +1,17 @@
+Template.languageBar.helpers({
+  languages: function() {
+    const languages = TAPi18n.getLanguages();
+    let languageOptions = [];
+    for (language in languages) {
+      let languageOption = languages[language];
+      languageOption.tag = language;
+      languageOptions.push(languageOption);
+    }
+    console.log(languageOptions);
+    return languageOptions;
+  }
+});
+
 Template.languageBar.events({
   "click #en": function(event, template) {
     TAPi18n.setLanguage('en');
