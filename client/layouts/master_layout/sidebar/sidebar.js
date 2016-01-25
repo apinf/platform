3846c7d19e66820547790b7769fd54558f9cee46
sebbel <sebastian@pancia.de>
2016-01-25 17:20:55 +0100

    Documentation Editor will not be shown if apiDocumentationEditor.host value is empty. added apiDocumentationEditor field to config.example.json

--- client/layouts/master_layout/sidebar/sidebar.js
+++ client/layouts/master_layout/sidebar/sidebar.js
@@ -6,10 +6,14 @@ Template.sidebar.helpers({
     var backendsCount  = ApiBackends.find({managerIds: currentUserId}).count();
     // return true if user has backends
     return backendsCount > 0;
+  },
+  apiDoc_notEmpty : function() {
+    return Settings.find({"apiDocumentationEditor.host": {$exists: true}}).count > 0
   }
 });
 
 Template.sidebar.created = function () {
   var instance = this;
   instance.subscribe('myManagedApis');
+  instance.subscribe('settings');
 }
