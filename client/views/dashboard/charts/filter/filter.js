acb2d6658e65434444a44d7da0e08e8b4bf91e0d
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-02-02 12:11:04 +0200

    Reset filters and charts data

--- client/views/dashboard/charts/filter/filter.js
+++ client/views/dashboard/charts/filter/filter.js
@@ -36,5 +36,41 @@ Template.chartsLayout.events({
 
     instance.getDashboardData(input)
 
+  },
+  "click #reset": function(event, template){
+    // appending loading state
+    $('#loadingState').html("Loading...");
+
+
+
+    // gets current month and year -> providing them for initial query
+    var currentYearAndMonth = moment().format("YYYY-MM");
+
+    // sets default items amount to be returned
+    var initialLimit = 10000;
+
+    // Initialize filter values
+    $('#month option[value="00"]').prop('selected', true);
+    $('#year option[value="0000"]').prop('selected', true);
+    $('#limit').val( initialLimit.toString() );
+
+    // sets query for elastic search
+    var input = {
+      index : "api-umbrella-logs-v1-"+currentYearAndMonth,
+      type  : "log",
+      limit : initialLimit,
+      fields: [
+        'request_at',
+        'request_ip_country',
+        'request_ip',
+        'response_time',
+        'request_path',
+        'request_ip_location.lon',
+        'request_ip_location.lat'
+      ]
+    };
+
+    // get data
+    template.getDashboardData(input);
   }
 });
