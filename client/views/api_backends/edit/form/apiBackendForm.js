46745db5e58064ffc9b6fd890ed4cb94ba7c827c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-08 12:46:38 +0200

    Move currentUserCanEditApi helper

--- client/views/api_backends/edit/form/apiBackendForm.js
+++ client/views/api_backends/edit/form/apiBackendForm.js
@@ -27,34 +27,6 @@ Template.apiBackendForm.helpers({
       return apiBackend;
     }
   },
-  'currentUserCanEditApi': function () {
-    if (Meteor.user()) {
-      // Get reference to template instance
-      let instance = Template.instance();
-
-      // Get reference to router
-      let router = Router.current();
-
-      // Get API Backend ID
-      var backendId = router.params._id;
-
-      // Make sure template subscriptions are ready
-      if (instance.subscriptionsReady()) {
-        // Get API Backend
-        var apiBackend = ApiBackends.findOne(backendId);
-        console.log(apiBackend);
-        if (apiBackend) {
-          // Check if current User can edit the API Backend
-          var userCanEdit = apiBackend.currentUserCanEdit();
-
-          if (userCanEdit) {
-            // User is authorized to edit
-            return true;
-          }
-        }
-      }
-    }
-  },
   'editMode': function () {
     // Get the current Router
     var router = Router.current();
