932c409f34612ddc5678948471896d6390d8ffb1
sebbel <sebastian@pancia.de>
2016-02-08 15:34:18 +0100

    indentation

--- both/collections/settings.js
+++ both/collections/settings.js
@@ -20,12 +20,12 @@ Schemas.SettingsSchema = new SimpleSchema({
     label: "Host",
     optional: true, // Optional must be true for custom validation
     custom: function () { // Custom validator logic
-        // get the value of apiDocumentationEditor.enabled field
-        let enabledFieldValue = this.field("apiDocumentationEditor.enabled").value;
-        // if enabled is true, host field is required
-        if (enabledFieldValue === true) {
-          return "required"; // host field is required
-        }
+      // get the value of apiDocumentationEditor.enabled field
+      let enabledFieldValue = this.field("apiDocumentationEditor.enabled").value;
+      // if enabled is true, host field is required
+      if (enabledFieldValue === true) {
+        return "required"; // host field is required
+      }
     }
   },
   apiUmbrella: {
