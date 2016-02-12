9fbcd6ac8acbee15979bddcad45cb08c83c70903
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-02-12 11:24:54 +0200

    Made suggested changes and tested

--- server/migrations.js
+++ server/migrations.js
@@ -2,15 +2,20 @@ Migrations.add({
   version: 1,
   name: 'Add manager role to all users that have APIs.',
   up: function() {
-    let allManagerIds = [];
     let allApis = ApiBackends.find().fetch();
-    // Iterate through allApis and push managerIds to allManagerIds array
-    for(api in allApis) {
-      allManagerIds.push.apply(allManagerIds, allApis[api].managerIds);
-    }
-    for(manager in allManagerIds) {
-      // Add user to manager Role
-      Roles.addUsersToRoles(allManagerIds[manager], ['manager']);
-    }
+    // Create array of all managerIds
+    let managerIds = allApis.map(function (api) {
+      return api.managerIds;
+    });
+    // Flatten the nested array
+    managerIds = _.flatten(managerIds);
+
+    managerIds.forEach(function (managerId) {
+      // Check that managerId is not null
+      if (managerId) {
+        // Add user to manager Role
+        Roles.addUsersToRoles(managerId, 'manager');
+      }
+    });
   }
 });
