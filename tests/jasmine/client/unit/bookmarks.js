5a54b220970f23338cc1f4de676040e2623f8530
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-02-24 12:23:25 +0200

    unit tests for bookmarks written

---
+++ tests/jasmine/client/unit/bookmarks.js
@@ -0,0 +1,28 @@
+describe("tests for bookmarked Apis", function() {
+
+    beforeEach(function() {
+        var Meteor = {
+            userId: function() {
+                return 1;
+            }
+        };
+        var ApiBookmarks = {
+            find: function() {
+                return 0;
+            }
+        };
+        
+    });
+
+    it("checks if the userId function of Meteor is called", function() {
+        spyOn(Meteor, 'userId').and.callThrough();
+        Template.bookmarks.__helpers[' userBookmarks']();
+        expect(Meteor.userId).toHaveBeenCalled();
+    });
+
+    it("checks if the find function of ApiBookmarks is called once", function() {
+        spyOn(ApiBookmarks, 'find').and.callThrough();
+        Template.bookmarks.__helpers[' userBookmarks']();
+        expect(ApiBookmarks.find.calls.count()).toBe(1);
+    });
+});
