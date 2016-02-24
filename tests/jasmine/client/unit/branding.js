6e851f610c5d53fce5c13244dc92cff3690787dd
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-02-24 17:50:48 +0200

    new tests added to branding helpers

--- tests/jasmine/client/unit/branding.js
+++ tests/jasmine/client/unit/branding.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe("branding helpers tests", function() {
 
     it("test findOne is called once in the branding helper", function() {
@@ -12,9 +14,23 @@ describe("branding helpers tests", function() {
         expect(ProjectLogo.findOne).toHaveBeenCalledWith({}, {sort: {uploadedAt: -1}});
     });
 
+    it("test projectLogo helper returns non-null when findOne returns non-null value", function() {
+        spyOn(ProjectLogo, 'findOne').and.callFake(function() {
+            return 1000;
+        });
+        expect(Template.branding.__helpers[' projectLogo']()).toEqual(1000);
+    });
+
     it("test arguments with which findOne is called in coverPhoto helper", function() {
         spyOn(CoverPhoto, 'findOne').and.callThrough();
         Template.branding.__helpers[' coverPhoto']();
         expect(CoverPhoto.findOne).toHaveBeenCalledWith({}, {sort: {uploadedAt: -1}});
+    });
+
+    it ("test coverPhoto helper returns non-null when findOne returns non-null value", function() {
+        spyOn(CoverPhoto, 'findOne').and.callFake(function() {
+            return 3000;
+        });
+        expect(Template.branding.__helpers[' coverPhoto']()).toEqual(3000);
     });
 });
