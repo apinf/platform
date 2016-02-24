c2218a69f36002650947fd1774e8cc23daa603ac
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-02-24 13:10:41 +0200

    Spying on userid

--- tests/jasmine/client/unit/bookmarks.js
+++ tests/jasmine/client/unit/bookmarks.js
@@ -21,6 +21,7 @@ describe("tests for bookmarked Apis", function() {
     });
 
     it("checks if the find function of ApiBookmarks is called once", function() {
+        spyOn(Meteor, 'userId').and.callThrough();
         spyOn(ApiBookmarks, 'find').and.callThrough();
         Template.bookmarks.__helpers[' userBookmarks']();
         expect(ApiBookmarks.find.calls.count()).toBe(1);
