7d047a53222e6c1ad66df1af164dc35874e63363
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-03-18 17:01:26 +0200

    API changes published to umbrelaa

--- client/views/api_backends/delete/deleteApiBackendConfirmation.js
+++ client/views/api_backends/delete/deleteApiBackendConfirmation.js
@@ -48,21 +48,31 @@ Template.deleteApiBackendConfirmation.events({
       // 204 is the HTTP response code when the API has been successfully removed in the umbrella
       if (apiUmbrellaWebResponse.http_status === 204) {
 
-        // call method to remove API backend from collections
-        Meteor.call('removeApiBackend', instance.backendId);
+        // Publish changes made to the deleted API to the Umbrella
+        Meteor.call('publishApiBackendOnApiUmbrella', apiUmbrellaApiId, function(error, apiUmbrellaWebResponse) {
 
-        // based on name of current route, load suitable parent page
-        const currentRoute = Router.current().route.getName();
+          // Changes to API backend successfully deleted
+          if (apiUmbrellaWebResponse.http_status == 201) {
 
-        if (currentRoute == 'viewApiBackend') {
-          // Go to catalogue page
-            Router.go('catalogue');
-        };
+            // call method to remove API backend from collections
+            Meteor.call('removeApiBackend', instance.backendId);
 
-        // Wait for template to render before showing alert to user
-        setTimeout(function () {
-          sAlert.success(instance.backendName + " was successfully deleted!");
-        }, 1000);
+            // based on name of current route, load suitable parent page
+            const currentRoute = Router.current().route.getName();
+
+            if (currentRoute == 'viewApiBackend') {
+              // Go to catalogue page
+              Router.go('catalogue');
+            }
+
+            // Wait for template to render before showing alert to user
+            setTimeout(function () {
+              sAlert.success(instance.backendName + " was successfully deleted!");
+            }, 1000);
+          } else {
+              sAlert.error("Changes to " + instance.backendName + " could not be published!" );
+          }
+        });
 
       } else {
         sAlert.error(instance.backendName + " could not be deleted!");
