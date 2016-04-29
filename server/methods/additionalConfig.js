ba3a92d20afa74cd3a5a3c78b244d36e5ebb5e4f
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-04-29 16:11:33 +0300

    Changes in server method for checking and updating initialSetupComplete flags

--- server/methods/additionalConfig.js
+++ server/methods/additionalConfig.js
@@ -1,9 +1,15 @@
 Meteor.methods({
   "additionalSetupRequired": function() {
-    const configRequired = Meteor.settings.initialSetupComplete;  
+    // check if settings details are complete
+    const settings = Settings.findOne();
+    const configRequired = settings.initialSetupComplete;  
     return configRequired === false || configRequired === undefined;
   },
   "initialSetupCompleteTrue": function() {
-    Meteor.settings.initialSetupComplete = true;
+    // get reference to settings object from collection
+    const settings = Settings.findOne();
+    // set 'initialSetupComplete' field value to true in document
+    Settings.update({ _id: settings._id }, { $set:{ initialSetupComplete : true }});
+    return true;
   }
 });
