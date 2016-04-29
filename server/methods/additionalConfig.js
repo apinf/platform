f4e81e9bb0e4117b18258ecde4c83118030e4c29
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-04-29 12:02:29 +0300

    New method added to set flag to true

--- server/methods/additionalConfig.js
+++ server/methods/additionalConfig.js
@@ -2,5 +2,8 @@ Meteor.methods({
   "additionalSetupRequired": function() {
     const configRequired = Meteor.settings.initialSetupComplete;  
     return configRequired === false || configRequired === undefined;
+  },
+  "initialSetupCompleteTrue": function() {
+    Meteor.settings.initialSetupComplete = true;
   }
 });
