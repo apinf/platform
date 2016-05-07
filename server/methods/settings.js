55866d15f7c0189135748597f89d7c36bc8bdf3b
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-05-08 02:16:16 +0300

    Method added to check initial Meteor.settings object structure

--- server/methods/settings.js
+++ server/methods/settings.js
@@ -12,5 +12,17 @@ Meteor.methods({
     var apiUmbrellaHost = apiUmbrellaBaseUrl.host();
 
     return apiUmbrellaHost;
+  },
+  'isSettingsObjectValidAndEmpty': function() {
+    // check structure of Meteor.settings object - initially, it should be { public: {} }
+
+    // array reference to keys in Meteor.settings object
+
+    const settingKeys = Object.keys(Meteor.settings);
+    if (settingKeys.length === 1 && settingKeys[0] === 'public') {
+      return JSON.stringify(Meteor.settings['public']) === JSON.stringify({});
+    } 
+
+    return false;
   }
 });
