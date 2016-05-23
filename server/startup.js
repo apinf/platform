e48ae9b5879e6ce746f6c89593cf858e591cd98e
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-05-23 17:17:46 +0300

    Fixing conflicts

--- server/startup.js
+++ server/startup.js
@@ -56,6 +56,20 @@ Meteor.startup(function () {
     "backend_host": 1
   });
 
+  // Init file upload server
+  UploadServer.init({
+    tmpDir: process.env.PWD + '/uploads/tmp',
+    uploadDir: process.env.PWD + '/uploads/',
+    checkCreateDirectories: true, //create the directories for you
+    getDirectory: function(fileInfo, formData) {
+      // create a sub-directory in the uploadDir based on the content type (e.g. 'images')
+      return formData.contentType;
+    },
+    finished: function(fileInfo, formFields) {
+      // perform a disk operation
+    }
+  });
+
   // Initialize cron jobs
   SyncedCron.start();
 });
