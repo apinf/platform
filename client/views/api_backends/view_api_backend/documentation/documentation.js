5cf3a16f6e5e882325354c5f8d919a4dd24cb5a9
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-05-23 15:10:02 +0300

    Add initial tab panel with upload button.

---
+++ client/views/api_backends/view_api_backend/documentation/documentation.js
@@ -0,0 +1,9 @@
+Template.viewApiBackendDocumentation.events({
+  'click #uploadSwaggerFile' : function (event, instance) {
+    // Get API Backend from database collection
+    var apiBackend = instance.data.apiBackend;
+
+    // Upload Swagger file JSON/YAML
+    
+  }
+});
