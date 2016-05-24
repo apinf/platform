715eecef4fd10680282424cc07956d9dfd970f97
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-05-24 10:13:49 +0300

    Uploading works

--- client/views/api_backends/view_api_backend/documentation/documentation.js
+++ client/views/api_backends/view_api_backend/documentation/documentation.js
@@ -2,13 +2,3 @@ Template.viewApiBackendDocumentation.onCreated(function(){
   // Init file uploader
   Uploader.init(this);
 });
-
-Template.viewApiBackendDocumentation.events({
-  'click #uploadSwaggerFile' : function (event, instance) {
-    // Get API Backend from database collection
-    var apiBackend = instance.data.apiBackend;
-
-    // Upload Swagger file JSON/YAML
-
-  }
-});
--- server/startup.js
+++ server/startup.js
@@ -60,14 +60,7 @@ Meteor.startup(function () {
   UploadServer.init({
     tmpDir: process.env.PWD + '/uploads/tmp',
     uploadDir: process.env.PWD + '/uploads/',
-    checkCreateDirectories: true, //create the directories for you
-    getDirectory: function(fileInfo, formData) {
-      // create a sub-directory in the uploadDir based on the content type (e.g. 'images')
-      return formData.contentType;
-    },
-    finished: function(fileInfo, formFields) {
-      // perform a disk operation
-    }
+    checkCreateDirectories: true //create the directories for you
   });
 
   // Initialize cron jobs
