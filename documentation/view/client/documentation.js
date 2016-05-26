1306e0b8add9bf92f6ee61d73a257a86233cc05c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-05-26 16:47:14 +0300

    Fix apiBackend related code

--- documentation/view/client/documentation.js
+++ documentation/view/client/documentation.js
@@ -1,5 +1,7 @@
 Template.documentation.events({
   'click #manage-api-documentation' () {
+    // Get reference to API backend
+    const apiBackend = instance.data.apiBackend
     // Show the manage API documentation form
     Modal.show('manageApiDocumentationModal', { apiBackend })
   }
