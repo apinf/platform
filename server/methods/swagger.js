d1a22b9222a5be521507587526a8e7ccda6b1670
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-05-30 14:14:50 +0300

    Improve function

--- server/methods/swagger.js
+++ server/methods/swagger.js
@@ -3,17 +3,16 @@ import SwaggerParser from 'swagger-parser';
 Meteor.methods({
   // Validate Swagger JSON/YAML
   // Params: URL or file path to Swagger file
-  // Returns: true if valid else false
-  validateSwagger: function(url) {
-     SwaggerParser.validate(url)
-      .then(function(api){
-        console.log("Your API is valid!");
-        console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+  // Returns: true if valid else return error object
+  isValidSwagger: function(swaggerFileUrl) {
+     return SwaggerParser.validate(swaggerFileUrl)
+      .then( (swaggerApi) => {
+        // Parsed and validated successfully
         return true;
       })
-      .catch(function(err){
-        console.log(err);
-        return false;
+      .catch( (err) => {
+        // Return error object
+        return err;
       });
   }
 });
