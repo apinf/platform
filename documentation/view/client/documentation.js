fde6dca3f9b0f49cb8a796c039b29f68aec10d8d
elnzv <illya.nizyev@gmail.com>
2016-06-01 14:43:21 +0300

    link helper; subscription

--- documentation/view/client/documentation.js
+++ documentation/view/client/documentation.js
@@ -1,5 +1,29 @@
+import { DocumentationFiles } from '/documentation/collection/collection';
+
 Template.documentation.onCreated(function(){
+  const instance = this;
   
+  // console.log(instance);
+  // Subscribe to documentation
+  Meteor.subscribe('allDocumentationFiles');
+});
+
+Template.documentation.helpers({
+  link: function() {
+    const currentDocumentationFileId = this.apiBackend.documentationFileId;
+
+    // Convert to Mongo ObjectID
+    const objectId = new Mongo.Collection.ObjectID(currentDocumentationFileId);
+
+    // Get documentation file Object
+    const currentDocumentationFile = DocumentationFiles.findOne(objectId);
+
+    // Check if documentation file is available
+    if (currentDocumentationFile) {
+      // Get documentation file URL
+      return Meteor.absoluteUrl().slice(0, -1) + DocumentationFiles.baseURL + "/md5/" + currentDocumentationFile.md5;
+    }
+  }
 });
 
 Template.documentation.events({
