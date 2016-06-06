54ae67280668c6f58fa89750edfa7dd073974dd6
elnzv <illya.nizyev@gmail.com>
2016-06-06 13:09:54 +0300

    permissions updated

--- documentation/collection/server/permissions.js
+++ documentation/collection/server/permissions.js
@@ -2,15 +2,15 @@ import { DocumentationFiles } from '/documentation/collection/collection';
 
 DocumentationFiles.allow({
   insert: function(userId, file) {
-    return true;
+    return Roles.userIsInRole(userId, ['manager']);
   },
   remove: function(userId, file) {
-    return true;
+    return Roles.userIsInRole(userId, ['manager']);
   },
   read: function(userId, file) {
     return true;
   },
   write: function(userId, file, fields) {
-    return true;
+    return Roles.userIsInRole(userId, ['manager']);
   }
 });
