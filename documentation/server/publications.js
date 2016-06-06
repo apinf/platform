69e25415f5696537f5a2da2537abb6fdc792ef66
Damir Mustafin <frenchthebread@gmail.com>
2016-06-06 16:40:20 +0300

    Change publication

--- documentation/server/publications.js
+++ documentation/server/publications.js
@@ -1,8 +1,13 @@
 import { DocumentationFiles } from '/documentation/collection/collection';
 
 Meteor.publish(
-  'allDocumentationFiles', function() {
+  'singleDocumentationFile', function(documentationFileId) {
+
+    // Convert _id value to Object Id instance
+    const objectId = new Mongo.Collection.ObjectID(documentationFileId);
+
     return DocumentationFiles.find({
+      _id: objectId,
       'metadata._Resumable': {
         $exists: false
       }
