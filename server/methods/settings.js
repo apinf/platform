f80c8f4bc247e1624af22f645f09ab1a9e47406a
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 14:37:27 +0300

    Cleanup

--- server/methods/settings.js
+++ server/methods/settings.js
@@ -6,24 +6,10 @@ Meteor.methods({
     const settings = Settings.findOne();
     Meteor.settings = settings;
   },
-  'getApiUmbrellaHost': function () {
-
-    if (apiUmbrellaSettingsValid(settings)) {
-
-      // Get API Umbrella base url from settings object
-      var apiUmbrellaBaseUrl = new URI(settings.apiUmbrella.baseUrl);
-
-      // Get host part of API Umbrella base URL
-      var apiUmbrellaHost = apiUmbrellaBaseUrl.host();
-
-      return apiUmbrellaHost;
-    }
-  },
   'settingsObjectIsValidAndEmpty': function() {
     // check structure of Meteor.settings object - initially, it should be { public: {} }
 
     // array reference to keys in Meteor.settings object
-
     const settingKeys = Object.keys(Meteor.settings);
     if (settingKeys.length === 1 && settingKeys[0] === 'public') {
       return JSON.stringify(Meteor.settings['public']) === JSON.stringify({});
