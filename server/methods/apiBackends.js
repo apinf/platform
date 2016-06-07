7c428bd09ebd868aa3b4cdf54d43a127bbdadfb2
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 13:18:26 +0300

    Remove check for settings file

--- server/methods/apiBackends.js
+++ server/methods/apiBackends.js
@@ -1,25 +1,22 @@
 Meteor.methods({
   "syncApiBackends":function () {
-    // Check if API Umbrella settings are available
-    if (Meteor.settings.apiUmbrella) {
-      // Get API Backends from API Umbrella instance
-      var response = apiUmbrellaWeb.adminApi.v1.apiBackends.getApiBackends();
-      var apiBackends = response.data.data;
+    // Get API Backends from API Umbrella instance
+    var response = apiUmbrellaWeb.adminApi.v1.apiBackends.getApiBackends();
+    var apiBackends = response.data.data;
 
-      _.each(apiBackends, function (apiBackend) {
-        // Get existing API Backend
-        var existingApiBackend = ApiBackends.findOne({'id': apiBackend.id});
+    _.each(apiBackends, function (apiBackend) {
+      // Get existing API Backend
+      var existingApiBackend = ApiBackends.findOne({'id': apiBackend.id});
 
-        // If API Backend doesn't exist in collection, insert into collection
-        if (existingApiBackend === undefined) {
-          try {
-            ApiBackends.insert(apiBackend);
-          } catch (error) {
-            console.error("Error inserting apiBackend(" + apiBackend.id + ") : " + error);
-          }
-        };
-      });
-    };
+      // If API Backend doesn't exist in collection, insert into collection
+      if (existingApiBackend === undefined) {
+        try {
+          ApiBackends.insert(apiBackend);
+        } catch (error) {
+          console.error("Error inserting apiBackend(" + apiBackend.id + ") : " + error);
+        }
+      };
+    });
   },
   createApiBackendOnApiUmbrella: function (apiBackendForm) {
     // Construct an API Backend object for API Umbrella with one 'api' key
@@ -105,5 +102,5 @@ Meteor.methods({
       apiUmbrellaWebResponse.http_status = 422;
     }
     return apiUmbrellaWebResponse;
-  }  
+  }
 });
--- server/methods/apiUmbrellaWeb.js
+++ server/methods/apiUmbrellaWeb.js
@@ -1,20 +1,21 @@
 Meteor.methods({
   "syncApiUmbrellaUsers": function () {
-    if (Meteor.settings.apiUmbrella) {
-      // Get users from API Umbrella instance
-      var response = apiUmbrellaWeb.adminApi.v1.apiUsers.getUsers();
 
-      // Add each user to collection if not already there
-      var apiUsers = response.data.data;
-      _.each(apiUsers, function (apiUser) {
-        // Get existing user
-        var existingUser = ApiUmbrellaUsers.findOne({'id': apiUser.id});
-        // If user doesn't exist in collection, insert into collection
-        if (existingUser === undefined) {
-          ApiUmbrellaUsers.insert(apiUser);
-        }
-      });
-    };
+    // Get users from API Umbrella instance
+    var response = apiUmbrellaWeb.adminApi.v1.apiUsers.getUsers();
+
+    // Add each user to collection if not already there
+    var apiUsers = response.data.data;
+
+    _.each(apiUsers, function (apiUser) {
+      // Get existing user
+      var existingUser = ApiUmbrellaUsers.findOne({'id': apiUser.id});
+
+      // If user doesn't exist in collection, insert into collection
+      if (existingUser === undefined) {
+        ApiUmbrellaUsers.insert(apiUser);
+      }
+    });
   },
   "createApiUmbrellaWeb": function () {
     // Check if something is on Settings collection
