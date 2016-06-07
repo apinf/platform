3bc5691d5e3a14d16766bc637f1715ba8dfe663c
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 13:37:00 +0300

    Replace Meteor.settings file with settings object

--- server/githubConfiguration.js
+++ server/githubConfiguration.js
@@ -1,23 +1,29 @@
+import { githubConfigurationValid } from '/lib/helperFunctions/githubConfigurationValid';
+
 // If settings are already in Meteor.settings
 try {
 
-  ServiceConfiguration.configurations.remove({
-    // removing existing configurations
-    service: 'github'
-  });
+  const settings = Settings.findOne();
 
-  ServiceConfiguration.configurations.insert({
-    /*extend settings.json with Client ID and Client Secret:
-  "githubConfiguration": {
-    "clientId" : "xxxx",
-    "secret" : "xxxx"
-  }*/
-    service: 'github',
-    clientId: Meteor.settings.githubConfiguration.clientId,
-    secret: Meteor.settings.githubConfiguration.secret
+  if (githubConfigurationValid(setting)) {
 
-  });
+    ServiceConfiguration.configurations.remove({
+      // removing existing configurations
+      service: 'github'
+    });
 
+    ServiceConfiguration.configurations.insert({
+      /*extend settings.json with Client ID and Client Secret:
+    "githubConfiguration": {
+      "clientId" : "xxxx",
+      "secret" : "xxxx"
+    }*/
+      service: 'github',
+      clientId: settings.githubConfiguration.clientId,
+      secret: settings.githubConfiguration.secret
+
+    });
+  }
 }
 //otherwise show an error
 catch (error) {
