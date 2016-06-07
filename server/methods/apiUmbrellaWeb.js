ea117516f366b491e5bc7db402901a1aaf433418
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 13:51:22 +0300

    Add check for apiUmbrella settings

--- server/methods/apiUmbrellaWeb.js
+++ server/methods/apiUmbrellaWeb.js
@@ -1,3 +1,5 @@
+import { apiUmbrellaSettigsValid } from '/lib/helperFunctions/validateSettings/apiUmbrellaSettigsValid';
+
 Meteor.methods({
   "syncApiUmbrellaUsers": function () {
 
@@ -18,26 +20,28 @@ Meteor.methods({
     });
   },
   "createApiUmbrellaWeb": function () {
+
+    const settings = Settings.findOne();
+
     // Check if something is on Settings collection
-    if ( Settings.findOne() ) {
+    if (apiUmbrellaSettigsValid(settings)) {
+
       // Create config object for API Umbrella Web interface from Settings collection
       var config = {
-        baseUrl: Settings.findOne().apiUmbrella.baseUrl,
-        apiKey: Settings.findOne().apiUmbrella.apiKey,
-        authToken: Settings.findOne().apiUmbrella.authToken
+        baseUrl: settings.apiUmbrella.baseUrl,
+        apiKey: settings.apiUmbrella.apiKey,
+        authToken: settings.apiUmbrella.authToken
       };
-    } else {
-      // Create config object for API Umbrella Web interface from Meteor.settings
-      var config = {
-        baseUrl: Meteor.settings.apiUmbrella.baseUrl,
-        apiKey: Meteor.settings.apiUmbrella.apiKey,
-        authToken: Meteor.settings.apiUmbrella.authToken
-      };
+
     }
+
     try {
+
       // Create API Umbrella Web object for REST calls
       apiUmbrellaWeb = new ApiUmbrellaWeb(config);
+
     } catch (error) {
+
       console.log(error);
     }
   }
