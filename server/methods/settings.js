1aa4331976ba7ba5e7c4d97ca62fc22a71cb825a
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 14:17:25 +0300

    Add apiUmbrella configs check

--- server/methods/settings.js
+++ server/methods/settings.js
@@ -1,3 +1,5 @@
+import { apiUmbrellaSettingsValid } from '/lib/helperFunctions/validateSettings';
+
 Meteor.methods({
   'updateMeteorSettings': function() {
     // Updating Meteor.settings from Settings collection
@@ -5,13 +7,17 @@ Meteor.methods({
     Meteor.settings = settings;
   },
   'getApiUmbrellaHost': function () {
-    // Get API Umbrella base url from settings object
-    var apiUmbrellaBaseUrl = new URI(Meteor.settings.apiUmbrella.baseUrl);
 
-    // Get host part of API Umbrella base URL
-    var apiUmbrellaHost = apiUmbrellaBaseUrl.host();
+    if (apiUmbrellaSettingsValid(settings)) {
 
-    return apiUmbrellaHost;
+      // Get API Umbrella base url from settings object
+      var apiUmbrellaBaseUrl = new URI(settings.apiUmbrella.baseUrl);
+
+      // Get host part of API Umbrella base URL
+      var apiUmbrellaHost = apiUmbrellaBaseUrl.host();
+
+      return apiUmbrellaHost;
+    }
   },
   'settingsObjectIsValidAndEmpty': function() {
     // check structure of Meteor.settings object - initially, it should be { public: {} }
@@ -21,7 +27,7 @@ Meteor.methods({
     const settingKeys = Object.keys(Meteor.settings);
     if (settingKeys.length === 1 && settingKeys[0] === 'public') {
       return JSON.stringify(Meteor.settings['public']) === JSON.stringify({});
-    } 
+    }
 
     return false;
   }
