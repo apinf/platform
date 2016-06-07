dc5d60e6fd546231a05834bcadf3e487c1b4c5b4
Damir Mustafin <frenchthebread@gmail.com>
2016-06-07 14:37:37 +0300

    Remove unneeded check

--- server/startup.js
+++ server/startup.js
@@ -6,39 +6,34 @@ Meteor.startup(function () {
 
   try {
 
-    const settings = Settings.findOne();
+    // Creating ApiUmbrellaWeb object
+    Meteor.call("createApiUmbrellaWeb");
 
-    if (apiUmbrellaSettingsValid(settings)) {
+    // Check if API Umbrella settings are available
+    SyncedCron.add({
+      name: 'Sync API Umbrella Users and API Backends',
+      schedule: function(parser) {
+        // parser is a later.parse object
+        return parser.text('every 5 minutes');
+      },
+      job: function() {
+        Meteor.call("syncApiUmbrellaUsers");
+        Meteor.call("syncApiUmbrellaAdmins");
+        Meteor.call("syncApiBackends");
+      }
+    });
 
-      // Creating ApiUmbrellaWeb object
-      Meteor.call("createApiUmbrellaWeb");
+    Meteor.call("syncApiUmbrellaUsers");
+    Meteor.call("syncApiBackends");
 
-      // Check if API Umbrella settings are available
-      SyncedCron.add({
-        name: 'Sync API Umbrella Users and API Backends',
-        schedule: function(parser) {
-          // parser is a later.parse object
-          return parser.text('every 5 minutes');
-        },
-        job: function() {
-          Meteor.call("syncApiUmbrellaUsers");
-          Meteor.call("syncApiUmbrellaAdmins");
-          Meteor.call("syncApiBackends");
-        }
-      });
+    // Create indexes for fields in MongoDB collection (API backends search functionality)
+    ApiBackends._ensureIndex({
+      "name": 1,
+      "backend_host": 1
+    });
 
-      Meteor.call("syncApiUmbrellaUsers");
-      Meteor.call("syncApiBackends");
-
-      // Create indexes for fields in MongoDB collection (API backends search functionality)
-      ApiBackends._ensureIndex({
-        "name": 1,
-        "backend_host": 1
-      });
-
-      // Initialize cron jobs
-      SyncedCron.start();
-    }
+    // Initialize cron jobs
+    SyncedCron.start();
   }
   // otherwise show an error
   catch (error) {
