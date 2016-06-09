5e0a8e8b62121b92cb267324ace7f0c98b60361f
Damir Mustafin <frenchthebread@gmail.com>
2016-06-09 13:30:31 +0300

    Comments

--- apis/server/methods/status.js
+++ apis/server/methods/status.js
@@ -1,26 +1,29 @@
 Meteor.methods({
-  'getApiStatus' : function (apiUrl) {
+  'getApiStatus' : function (uri) {
 
     this.unblock();
 
+    // Init empty status object
     let status = {
       code: 0,
       errorMessage: "",
       responseContext: {}
     };
 
+    // Try-catch wrapper
+    // Because if requested host is not available, error is thrown
     try {
 
       // response object from GET request to api host
-      const result = Meteor.http.call("GET", apiUrl);
+      const result = Meteor.http.call("GET", uri);
 
-      // Check that we get result and get statusCode
+      // Check result is received
       if(result) {
 
-        // Get statusCode
+        // Get reposnce status code
         status.code = result.statusCode;
 
-        // Keep the entire response object
+        // Keep response object
         status.responseContext = result;
       }
 
@@ -29,9 +32,11 @@ Meteor.methods({
     } catch (error) {
 
       // Got a network error, time-out or HTTP error in the 400 or 500 range.
-      // keeps error message
 
+      // Keep response error code
       status.code = error.response.statusCode;
+
+      // Keep reponse error message
       status.errorMessage = error;
 
       return status;
