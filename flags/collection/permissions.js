6884f978308bf1545c1596a995c9de835528ffc2
Damir Mustafin <frenchthebread@gmail.com>
2016-06-09 17:09:39 +0300

    Add update permission

--- flags/collection/permissions.js
+++ flags/collection/permissions.js
@@ -2,6 +2,9 @@ ApiFlags.allow({
   insert: function (userId) {
     return Roles.userIsInRole(userId, ['admin']);
   },
+  update: function (userId) {
+    return Roles.userIsInRole(userId, ['admin']);
+  },
   remove: function (userId) {
     return Roles.userIsInRole(userId, ['admin']);
   }
