586e4c5a6ef21509223aee490961d91b6048a641
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-06-09 16:27:21 +0300

    Subscribe and get all apiBackends

--- catalogue/client/catalogue.js
+++ catalogue/client/catalogue.js
@@ -1,20 +1,10 @@
+Template.catalogue.onCreated(function (){
+  this.subscribe('allApiBackends');
+});
+
 Template.catalogue.helpers({
-  userHasBookmarks: function () {
-    var apiIds;
-
-    // Get current user bookmarks object
-    var userBookmarks = ApiBookmarks.findOne();
-
-    if (userBookmarks) {
-      // get array of API IDs
-      apiIds = userBookmarks.apiIds;
-    }
-
-    // Check if user has bookmarked apis
-    if(apiIds && apiIds.length > 0) {
-      return true;
-    } else {
-      return false;
-    }
+  apiBackends: function () {
+    // Return cursor to all apiBackends
+    return ApiBackends.find();
   }
 });
