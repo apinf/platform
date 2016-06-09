740c7455f7d8b793e5ee15de7a080f90749e4b16
Damir Mustafin <frenchthebread@gmail.com>
2016-06-09 14:59:03 +0300

    Pass apiBackendId to a template

--- flags/client/flagApiButton/flagApiButton.js
+++ flags/client/flagApiButton/flagApiButton.js
@@ -1,3 +1,11 @@
+Template.flagApiButton.onCreated(function () {
+
+  const instance = this;
+
+  const apiBackendId = instance.data.apiBackendId;
+
+})
+
 Template.flagApiButton.events({
   'click #openFlagApiModal': function () {
 
