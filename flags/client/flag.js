7b0ccc34b77def4928a47c501262d71304a7917a
Damir Mustafin <frenchthebread@gmail.com>
2016-06-09 17:00:54 +0300

    Remove functionality

--- flags/client/flag.js
+++ flags/client/flag.js
@@ -1,18 +1,43 @@
+Template.flagApiModal.onCreated(function () {
+
+  const instance = this;
+
+  instance.apiFlag = instance.data.apiFlag;
+
+});
+
 Template.flagApiModal.helpers({
   formType () {
 
     const instance = Template.instance();
-    const apiFlag = instance.data.apiFlag;
 
-    return (apiFlag) ? 'update' : 'insert';
+    return (instance.apiFlag) ? 'update' : 'insert';
   },
   apiFlag () {
 
     const instance = Template.instance();
 
-    const apiFlag = instance.data.apiFlag;
+    return instance.apiFlag;
+  },
+  apiIsFlagged () {
+    const instance = Template.instance();
 
-    return apiFlag;
+    return (instance.apiFlag) ? true : false;
+  }
+});
 
+Template.flagApiModal.events({
+  'click #remove-apiFlag': function () {
+
+    const instance = Template.instance();
+
+    const removeApiFlag = ApiFlags.remove({ _id: instance.apiFlag._id });
+
+    if (removeApiFlag > 0) {
+
+      sAlert.success('Flag has been successfully removed from API');
+
+      Modal.hide('flagApiModal');
+    }
   }
 })
