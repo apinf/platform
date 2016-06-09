b83c501cc51e20c2f6f5f1b809bbbeb85d992036
Damir Mustafin <frenchthebread@gmail.com>
2016-06-09 15:35:47 +0300

    Pass apiFlag to modal

--- flags/client/flagApiButton/flagApiButton.js
+++ flags/client/flagApiButton/flagApiButton.js
@@ -2,14 +2,38 @@ Template.flagApiButton.onCreated(function () {
 
   const instance = this;
 
-  const apiBackendId = instance.data.apiBackendId;
+  const apiBackendId = instance.data.apiBackend._id;
+
+  console.log(apiBackendId)
+
+  instance.apiFlag = new ReactiveVar();
+
+  instance.subscribe('singleApiFlag', apiBackendId);
+
+  instance.autorun(() => {
+    if (instance.subscriptionsReady()) {
+      instance.apiFlag.set(ApiFlags.findOne({ apiBackendId }));
+    }
+  });
 
 })
 
 Template.flagApiButton.events({
   'click #openFlagApiModal': function () {
 
+    const instance = Template.instance();
+
     // Show modal
-    Modal.show('flagApiModal');
+    Modal.show('flagApiModal', { apiFlag: instance.apiFlag.get() });
+  }
+})
+
+Template.flagApiButton.helpers({
+  apiFlag () {
+    const instance = Template.instance();
+
+    const apiFlag = instance.apiFlag.get();
+
+    return (apiFlag) ? true : false;
   }
 })
