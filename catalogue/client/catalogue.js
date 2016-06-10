59cd941b1663fe5092ea48dc9e467f83755864e9
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-06-10 14:54:21 +0300

    Simplify catalogue container

--- catalogue/client/catalogue.js
+++ catalogue/client/catalogue.js
@@ -1,20 +1,5 @@
-Template.catalogue.helpers({
-  userHasBookmarks: function () {
-    var apiIds;
+import { Template } from 'meteor/templating';
 
-    // Get current user bookmarks object
-    var userBookmarks = ApiBookmarks.findOne();
-
-    if (userBookmarks) {
-      // get array of API IDs
-      apiIds = userBookmarks.apiIds;
-    }
-
-    // Check if user has bookmarked apis
-    if(apiIds && apiIds.length > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+Template.catalogue.onCreated(function () {
+  console.log("Catalogue container created.");
 });
