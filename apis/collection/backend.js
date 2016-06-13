9b059521f7006aad2e40f8a3024c525e60381095
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-06-13 13:51:13 +0300

    Move permisisons to own file

--- apis/collection/backend.js
+++ apis/collection/backend.js
@@ -658,44 +658,6 @@ Schemas.ApiBackendsSchema = new SimpleSchema({
 
 ApiBackends.attachSchema(Schemas.ApiBackendsSchema);
 
-ApiBackends.allow({
-  insert: function () {
-    return true;
-  },
-  update: function (userId, apiBackendDoc) {
-    // Save ID of API Backend
-    const apiBackendId = apiBackendDoc._id;
-    // Get API backend with ID
-    const apiBackend = ApiBackends.findOne(apiBackendId);
-    // Check if current user can edit API Backend
-    let currentUserCanEdit = apiBackend.currentUserCanEdit();
-
-    if (currentUserCanEdit) {
-      // User is allowed to perform action
-      return true;
-    } else {
-      // User is not allowded to perform action
-      return false;
-    }
-  },
-  remove: function (userId, apiBackendDoc) {
-    // Save ID of API Backend
-    const apiBackendId = apiBackendDoc._id;
-    // Get API backend with ID
-    const apiBackend = ApiBackends.findOne(apiBackendId);
-    // Check if current user can edit API Backend
-    let currentUserCanEdit = apiBackend.currentUserCanEdit();
-
-    if (currentUserCanEdit) {
-      // User is allowed to perform action
-      return true;
-    } else {
-      // User is not allowded to perform action
-      return false;
-    }
-  }
-});
-
 SimpleSchema.messages({
   // RegEx messages
   regEx: [
