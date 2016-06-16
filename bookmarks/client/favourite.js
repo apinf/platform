12aecbb3cae71c03095af2343a5c4f6c1962e358
elnzv <illya.nizyev@gmail.com>
2016-06-16 12:08:23 +0300

    merged changes

--- bookmarks/client/favourite.js
+++ bookmarks/client/favourite.js
@@ -9,11 +9,11 @@ Template.favourite.created = function () {
 Template.favourite.events({
   'click .bookmark': function () {
 
-    //Store api id being clicked
-    var backendId = this.apiBackend._id;
+    // Get api backend Id from the context
+    var backendId = (this.apiBackend) ? this.apiBackend._id : this._id;
 
     //Store the user ID of the current user clicking the button
-    var currentUserId = Meteor.userId();
+    var currentUserId = Meteor.user()._id;
 
     // Toggle (add/remove) the bookmark with method toogleBookmarkApi
     Meteor.call("toggleBookmarkApi", backendId, currentUserId);
@@ -32,16 +32,11 @@ Template.favourite.helpers({
     // Make sure bookmark subscription is ready
     if (instance.bookmarksSubscription.ready()) {
 
-      // Get current user bookmark
-      var userBookmarks = ApiBookmarks.findOne({ userId: Meteor.userId() });
+      // Get current user bookmark (should be only one API Bookmarks result available)
+      var userBookmarks = ApiBookmarks.findOne({ userId: Meteor.user()._id, apiIds: apiBackendId });
 
       // Make sure user has bookmarks
       if (userBookmarks) {
-        // get array of API IDs
-        var apiIds = userBookmarks.apiIds;
-
-        //Store api id being clicked
-        var backendId = this.apiBackend._id;
 
         return true;
       }
