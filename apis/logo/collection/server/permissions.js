ec9296241fd78e4a067ba2861c01aa67b6c6017b
elnzv <illya.nizyev@gmail.com>
2016-06-17 11:08:38 +0300

    edit permissions

--- apis/logo/collection/server/permissions.js
+++ apis/logo/collection/server/permissions.js
@@ -2,10 +2,10 @@ import { ApiLogo } from '/apis/logo/collection/collection';
 
 ApiLogo.allow({
   insert: function(userId, file) {
-    return true;
+    return Roles.userIsInRole(userId, ['admin','manager']);
   },
   remove: function(userId, file) {
-    return true;
+    return Roles.userIsInRole(userId, ['admin','manager']);
   },
   read: function(userId, file) {
     return true;
