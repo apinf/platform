fab9f806631c03398569f79a5ed465344783e374
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-06-20 13:10:14 +0300

    Add subscription, editor settings check

--- documentation/design/client/documentation-editor.js
+++ documentation/design/client/documentation-editor.js
@@ -1,9 +1,22 @@
+Template.apiDocumentationEditor.onCreated(function () {
+  const instance = this;
+  // Subscribe to documentation editor settings
+  instance.subscribe('singleSetting', 'apiDocumentationEditor');
+});
+
 Template.apiDocumentationEditor.helpers({
   // Creates a variable from the
   editorUrl: function () {
-    // Get the URL of the API Documentation Editor from public settings
-    var apiDocumentationEditorHost = Meteor.settings.public.apiDocumentationEditor.host;
+    // Get settings
+    const settings = Settings.findOne();
 
-    return apiDocumentationEditorHost;
+    // Check settings exists, editor is enabled and host setting exists
+    if(settings && settings.apiDocumentationEditor.enabled && settings.apiDocumentationEditor.host) {
+      // Return the URL of the API Documentation Editor from Settings collection
+      return settings.apiDocumentationEditor.host;
+    } else {
+      // Otherwise return false
+      return false;
+    }
   }
 });
