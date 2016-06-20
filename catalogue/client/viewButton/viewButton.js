88b11b01bdcc3b1411f2bc2ecd52da00e03dbeed
elnzv <illya.nizyev@gmail.com>
2016-06-20 14:44:22 +0300

    cleaned up; removed unused files

--- catalogue/client/viewButton/viewButton.js
+++
@@ -1,9 +0,0 @@
-Template.viewApiBackendButton.events({
-  "click button": function (/*event, template*/) {
-    // Get reference to API Backend object
-    var apiBackend = this;
-
-    // Redirect to the API Backend page
-    Router.go('viewApiBackend', {_id: apiBackend._id});
-  }
-});
