2b3dc2fd381ef8b708bac18153a8f095a665e2de
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-06-23 13:38:22 +0300

    Fix & improve API removal process

--- apis/server/methods/delete.js
+++ apis/server/methods/delete.js
@@ -1,12 +1,28 @@
-Meteor.methods({
-  // remove document and its references in ApiBackends, ApiBacklogItems, 
-  // Feedback, ApiMetadata and ApiDocs collections
-  "removeApiBackend": function(apiBackendId) {
-    ApiBackends.remove(apiBackendId);
-    ApiBacklogItems.remove({"apiBackendId": apiBackendId});
-    Feedback.remove({"apiBackendId": apiBackendId});
-    ApiMetadata.remove({"apiBackendId": apiBackendId});
-    ApiDocs.remove(apiBackendId);
-  }
+import { DocumentationFiles } from '/documentation/collection/collection';
 
+Meteor.methods({
+  "removeApiBackend": function(apiBackendId) {
+    // Remove API doc
+    removeApiDoc(apiBackendId);
+    // Remove backlog items
+    ApiBacklogItems.remove({"apiBackendId": apiBackendId});
+    // Remove feedbacks
+    Feedback.remove({"apiBackendId": apiBackendId});
+    // Remove metadata
+    ApiMetadata.remove({"apiBackendId": apiBackendId});
+    // Finally remove the API
+    ApiBackends.remove(apiBackendId);
+  }
 });
+
+// Helper to remove API documentation file
+function removeApiDoc(apiBackendId) {
+  // Get API object
+  const api = ApiBackends.findOne(apiBackendId);
+  // Get documentationFileId
+  const documentationFileId = api.documentationFileId;
+  // Convert to Mongo ObjectID
+  const objectId = new Mongo.Collection.ObjectID(documentationFileId);
+  // Remove documentation object
+  DocumentationFiles.remove(objectId);
+}
