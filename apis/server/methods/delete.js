22d87d119e64aaf81209d9f840f5f0ef2034dfc9
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-06-30 14:35:10 +0300

    Upgrade local function to Meteor method

--- apis/server/methods/delete.js
+++ apis/server/methods/delete.js
@@ -1,9 +1,10 @@
 import { DocumentationFiles } from '/documentation/collection/collection';
 
 Meteor.methods({
-  "removeApiBackend": function(apiBackendId) {
+  // Remove API backend and related items
+  removeApiBackend (apiBackendId) {
     // Remove API doc
-    removeApiDoc(apiBackendId);
+    Meteor.call('removeApiDoc', apiBackendId);
     // Remove backlog items
     ApiBacklogItems.remove({"apiBackendId": apiBackendId});
     // Remove feedbacks
@@ -12,17 +13,16 @@ Meteor.methods({
     ApiMetadata.remove({"apiBackendId": apiBackendId});
     // Finally remove the API
     ApiBackends.remove(apiBackendId);
+  },
+  // Remove API documentation file
+  removeApiDoc (apiBackendId) {
+    // Get API object
+    const api = ApiBackends.findOne(apiBackendId);
+    // Get documentationFileId
+    const documentationFileId = api.documentationFileId;
+    // Convert to Mongo ObjectID
+    const objectId = new Mongo.Collection.ObjectID(documentationFileId);
+    // Remove documentation object
+    DocumentationFiles.remove(objectId);
   }
 });
-
-// Helper to remove API documentation file
-function removeApiDoc(apiBackendId) {
-  // Get API object
-  const api = ApiBackends.findOne(apiBackendId);
-  // Get documentationFileId
-  const documentationFileId = api.documentationFileId;
-  // Convert to Mongo ObjectID
-  const objectId = new Mongo.Collection.ObjectID(documentationFileId);
-  // Remove documentation object
-  DocumentationFiles.remove(objectId);
-}
