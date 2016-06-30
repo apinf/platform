1d35086bc4824fece270b66845785c4945e7b01e
Damir Mustafin <frenchthebread@gmail.com>
2016-06-30 15:57:20 +0300

    Cleanup

--- client/views/dashboard/charts/charts.js
+++ client/views/dashboard/charts/charts.js
@@ -1,8 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
 import _ from 'lodash';
 import moment from 'moment';
 import dc from 'dc';
