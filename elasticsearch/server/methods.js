51d4182054006daef40bdeea759248106f63e461
Damir Mustafin <frenchthebread@gmail.com>
2016-06-30 16:03:26 +0300

    Add comments

--- elasticsearch/server/methods.js
+++ elasticsearch/server/methods.js
@@ -5,15 +5,16 @@ import _ from 'lodash';
 Meteor.methods({
   getElasticSearchData (opts) {
 
+    // Check if user is authorised
     if (Meteor.user()) {
 
-      const settings = Settings.findOne();
+      const settings = Settings.findOne(); // Get settings object
 
-      let host;
+      let host; // init variable for host
 
       try {
 
-        host = settings.elasticsearch.host;
+        host = settings.elasticsearch.host; // Try-catch if host value exists
 
       } catch (e) {
 
@@ -22,15 +23,23 @@ Meteor.methods({
         return false;
       }
 
-      const esClient = new ElasticSearch.Client({ host });
+      const esClient = new ElasticSearch.Client({ host }); // Init ES client
 
+      // Get elasticsearch data and return
       return esClient.search(opts).then((res) => {
+
         return res;
       }, (err) => {
+
         throw new Meteor.error(500, 'Analytics data is not found.');
+
+        return false;
       });
     } else {
+      
       throw new Meteor.error(500, 'User is not authorised.');
+
+      return false;
     }
   }
 });
