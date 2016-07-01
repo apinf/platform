3311112a13d2f16f5e77aae3d11ef62b35620850
Damir Mustafin <frenchthebread@gmail.com>
2016-07-01 12:15:37 +0300

    Add comments

--- client/views/dashboard/charts/table/table.js
+++ client/views/dashboard/charts/table/table.js
@@ -1,73 +1,87 @@
 Template.dataTable.onCreated(function () {
 
-  const instance = this;
+  const instance = this; // Get reference to template instance
 
-  instance.rowCount = new ReactiveVar(10);
-  instance.pageNumber = new ReactiveVar(1);
+  instance.rowCount = new ReactiveVar(10); // Add initial row count to a table
+  instance.pageNumber = new ReactiveVar(1); // Add inital page number
 
 });
 
 Template.dataTable.events({
   'click #prev': function (event, instance) {
 
-    let oldPageNumber = instance.pageNumber.get();
+    let currentPageNumber = instance.pageNumber.get(); // Get current page number
 
-    if (oldPageNumber > 1) {
-      instance.pageNumber.set(oldPageNumber - 1);
+    // Check if page number the first page in the table
+    if (currentPageNumber > 1) {
+
+      instance.pageNumber.set(currentPageNumber - 1); // Decrement page number value
     }
   },
   'click #next': function (event, instance) {
 
-    let oldPageNumber = instance.pageNumber.get()
+    let currentPageNumber = instance.pageNumber.get(); // Get current page number
 
-    instance.pageNumber.set(oldPageNumber + 1);
+    const rowCount = instance.rowCount.get(); // Get table row count
+    const dataSetLength = Template.currentData().tableDataSet.length; // Get table dataset length
+
+    // Check if current page is the last one in the table
+    if ((dataSetLength / rowCount) < currentPageNumber) {
+
+      instance.pageNumber.set(currentPageNumber + 1); // Increment page number
+    }
   }
 })
 
 Template.dataTable.helpers({
   tableDataSet () {
 
-    const instance = Template.instance();
+    const instance = Template.instance(); // Get reference to template instance
 
-    const rowCount = instance.rowCount.get();
-    const pageNumber = instance.pageNumber.get();
+    const rowCount = instance.rowCount.get(); // Get table row count
+    const pageNumber = instance.pageNumber.get(); // Get current page number
 
+    // Get start and end value positions in array for current page
     const arrStart = rowCount * pageNumber;
     const arrEnd = arrStart + rowCount;
 
+    // Slice array for current page
     return Template.currentData().tableDataSet.slice(arrStart, arrEnd);
   },
   showPrevButton () {
 
-    const instance = Template.instance();
+    const instance = Template.instance(); // Get reference to template instance
 
-    const pageNumber = instance.pageNumber.get();
+    const pageNumber = instance.pageNumber.get(); // Ger current page number
 
-    return pageNumber > 1;
+    return pageNumber > 1; // Check if current page is the first one in table
   },
   showNextButton () {
 
-    const instance = Template.instance();
+    const instance = Template.instance(); // Get reference to template instance
 
-    const rowCount = instance.rowCount.get();
-    const pageNumber = instance.pageNumber.get();
+    const rowCount = instance.rowCount.get(); // Get table row count
+    const pageNumber = instance.pageNumber.get(); // Get current page number
 
+    // Get table dataset length
     const dataSetLength = Template.currentData().tableDataSet.length;
 
+    // Check if current page is the last one in the table
     return pageNumber < (dataSetLength / rowCount - 1);
   },
   currentPageNumber () {
 
-    const instance = Template.instance();
+    const instance = Template.instance(); // Get reference to a template instance
 
-    return instance.pageNumber.get();
+    return instance.pageNumber.get(); // Get current page number
   },
   totalPageNumber () {
 
-    const instance = Template.instance();
+    const instance = Template.instance(); // Get reference to template instance
 
-    const rowCount = instance.rowCount.get();
+    const rowCount = instance.rowCount.get(); // Get row count
 
+    // Get table dataset length
     const dataSetLength = Template.currentData().tableDataSet.length;
 
     return (dataSetLength / rowCount) | 0; // Calculate total page number and make it integer
