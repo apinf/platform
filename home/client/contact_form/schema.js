ca2015419c8eb4edb29052c597fa9cc2eded5d08
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-04 15:13:53 +0300

    Enable localization

--- home/client/contact_form/schema.js
+++ home/client/contact_form/schema.js
@@ -1,30 +1,23 @@
 Schemas.contact = new SimpleSchema({
   name: {
     type: String,
-    label: "Your name",
     max: 50,
-    optional: false,
-    autoform: {
-      placeholder: 'Your name'
-    }
+    optional: false
   },
   email: {
     type: String,
     regEx: SimpleSchema.RegEx.Email,
-    label: "E-mail address",
-    optional: false,
-    autoform: {
-      placeholder: 'Your email'
-    }
+    optional: false
   },
   message: {
     type: String,
-    label: "Message",
     max: 1000,
     optional: false,
     autoform: {
-      rows: 5,
-      placeholder: 'Your message'
+      rows: 5
     }
   }
 });
+
+// Enable translations (i18n)
+ApiMetadata.schema.i18n("schemas.contact");
