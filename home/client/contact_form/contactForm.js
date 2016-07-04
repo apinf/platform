ce5b0854696b84232c463a886152d30a617006ee
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-04 15:05:25 +0300

    Move FlashMessages configuration

--- home/client/contact_form/contactForm.js
+++ home/client/contact_form/contactForm.js
@@ -3,10 +3,3 @@ Template.contactForm.helpers({
     return Schemas.contact;
   }
 });
-
-FlashMessages.configure({
-  // Configuration for FlashMessages.
-  autoHide: true,
-  hideDelay: 5000,
-  autoScroll: false
-});
