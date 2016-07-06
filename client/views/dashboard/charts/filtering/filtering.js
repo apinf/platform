4eea564be4ddc9c015c3d31a156b04329f635f74
Damir Mustafin <frenchthebread@gmail.com>
2016-07-06 12:38:12 +0300

    Import Template & ReactiveVar

--- client/views/dashboard/charts/filtering/filtering.js
+++ client/views/dashboard/charts/filtering/filtering.js
@@ -1,3 +1,5 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import { ApiBackends } from '/apis/collection/backend';
 
 Template.dashboardChartsFiltering.onCreated(function () {
