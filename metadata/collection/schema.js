7186fe9e1452b82f1715defb6fe3e52a90ddb1d4
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 12:51:17 +0300

    Move schema

---
+++ metadata/collection/schema.js
@@ -0,0 +1,65 @@
+import { ApiMetadata } from './collection';
+
+ApiMetadata.schema = new SimpleSchema({
+  "apiBackendId": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id
+  },
+  "organization": {
+    type: Object,
+    optional: true
+  },
+  "organization.name": {
+    type: String,
+    optional: true
+  },
+  "organization.description": {
+    type: String,
+    optional: true
+  },
+  "contact": {
+    type: Object,
+    optional: true
+  },
+  "contact.name": {
+    type: String,
+    optional: true
+  },
+  "contact.phone": {
+    type: String,
+    optional: true
+  },
+  "contact.email": {
+    type: String,
+    optional: true
+  },
+  "service": {
+    type: Object,
+    optional: true
+  },
+  "service.name": {
+    type: String,
+    optional: true
+  },
+  "service.description": {
+    type: String,
+    optional: true
+  },
+  "service.validSince": {
+    type: Date,
+    optional: true
+  },
+  "service.validUntil": {
+    type: Date,
+    optional: true
+  },
+  "service.serviceLevelAgreement": {
+    type: String,
+    optional: true
+  }
+});
+
+// Enable translations (i18n)
+ApiMetadata.schema.i18n("schemas.apiMetadata");
+
+ApiMetadata.attachSchema(ApiMetadata.schema);
