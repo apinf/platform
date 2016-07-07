85ba8e1986265730f8b51384e07b6e91aa1f62ec
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 13:29:09 +0300

    Rename publication

--- metadata/collection/server/apiBackendMetadata.js
+++
@@ -1,6 +0,0 @@
-Meteor.publish("apiMetadata", function (apiBackendId) {
-  // Get metadata document for API Backend
-  let apiBackendMetadata = ApiMetadata.find({apiBackendId});
-
-  return apiBackendMetadata;
-});
