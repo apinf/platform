2bae847ac609595199e140a9dd0f9eec2fb6446a
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 13:12:22 +0300

    Add metadata helper; alphabetize helpers

--- metadata/client/edit/edit.js
+++ metadata/client/edit/edit.js
@@ -1,12 +1,21 @@
 import { ApiMetadata } from '/metadata/collection/collection';
 
 Template.editApiMetadata.helpers({
-  submitButtonText () {
-    // Get translation string for submit button text
-    return TAPi18n.__("editApiMetadata_submitButtonText");
-  },
   apiMetadataCollection () {
     // Return a reference to the API Metadata collection
     return ApiMetadata;
+  }
+  metadata () {
+    // Get the API Backend ID from the route
+    let apiBackendId = Router.current().params._id;
+
+    // Get API Backend metadata
+    let apiMetadata = ApiMetadata.findOne({apiBackendId});
+
+    return apiMetadata;
+  },
+  submitButtonText () {
+    // Get translation string for submit button text
+    return TAPi18n.__("editApiMetadata_submitButtonText");
   }
 });
