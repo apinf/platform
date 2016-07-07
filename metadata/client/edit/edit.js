2360299b36005a1426acee89b167e0c095fe1da8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 13:10:24 +0300

    Add apiMetadataCollection helper

--- metadata/client/edit/edit.js
+++ metadata/client/edit/edit.js
@@ -1,6 +1,12 @@
+import { ApiMetadata } from '/metadata/collection/collection';
+
 Template.editApiMetadata.helpers({
   submitButtonText () {
     // Get translation string for submit button text
     return TAPi18n.__("editApiMetadata_submitButtonText");
+  },
+  apiMetadataCollection () {
+    // Return a reference to the API Metadata collection
+    return ApiMetadata;
   }
 });
