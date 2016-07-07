692e32a133df106c8f7f4421c6c2e962e3a9573d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 15:00:14 +0300

    Use apiBackend from instance data

--- metadata/client/view/metadata.js
+++ metadata/client/view/metadata.js
@@ -5,7 +5,7 @@ Template.viewApiBackendMetadata.onCreated(function () {
   const instance = this;
 
   // Subscribe to metadata for this API Backend
-  instance.subscribe("apiMetadata", instance.apiBackendId);
+  instance.subscribe("apiMetadata", instance.data.apiBackend._id);
 });
 
 Template.viewApiBackendMetadata.helpers({
@@ -15,15 +15,21 @@ Template.viewApiBackendMetadata.helpers({
      Make sure user can edit API Backend before allowing Metadata permissions
     */
 
+    // Get reference to template instance
+    const instance = Template.instance();
+
     // Get current API backend ID
-    var apiBackend = this.apiBackend;
+    var apiBackend = instance.data.apiBackend;
 
     // Check if current user can edit API Backend
     return apiBackend.currentUserCanEdit();
   },
   metadata () {
-    // Get the API Backend ID from the route
-    let apiBackendId = Router.current().params._id;
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Get the API Backend ID from template instance
+    let apiBackendId = instance.data.apiBackend._id;
 
     // Get API Backend metadata
     let apiMetadata = ApiMetadata.findOne({apiBackendId});
