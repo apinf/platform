a7c192dc3af4bcc001680df685048cdd2b324191
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 14:59:40 +0300

    Remove metadata helper

--- metadata/client/edit/edit.js
+++ metadata/client/edit/edit.js
@@ -5,15 +5,6 @@ Template.editApiMetadata.helpers({
     // Return a reference to the API Metadata collection
     return ApiMetadata;
   },
-  metadata () {
-    // Get the API Backend ID from the route
-    let apiBackendId = Router.current().params._id;
-
-    // Get API Backend metadata
-    let apiMetadata = ApiMetadata.findOne({apiBackendId});
-
-    return apiMetadata;
-  },
   submitButtonText () {
     // Get translation string for submit button text
     return TAPi18n.__("editApiMetadata_submitButtonText");
