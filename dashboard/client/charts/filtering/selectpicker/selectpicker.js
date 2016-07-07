a9324acedd1fb3e677132562ebfa3e24ff0e71aa
Damir Mustafin <frenchthebread@gmail.com>
2016-07-07 16:07:38 +0300

    Comment

--- dashboard/client/charts/filtering/selectpicker/selectpicker.js
+++ dashboard/client/charts/filtering/selectpicker/selectpicker.js
@@ -1,6 +1,8 @@
 Template.apiBackendSelectPicker.onRendered(function () {
 
   const selectPickerElement = $('#api-frontend-prefix');
+
+  // Initialize select picker widget
   selectPickerElement.selectpicker({});
 
 });
