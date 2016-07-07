567158290eb81856fb1618f75ead366f9f5bbc47
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-07-07 13:29:39 +0300

    Import ApiMetadata

--- metadata/collection/server/publication.js
+++ metadata/collection/server/publication.js
@@ -1,3 +1,5 @@
+import { ApiMetadata } from '/metadata/collection/collection';
+
 Meteor.publish("apiMetadata", function (apiBackendId) {
   // Get metadata document for API Backend
   let apiBackendMetadata = ApiMetadata.find({apiBackendId});
