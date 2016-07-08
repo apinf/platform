1c033c50343d2de574186451b89b7fc98eb2b907
elnzv <illya.nizyev@gmail.com>
2016-07-08 16:05:30 +0300

    comments

--- dashboard/client/charts/filtering/selectpicker/selectpicker.js
+++ dashboard/client/charts/filtering/selectpicker/selectpicker.js
@@ -10,24 +10,31 @@ Template.apiBackendSelectPicker.onRendered(function () {
 Template.apiBackendSelectPicker.helpers({
   myApis () {
 
+    // Get reference to template instance
     const instance = Template.instance();
 
+    // Get apis from template context
     const apis = instance.data.apis;
 
+    // Get apis that user manages
     const myApis = _.filter(apis, (api) => {
       return api.currentUserIsManager();
     });
 
+    // Check if there are any apis that user manages
     if (myApis.length > 0) {
       return myApis;
     }
   },
   otherApis () {
 
+    // Get reference to template instance
     const instance = Template.instance();
 
+    // Get apis from template context
     const apis = instance.data.apis;
 
+    // Get apis that user doesn't manage
     return _.filter(apis, (api) => {
       return !api.currentUserIsManager();
     });
