2eaa905a33a5483162591076db51836529742bbf
elnzv <illya.nizyev@gmail.com>
2016-07-11 14:56:15 +0300

    cleaned up; added initial helpers

--- dashboard/client/statistics/statistics.js
+++ dashboard/client/statistics/statistics.js
@@ -1,12 +1,14 @@
-Template.statisticsSummary.onRendered(function() {
-  $('.statistics-summary-number').each(function () {
-    var $this = $(this);
-    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-      duration: 1000,
-      easing: 'swing',
-      step: function () {
-        $this.text(Math.ceil(this.Counter));
-      }
-    });
-  });
+Template.statisticsSummary.helpers({
+  requests () {
+
+  },
+  averageResponseTime () {
+
+  },
+  averageResponseRate () {
+
+  },
+  uniqueUsers () {
+
+  }
 });
