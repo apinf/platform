1ad6e452aafa2125f2ffb2f43dd6ea15c0aec4ae
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-12 16:10:54 +0300

    Change output to true/false

--- apis/server/methods/apiBackends.js
+++ apis/server/methods/apiBackends.js
@@ -1,17 +1,17 @@
 import { ApiBackends } from '/apis/collection/backend';
 
 Meteor.methods({
-  currentUserCanViewApi (apiBackendId, route) {
+  currentUserCanViewApi (apiBackendId) {
     const apiBackend = ApiBackends.findOne(apiBackendId);
 
     if (apiBackend && apiBackend.currentUserCanView() ) {
       // User is authorized to view this API
       console.log("current user can view")
-      return {route, authorized: true};
+      return  true;
     } else {
       console.log("current user cannot view")
       // User is NOT authorized to view this API
-      return {route, authorized: false};
+      return false;
     }
   }
 });
