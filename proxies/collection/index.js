e9f6bd72a5fe1bc4420d886efb7ab4102daa9c32
Damir Mustafin <frenchthebread@gmail.com>
2016-08-16 13:04:46 +0300

    Extend schema, add permissions

--- proxies/collection/index.js
+++ proxies/collection/index.js
@@ -1,10 +1,74 @@
+import initialProxies from '../initialProxies';
+
 const Proxies = new Mongo.Collection('proxies');
 
-Schemas.ProxiesSchema = new SimpleSchema({
+Proxies.schema = new SimpleSchema({
   name: {
     type: String
   },
   description: {
     type: String
+  },
+  type: {
+    type: String,
+    allowedValues: initialProxies
+  },
+  apiUmbrella: {
+    type: Object,
+    optional: true
+  },
+  'apiUmbrella.url': {
+    type: String
+  },
+  'apiUmbrella.apiKey': {
+    type: String
+  },
+  'apiUmbrella.authToken': {
+    type: String
+  },
+  'apiUmbrella.elasticsearch': {
+    type: String
+  },
+  kong: {
+    type: Object,
+    optional: true
+  },
+  'kong.url': {
+    type: String
+  },
+  'kong.apiKey': {
+    type: String
+  },
+  'kong.authToken': {
+    type: String
+  },
+  tyk: {
+    type: Object,
+    optional: true
+  },
+  'tyk.url': {
+    type: String
+  },
+  'tyk.apiKey': {
+    type: String
+  },
+  'tyk.authToken': {
+    type: String
   }
 });
+
+Proxies.attachSchema(Proxies.schema);
+
+Proxies.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  }
+});
+
+export { Proxies };
