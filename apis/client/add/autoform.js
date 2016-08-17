6dcf150b2df23de4f11ccef590da5d5d54dec77e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-17 12:14:52 +0300

    Working befor insert and onSuccess hooks

--- apis/client/add/autoform.js
+++ apis/client/add/autoform.js
@@ -1,18 +1,20 @@
-AutoForm.hooks("addApiForm", {
-  before: {
-    insert (api) {
-      // Get current user ID
-      const userId = Meteor.userId()
+AutoForm.hooks({
+  "addApiForm": {
+    before: {
+      insert (api) {
+        // Get current user ID
+        const userId = Meteor.userId()
 
-      // Add current user as API manager
-      api.managerIds = [userId];
+        // Add current user as API manager
+        api.managerIds = [userId];
 
-      // Submit the form
-      return api;
+        // Submit the form
+        return api;
+      }
+    },
+    onSuccess (formType, apiId) {
+      // Redirect to newly added API
+      Router.go("viewApiBackend", { _id: apiId });
     }
-  },
-  onSuccess (formType, apiId) {
-    // Redirect to newly added API
-    Router.go("viewApiBackend", { _id: apiId });
   }
-})
+});
