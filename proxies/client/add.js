c2d4b5ea036f9f11c123749d5594b091c479360b
Damir Mustafin <frenchthebread@gmail.com>
2016-08-17 10:52:18 +0300

    Add template helper for proxies collection

--- proxies/client/add.js
+++ proxies/client/add.js
@@ -1,10 +1,11 @@
 import { Proxies } from '../collection';
 import proxiesList from '../default/list';
 
-window.Proxies = Proxies;
-
 Template.addProxy.helpers({
   proxiesList () {
     return proxiesList;
+  },
+  proxiesCollection () {
+    return Proxies;
   }
 });
