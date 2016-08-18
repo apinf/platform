ee4b180729e0f92cbab22f6574cac7faf73841a7
marla-singer <d.voytova@2glab.com>
2016-08-18 16:05:21 +0300

    integrate sdk generator: add modal form, add button, add list of popular list

---
+++ documentation/codegenerator/client/codegenerator.js
@@ -0,0 +1,103 @@
+import { HTTP } from 'meteor/http';
+import { LanguageParams, LanguageList, DocumentationFiles } from '/documentation/collection/collection';
+import { ApiBackends } from '/apis/collection/backend';
+
+Template.sdkCodeGeneratorModal.onCreated(function () {
+  const instance = this;
+
+  instance.autorun(function () {
+    const apiBackend = ApiBackends.findOne(instance.data.apiBackend._id);
+    // Save apibackend id
+    Session.set('currentApiBackend', apiBackend);
+
+    // Get documentation file id
+    const documentationFileId = instance.data.apiBackend.documentationFileId;
+
+    // Get documentation file URL
+    const documentationFileURL = Meteor.absoluteUrl().slice(0, -1) + DocumentationFiles.baseURL + '/id/' + documentationFileId;
+
+    // Save documentation file URL
+    Session.set('currentDocumentationFileURL', documentationFileURL);
+  });
+});
+
+Template.sdkCodeGeneratorModal.onDestroyed(function () {
+  // Unset session
+  Session.set('currentApiBackend', undefined);
+  Session.set('currentDocumentationFileURL', undefined);
+});
+
+Template.sdkCodeGeneratorModal.helpers({
+  // Schema for SDK Code Generator form
+  generateSDK: function () {
+    var sdkSchema = new SimpleSchema({
+      selectLanguage: {
+        type: String,
+        allowedValues: LanguageList,
+        autoform: {
+          afFieldInput: {
+            firstOption: '(Language)'
+          }
+        }
+      }
+    });
+    return sdkSchema;
+  }
+});
+
+AutoForm.addHooks('downloadSDK', {
+  onSubmit: function (formValues) {
+    // Prevent form from submitting
+    this.event.preventDefault();
+
+    // Get selected language from dropdown
+    const selectedLanguage = formValues.selectLanguage;
+
+    // Create parameter for URL on depecing selected language
+    // Check on specific language name
+    let languageHashName = LanguageParams[selectedLanguage];
+
+    if (_.isUndefined(languageHashName)) {
+        // Use standart method for creating parameter
+      languageHashName = selectedLanguage.toLowerCase();
+      languageHashName = languageHashName.replace(/\s+/g, '-');
+    }
+
+    // Create URL to send request
+    const url = 'https://generator.swagger.io/api/gen/clients/' + languageHashName;
+
+    // Get path to documentation file
+    const pathToFile = Session.get('currentDocumentationFileURL');
+
+    // Create POST options
+    const options = {
+      'swaggerUrl': pathToFile
+    };
+
+    // Send POST request
+    HTTP.post(url, { data: options }, function (error, result) {
+      // Get information from Swagger API response
+      let response = JSON.parse(result.content);
+
+      if (result.statusCode === 200) {
+        // Hide modal
+        Modal.hide('sdkCodeGeneratorModal');
+
+        // Go to link and download file
+        window.location.href = response.link;
+      } else {
+        $('button').removeAttr('disabled');
+
+        // Otherwise show an error message
+        FlashMessages.sendError(response.message);
+      }
+    });
+  }
+});
+
+FlashMessages.configure({
+  // Configuration for FlashMessages.
+  autoHide: true,
+  hideDelay: 5000,
+  autoScroll: false
+});
--- documentation/collection/collection.js
+++ documentation/collection/collection.js
@@ -14,4 +14,21 @@ const DocumentationFiles = new FileCollection('DocumentationFiles', {
   ]
 });
 
-export { DocumentationFiles };
+// List of popular language for SDK Code Generator
+const LanguageList = ['Akka Scala', 'Android', 'Async Scala',
+                     'Clojure', 'Cpprest', 'C#', 'C# .NET 2.0', 'Cwiki', 
+                     'Dart', 'Dynamic HTML', 'Flash', 'Go', 'Groovy',
+                     'HTML', 'Html2', 'Java', 'Javascript', 'Javascript Closure Angular', 'Jmeter', 
+                     'Objective-C', 'Perl', 'PHP', 'Python', 'Qt 5 C++', 'Ruby', 
+                     'Scala', 'Swagger JSON', 'Swagger YAML', 'Swift',
+                     'Tizen', 'Typescript Angular', 'Typescript Angular2', 'Typescript Fetch', 'Typescript Node'];
+
+// Interpretation some languages to parameter for creating URL 
+const LanguageParams = {
+      "C#": "csharp",
+      "C# .NET 2.0": "CsharpDotNet2",
+      "Objective-C": "objc",
+      "Qt 5 C++": "qt5cpp",
+      "Swagger JSON": "swagger"
+    }
+export { DocumentationFiles, LanguageList, LanguageParams };
