25904dd3e6ea0731839981efa1f80f5cc27d245b
Damir Mustafin <frenchthebread@gmail.com>
2016-08-18 12:43:44 +0300

    isActive -> isActiveRoute

--- settings/settings/client/menu/menu.js
+++
@@ -1,7 +0,0 @@
-import { Template } from 'meteor/templating';
-
-Template.settingsMenu.helpers({
-  isActive (route) {
-    return (route === Router.current().route.getName()) ? 'active' : '';
-  }
-});
