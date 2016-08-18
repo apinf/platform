a85085908b904ed2ed48ffa2d7eb9c54361cf8a4
Damir Mustafin <frenchthebread@gmail.com>
2016-08-18 12:31:52 +0300

    Comments

--- settings/proxies/collection/permission.js
+++ settings/proxies/collection/permission.js
@@ -3,12 +3,17 @@ import { Proxies } from './';
 Proxies.allow({
   insert: function () {
 
+    // Check if user has admin role
     return Roles.userIsInRole(Meteor.userId(), ['admin']);
   },
   update: function () {
+
+    // Check if user has admin role
     return Roles.userIsInRole(Meteor.userId(), ['admin']);
   },
   remove: function () {
+
+    // Check if user has admin role
     return Roles.userIsInRole(Meteor.userId(), ['admin']);
   }
 });
