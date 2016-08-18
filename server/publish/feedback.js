842a4d7c570a11c0ad3720c80d3f735080f6180d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-18 15:08:45 +0300

    Move status methods to core

--- server/publish/feedback.js
+++
@@ -1,9 +0,0 @@
-Meteor.publish('apiBackendFeedback', function(apiBackendId){
-  // show feedback to specific API
-  return Feedback.find({apiBackendId: apiBackendId});
-});
-
-Meteor.publish('getAllVotesForSingleFeedback', function(feedbackId){
-  // show feedbackvotes for single feedback
-  return FeedbackVotes.find({feedbackId: feedbackId});
-});
