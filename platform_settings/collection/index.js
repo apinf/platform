cdf004193743de36b2c41336933b97db036c5ea3
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-22 15:55:16 +0300

    Move Settings permissions

--- platform_settings/collection/index.js
+++ platform_settings/collection/index.js
@@ -1,26 +1,3 @@
 const Settings = new Mongo.Collection('Settings');
 
 export { Settings };
-
-Meteor.startup(function () {
-
-  Settings.allow({
-    insert: function() {
-      // get settings
-      var dbSettingsCount = Settings.find().count();
-      // if no settings exist
-      if ( dbSettingsCount > 0 ) {
-        // don't allow insert
-        return false;
-      } else {
-        // insert
-        return true;
-      }
-    },
-    update: function() {
-      return true;
-    }
-
-  });
-
-});
