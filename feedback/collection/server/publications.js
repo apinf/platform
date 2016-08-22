ecfe1d0fa4f59a4e1994dedde3ebb19533731a8d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-22 12:49:46 +0300

    Move FeedbackVotes publication

--- feedback/collection/server/publications.js
+++ feedback/collection/server/publications.js
@@ -2,8 +2,3 @@ Meteor.publish('apiBackendFeedback', function(apiBackendId){
   // show feedback to specific API
   return Feedback.find({apiBackendId: apiBackendId});
 });
-
-Meteor.publish('getAllVotesForSingleFeedback', function(feedbackId){
-  // show feedbackvotes for single feedback
-  return FeedbackVotes.find({feedbackId: feedbackId});
-});
