3ce4bc7295dc5e155cc95d4ac43cc20cbab47bce
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-22 16:23:19 +0300

    Move permissions

--- ratings/collection/index.js
+++ ratings/collection/index.js
@@ -1,15 +1,3 @@
 const ApiBackendRatings = new Mongo.Collection('apiBackendRatings');
 
 export { ApiBackendRatings };
-
-ApiBackendRatings.allow({
-  insert: function () {
-    // User must be logged in to vote
-    if (Meteor.userId()) {
-      return true;
-    }
-  },
-  update: function () {
-    return true;
-  }
-});
