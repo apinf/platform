fb4b634514d7be54c1447f353cb846f5551567b3
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-22 12:26:41 +0300

    Move Branding permissions

--- platform_branding/collection/index.js
+++ platform_branding/collection/index.js
@@ -1,15 +1,3 @@
 const Branding = new Mongo.Collection('branding');
 
 export { Branding };
-
-Branding.allow({
-  insert: function (userId) {
-    return Roles.userIsInRole(userId, ['admin']) && Branding.find().count() === 0;
-  },
-  update: function (userId) {
-    return Roles.userIsInRole(userId, ['admin']);
-  },
-  remove: function (userId) {
-    return Roles.userIsInRole(userId, ['admin']);
-  }
-});
