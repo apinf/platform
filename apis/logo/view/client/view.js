ced93ea31b7948df52cae942588bf0360564a8a6
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-23 15:00:23 +0300

    Check for apiLogoFileId for safety

--- apis/logo/view/client/view.js
+++ apis/logo/view/client/view.js
@@ -5,7 +5,6 @@ Template.viewApiLogo.onCreated(function() {
   const instance = this;
   // Subscribe to API logo
   instance.subscribe('allApiLogo');
-
 });
 
 Template.viewApiLogo.helpers({
@@ -13,19 +12,21 @@ Template.viewApiLogo.helpers({
     // Get API current API Backend from template data
     const currentApiBackend = Template.currentData().apiBackend;
 
-    const currentApiLogoFileId = Apis.findOne(currentApiBackend._id).apiLogoFileId;
+    if (currentApiBackend && currentApiBackend.apiLogoFileId) {
+      const currentApiLogoFileId = currentApiBackend.apiLogoFileId;
 
-    // Convert to Mongo ObjectID
-    const objectId = new Mongo.Collection.ObjectID(currentApiLogoFileId);
+      // Convert to Mongo ObjectID
+      const objectId = new Mongo.Collection.ObjectID(currentApiLogoFileId);
 
-    // Get API logo file Object
-    const currentApiLogoFile = ApiLogo.findOne(objectId);
+      // Get API logo file Object
+      const currentApiLogoFile = ApiLogo.findOne(objectId);
 
-    // Check if API logo file is available
-    if (currentApiLogoFile) {
+      // Check if API logo file is available
+      if (currentApiLogoFile) {
 
-      // Get API logo file URL
-      return Meteor.absoluteUrl().slice(0, -1) + ApiLogo.baseURL + "/md5/" + currentApiLogoFile.md5;
+        // Get API logo file URL
+        return Meteor.absoluteUrl().slice(0, -1) + ApiLogo.baseURL + "/md5/" + currentApiLogoFile.md5;
+      }
     }
   },
   apiLogoExists: function () {
