e45691bd1055ef5d282db0887c40f3894f666742
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-23 13:42:18 +0300

    Lint

--- bookmarks/collection/helpers.js
+++ bookmarks/collection/helpers.js
@@ -3,6 +3,6 @@ import { ApiBackends } from '/apis/collection/backend';
 
 ApiBookmarks.helpers({
   apis : function() {
-    return ApiBackends.find({_id :{$in: this.apiIds}})
+    return ApiBackends.find({_id :{$in: this.apiIds}});
   }
 });
