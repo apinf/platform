aab2670e8792517e25254e96a2d2b35b0c91bfee
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-24 11:11:16 +0300

    Move AutoForm code

---
+++ backlog/client/form/autoform.js
@@ -0,0 +1,29 @@
+AutoForm.hooks({
+  apiBacklogForm: {
+    before: {
+      insert: function (backlogItem) {
+        // Attach API Backend Id to backlog item schema
+        backlogItem.apiBackendId = Router.current().params._id;
+        return backlogItem;
+      }
+    },
+    beginSubmit: function () {
+      // Disable form elements while submitting form
+      $('[data-schema-key],button').attr("disabled", "disabled");
+    },
+    endSubmit: function () {
+      // Enable form elements after form submission
+      $('[data-schema-key],button').removeAttr("disabled");
+    }
+  }
+});
+
+AutoForm.addHooks(['apiBacklogForm'], {
+  // Success message
+  onSuccess: function () {
+    // Close modal window "manually" after form submit successfully
+    $('#apiBacklogFormModal').modal('hide');
+    // Push flash message to user
+    FlashMessages.sendSuccess('Thank you! Your backlog item has been successfully published.');
+  }
+});
