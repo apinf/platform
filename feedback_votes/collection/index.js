859530193e2f30888fab472368fe493e9f7a4842
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-24 11:46:16 +0300

    Move FeedbackVotes schema

--- feedback_votes/collection/index.js
+++ feedback_votes/collection/index.js
@@ -2,26 +2,6 @@ const FeedbackVotes = new Mongo.Collection('feedbackVotes');
 
 export { FeedbackVotes };
 
-FeedbackVotes.schema = new SimpleSchema({
-  /*
-  feedbackId - string - the ID of the Feedback document
-  userId - string - the ID of the user
-  vote - integer - the numeric vote for the user (allowed values: 1 and -1)
-  */
-  feedbackId: {
-    type: String
-  },
-  userId: {
-    type: String
-  },
-  vote: {
-    type: Number,
-    allowedValues: [1,-1]
-  }
-});
-
-FeedbackVotes.attachSchema(FeedbackVotes.schema);
-
 FeedbackVotes.allow({
   insert: function () {
     // Only allow logged in user to vote
