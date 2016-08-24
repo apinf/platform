a6f65738407c92f685bb821d24be7311c8afb28a
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-24 11:12:40 +0300

    Add apiBacklogItemsCollection helper

--- backlog/client/form/form.js
+++ backlog/client/form/form.js
@@ -1,1 +1,8 @@
+import { ApiBacklogItems } from '../../collection';
 
+Template.apiBacklogForm.helpers({
+  apiBacklogItemsCollection () {
+    // Return a reference to ApiBacklogItems collection
+    return ApiBacklogItems;
+  }
+});
