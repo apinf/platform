7217a4407acedc8ae758f7a8c48c37ccd9d5b558
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-24 11:14:44 +0300

    Import ApiBacklogItems

--- core/server/publish/apiBacklog.js
+++ core/server/publish/apiBacklog.js
@@ -1,4 +1,6 @@
-Meteor.publish('apiBacklogItems', function(apiBackendId){
+import { ApiBacklogItems } from '/backlog/collection';
 
+Meteor.publish('apiBacklogItems', function(apiBackendId){
+  // Return backlog items for a given API
   return ApiBacklogItems.find({ apiBackendId: apiBackendId });
 });
