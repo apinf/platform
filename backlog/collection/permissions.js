78653a7cafcb7ad4463ac0fd1fd1eb544d197b69
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-25 15:30:58 +0300

    Fix allow remove rule

--- backlog/collection/permissions.js
+++ backlog/collection/permissions.js
@@ -39,7 +39,7 @@ ApiBacklogItems.allow({
     var apiBackendId = backlog.apiBackendId;
 
     // Find related API Backend, select only "managerIds" field
-    var apiBackend = ApiBackends.findOne(apiBackendId, {fields: {managerIds: 1}});
+    var apiBackend = Apis.findOne(apiBackendId, {fields: {managerIds: 1}});
 
     // Check if current user can edit API Backend
     return apiBackend.currentUserCanEdit();
