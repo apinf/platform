87f6095a2592178c0d2a1c947f7d5adcab2a5a17
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-25 15:30:49 +0300

    Add code to delete backlog item

--- backlog/client/delete/delete.js
+++ backlog/client/delete/delete.js
@@ -1,5 +1,14 @@
+import { ApiBacklogItems } from '../../collection';
+
 Template.deleteBacklogItem.events({
   'click #confirm-delete': function (event, instance) {
-    console.log("clicked!");
+    // Get BAcklog Item ID
+    const backlogItemId = instance.data.backlogItem._id;
+
+    // Remove the Backlog Item
+    ApiBacklogItems.remove(backlogItemId);
+
+    // Close the modal
+    Modal.hide("deleteBacklogItem");
   }
 });
