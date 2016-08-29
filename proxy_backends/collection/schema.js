112a97420a69258ee9fb474f570792fb3ae92d23
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-29 14:15:50 +0300

    Basic schema fields and ApiUmbrellaSchema import

--- proxy_backends/collection/schema.js
+++ proxy_backends/collection/schema.js
@@ -1,7 +1,14 @@
 import { ProxyBackends } from './';
+import { ApiUmbrellaSchema } from './apiUmbrellaSchema';
 
 ProxyBackends.schema = new SimpleSchema({
-
+  "proxyId": {
+    type: String
+  },
+  "apiUmbrella": {
+    type: ApiUmbrellaSchema,
+    optional: true
+  }
 });
 
 ProxyBackends.attachSchema(ProxyBackends.schema);
