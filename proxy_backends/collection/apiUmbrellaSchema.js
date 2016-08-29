0879bd8af929c614f5bb5e6d56d8d6450c457e90
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-08-29 14:16:02 +0300

    Basic API Umbrella schema

---
+++ proxy_backends/collection/apiUmbrellaSchema.js
@@ -0,0 +1,46 @@
+const ApiUmbrellaSchema = new SimpleSchema({
+  "name": {
+    type: String,
+    optional: true
+  },
+  "frontend_host": {
+    type: String,
+    optional: true
+  },
+  "backend_host": {
+    type: String,
+    optional: true
+  },
+  "backend_protocol": {
+    type: String,
+    optional: true
+  },
+  "balance_algorithm": {
+    type: String,
+    optional: true
+  },
+  "url_matches": {
+    type: [Object]
+    optional: true
+  },
+  "url_matches.$.frontend_prefix": {
+    type: String,
+    optional: true
+  },
+  "url_matches.$.backend_prefix": {
+    type: String,
+    optional: true
+  },
+  "servers": {
+    type: [Object],
+    optional: true
+  },
+  "servers.$.host": {
+    type: String,
+    optional: true
+  },
+  "servers.$.port": {
+    type: Number,
+    optional: true
+  }
+});
