3c1ca0e74bf0db1c5d9d088d476157909bc17dae
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-08-29 16:10:13 +0300

    Add apikey helpers, events

---
+++ apikeys/client/apikey.js
@@ -0,0 +1,27 @@
+Template.apikey.events({
+  'click #umbrella-apikey-button' (event) {
+    Meteor.call('createApiKeyForCurrentUser', function(error, result) {
+      if(error) {
+        sAlert.error(error);
+      }
+    });
+  }
+});
+
+Template.apikey.helpers({
+  currentUser () {
+    return Meteor.user();
+  },
+  apiKey () {
+    // Get current user
+    const currentUser = Meteor.user();
+
+    // Make sure user exitsts and has API key
+    if (currentUser) {
+      // Get API Key
+      //const apiKey = currentUser.profile.apiKey;
+
+      return apiKey;
+    }
+  }
+});
