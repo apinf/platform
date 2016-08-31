d0dbfbfd4ebe719747ab9f512b791285c89ab5b0
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-08-31 15:33:02 +0300

    Move copyButton

--- apikeys/client/copyButton/copyButton.js
+++ apikeys/client/copyButton/copyButton.js
@@ -1,13 +1,13 @@
 import Clipboard from "clipboard";
 
-Template.profileApiKeyCopyButton.onRendered(function () {
+Template.apiKeyCopyButton.onRendered(function () {
   // Initialize Clipboard copy button
   let copyButton = new Clipboard("#copyApiKey");
 
   // Tell the user when copy is successful
   copyButton.on("success", function () {
     // Get localized success message
-    const successMessage = TAPi18n.__("profile_apiKey_copySuccessful");
+    const successMessage = TAPi18n.__("apiKeys_copySuccessful");
 
     // Display success message to user
     sAlert.success(successMessage);
