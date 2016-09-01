c7849ea663d867f962919ef8fb7e989f46e0ce4c
illya <in11092008@gmail.com>
2016-09-01 22:45:33 +0300

    added comment

--- backlog/collection/server/publications.js
+++ backlog/collection/server/publications.js
@@ -2,5 +2,6 @@ import { ApiBacklogItems } from '/backlog/collection';
 
 Meteor.publish('apiBacklogItems', function(apiBackendId){
 
+  // returning backlog items object for current apibackend
   return ApiBacklogItems.find({ apiBackendId: apiBackendId });
 });
