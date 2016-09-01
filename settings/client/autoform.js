5347bfe06a95e140c5252efdf626a0fdb9b62cce
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-01 13:20:40 +0300

    Add setttingsForm success message i18n

--- settings/client/autoform.js
+++ settings/client/autoform.js
@@ -1,7 +1,11 @@
 AutoForm.hooks({
   settingsForm: {
     onSuccess () {
-      FlashMessages.sendSuccess('Settings saved.');
+      // Get success message translation string
+      const successMessage = TAPi18n.__('settingsForm-successMessage');
+
+      // Display success message to user
+      FlashMessages.sendSuccess(successMessage);
     },
     onError (formType, error) {
       FlashMessages.sendError(error);
