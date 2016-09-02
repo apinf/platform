57ceaf947bbed76c776bfcb7e4468796952aea80
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-02 16:04:17 +0300

    Add publicProxyDetails publication

--- proxies/collection/server/publications.js
+++ proxies/collection/server/publications.js
@@ -1,9 +1,19 @@
 import { Proxies } from '../';
 
 Meteor.publish('allProxies', function () {
-
   // Check user permissions
   if (Roles.userIsInRole(this.userId, ['admin'])) {
     return Proxies.find();
   }
+});
+
+Meteor.publish('publicProxyDetails', function () {
+  // Return all proxies
+  // with only name and ID fields
+  const publicProxyDetails = Proxies.find({
+    fields: {
+      _id: 1,
+      name: 1,
+    },
+  });
 });
