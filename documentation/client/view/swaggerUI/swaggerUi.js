139f487ae650941364bea38970b106cbc8e60fd0
marla-singer <d.voytova@2glab.com>
2016-09-02 17:01:18 +0300

    add swagger-ui-browserify package

---
+++ documentation/client/view/swaggerUI/swaggerUi.js
@@ -0,0 +1,33 @@
+import { Template } from 'meteor/templating';
+import SwaggerUi from 'swagger-ui-browserify'
+
+Template.swaggerUi.onCreated(function () {
+  const documentationURL = this.data.apiDocumentation;
+
+  const swagger = new SwaggerUi({
+    url: documentationURL,
+    dom_id: 'swagger-ui-container',
+    useJQuery: true,
+    supportHeaderParams: true,
+    supportedSubmitMethods: ['get', 'post'],
+    onComplete: function (swaggerApi, swaggerUi) {
+      console.log('Loaded SwaggerUI')
+    },
+    onFailure: function (error) {
+      console.error('Unable to Load SwaggerUI', error)
+    },
+    apisSorter: 'alpha',
+    operationsSorter: 'alpha',
+    docExpansion: 'none'
+  });
+
+
+  swagger.load()
+});
+
+Template.swaggerUi.onRendered(function () {
+  const documentationURL = this.data.apiDocumentation;
+
+  $('#input_baseUrl').val(documentationURL);
+
+});
