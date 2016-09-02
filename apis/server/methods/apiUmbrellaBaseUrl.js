0752624707546bcfbe096c30fdb8c87eaf062d68
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-02 11:19:02 +0300

    Remove unused methods

--- apis/server/methods/apiUmbrellaBaseUrl.js
+++
@@ -1,9 +0,0 @@
-import { Settings } from '/settings/collection';
-
-Meteor.methods({
-  'getApiUmbrellaBaseUrl': function () {
-    // Get base url from settings collection
-    const apiUmbrellaBaseUrl = Settings.findOne().apiUmbrella.host;
-    return apiUmbrellaBaseUrl;
-  },
-});
