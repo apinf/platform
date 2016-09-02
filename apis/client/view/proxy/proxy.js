d5cd2e368245e10f87225a630717af2b3cc58024
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-02 15:54:37 +0300

    Add proxyId helper

--- apis/client/view/proxy/proxy.js
+++ apis/client/view/proxy/proxy.js
@@ -4,5 +4,15 @@ Template.apiProxy.helpers({
   proxyBackendsCollection () {
     // Return a reference to ProxyBackends collection, for AutoForm
     return ProxyBackends;
-  }
+  },
+  proxyId () {
+    // TODO: determine how to provide proxyId for the ProxyBackend form
+    // e.g. will we have more than one proxy?
+    // if no, we need also to limit the number of proxies that can be added
+
+    // Get a single Proxy
+    const proxy = Proxies.findOne();
+
+    return proxy._id;
+  },
 });
