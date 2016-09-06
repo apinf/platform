4d307eb1387efa9dbcb56bbb939ed2519af97deb
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-06 14:40:46 +0300

    Refactor apiKey creation functions

--- apikeys/server/methods.js
+++ apikeys/server/methods.js
@@ -1,5 +1,42 @@
+import { Proxies } from '/proxies/collection';
+
 Meteor.methods({
   createApiKey () {
-    // Get proxy
+    // Get logged in user
+    const currentUser = Meteor.user();
+    // Check currentUser exists
+    if (currentUser) {
+      // TODO: Fix for multi-proxy support
+      const proxy = Proxies.findOne();
+
+      // Check type
+      if(proxy && proxy.type === "apiUmbrella") {
+        // Call Umbrella method to create user with API key
+        Meteor.call('createApiUmbrellaUser', function(result, error) {
+          if(error) {
+            console.log(error);
+          } else {
+            /*
+            // Set fieldsToBeUpdated
+            const fieldsToBeUpdated = {
+              'apiUmbrellaUserId': response.data.user.id,
+              'profile.apiKey': response.data.user.api_key,
+            };
+
+            // Update currentUser
+            Meteor.users.update({ _id: currentUser._id }, { $set: fieldsToBeUpdated });
+
+            */
+          }
+
+        });
+      }
+    } else {
+      // Meteor Error (User not logged in)
+      throw new Meteor.Error(
+        'apinf-usernotloggedin-error',
+        TAPi18n.__('apinf_usernotloggedin_error')
+      );
+    }
   }
 });
