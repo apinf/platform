fd957c0e52493d5c4a8abdaede1594c534ddb36d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-07 13:30:44 +0300

    Cleanup project logo semantics

--- core/client/navbar/navbar.js
+++ core/client/navbar/navbar.js
@@ -31,18 +31,18 @@ Template.navbar.helpers({
 
     // Make sure branding and project logo exist
     if (branding && branding.projectLogoFileId) {
-      const currentProjectLogoFileId = Branding.findOne().projectLogoFileId;
+      const projectLogoFileId = branding.projectLogoFileId;
 
       // Convert to Mongo ObjectID
-      const objectId = new Mongo.Collection.ObjectID(currentProjectLogoFileId);
+      const objectId = new Mongo.Collection.ObjectID(projectLogoFileId);
 
       // Get project logo file Object
-      const currentProjectLogoFile = ProjectLogo.findOne(objectId);
+      const projectLogoFile = ProjectLogo.findOne(objectId);
 
       // Check if project logo file is available
-      if (currentProjectLogoFile) {
+      if (projectLogoFile) {
         // Get API logo file URL
-        return Meteor.absoluteUrl().slice(0, -1) + ProjectLogo.baseURL + '/md5/' + currentProjectLogoFile.md5;
+        return Meteor.absoluteUrl().slice(0, -1) + ProjectLogo.baseURL + '/md5/' + projectLogoFile.md5;
       }
     }
   },
