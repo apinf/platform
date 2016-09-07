4ee36c5033705651aff185e916900b8667d13baf
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-07 13:47:16 +0300

    Move onSuccess hook

--- home/client/contact/autoform.js
+++ home/client/contact/autoform.js
@@ -8,11 +8,8 @@ AutoForm.hooks({
       // Enable form elements after form submission
       $('[data-schema-key],button').removeAttr('disabled');
     },
-  },
-});
-
-AutoForm.addHooks(['contactForm'], {
-  onSuccess () {
-    FlashMessages.sendSuccess('Thank you! Your message has been successfully sent.');
+    onSuccess () {
+      FlashMessages.sendSuccess('Thank you! Your message has been successfully sent.');
+    },
   },
 });
