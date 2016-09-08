8769fad72cef22e5de42f35f6f624386856be876
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-08 15:03:23 +0300

    Add imports; lint

--- feedback/collection/server/publications.js
+++ feedback/collection/server/publications.js
@@ -1,9 +1,13 @@
-Meteor.publish('apiBackendFeedback', function(apiBackendId){
+// Collection imports
+import { Feedback } from '../collection';
+import { FeedbackVotes } from '/feedback_votes/collection';
+
+Meteor.publish('apiBackendFeedback', function (apiBackendId) {
   // show feedback to specific API
-  return Feedback.find({apiBackendId: apiBackendId});
+  return Feedback.find({ apiBackendId });
 });
 
-Meteor.publish('getAllVotesForSingleFeedback', function(feedbackId){
+Meteor.publish('getAllVotesForSingleFeedback', function (feedbackId) {
   // show feedbackvotes for single feedback
-  return FeedbackVotes.find({feedbackId: feedbackId});
+  return FeedbackVotes.find({ feedbackId });
 });
