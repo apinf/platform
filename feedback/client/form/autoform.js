e4744d5d97895b8f304f7ce941d5efe8abc5af3c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-08 15:01:00 +0300

    Change ID; cleanup

--- feedback/client/form/autoform.js
+++ feedback/client/form/autoform.js
@@ -1,21 +1,14 @@
 AutoForm.hooks({
-  feedback: {
+  feedbackForm: {
     before: {
       insert (feedback) {
         feedback.apiBackendId = Router.current().params._id;
+
         return feedback;
       },
     },
-    beginSubmit () {
-      // Disable form elements while submitting form
-      $('[data-schema-key],button').attr('disabled', 'disabled');
-    },
-    endSubmit () {
-      // Enable form elements after form submission
-      $('[data-schema-key],button').removeAttr('disabled');
-    },
     onSuccess () {
-      FlashMessages.sendSuccess('Thank you! Your feedback has been successfully sent.');
+      sAlert.success('Thank you! Your feedback has been successfully sent.');
     },
   },
 });
