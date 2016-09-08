bdf9f88baf7a335e880bc61151107c6fe49e3d4d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-08 14:42:45 +0300

    Refactor

--- feedback/client/form/form.js
+++ feedback/client/form/form.js
@@ -1,32 +1,21 @@
 AutoForm.hooks({
   feedback: {
     before: {
-      insert: function (feedback) {
+      insert (feedback) {
         feedback.apiBackendId = Router.current().params._id;
         return feedback;
-      }
+      },
     },
-    beginSubmit: function () {
+    beginSubmit () {
       // Disable form elements while submitting form
-      $('[data-schema-key],button').attr("disabled", "disabled");
+      $('[data-schema-key],button').attr('disabled', 'disabled');
     },
-    endSubmit: function () {
+    endSubmit () {
       // Enable form elements after form submission
-      $('[data-schema-key],button').removeAttr("disabled");
-    }
-  }
-});
-
-AutoForm.addHooks(['feedback'], {
-  // Success message
-  onSuccess: function () {
-    FlashMessages.sendSuccess('Thank you! Your feedback has been successfully sent.');
-  }
-});
-
-FlashMessages.configure({
-  // Configuration for FlashMessages
-  autoHide: true,
-  hideDelay: 5000,
-  autoScroll: false
+      $('[data-schema-key],button').removeAttr('disabled');
+    },
+    onSuccess () {
+      FlashMessages.sendSuccess('Thank you! Your feedback has been successfully sent.');
+    },
+  },
 });
