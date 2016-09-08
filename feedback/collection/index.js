52e747e91e27f03f9677d712941b30b17bbb3cd0
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-08 14:33:38 +0300

    Move/refactor Feedback helpers

--- feedback/collection/index.js
+++ feedback/collection/index.js
@@ -1,19 +1,1 @@
-import ss from 'simple-statistics';
-
 export const Feedback = new Mongo.Collection('feedback');
-
-Feedback.helpers({
-  'sumOfVotes': function () {
-    // Get all votes for current feedback
-    const feedbacks = FeedbackVotes.find({ feedbackId: this._id }).fetch();
-
-    // Create a list of all feedback vote values
-    const votes = _(feedbacks).map(function (feedback) {
-      return feedback.vote;
-    });
-
-    // Calculate the sum of all vote values
-    const sum = ss.sum(votes);
-    return sum;
-  },
-});
