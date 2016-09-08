205223207d3898532f5a2e902403256011bdeda8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-08 14:30:08 +0300

    Move Feedback schema

--- feedback/collection/index.js
+++ feedback/collection/index.js
@@ -2,68 +2,6 @@ import ss from 'simple-statistics';
 
 export const Feedback = new Mongo.Collection('feedback');
 
-Schemas.FeedbackSchema = new SimpleSchema({
-  topic: {
-    type: String,
-    label: 'Topic',
-    max: 50,
-    optional: false,
-    autoform: {
-      placeholder: 'Feedback topic',
-    },
-  },
-  message: {
-    type: String,
-    label: 'Your Message',
-    max: 1000,
-    optional: false,
-    autoform: {
-      rows: 5,
-      placeholder: 'Your message',
-    },
-  },
-  messageType: {
-    type: String,
-    label: 'Choose message type',
-    allowedValues: ['Feedback', 'Error report', 'Feature request'],
-    autoform: {
-      options: [
-        { label: 'Feedback', value: 'Feedback' },
-        { label: 'Error report', value: 'Error report' },
-        { label: 'Feature request', value: 'Feature request' },
-      ],
-    },
-  },
-  authorId: {
-    type: String,
-    autoValue () {
-      if (this.isInsert) {
-        return Meteor.userId();
-      } else {
-        this.unset();
-      }
-    },
-    denyUpdate: true,
-  },
-  apiBackendId: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    autoValue () {
-      if (this.isInsert) {
-        return new Date();
-      } else if (this.isUpsert) {
-        return { $setOnInsert: new Date() };
-      } else {
-        this.unset();
-      }
-    },
-  },
-});
-
-Feedback.attachSchema(Schemas.FeedbackSchema);
-
 Feedback.allow({
   insert () {
     return true;
