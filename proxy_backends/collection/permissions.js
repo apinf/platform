553028d6af77e09977f17c47e7ba3a483155ed1c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-09 11:56:04 +0300

    Pass in backend document as argument

--- proxy_backends/collection/permissions.js
+++ proxy_backends/collection/permissions.js
@@ -3,33 +3,33 @@ import { ProxyBackends } from './';
 import { Apis } from '/apis/collection';
 
 ProxyBackends.allow({
-  insert () {
+  insert (backendId, backend) {
     // Only allow API Managers or Administrators to insert
 
     // Get API document
-    const api = Apis.findOne(this.apiId);
+    const api = Apis.findOne(backend.apiId);
 
     // Check if current user can edit API
     if (api && api.currentUserCanEdit()) {
       return true;
     }
   },
-  update () {
+  update (backendId, backend) {
     // Only allow API Managers or Administrators to update
 
     // Get API document
-    const api = Apis.findOne(this.apiId);
+    const api = Apis.findOne(backend.apiId);
 
     // Check if current user can edit API
     if (api && api.currentUserCanEdit()) {
       return true;
     }
   },
-  remove () {
+  remove (backendId, backend) {
     // Only allow API Managers or Administrators to remove
 
     // Get API document
-    const api = Apis.findOne(this.apiId);
+    const api = Apis.findOne(backend.apiId);
 
     // Check if current user can edit API
     if (api && api.currentUserCanEdit()) {
