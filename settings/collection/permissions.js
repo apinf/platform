c1fa098d87f5f2dd02a7d55e0ad200c7ab75c64d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-09 12:07:08 +0300

    Only admin can insert/update

--- settings/collection/permissions.js
+++ settings/collection/permissions.js
@@ -1,19 +1,29 @@
 import { Settings } from './';
 
 Settings.allow({
-  insert () {
+  insert (userId, settings) {
     // get settings
     const dbSettingsCount = Settings.find().count();
+
+    // Check if current user is admin
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
     // if no settings exist
     if (dbSettingsCount > 0) {
       // don't allow insert
       return false;
-    } else {
+    } else if (userIsAdmin) {
       // insert
       return true;
     }
   },
-  update () {
-    return true;
+  update (userId, settings) {
+    // Check if current user is admin
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
+    // Only admin can update
+    if (userIsAdmin) {
+      return true;
+    }
   },
 });
