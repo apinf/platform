c88d9497436126479b13f6432f5ffba759788385
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-09 12:13:23 +0300

    Clarify insert logic

--- settings/collection/permissions.js
+++ settings/collection/permissions.js
@@ -2,17 +2,14 @@ import { Settings } from './';
 
 Settings.allow({
   insert (userId, settings) {
-    // get settings
-    const dbSettingsCount = Settings.find().count();
-
     // Check if current user is admin
     const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
 
+    // Check if settings exist (settings count is zero)
+    const noSettingsExist = Settings.find().count() === 0;
+
     // if no settings exist
-    if (dbSettingsCount > 0) {
-      // don't allow insert
-      return false;
-    } else if (userIsAdmin) {
+    if (userIsAdmin && noSettingsExist) {
       // insert
       return true;
     }
