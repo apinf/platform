1a10ea9efeff6779a5911657e819d3bc9347698d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-09 12:09:07 +0300

    Move schema

--- ratings/collection/index.js
+++ ratings/collection/index.js
@@ -1,23 +1,5 @@
 export const ApiBackendRatings = new Mongo.Collection('apiBackendRatings');
 
-Schemas.ApiBackendRating = new SimpleSchema({
-  'apiBackendId': {
-    type: String,
-    regEx: SimpleSchema.RegEx.Id,
-  },
-  'userId': {
-    type: String,
-    regEx: SimpleSchema.RegEx.Id,
-  },
-  'rating': {
-    type: Number,
-    min: 0,
-    max: 4,
-  },
-});
-
-ApiBackendRatings.attachSchema(Schemas.ApiBackendRating);
-
 ApiBackendRatings.allow({
   insert (userId, rating) {
     // User must be logged in to vote
