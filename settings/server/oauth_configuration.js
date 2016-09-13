b6936566de3e8beffd58c50d456a77b07c234256
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-13 15:04:04 +0300

    Remove isServer check

--- settings/server/oauth_configuration.js
+++ settings/server/oauth_configuration.js
@@ -1,15 +1,13 @@
-if (Meteor.isServer) {
-  // TODO: refactor this to use Settings collection
-  // TODO: remove from Meteor.startup, run when Settings updated
-  Meteor.startup(function () {
-    if (Meteor.settings && Meteor.settings.serviceConfigurations) {
-      return _.each(Meteor.settings.serviceConfigurations, function (config, service) {
-        return ServiceConfiguration.configurations.upsert({
-          service,
-        }, {
-          $set: config,
-        });
+// TODO: refactor this to use Settings collection
+// TODO: remove from Meteor.startup, run when Settings updated
+Meteor.startup(function () {
+  if (Meteor.settings && Meteor.settings.serviceConfigurations) {
+    return _.each(Meteor.settings.serviceConfigurations, function (config, service) {
+      return ServiceConfiguration.configurations.upsert({
+        service,
+      }, {
+        $set: config,
       });
-    }
-  });
-}
+    });
+  }
+});
