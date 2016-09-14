e14f8b7e35402d65d4e00fdb0597ccc88998b0b0
illya <in11092008@gmail.com>
2016-09-14 21:31:56 +0300

    fixed i18n string

--- users/collection/users_schema_messages.js
+++ users/collection/users_schema_messages.js
@@ -1,5 +1,5 @@
 // Fetch the profile username is taken message
-var usernameIsTaken = TAPi18n.__("profile-usernameIsTaken");
+var usernameIsTaken = TAPi18n.__("profile_usernameIsTaken");
 
 // Configure SimpleSchema with username is taken message
 SimpleSchema.messages({usernameTaken: usernameIsTaken});
