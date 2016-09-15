f9adc6bbe3885f3b64a45fa107b2a7b153cfafa9
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-15 15:44:27 +0300

    Basic insert and update hook functions

--- proxy_backends/client/form/autoform.js
+++ proxy_backends/client/form/autoform.js
@@ -1,5 +1,14 @@
 AutoForm.hooks({
   proxyBackendForm: {
+    before: {
+      insert (api) {
+        console.log(api);
+      },
+      update () {
+        // TODO: update backend on API Umbrella, and publish changes
+        console.log(this.currentDoc);
+      },
+    },
     onSuccess () {
       // Get success message translation
       const message = TAPi18n.__('proxyBackendForm_successMessage');
