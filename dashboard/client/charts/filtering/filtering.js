4ba49959b7f37f755e016634cec7f0a6a91bd8af
Damir Mustafin <frenchthebread@gmail.com>
2016-09-15 17:45:07 +0300

    Add comments & refactor

--- dashboard/client/charts/filtering/filtering.js
+++ dashboard/client/charts/filtering/filtering.js
@@ -8,34 +8,48 @@ Template.dashboardChartsFiltering.onCreated(function () {
   // Get reference to template instance
   const instance = this;
 
-  instance.apisManagedByUser = new ReactiveVar();
-
-  instance.otherApis = new ReactiveVar();
-
   // Subscribe to publication
   instance.subscribe('proxyApis');
 
+  // Keeps APIs managed by user
+  instance.apisManagedByUser = new ReactiveVar();
+  // Keeps other APIs
+  instance.otherApis = new ReactiveVar();
+
+  instance.fetchApis = () => {
+    // Fetch proxy backends
+    const proxyBackends = ProxyBackends.find().fetch();
+
+    // Placeholders for APIs
+    const apisManagedByUser = [];
+    const otherApis = [];
+
+    // Update variable with data
+    _.forEach(proxyBackends, (proxyBackend) => {
+
+      // Get apiUmbrella object
+      const umbrellaApi = proxyBackend.apiUmbrella;
+
+      // Attach _id field to apiUmbrella object
+      umbrellaApi._id = proxyBackend.apiId;
+
+      // Check user permissions for each API
+      // push it to related opt group
+      if (proxyBackend.currentUserIsManager()) {
+        apisManagedByUser.push(umbrellaApi);
+      } else {
+        otherApis.push(umbrellaApi);
+      }
+    });
+
+    // Update reactive variables
+    instance.apisManagedByUser.set(apisManagedByUser);
+    instance.otherApis.set(otherApis);
+  };
+
   instance.autorun(() => {
     if (instance.subscriptionsReady()) {
-      const proxyBackends = ProxyBackends.find().fetch();
-
-      const apisManagedByUser = [];
-      const otherApis = [];
-
-      // Update variable with data
-      _.forEach(proxyBackends, (proxyBackend) => {
-        const umbrellaApi = proxyBackend.apiUmbrella;
-        umbrellaApi._id = proxyBackend.apiId;
-
-        if (proxyBackend.currentUserIsManager()) {
-          apisManagedByUser.push(umbrellaApi);
-        } else {
-          otherApis.push(umbrellaApi);
-        }
-      });
-
-      instance.apisManagedByUser.set(apisManagedByUser);
-      instance.otherApis.set(otherApis);
+      instance.fetchApis();
     }
   });
 });
