215ebd4249e6a2f66eae8fa26ba4b5caf4738b08
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-16 13:59:17 +0300

    Rename

---
+++ proxies/client/form/form.js
@@ -0,0 +1,18 @@
+import { Template } from 'meteor/templating';
+import { Proxies } from '../../collection';
+
+Template.proxyForm.helpers({
+  proxiesCollection () {
+    return Proxies;
+  },
+  proxy () {
+    const instance = Template.instance();
+    return instance.data.proxy;
+  },
+  formType () {
+    const instance = Template.instance();
+
+    // Return formType depending on the action e.g. editing or adding
+    return (instance.data.isEdit) ? 'update' : 'insert';
+  },
+});
