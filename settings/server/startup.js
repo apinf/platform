fa5936d82aa1a2878f9d1b72860707b1368596de
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-16 17:09:02 +0300

    imports not needed

--- settings/server/startup.js
+++ settings/server/startup.js
@@ -1,6 +1,3 @@
-import { mailSettingsValid } from '/core/helper_functions/validate_settings';
-import { Settings } from '../collection';
-
 Meteor.startup(function () {
   // Call mail configuration update
   Meteor.call('updateMailConfiguration');
