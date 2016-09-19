4e6037dd9d3d404f006cddae58a957f64d308d96
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-19 16:56:31 +0300

    Use mail helpers, add i18n

--- users/server/emails.js
+++ users/server/emails.js
@@ -1,51 +1,18 @@
-import { Branding } from '/branding/collection';
-import { Settings } from '/settings/collection';
+import { getSiteTitle, getFromEmail } from '/core/helper_functions/mail_helpers';
 
-Accounts.emailTemplates.siteName = function () {
-  // Get branding
-  const branding = Branding.findOne();
+Accounts.emailTemplates.siteName = getSiteTitle();
 
-  // Check branding exists
-  if( branding ) {
-    return branding.siteTitle;
-  }
-};
-Accounts.emailTemplates.from = function () {
-  // Get settings
-  const Settings = Settings.findOne();
-
-  // Check settings exists
-  if( settings ) {
-    // Check if siteName is defined
-    if( Accounts.emailTemplates.siteName ) {
-      // Construct from email (eg. Apinf "<info@apinf.io>")
-     const fromEmail = Accounts.emailTemplates.siteName +
-       ' <' + settings.mail.fromEmail + '>';
-    } else {
-      // Construct from email (eg. "info@apinf.io")
-     const fromEmail = settings.mail.fromEmail;
-    }
-
-    return fromEmail;
-  }
-};
+Accounts.emailTemplates.from = getFromEmail();
 
 Accounts.emailTemplates.verifyEmail = {
   subject() {
-    const siteName = Accounts.emailTemplates.siteName;
-
-    // Check if siteName has been given
-    if( siteName ) {
-      return '[' + siteName + '] ' + 'Verify Your Email Address';
-    } else {
-      return 'Verify Your Email Address';
-    }
+    return TAPi18n.__('emailTemplates_verifyEmail_title');
   },
   text( user, url ) {
     let emailAddress   = user.emails[0].address,
         // Take out hash from url to use custom route behaviour
         urlWithoutHash = url.replace( '#/', '' ),
-        emailMessage = TAPi18n.__('emailTemplates_verifyEmail'),
+        emailMessage = TAPi18n.__('emailTemplates_verifyEmail_message'),
         // Construct emailBody
         emailBody = `${emailMessage}\n\n${urlWithoutHash}`;
 
