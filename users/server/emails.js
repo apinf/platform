184f989b756e4a8fbaef76a0f3747368915fa2e3
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-19 15:55:07 +0300

    Make configurable, add i18n

--- users/server/emails.js
+++ users/server/emails.js
@@ -1,18 +1,53 @@
-// TODO: Get from settings?
-Accounts.emailTemplates.siteName = "Apinf";
-Accounts.emailTemplates.from     = "Apinf <info@apinf.io>";
+import { Branding } from '/branding/collection';
+import { Settings } from '/settings/collection';
+
+Accounts.emailTemplates.siteName = function () {
+  // Get branding
+  const branding = Branding.findOne();
+
+  // Check branding exists
+  if( branding ) {
+    return branding.siteTitle;
+  }
+};
+Accounts.emailTemplates.from = function () {
+  // Get settings
+  const Settings = Settings.findOne();
+
+  // Check settings exists
+  if( settings ) {
+    // Check if siteName is defined
+    if( Accounts.emailTemplates.siteName ) {
+      // Construct from email (eg. Apinf "<info@apinf.io>")
+     const fromEmail = Accounts.emailTemplates.siteName +
+       ' <' + settings.mail.fromEmail + '>';
+    } else {
+      // Construct from email (eg. "info@apinf.io")
+     const fromEmail = settings.mail.fromEmail;
+    }
+
+    return fromEmail;
+  }
+};
 
 Accounts.emailTemplates.verifyEmail = {
   subject() {
-    return "[Apinf] Verify Your Email Address";
+    const siteName = Accounts.emailTemplates.siteName;
+
+    // Check if siteName has been given
+    if( siteName ) {
+      return '[' + siteName + '] ' + 'Verify Your Email Address';
+    } else {
+      return 'Verify Your Email Address';
+    }
   },
   text( user, url ) {
     let emailAddress   = user.emails[0].address,
         // Take out hash from url to use custom route behaviour
         urlWithoutHash = url.replace( '#/', '' ),
-        emailBody      = `To verify your email address (${emailAddress}) visit \
-        the following link:\n\n${urlWithoutHash}\n\n If you did not request \
-        this verification, please ignore this email.`;
+        emailMessage = TAPi18n.__('emailTemplates_verifyEmail'),
+        // Construct emailBody
+        emailBody = `${emailMessage}\n\n${urlWithoutHash}`;
 
     return emailBody;
   }
