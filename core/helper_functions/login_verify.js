28b742cb50cfeb89077a095bb8eb018e6cceb38f
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-19 14:54:22 +0300

    Add comments

--- core/helper_functions/login_verify.js
+++ core/helper_functions/login_verify.js
@@ -1,11 +1,11 @@
 // Login attempt verifier to require verified email before login
 export function loginAttemptVerifier (parameters) {
   if (parameters.user && parameters.user.emails && (parameters.user.emails.length > 0)) {
-    const found = _.find(
-     parameters.user.emails,
+    // Find user email & callback check if it's verified
+    const found = _.find( parameters.user.emails,
      function(thisEmail) { return thisEmail.verified }
     );
-
+    // Check if recently found email is verified
     if (!found) {
       throw new Meteor.Error(500, TAPi18n.__('loginVerify_errorMessage'));
     }
