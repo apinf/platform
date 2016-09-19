f3934d1118201770e760705adf387db553fdcd63
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-09-19 14:55:25 +0300

    do what function says

--- home/client/body/homeBody.js
+++ home/client/body/homeBody.js
@@ -1,4 +1,4 @@
-import { contactEmailValid } from '/core/helper_functions/validate_settings';
+import { mailSettingsValid, contactEmailValid } from '/core/helper_functions/validate_settings';
 import { Settings } from '/settings/collection';
 
 Template.homeBody.onCreated(function () {
@@ -16,7 +16,7 @@ Template.homeBody.helpers({
   contactFormEnabled () {
     const settings = Settings.findOne();
 
-    // Check mail is enabled & contact email has been given
-    return contactEmailValid(settings);
+    // Check mail settings are valid & contact email has been given
+    return mailSettingsValid(settings) && contactEmailValid(settings);
   },
 });
