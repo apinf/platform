206684347b6395444bcad995414cf7e426616993
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-19 15:12:41 +0300

    Basic multi-step intro

--- apis/client/view/intro/intro.js
+++ apis/client/view/intro/intro.js
@@ -13,6 +13,30 @@ Template.apiIntro.onRendered(() => {
         element: '#api-details-tab',
         intro: 'This is the API Details tab',
       },
+      {
+        element: '#api-documentation-tab',
+        intro: 'This is the API Documentation tab',
+      },
+      {
+        element: '#api-metadata-tab',
+        intro: 'This is the API Metadata tab',
+      },
+      {
+        element: '#api-feedback-tab',
+        intro: 'This is the API Feedback tab',
+      },
+      {
+        element: '#api-backlog-tab',
+        intro: 'This is the API Details tab',
+      },
+      {
+        element: '#api-export-tab',
+        intro: 'This is the API Export tab',
+      },
+      {
+        element: '#api-settings-tab',
+        intro: 'This is the API Settings tab',
+      },
     ],
   });
 
