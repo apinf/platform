19d0295f90abe0f2d65c4eb1cf611db705ea14dc
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-20 12:31:48 +0300

    Fix modal dialogue; cleanup/comment

--- proxies/client/proxies.js
+++ proxies/client/proxies.js
@@ -5,14 +5,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 Template.proxies.onCreated(function () {
   const instance = this;
 
-  instance.proxies = new ReactiveVar();
-
   instance.subscribe('allProxies');
 });
 
 Template.proxies.events({
   'click #add-proxy': function (event, instance) {
-    Modal.show('addProxy');
+    // Show the add proxy form
+    Modal.show('proxyForm');
   },
 });
 
