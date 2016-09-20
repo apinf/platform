52a40027a03ca4917d5db35c38af1309a2b91476
Damir Mustafin <frenchthebread@gmail.com>
2016-09-20 21:21:16 +0300

    lint

--- proxies/client/proxies.js
+++ proxies/client/proxies.js
@@ -2,7 +2,7 @@ import { Proxies } from '../collection';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-Template.proxies.onCreated(function(){
+Template.proxies.onCreated(function () {
   const instance = this;
 
   instance.proxies = new ReactiveVar();
@@ -20,7 +20,7 @@ Template.proxies.onCreated(function(){
 Template.proxies.events({
   'click #add-proxy': function (event, instance) {
     Modal.show('addProxy', { proxy: {}, isEdit: false });
-  }
+  },
 });
 
 Template.proxies.helpers({
