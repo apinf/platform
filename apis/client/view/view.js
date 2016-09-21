9fac6552d0f32e5a8e8ce57316c106cdd64fd16e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-09-21 14:20:22 +0300

    Subscribe to public proxy details

--- apis/client/view/view.js
+++ apis/client/view/view.js
@@ -23,6 +23,9 @@ Template.viewApi.onCreated(function () {
 
   // Subscribe to proxy settings for this API
   instance.subscribe('apiProxySettings', instance.apiId);
+
+  // Subscribe to public proxy details for relevant Proxy
+  instance.subscribe('publicProxyDetails');
 });
 
 Template.viewApi.helpers({
