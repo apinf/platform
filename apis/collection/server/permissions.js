fde5369217c2f9b55b35276b18fb0d67a3ef4b41
Damir Mustafin <damir@Damirs-MacBook-Pro.local>
2016-09-28 02:50:28 +0300

    eslint

--- apis/collection/server/permissions.js
+++ apis/collection/server/permissions.js
@@ -1,54 +1,55 @@
+import _ from 'lodash';
 import { Apis } from '../';
 
 Apis.allow({
-  insert: function (userId, apiBackendDoc) {
+  insert (userId, apiBackendDoc) {
     return true;
   },
-  update: function (userId, apiBackendDoc) {
+  update (userId, apiBackendDoc) {
     // Save ID of API Backend
     const apiBackendId = apiBackendDoc._id;
     // Get API backend with ID
     const apiBackend = Apis.findOne(apiBackendId);
     // Check if current user can edit API Backend
-    let currentUserCanEdit = apiBackend.currentUserCanEdit();
+    const currentUserCanEdit = apiBackend.currentUserCanEdit();
 
     if (currentUserCanEdit) {
       // User is allowed to perform action
       return true;
-    } else {
-      // User is not allowded to perform action
-      return false;
     }
+    // User is not allowded to perform action
+    return false;
   },
-  remove: function (userId, apiBackendDoc) {
+  remove (userId, apiBackendDoc) {
     // Save ID of API Backend
     const apiBackendId = apiBackendDoc._id;
     // Get API backend with ID
     const apiBackend = Apis.findOne(apiBackendId);
     // Check if current user can edit API Backend
-    let currentUserCanEdit = apiBackend.currentUserCanEdit();
+    const currentUserCanEdit = apiBackend.currentUserCanEdit();
 
     if (currentUserCanEdit) {
       // User is allowed to perform action
       return true;
-    } else {
-      // User is not allowded to perform action
-      return false;
     }
-  }
+    // User is not allowded to perform action
+    return false;
+  },
 });
 
 Apis.deny({
   insert (fields) {
     // Don't allow user to set average rating or bookmark count fields
-    if (_.contains(fields, "averageRating") || _.contains(fields, "bookmarkCount")) {
+    if (_.contains(fields, 'averageRating') || _.contains(fields, 'bookmarkCount')) {
       return true;
     }
+    return false;
   },
   update (fields) {
     // Don't allow user to set average rating or bookmark count fields
-    if (_.contains(fields, "averageRating") || _.contains(fields, "bookmarkCount")) {
+    if (_.contains(fields, 'averageRating') || _.contains(fields, 'bookmarkCount')) {
       return true;
     }
-  }
+    return false;
+  },
 });
