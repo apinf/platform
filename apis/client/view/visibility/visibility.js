6dbef042f28b8840cc453bbefc53fdd28baff64d
illya <in11092008@gmail.com>
2016-09-28 00:24:18 +0300

    visibility toggle event

--- apis/client/view/visibility/visibility.js
+++ apis/client/view/visibility/visibility.js
@@ -1,1 +1,17 @@
 import { Apis } from '/apis/collection';
+
+Template.visibilityToggle.events({
+  'click .changeVisibility': function (event) {
+    // Get reference to current template instance
+    const instance = this;
+
+    // Get API Backend from data context
+    const api = this.api;
+
+    // Get ID of current service
+    const apiId = api._id;
+
+    // Set the isPublic property to the opposite of its current value
+    Apis.update(apiId, { $set: { isPublic: !api.isPublic } });
+  },
+});
