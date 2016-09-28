9b98f392da3bf6b9887dd1e3b5005550d9ed4207
Damir Mustafin <damir@Damirs-MacBook-Pro.local>
2016-09-28 03:26:59 +0300

    Permission check on insert

--- apis/collection/server/permissions.js
+++ apis/collection/server/permissions.js
@@ -1,9 +1,33 @@
 import _ from 'lodash';
+
+import { Roles } from 'meteor/alanning:roles';
+
+import { Settings } from '/settings/collection';
 import { Apis } from '../';
 
 Apis.allow({
-  insert (userId, apiBackendDoc) {
-    return true;
+  insert (userId) {
+    // Get settings document
+    const settings = Settings.findOne();
+
+    try {
+      // Get access setting value
+      const onlyAdminsCanAddApi = settings.access.onlyAdminsCanAddApi;
+
+      if (!onlyAdminsCanAddApi) {
+        return true;
+      }
+
+      // Check if current user is admin
+      const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
+      // Check if current user is admin and access settings is set
+      return onlyAdminsCanAddApi && userIsAdmin;
+    } catch (e) {
+      // If caught an error, then returning true because no access settings is set
+      // By default allowing all user to add an API
+      return true;
+    }
   },
   update (userId, apiBackendDoc) {
     // Save ID of API Backend
@@ -40,14 +64,14 @@ Apis.allow({
 Apis.deny({
   insert (fields) {
     // Don't allow user to set average rating or bookmark count fields
-    if (_.contains(fields, 'averageRating') || _.contains(fields, 'bookmarkCount')) {
+    if (_.hasIn(fields, 'averageRating') || _.hasIn(fields, 'bookmarkCount')) {
       return true;
     }
     return false;
   },
   update (fields) {
     // Don't allow user to set average rating or bookmark count fields
-    if (_.contains(fields, 'averageRating') || _.contains(fields, 'bookmarkCount')) {
+    if (_.hasIn(fields, 'averageRating') || _.hasIn(fields, 'bookmarkCount')) {
       return true;
     }
     return false;
