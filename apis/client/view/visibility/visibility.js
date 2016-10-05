41b1b84f59ab15691751d6f9f4087d812ce76515
illya <in11092008@gmail.com>
2016-10-05 16:06:12 +0300

    remove unneeded event

--- apis/client/view/visibility/visibility.js
+++ apis/client/view/visibility/visibility.js
@@ -2,20 +2,6 @@ import { Template } from 'meteor/templating';
 
 import { Apis } from '/apis/collection';
 
-
-Template.visibilityToggle.events({
-  'click .changeVisibility': function (event) {
-    // Get API from template data
-    const api = Template.currentData().api;
-
-    // Get ID of current service
-    const apiId = api._id;
-
-    // Set the isPublic property to the opposite of its current value
-    Apis.update(apiId, { $set: { isPublic: !api.isPublic } });
-  },
-});
-
 Template.visibilityToggle.helpers({
   visible () {
     // Get API from template data
