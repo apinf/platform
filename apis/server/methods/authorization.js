8423e76ce46fda92f17e749ef2d73745e3a87641
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-06 14:49:28 +0300

    Basic working addAuthorizedUserByEmail method

--- apis/server/methods/authorization.js
+++ apis/server/methods/authorization.js
@@ -1,6 +1,15 @@
+import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
+import { Apis } from '/apis/collection';
+
 Meteor.methods({
   addAuthorizedUserByEmail (apiId, email) {
     // Get user with matching email
     const user = Accounts.findUserByEmail(email);
-  }
+
+    // TODO: check whether user is already in list before adding
+
+    // Add user ID to API authorized user IDs field
+    Apis.update(apiId, { $push: { authorizedUserIds: user._id } });
+  },
 });
