5b504eb3cf1128ad11bde922d0ae757131848e83
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-06 14:30:29 +0300

    Call addAuthorizedUserByEmail

--- apis_authorization/client/form/autoform.js
+++ apis_authorization/client/form/autoform.js
@@ -2,9 +2,12 @@ import { AutoForm } from 'meteor/aldeed:autoform';
 
 AutoForm.hooks({
   authorizedUserForm: {
-    onSubmit (emailDocument) {
+    onSubmit (submission) {
       // Get email address from form submission
-      const email = emailDocument.emailAddress;
+      const email = submission.emailAddress;
+
+      // Get reference to API ID
+      const apiId = submission.apiId;
 
       // Get reference to form, for use in method callback(s)
       const form = this;
@@ -13,8 +16,14 @@ AutoForm.hooks({
       Meteor.call('checkIfEmailIsRegistered', email, function (error, emailIsRegistered) {
         if (emailIsRegistered) {
           console.log('email is registered')
-          // Continue with form submission
-          form.done();
+
+          // Add user to API authorized users list
+          Meteor.call('addAuthorizedUserByEmail', apiId, email, function (error, response) {
+            if (!error) {
+              // Continue with form submission
+              form.done();
+            }
+          });
         } else {
           // throw an error
           form.done(new Error('email-not-registered'));
