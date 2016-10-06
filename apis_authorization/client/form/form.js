84566aee00d4aed485c21399c7f0a74040e488ed
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-06 14:20:35 +0300

    Add apiId helper

--- apis_authorization/client/form/form.js
+++ apis_authorization/client/form/form.js
@@ -1,9 +1,15 @@
 import { emailSchema } from './schema';
 
 Template.apiUserAuthorizationForm.helpers({
+  apiId () {
+    // Get API ID
+    const apiId = Template.instance().data.api._id;
+
+    return apiId;
+  },
   emailSchema () {
     return emailSchema;
-  }
+  },
 });
 
 Template.apiUserAuthorizationForm.events({
