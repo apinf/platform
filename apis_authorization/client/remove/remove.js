c2cb7dee546a91048d64981f3f3de51ca53ba9df
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-07 14:48:23 +0300

    Working remove click handler

--- apis_authorization/client/remove/remove.js
+++ apis_authorization/client/remove/remove.js
@@ -1,5 +1,23 @@
+import { Apis } from '/apis/collection';
+import { Template } from 'meteor/templating';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
+
 Template.apiRemoveAuthorizedUser.events({
-  'click #confirm-remove' () {
-    
-  }
+  'click #confirm-remove' (event, templateInstance) {
+    // Get API ID
+    const apiId = templateInstance.data.api._id;
+
+    // Get User ID
+    const userId = templateInstance.data.user._id;
+
+    // Remove User ID from authorized users array
+    Apis.update({ _id: apiId },
+       { $pull:
+         { authorizedUserIds: userId },
+       }
+     );
+
+    // Dismiss the modal dialogue
+    Modal.hide('apiRemoveAuthorizedUser');
+  },
 });
