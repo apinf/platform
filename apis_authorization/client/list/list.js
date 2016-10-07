bfc6f8ed818d7bea638d374183ac85b60c787152
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-07 11:07:52 +0300

    Flatten authorizedUsers list

--- apis_authorization/client/list/list.js
+++ apis_authorization/client/list/list.js
@@ -1,13 +1,25 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { _ } from 'lodash';
+
 Template.apiAuthorizedUsersList.helpers({
   authorizedUsers () {
     // Get API document, reactively
     const api = Template.currentData().api;
 
     // Get all authorized users for current API
-    const authorizedUsers = Meteor.users.find({ _id: {$in: api.authorizedUserIds } }).fetch();
+    let authorizedUsers = Meteor.users.find({
+      _id: { $in: api.authorizedUserIds },
+    }).fetch();
 
-    console.log(authorizedUsers);
-
+    // flatten structure of users within authorized users array
+    authorizedUsers = _.map(authorizedUsers, function (user) {
+      return {
+        username: user.username,
+        email: user.emails[0].address,
+      };
+    });
+    
     return authorizedUsers;
-  }
+  },
 });
