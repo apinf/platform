2575123782926f3871b44ef027595bdc330fe4be
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-10-12 14:01:30 +0300

    Router check redirect anonymous user

--- dashboard/client/lib/routes.js
+++ dashboard/client/lib/routes.js
@@ -1,6 +1,18 @@
-Router.route("/dashboard", {
-  layout: "masterLayout",
-  template: "dashboard",
-}, {
-  name: "dashboard"
+import { Meteor } from 'meteor/meteor';
+import { Router } from 'meteor/iron:router';
+
+Router.route('/dashboard', {
+  name: 'dashboard',
+  layout: 'masterLayout',
+  template: 'dashboard',
+  action () {
+    // Check user is logged in
+    if (Meteor.user()) {
+      this.next();
+    } else {
+      // If not redirect to sign in
+      this.redirect('/sign-in');
+      this.next();
+    }
+  },
 });
