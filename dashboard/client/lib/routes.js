c00a37c6018154c75d4f390f3aae17a2682596c7
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-10-12 14:27:04 +0300

    Use ensureSignedIn

--- dashboard/client/lib/routes.js
+++ dashboard/client/lib/routes.js
@@ -1,18 +1,7 @@
-import { Meteor } from 'meteor/meteor';
 import { Router } from 'meteor/iron:router';
 
 Router.route('/dashboard', {
   name: 'dashboard',
   layout: 'masterLayout',
   template: 'dashboard',
-  action () {
-    // Check user is logged in
-    if (Meteor.user()) {
-      this.next();
-    } else {
-      // If not redirect to sign in
-      this.redirect('/sign-in');
-      this.next();
-    }
-  },
 });
