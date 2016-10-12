1565c7c137c11ceecb1e2e3f51d51c0ac2cf3db2
Daria Voytova <d.voytova@2glab.com>
2016-10-12 11:39:09 +0300

    lint; improve logging message

--- settings/server/methods.js
+++ settings/server/methods.js
@@ -1,9 +1,15 @@
-import { Settings } from '../collection';
+// Meteor packages import
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+
+// Apinf import
 import { githubSettingsValid, mailSettingsValid } from '/core/helper_functions/validate_settings';
 import { loginAttemptVerifier } from '/core/helper_functions/login_verify';
+import { Settings } from '../collection';
 
 Meteor.methods({
-  'updateGithubConfiguration': function () {
+  updateGithubConfiguration () {
     // Try if settings exist
     try {
       const settings = Settings.findOne();
@@ -22,13 +28,14 @@ Meteor.methods({
           secret: settings.githubConfiguration.secret,
         });
       }
-    }
-    // otherwise show an error
-    catch (error) {
-      console.log(error);
+    } catch (error) {
+      // otherwise show an error
+      const message = `Update gitHub configuration: ${error}`;
+
+      console.log(message);
     }
   },
-  'updateMailConfiguration': function () {
+  updateMailConfiguration () {
     // Try if settings exist
     try {
       const settings = Settings.findOne();
@@ -42,19 +49,22 @@ Meteor.methods({
         const smtpPort = settings.mail.smtpPort;
 
         // Set MAIL_URL env variable
-        process.env.MAIL_URL = 'smtp://' +
-          encodeURIComponent(username) + ':' +
-          encodeURIComponent(password) + '@' +
-          encodeURIComponent(smtpHost) + ':' +
-          encodeURIComponent(smtpPort);
+        process.env.MAIL_URL = `
+          smtp://${encodeURIComponent(username)}:
+          ${encodeURIComponent(password)}@
+          ${encodeURIComponent(smtpHost)}:
+          ${encodeURIComponent(smtpPort)}
+        `;
 
         // Toggle loginAttemptVerifier ON when Mail settings exist to allow first user
         Accounts.validateLoginAttempt(loginAttemptVerifier);
       }
+    } catch (error) {
+      // otherwise preapare message about error
+      const message = `Update mail configuration: ${error}`;
+
+      // Show an error message
+      console.log(message);
     }
-    // otherwise show an error
-    catch (error) {
-      console.log(error);
-    }
-  }
+  },
 });
