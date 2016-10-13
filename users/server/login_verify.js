4107bf05d366db444d25517f9d6bdfe67aa8dc40
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-13 15:36:56 +0300

    Refactor and lint

--- users/server/login_verify.js
+++ users/server/login_verify.js
@@ -1,18 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { _ } from 'lodash';
+
 // Login attempt verifier to require verified email before login
 export function loginAttemptVerifier (parameters) {
+  // Placeholder for user login allowed
+  let userLoginAllowed;
+
   if (parameters.user && parameters.user.emails && (parameters.user.emails.length > 0)) {
-    // Find user email & callback check if it's verified
-    const found = _.find( parameters.user.emails,
-     function(thisEmail) { return thisEmail.verified }
-    );
-    // Check if recently found email is verified
-    if (!found) {
+    // Get user emails
+    const emails = parameters.user.emails;
+
+    // Check if any of user's emails are verified
+    const verified = _.find(emails, function (email) { return email.verified; });
+
+    // If no email is verified, throw an error
+    if (!verified) {
       throw new Meteor.Error(500, TAPi18n.__('loginVerify_errorMessage'));
     }
-    // return true if verified email, false otherwise.
-    return found && parameters.allowed;
+
+    // If email is verified and parameters.allowed is true, user login is allowed
+    if (verified && parameters.allowed) {
+      userLoginAllowed = true;
+    }
   } else {
-    // User doesn't have registered emails
-    return false;
+    // User doesn't have registered email, so login not allowed
+    userLoginAllowed = false;
   }
+
+  return userLoginAllowed;
 }
