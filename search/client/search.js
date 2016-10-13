3c809210b620c00a22c59e0eb4a53e2a74643aa5
Daria Voytova <d.voytova@2glab.com>
2016-10-13 16:04:05 +0300

    lint

--- search/client/search.js
+++ search/client/search.js
@@ -1,9 +1,16 @@
+// Meteor packages import
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Router } from 'meteor/useraccounts:iron-routing';
+
+// APINF import
 import { Apis } from '/apis/collection';
+// npm import
 import moment from 'moment';
 
-Template.search.created = function () {
+Template.search.onCreated(() => {
   // Get reference to Template instance
-  var instance = this;
+  const instance = this;
 
   // Init reactive var for search value with empty string for all results
   instance.searchValue = new ReactiveVar('');
@@ -14,17 +21,17 @@ Template.search.created = function () {
   // Check if search parameter is set
   if (Router.current().params.query.q) {
     // Get the query string parameter
-    var searchValue = Router.current().params.query.q;
+    const searchValue = Router.current().params.query.q;
 
     // Assign current search value to the reactive variable
     instance.searchValue.set(searchValue);
   }
 
-  instance.autorun(function () {
-    var searchValue = instance.searchValue.get();
+  instance.autorun(() => {
+    const searchValue = instance.searchValue.get();
 
     // Update API Backends subscription with search value
-    instance.subscribe("searchApiBackends", searchValue);
+    instance.subscribe('searchApiBackends', searchValue);
 
     // Construct query using regex using search value
     instance.query.set({
@@ -33,15 +40,15 @@ Template.search.created = function () {
           name: {
             $regex: searchValue,
             $options: 'i' // case-insensitive option
-          }
+          },
         },
         {
           backend_host: {
             $regex: searchValue,
             $options: 'i' // case-insensitive option
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
   });
 
@@ -57,64 +64,62 @@ Template.search.created = function () {
 
     return filteredResult;
   };
-};
+});
 
-Template.search.rendered = function () {
+Template.search.onRendered(() => {
   // Get reference to Template
-  var instance = this;
+  const instance = this;
 
   // Update search field with current search value
   $('#search-text').val(instance.searchValue.get());
 
   // Check if search parameter is set
   if (instance.searchValue.get()) {
-
     // Update search field with search value provided in the URL
     $('#search-text').val(instance.searchValue.get());
   }
 
   // Put focus of search field on a page
   $('#search-text').focus();
-};
+});
 
 Template.search.helpers({
-  searchResults: function() {
+  searchResults () {
     // Get reference to Template instance
-    var instance = Template.instance();
+    const instance = Template.instance();
 
     // Init search results variable
-    var searchResults = instance.getSearchResults();
+    const searchResults = instance.getSearchResults();
 
     if (searchResults !== undefined) {
       // Iterate through all documents
-      _.each(searchResults, function (result) {
-
+      _.each(searchResults, (result) => {
         // Return to user human-readable timestamp
         result.relative_created_at = moment(result.created_at).fromNow();
       });
     }
     return searchResults;
   },
-  searchResultsCount: function () {
+  searchResultsCount () {
     // Get reference to Template instance
-    var instance = Template.instance();
+    const instance = Template.instance();
 
-    // Get the amount of api backends found from reactive var
-    var searchResultsCount = instance.getSearchResults().length;
+    // Get the amount of api found from reactive var
+    const searchResultsCount = instance.getSearchResults().length;
 
     return searchResultsCount;
-  }
+  },
 });
 
 Template.search.events({
-  "keyup #search-text": function (event, template) {
+  'keyup #search-text': function (event, template) {
     event.preventDefault();
 
     // Get reference to Template instance
-    var instance = Template.instance();
+    const instance = Template.instance();
 
     // Get search text from a text field.
-    var searchValue = $('#search-text').val();
+    const searchValue = $('#search-text').val();
 
     // Assign searchValue to a reactive variable
     instance.searchValue.set(searchValue);
@@ -123,5 +128,5 @@ Template.search.events({
     UniUtils.url.setQuery("q", searchValue);
 
     return false;
-  }
+  },
 });
