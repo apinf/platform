27acc66402f4fa5b18776bf62db79ba92794e509
Daria Voytova <d.voytova@2glab.com>
2016-10-18 17:18:29 +0300

    Rewrite condition for require fields, for unique field

--- test/addapibackend.js
+++ test/addapibackend.js
@@ -11,7 +11,8 @@ test.describe('Add API Backend', function() {
     var driver;
     var newUser = {
         added: false,
-        email: ""
+        email: "",
+        password: ""
     };
     test.before(function() {
         driver = CommonUtils.buildDriver();
@@ -22,7 +23,10 @@ test.describe('Add API Backend', function() {
     test.afterEach(function() {
         CommonUtils.dashBoardSignOut(driver);
         if (newUser.added) {
-            CommonUtils.deleteNewUser(driver, newUser.email);
+            var mainPage = CommonUtils.deleteNewUser(driver, newUser.email, newUser.password);
+            mainPage.getText().then(function(text) {
+                assert.equal('Latest APIs', text);
+            });
         }
     });
     test.it('8.1 should add/publish api backend as a new user', function() {
@@ -32,21 +36,20 @@ test.describe('Add API Backend', function() {
             userName = faker.internet.userName();
         }
         var email = faker.internet.email();
+        var password = faker.internet.password();
         newUser.added = true;
         newUser.email = email;
-        CommonUtils.fillSignUpForm(driver, userName, email, faker.internet.password());
+        newUser.password = password;
+        CommonUtils.fillSignUpForm(driver, userName, email, password);
         CommonUtils.goToDashboard(driver);
         AddAPIBackendUtil.addNewBackend(driver, {
-            backendName: 'NewBackend',
-            description: 'Test Description',
-            hostName: 'google.com',
-            portNumber: '80',
-            frontendPrefix: '/test',
-            backendPrefix: '/test'
+            apiName: 'NewBackend v2',
+            apiDescription: 'Test Description',
+            apiURL: 'http://google.com'
         });
-        var publishedAPIHostElement = driver.findElement(By.xpath('//*[@class="page-title"]'));
+        var publishedAPIHostElement = driver.findElement(By.id('api-header'));
         publishedAPIHostElement.getText().then(function(text){
-            assert.equal(text, 'NewBackend');
+            assert.equal(text, 'NewBackend v2');
         });
     });
     test.it('8.2 should add/publish api backend as a registered user who doesn\'t own any api', function(){
@@ -55,35 +58,49 @@ test.describe('Add API Backend', function() {
         CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
         CommonUtils.goToDashboard(driver);
         AddAPIBackendUtil.addNewBackend(driver, {
-            backendName: 'NewBackend',
-            description: 'Test Description',
-            hostName: 'google.com',
-            portNumber: '80',
-            frontendPrefix: '/test',
-            backendPrefix: '/test'
+            apiName: 'NewBackend',
+            apiDescription: 'Test Description',
+            apiURL: 'http://google.com'
         });
-        var publishedAPIHostElement = driver.findElement(By.xpath('//*[@class="page-title"]'));
+        var publishedAPIHostElement = driver.findElement(By.id('api-header'));
         publishedAPIHostElement.getText().then(function(text){
             assert.equal(text, 'NewBackend');
         });
     });
-    test.it('8.3 should not add/publish api backend with missing fields', function() {
+    test.it('8.3.1 should not add/publish api backend with missing API name field', function() {
         newUser.added = false;
         CommonUtils.signIn(driver);
         CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
         CommonUtils.goToDashboard(driver);
         var values = {
-            backendName: 'NewBackend',
-            description: 'Test Description',
-            hostName: '',
-            portNumber: '80',
+            apiDescription: 'Test Description',
+            apiURL: 'http://google.com'
         };
         AddAPIBackendUtil.clickAddNewBackend(driver);
-        AddAPIBackendUtil.fillBaseInformation(driver, values);
-        AddAPIBackendUtil.fillBackendInformation(driver, values);
-        var hostRequiredErrorElement = driver.findElement(By.xpath('//*[@id="backend-information-form"]/div[2]/span'));
+        AddAPIBackendUtil.fillApiDescription(driver, values);
+        AddAPIBackendUtil.fillApiURL(driver, values);
+        AddAPIBackendUtil.savingInformational(driver);
+        var hostRequiredErrorElement = driver.findElement(By.xpath('//*[@id="addApiForm"]/fieldset/div[1]/span'));
         hostRequiredErrorElement.getText().then(function(text) {
-            assert.equal(text, 'Host is required');
+            assert.equal(text, 'API Name is required');
+        });
+    });
+    test.it('8.3.2 should not add/publish api backend with missing URL field', function() {
+        newUser.added = false;
+        CommonUtils.signIn(driver);
+        CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
+        CommonUtils.goToDashboard(driver);
+        var values = {
+            apiName: 'NewBackend',
+            apiDescription: 'Test Description'
+        };
+        AddAPIBackendUtil.clickAddNewBackend(driver);
+        AddAPIBackendUtil.fillApiName(driver, values);
+        AddAPIBackendUtil.fillApiDescription(driver, values);
+        AddAPIBackendUtil.savingInformational(driver);
+        var hostRequiredErrorElement = driver.findElement(By.xpath('//*[@id="addApiForm"]/fieldset/div[3]/span'));
+        hostRequiredErrorElement.getText().then(function(text) {
+            assert.equal(text, 'URL is required');
         });
     });
     test.it('8.4 should not add/publish api backend with invalid host name', function() {
@@ -92,16 +109,30 @@ test.describe('Add API Backend', function() {
         CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
         CommonUtils.goToDashboard(driver);
         AddAPIBackendUtil.addNewBackend(driver, {
-            backendName: 'NewBackend',
-            description: 'Test Description',
-            hostName: 'google',
-            portNumber: '80',
-            frontendPrefix: '/test',
-            backendPrefix: '/test'
+            apiName: 'NewBackend',
+            apiDescription: 'Test Description',
+            apiURL: 'google.com'
         });
-        var errorElement = driver.findElement(By.xpath('//*[@class="s-alert-box-inner"]/p'));
+        var errorElement = driver.findElement(By.xpath('//*[@id="addApiForm"]/fieldset/div[3]/span'));
         errorElement.getText().then(function(text){
-            assert.include(text, 'Could not resolve host: no address for google');
+            assert.include(text, 'URL must be a valid URL');
+        });
+    });
+    test.it('8.5 should not add/publish api with no unique name', function() {
+        newUser.added = false;
+        CommonUtils.signIn(driver);
+        CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
+        CommonUtils.goToDashboard(driver);
+        var values = {
+            apiName: 'NewBackend',
+            apiDescription: 'Test Description',
+            apiURL: 'http://google.com'
+        };
+        AddAPIBackendUtil.addNewBackend(driver, values);
+        AddAPIBackendUtil.addNewBackend(driver, values);
+        var errorElement = driver.findElement(By.xpath('//*[@id="addApiForm"]/fieldset/div[1]/span'));
+        errorElement.getText().then(function(text){
+            assert.include(text, 'API Name must be unique');
         });
     });
 });
