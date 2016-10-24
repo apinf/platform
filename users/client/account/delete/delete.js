64f1c2ac03e95753931c5dc169e57cf4df38984e
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-10-24 12:32:29 +0300

    Lint

--- users/client/account/delete/delete.js
+++ users/client/account/delete/delete.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { TAPi18n } from 'meteor/tap:i18n';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+import { Router } from 'meteor/iron:router';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
 Template.deleteAccount.events({
   'click #delete-account-confirm': function () {
