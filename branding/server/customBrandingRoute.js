358e4bd71597b184a8ee0827e48e8d6a8a52182e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-24 14:56:50 +0300

    Initial custom branding css generator

--- branding/server/customBrandingRoute.js
+++ branding/server/customBrandingRoute.js
@@ -1,10 +1,33 @@
+import { Router } from 'meteor/iron:router';
+import { Branding } from '/branding/collection';
+
 Router.route('customStyle', {
   where: 'server',
   path: '/custom.style.css',
   action () {
-    const css = `color: #123`;
+    const branding = Branding.findOne();
+    let styleString;
+    let primaryColor;
+    let primaryColorText;
+
+
+    if (branding && branding.colors && branding.colors.primary) {
+      primaryColor = branding.colors.primary;
+    }
+
+    if (branding && branding.colors && branding.colors.primaryText) {
+      primaryColorText = branding.colors.primaryText;
+    }
+
+    styleString = `
+.main-navigation {
+  background-color: ${primaryColor} !important;
+  color: ${primaryColorText} !important;
+}
+`
+
     const headers = { 'Content-type': 'text/css' };
     this.response.writeHead(200, headers);
-    this.response.end(css);
+    this.response.end(styleString);
   },
 });
