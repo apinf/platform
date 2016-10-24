03e1d8734646280872117c2f055a914edbd56b0c
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-10-24 13:41:21 +0300

    edit, remove DB restrictions

--- feedback/collection/permissions.js
+++ feedback/collection/permissions.js
@@ -1,9 +1,35 @@
+import { Apis } from '/apis/collection';
 import { Feedback } from './';
 
 Feedback.allow({
-  insert (userId, feedback) {
+  insert (userId) {
+    // Allow only registered users to send feedback
     if (userId) {
       return true;
     }
+    // Otherwise reject
+    return false;
+  },
+  update (userId, feedback) {
+    // Check if user is allowed to edit feedback
+    if (feedback && feedback.apiBackendId) {
+      const api = Apis.findOne(feedback.apiBackendId);
+      // Check api exists
+      if (api) {
+        return api.currentUserCanEdit();
+      }
+    }
+    return false;
+  },
+  remove (userId, feedback) {
+    // Check if user is allowed to edit feedback
+    if (feedback && feedback.apiBackendId) {
+      const api = Apis.findOne(feedback.apiBackendId);
+      // Check api exists
+      if (api) {
+        return api.currentUserCanEdit();
+      }
+    }
+    return false;
   },
 });
