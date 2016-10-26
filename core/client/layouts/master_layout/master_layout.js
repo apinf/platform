06ef5843903ddcff318db9d13464051536520b78
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-26 10:44:24 +0300

    Lint

--- core/client/layouts/master_layout/master_layout.js
+++ core/client/layouts/master_layout/master_layout.js
@@ -1,12 +1,12 @@
 import { Branding } from '/branding/collection';
 import { Template } from 'meteor/templating';
 
-Template.masterLayout.created = function () {
+Template.masterLayout.onCreated(function () {
   // Subscription to branding collection
   this.subscribe('branding');
   // Subscribe to project logo
   this.subscribe('projectLogo');
-};
+});
 
 Template.masterLayout.helpers({
   branding () {
