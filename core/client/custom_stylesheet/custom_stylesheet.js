e583ee49f1dd265f1c6cbd14a6e384ae0b2f1c0d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-26 14:19:42 +0300

    Add mostReadableBackground helper

--- core/client/custom_stylesheet/custom_stylesheet.js
+++ core/client/custom_stylesheet/custom_stylesheet.js
@@ -1,7 +1,50 @@
 import { Template } from 'meteor/templating';
 import { Branding } from '/branding/collection';
+const tinycolor = require('tinycolor2');
 
 Template.customStylesheet.helpers({
+  mostReadableBackgound () {
+    // Get branding
+    const branding = Branding.findOne();
+
+    // Placeholder variables
+    let backgroundColor;
+    let textColor;
+    let lighterBackgroundReadability;
+    let darkerBackgroundReadability;
+    let mostReadableBackground;
+
+    if (branding && branding.colors && branding.colors.primary) {
+      // Get background color
+      backgroundColor = branding.colors.primary;
+    }
+
+    if (branding && branding.colors && branding.colors.primaryText) {
+      // Get text color
+      textColor = branding.colors.primaryText;
+    }
+
+    if (backgroundColor && textColor) {
+      // Lighten and darken the background color, for contrast comparison
+      const lighterBackground = tinycolor(backgroundColor).lighten();
+      const darkerBackground = tinycolor(backgroundColor).darken();
+
+      // Get readability values for each combination of background/text color
+      lighterBackgroundReadability = tinycolor.readability(lighterBackground, textColor);
+      darkerBackgroundReadability = tinycolor.readability(darkerBackground, textColor);
+
+      // Select darker or lighter background based on best readability
+      if (lighterBackgroundReadability > darkerBackgroundReadability) {
+        // lighter background is most readable
+        mostReadableBackground = lighterBackground;
+      } else {
+        // darker background is most readable
+        mostReadableBackground = darkerBackground;
+      }
+
+      return mostReadableBackground;
+    }
+  },
   primaryColor () {
     // Get branding
     const branding = Branding.findOne();
