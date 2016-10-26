8ac246527060ee0944668c09335f259e34c3f6fd
Daria Voytova <d.voytova@2glab.com>
2016-10-26 14:12:42 +0300

    fix login test

--- test/logintests.js
+++ test/logintests.js
@@ -3,7 +3,10 @@ var webdriver = require('selenium-webdriver'),
     By = require('selenium-webdriver').By,
     test = require('selenium-webdriver/testing');
 var CommonUtils = require('./common');
-
+var robotUser = {
+    email: 'robot@test.com',
+    psswd: 'robottest'
+};
 
 
 test.describe('Login', function() {
@@ -17,24 +20,24 @@ test.describe('Login', function() {
     });
     test.it('2.1 should login with valid username and password', function() {
         CommonUtils.signIn(driver);
-        CommonUtils.fillSignInForm(driver, 'test@test.test', 'testuser');
+        CommonUtils.fillSignInForm(driver, robotUser.email, robotUser.psswd);
         var userNameElement = CommonUtils.signOut(driver);
         userNameElement.getText().then(function(text) {
-           assert.equal(text, 'testuser'); 
+           assert.include(text, 'seleniumrobot');
         });
     });
     test.it('2.2 should show login forbidden message with invalid username', function() {
         CommonUtils.signIn(driver);
-        CommonUtils.fillSignInForm(driver, 'testName', 'password');
-        var errorElement = driver.findElement(By.xpath('//*[@class="at-error alert alert-danger"]/p'));
+        CommonUtils.fillSignInForm(driver, 'testName', robotUser.psswd);
+        var errorElement = driver.findElement(By.css('.at-error.alert.alert-danger p'));
         errorElement.getText().then(function(text) {
            assert.equal(text, 'Login forbidden'); 
         });
     });
     test.it('2.3 should show login forbidden message with invalid password', function() {
         CommonUtils.signIn(driver);
-        CommonUtils.fillSignInForm(driver, 'test@test.test', 'password');
-        var errorElement = driver.findElement(By.xpath('//*[@class="at-error alert alert-danger"]/p'));
+        CommonUtils.fillSignInForm(driver, robotUser.email, 'password');
+        var errorElement = driver.findElement(By.css('.at-error.alert.alert-danger p'));
         errorElement.getText().then(function(text) {
            assert.equal(text, 'Login forbidden'); 
         });
