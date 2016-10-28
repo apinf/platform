556f6a5c1afb17c1b6e4b113e0dd05b49f0bf641
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-28 12:34:01 +0300

    Add checkIfApiExists

--- apis/server/methods/apiBackends.js
+++ apis/server/methods/apiBackends.js
@@ -1,5 +1,20 @@
 import { Apis } from '/apis/collection';
 
 Meteor.methods({
-  
+  checkIfApiExists (apiId) {
+    // Placeholder for API check
+    let apiExists;
+
+    // Check for API
+    const api = Apis.findOne(apiId);
+
+    // Check for existance of API
+    if (api) {
+      apiExists = true;
+    } else {
+      apiExists = false;
+    }
+
+    return apiExists;
+  }
 });
