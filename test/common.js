1fd9e41651c11de81a69033a5026d889e20e0e76
Daria Voytova <d.voytova@2glab.com>
2016-10-28 11:42:36 +0300

    Add waiting for sign up

--- test/common.js
+++ test/common.js
@@ -30,7 +30,9 @@ var CommonUtils = {
         });
     },
     signUp: function(driver) {
-        driver.get(this.url);
+        driver.get(this.url).then(function () {
+            driver.sleep(1000);
+        });
         driver.findElement(By.linkText('Sign Up')).click();
     },
     fillSignInForm: function(driver, userName, password) {
