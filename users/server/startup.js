6b04bc2c9af670c87052ccc0472ac4bab14f6f72
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-28 15:04:31 +0300

    Simplify; attach login validate always

--- users/server/startup.js
+++ users/server/startup.js
@@ -1,13 +1,8 @@
-import { Settings } from '/settings/collection';
-import { mailSettingsValid } from '/core/helper_functions/validate_settings';
+import { Accounts } from 'meteor/accounts-base';
+import { Meteor } from 'meteor/meteor';
 import { loginAttemptVerifier } from './login_verify';
 
 Meteor.startup(function() {
-  // Get settings
-  const settings = Settings.findOne();
-
-  // Toggle loginAttemptVerifier ON when Mail settings exist to allow first user
-  if( mailSettingsValid(settings) ) {
-    Accounts.validateLoginAttempt(loginAttemptVerifier);
-  }
+  // Toggle attach login attempt verifier function to account login
+  Accounts.validateLoginAttempt(loginAttemptVerifier);
 });
