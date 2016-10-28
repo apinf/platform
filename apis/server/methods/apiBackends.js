1d6cda44213ec48371c42a87442359c9233d25da
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-10-28 12:28:24 +0300

    Move currentUserCanViewApi

--- apis/server/methods/apiBackends.js
+++ apis/server/methods/apiBackends.js
@@ -1,26 +1,5 @@
 import { Apis } from '/apis/collection';
 
 Meteor.methods({
-  currentUserCanViewApi (apiBackendId) {
-    const apiBackend = Apis.findOne(apiBackendId);
-
-    if (apiBackend && apiBackend.currentUserCanView() ) {
-      // User is authorized to view this API
-      return  true;
-    } else {
-      // User is NOT authorized to view this API
-      return false;
-    }
-  },
-  currentUserCanEditApi (apiBackendId) {
-    const apiBackend = Apis.findOne(apiBackendId);
-
-    if (apiBackend && apiBackend.currentUserCanEdit() ) {
-      // User is authorized to view this API
-      return  true;
-    } else {
-      // User is NOT authorized to view this API
-      return false;
-    }
-  }
+  
 });
--- apis/server/methods/authorization.js
+++ apis/server/methods/authorization.js
@@ -19,4 +19,26 @@ Meteor.methods({
       Apis.update(apiId, { $push: { authorizedUserIds: user._id } });
     }
   },
+  currentUserCanViewApi (apiBackendId) {
+    const apiBackend = Apis.findOne(apiBackendId);
+
+    if (apiBackend && apiBackend.currentUserCanView() ) {
+      // User is authorized to view this API
+      return  true;
+    } else {
+      // User is NOT authorized to view this API
+      return false;
+    }
+  },
+  currentUserCanEditApi (apiBackendId) {
+    const apiBackend = Apis.findOne(apiBackendId);
+
+    if (apiBackend && apiBackend.currentUserCanEdit() ) {
+      // User is authorized to view this API
+      return  true;
+    } else {
+      // User is NOT authorized to view this API
+      return false;
+    }
+  },
 });
