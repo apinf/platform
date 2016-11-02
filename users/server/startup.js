5aa84a9a899298fde5dbec3062fb03ad3087e156
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-02 16:23:13 +0200

    Call configure SMTP settings on startup

--- users/server/startup.js
+++ users/server/startup.js
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { loginAttemptVerifier } from './login_verify';
 
 Meteor.startup(() => {
+  // In case of server restart, we need to set MAIL_URL
+  Meteor.call('configureSmtpSettings');
   // Toggle attach login attempt verifier function to account login
   Accounts.validateLoginAttempt(loginAttemptVerifier);
 });
