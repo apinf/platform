759695133b3901e103709e3a582056c258626915
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-07 11:19:08 +0200

    Take away restrictions for only one proxy

--- proxies/collection/permissions.js
+++ proxies/collection/permissions.js
@@ -3,11 +3,6 @@ import { Roles } from 'meteor/alanning:roles';
 
 Proxies.allow({
   insert (userId, proxy) {
-    // Check if proxy is already exist
-    if (Proxies.find().count() >= 1) {
-      return false;
-    }
-
     // Check if user has admin role
     return Roles.userIsInRole(userId, ['admin']);
   },
