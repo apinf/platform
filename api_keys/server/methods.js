09087d3375f5e57f6a7ed53df53b33b784e0d4dd
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-07 16:58:12 +0200

    Add multiproxy support for method

--- api_keys/server/methods.js
+++ api_keys/server/methods.js
@@ -1,37 +1,48 @@
 import { Meteor } from 'meteor/meteor';
 import { ApiKeys } from '/api_keys/collection';
+import { ProxyBackends } from '/proxy_backends/collection';
 import { Proxies } from '/proxies/collection';
 
 Meteor.methods({
-  createApiKey () {
+  createApiKey (apiId) {
     // Get logged in user
     const currentUser = Meteor.user();
     // Check currentUser exists
     if (currentUser) {
-      // TODO: Fix for multi-proxy support
-      const proxy = Proxies.findOne();
+      const proxyBackend = ProxyBackends.findOne({ apiId });
 
-      // Check type & call appropriate function
-      if (proxy && proxy.type === 'apiUmbrella') {
-        // Call Umbrella method to create user with API key
-        Meteor.call('createApiUmbrellaUser', currentUser, (error, umbrellaUser) => {
-          if (error) {
-            console.log(error);
-          } else {
-            // Construct apiKey object
-            const apiKey = {
-              apiUmbrella: {
-                id: umbrellaUser.id,
-                apiKey: umbrellaUser.api_key,
-              },
-              userId: currentUser._id,
-              proxyId: proxy._id,
-            };
+      // Check proxyBackend is defined, and it has proxyId
+      if (proxyBackend && proxyBackend.proxyId) {
+        // Get Proxy by proxyId of proxyBackend
+        const proxyId = proxyBackend.proxyId;
+        const proxy = Proxies.findOne({ proxyId });
 
-            // Insert apiKey
-            ApiKeys.insert(apiKey);
-          }
-        });
+        // Check type & call appropriate function
+        if (proxy && proxy.type === 'apiUmbrella') {
+          // Call Umbrella method to create user with API key
+          Meteor.call('createApiUmbrellaUser', currentUser, (error, umbrellaUser) => {
+            if (error) {
+              console.log(error);
+            } else {
+              // Construct apiKey object
+              const apiKey = {
+                apiUmbrella: {
+                  id: umbrellaUser.id,
+                  apiKey: umbrellaUser.api_key,
+                },
+                userId: currentUser._id,
+                proxyId: proxy._id,
+              };
+
+              // Insert apiKey
+              ApiKeys.insert(apiKey);
+            }
+          });
+        } else {
+          console.log('no proxy, sorry');
+        }
+      } else {
+        console.log('no proxybackend, sorry');
       }
     } else {
       // Meteor Error (User not logged in)
