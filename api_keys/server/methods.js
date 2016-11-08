bf5c221bb017171657d74b835271330a7d0e50e6
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-08 13:29:41 +0200

    Throw errors for client

--- api_keys/server/methods.js
+++ api_keys/server/methods.js
@@ -23,7 +23,13 @@ Meteor.methods({
           // Call Umbrella method to create user with API key
           Meteor.call('createApiUmbrellaUser', currentUser, proxyId, (error, umbrellaUser) => {
             if (error) {
+              // Log error for server
               console.log(error);
+              // Throw apiumbrellauser error for client
+              throw new Meteor.Error(
+                'apinf-apiumbrellauser-error',
+                TAPi18n.__('apinf_apiumbrellauser_error')
+              );
             } else {
               // Construct apiKey object
               const apiKey = {
@@ -40,13 +46,21 @@ Meteor.methods({
             }
           });
         } else {
-          console.log('no proxy, sorry');
+          // Throw no proxy error for client
+          throw new Meteor.Error(
+            'apinf-noproxy-error',
+            TAPi18n.__('apinf_noproxy_error')
+          );
         }
       } else {
-        console.log('no proxybackend, sorry');
+        // Throw no proxybackend error for client
+        throw new Meteor.Error(
+          'apinf-noproxybackend-error',
+          TAPi18n.__('apinf_noproxybackend_error')
+        );
       }
     } else {
-      // Meteor Error (User not logged in)
+      // Throw usernotloggedin error for client
       throw new Meteor.Error(
         'apinf-usernotloggedin-error',
         TAPi18n.__('apinf_usernotloggedin_error')
