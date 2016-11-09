0533ad89d26bb5b8dbb534f8bb5e9f253c40b426
Daria Voytova <d.voytova@2glab.com>
2016-11-09 15:17:48 +0300

    Show button for uploading or deletion button, handlers for them, css

---
+++ branding/cover_photo/client/upload/upload.js
@@ -0,0 +1,69 @@
+// Import meteor packages
+import { Template } from 'meteor/templating';
+import { Mongo } from 'meteor/mongo';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+// Import apinf collections
+import { Branding } from '/branding/collection';
+import CoverPhoto from '/branding/cover_photo/collection';
+
+const uploadingSpinner = new ReactiveVar(false);
+
+Template.uploadCoverPhoto.onCreated(function () {
+  const instance = this;
+
+  // Subscribe to Branding collection
+  instance.subscribe('branding');
+  // Subscribe to Cover Photo collection
+  instance.subscribe('coverPhoto');
+  // Turn off spinner if it was on
+  uploadingSpinner.set(false);
+});
+
+Template.uploadCoverPhoto.helpers({
+  uploadedCoverPhotoFile () {
+    // Get cover photo ID
+    const currentCoverPhotoFileId = this.branding.coverPhotoFileId;
+
+    // Convert to Mongo ObjectID
+    const objectId = new Mongo.Collection.ObjectID(currentCoverPhotoFileId);
+
+    // Check if cover photo file is available
+    return CoverPhoto.findOne(objectId);
+  },
+  spinnerEnabled () {
+    // Get value of spinner
+    return uploadingSpinner.get();
+  },
+});
+
+Template.uploadCoverPhoto.events({
+  'click .delete-cover-photo': function () {
+    // Show confirmation dialog to user
+    const confirmation = confirm(TAPi18n.__('uploadCoverPhoto_confirm_delete'));
+
+    // Check if user clicked "OK"
+    if (confirmation) {
+      // Get cover photo file id from branding
+      const coverPhotoFileId = this.branding.coverPhotoFileId;
+
+      // Convert to Mongo ObjectID
+      const objectId = new Mongo.Collection.ObjectID(coverPhotoFileId);
+
+      // Remove the cover photo object
+      CoverPhoto.remove(objectId);
+
+      // Remove the cover photo file id field
+      Branding.update(this.branding._id, { $unset: { coverPhotoFileId: '' } });
+
+      // Get deletion success message translation
+      const message = TAPi18n.__('uploadCoverPhoto_successfully_deleted');
+
+      // Alert user of successful delete
+      sAlert.success(message);
+    }
+  },
+});
+
+export default uploadingSpinner;
