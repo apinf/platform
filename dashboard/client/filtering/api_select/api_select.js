aa0b18b6c48d289fbf053bc292c63ca09560cb8d
Brylie Christopher Oxley <brylie@amble.fi>
2016-11-09 11:18:12 +0200

    Rename

---
+++ dashboard/client/filtering/api_select/api_select.js
@@ -0,0 +1,31 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import _ from 'lodash'
+
+Template.apiSelectPicker.onRendered(function () {
+
+  const instance = this;
+
+  instance.selectPickerElement = $('#api-frontend-prefix');
+
+  // Initialize select picker widget
+  instance.selectPickerElement.selectpicker({});
+
+});
+
+Template.apiSelectPicker.helpers({
+  apiUmbrellaOption () {
+    // Get current user Id
+    const userId = Meteor.userId();
+    // Check if current user has admin privileges
+    if (Roles.userIsInRole(userId, ['admin'])) {
+      return {
+        name: 'Proxy Admin API',
+        prefix: '/api-umbrella/'
+      }
+    }
+
+    return {};
+  }
+});
