1e7e381d55cb792297881796f6409e8a876401f4
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-14 16:57:22 +0200

    add event handler for feedbackForm

---
+++ feedback/client/feedback.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
+
+Template.apiFeedback.events({
+  'click #add-feedback': function () {
+    // Show feedbackForm modal
+    Modal.show('feedbackForm');
+  },
+});
