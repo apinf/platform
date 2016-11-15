b434bf186064fe1c29ff8bde85ff89899d421124
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-15 17:55:16 +0200

    add collection helper & use it, some linting involved

--- feedback/collection/helpers.js
+++ feedback/collection/helpers.js
@@ -1,7 +1,9 @@
-import { Feedback } from './';
+import { Meteor } from 'meteor/meteor';
+import { Apis } from '/apis/collection';
 import { FeedbackVotes } from '/feedback_votes/collection';
-
 import ss from 'simple-statistics';
+import _ from 'underscore';
+import { Feedback } from './';
 
 Feedback.helpers({
   sumOfVotes () {
@@ -9,12 +11,28 @@ Feedback.helpers({
     const allFeedback = FeedbackVotes.find({ feedbackId: this._id }).fetch();
 
     // Create a list of all feedback vote values
-    const votes = _(allFeedback).map(function (feedback) {
+    const votes = _(allFeedback).map((feedback) => {
       return feedback.vote;
     });
 
     // Calculate the sum of all vote values
     const sum = ss.sum(votes);
     return sum;
+  },
+  currentUserCanEdit () {
+    // Get current userId
+    const userId = Meteor.userId();
+    /* 1. Author is allowed to edit feedback */
+    if (this.authorId === userId) {
+      return true;
+    }
+    /* 2. Users that can edit API are allowed to edit feedback */
+    // Get API by feedback's apiBackendId
+    const api = Apis.findOne(this.apiBackendId);
+    if (api && api.currentUserCanEdit()) {
+      return true;
+    }
+    // Otherwise not allowed to edit feedback
+    return false;
   },
 });
