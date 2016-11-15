fb325ffedb7422fa4e9a12b3402b298cf440084e
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-15 16:01:04 +0200

    Deprecate separate edit form

--- backlog/client/edit/edit.js
+++
@@ -1,8 +0,0 @@
-import { ApiBacklogItems } from '../../collection';
-
-Template.editBacklogItem.helpers({
-  apiBacklogItemsCollection () {
-    // Return a reference to ApiBacklogItems collection
-    return ApiBacklogItems;
-  }
-});
