59b691b788ba79485a2766597a977448fbf4a659
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-15 13:38:57 +0200

    allow remove for users with edit permissions of current API

--- feedback_votes/collection/permissions.js
+++ feedback_votes/collection/permissions.js
@@ -1,3 +1,5 @@
+import { Apis } from '/apis/collection';
+import { Feedback } from '/feedback/collection';
 import { FeedbackVotes } from './';
 
 FeedbackVotes.allow({
@@ -17,11 +19,23 @@ FeedbackVotes.allow({
     return false;
   },
   remove (userId, feedbackVote) {
-    // only allow user to remove own vote
-    if (userId && feedbackVote && feedbackVote.userId &&
-      userId === feedbackVote.userId) {
-      return true;
+    if (feedbackVote) {
+      /* 1. Allow user to remove own vote */
+      if (userId && feedbackVote.userId &&
+        userId === feedbackVote.userId) {
+        return true;
+      }
+      /* 2. Allow admins & API owners/managers remove votes */
+      // Get feedback by feedbackId of the feedback vote
+      const feedback = Feedback.findOne(feedbackVote.feedbackId);
+      if (feedback) {
+        // Get api by feedback apiBackendId
+        const api = Apis.findOne(feedback.apiBackendId);
+        // Return true/false based on edit permissions of api
+        return (api && api.currentUserCanEdit());
+      }
     }
+    // Otherwise deny remove vote
     return false;
   },
 });
