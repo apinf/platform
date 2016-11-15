87978e38b025fa05e7dd35378cb57cbdb270c4d1
Daria Voytova <d.voytova@2glab.com>
2016-11-15 17:29:11 +0300

    Update type for proxy id in schema

--- proxy_backends/collection/schema.js
+++ proxy_backends/collection/schema.js
@@ -1,9 +1,21 @@
+import { Proxies } from '/proxies/collection';
 import { ProxyBackends } from './';
 import { ApiUmbrellaSchema } from './apiUmbrellaSchema';
 
 ProxyBackends.schema = new SimpleSchema({
-  'proxyId': {
+  proxyId: {
     type: String,
+    optional: true,
+    autoform: {
+      options: function () {
+        return _.map(Proxies.find().fetch(), function (proxy) {
+          return {
+            label: proxy.name,
+            value: proxy._id,
+          };
+        });
+      },
+    },
   },
   'apiId': {
     type: String,
