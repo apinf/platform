b1fb2126f99b3125dc32fabd2746263b0ecdddea
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-11-15 15:28:32 +0200

    Bind granularity and URL parameter

--- dashboard/client/filtering/granularity/granularity.js
+++ dashboard/client/filtering/granularity/granularity.js
@@ -1,7 +1,30 @@
 import { Template } from 'meteor/templating';
 
+Template.granularity.onRendered(function () {
+  // Get reference to template instance
+  const instance = this;
+
+// Check URL parameters for granularity
+  const granularityParameter = UniUtils.url.getQuery('granularity');
+
+  if (granularityParameter) {
+    // Set the granularity UI state from URL parameter
+    instance.$('[name="granularity"]').val([granularityParameter]);
+  } else {
+    // Get granularity from template
+    const granularity = instance.$('[name=granularity]:checked').val();
+
+    // Set granularity URL parameter from template value
+    UniUtils.url.setQuery('granularity', granularity);
+  }
+});
+
 Template.granularity.events({
   'change #date-granularity-selector': function (event) {
-    console.log(event.target.value);
+    // Get granularity value
+    const granularity = event.target.value;
+
+    // Set granularity URL parameter
+    UniUtils.url.setQuery('granularity', granularity);
   },
 });
