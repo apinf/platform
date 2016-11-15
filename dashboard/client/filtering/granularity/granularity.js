b684d209d04388a9586f7fdf2d9a808820986c88
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-11-15 14:34:53 +0200

    Lint

--- dashboard/client/filtering/granularity/granularity.js
+++ dashboard/client/filtering/granularity/granularity.js
@@ -1,5 +1,7 @@
+import { Template } from 'meteor/templating';
+
 Template.granularity.events({
-  'change #date-granularity-selector' (event, templateInstance) {
+  'change #date-granularity-selector': function (event) {
     console.log(event.target.value);
-  }
+  },
 });
