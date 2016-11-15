c700afbedcd7f08b4ab9797f7f387665eddbcd50
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-11-15 17:53:57 +0200

    error check, lint

--- feedback/server/methods.js
+++ feedback/server/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
 // Collection imports
 import { FeedbackVotes } from '/feedback_votes/collection';
 import { Feedback } from '../collection';
@@ -11,38 +12,42 @@ Meteor.methods({
     Feedback.remove(feedbackItemId);
   },
   submitVote (feedbackId, vote) {
-    // Get current User ID
+    // Get current user ID
     const userId = Meteor.userId();
 
-    // Get feedback vote for current User / Feedback ID
-    const userVote = FeedbackVotes.findOne({ feedbackId, userId });
+    // Check user is loggedin
+    if (userId) {
+      // Get feedback vote for current User / Feedback ID
+      const userVote = FeedbackVotes.findOne({ feedbackId, userId });
 
-    // If user has voted
-    if (userVote) {
-      // Get existing vote value
-      const existingVote = userVote.vote;
+      // If user has voted
+      if (userVote) {
+        // Get existing vote value
+        const existingVote = userVote.vote;
 
-      // If the existing vote is not same as submitted vote, update users vote.
-      if (vote !== existingVote) {
-        // Update existing vote, replacing the existing value with new value
-        FeedbackVotes.update({
-          feedbackId,
-          userId: Meteor.userId(),
-        }, {
-          $set: { vote },
-        });
-
-      // Otherwise cancel/remove the vote.
+        // If the existing vote is not same as submitted vote, update users vote.
+        if (vote !== existingVote) {
+          // Update existing vote, replacing the existing value with new value
+          FeedbackVotes.update({
+            feedbackId,
+            userId,
+          }, {
+            $set: { vote },
+          });
+        // Otherwise cancel/remove the vote.
+        } else {
+          FeedbackVotes.remove(userVote._id);
+        }
       } else {
-        FeedbackVotes.remove(userVote._id);
+        // User has not voted -> add new user vote
+        FeedbackVotes.insert({ feedbackId, userId, vote });
       }
     } else {
-      // User has not voted -> add new user vote
-      FeedbackVotes.insert({
-        feedbackId,
-        userId: Meteor.userId(),
-        vote,
-      });
+      // Throw usernotloggedin error for client
+      throw new Meteor.Error(
+        'apinf-usernotloggedin-error',
+        TAPi18n.__('apinf_usernotloggedin_error')
+      );
     }
   },
 });
