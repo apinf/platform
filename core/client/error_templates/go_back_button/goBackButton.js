7b69d231bcabb107000c1856d817906b943660bb
illya <in11092008@gmail.com>
2016-11-17 12:08:37 +0200

    go back button template

--- core/client/error_templates/go_back_button/goBackButton.js
+++ core/client/error_templates/go_back_button/goBackButton.js
@@ -1,8 +1,8 @@
 import { Template } from 'meteor/templating';
 import $ from 'jquery';
 
-Template.errorsLayout.onRendered(() => {
-  $('.go-back').click(() => {
+Template.goBackButton.onRendered(() => {
+  $('#go-back').click(() => {
     history.go(-1);
     return false;
   });
