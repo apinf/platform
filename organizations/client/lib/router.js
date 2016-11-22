2d47b6ca1e667709de25f8a82751d0334b6229c9
illya <in11092008@gmail.com>
2016-11-22 16:07:31 +0200

    imports

--- organizations/client/lib/router.js
+++ organizations/client/lib/router.js
@@ -1,3 +1,6 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
 FlowRouter.route('/organization/:slug', {
   name: 'singleOrganization',
   action (params) {
