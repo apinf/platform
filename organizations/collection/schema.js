fe5affeac3e4f2c51f569bc6947ac0d8c3c288e5
Damir Mustafin <frenchthebread@gmail.com>
2016-11-22 15:21:18 +0200

    Move field

--- organizations/collection/schema.js
+++ organizations/collection/schema.js
@@ -3,10 +3,6 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Organizations } from './';
 
 Organizations.schema = new SimpleSchema({
-  organizationLogoFileId: {
-    type: String,
-    optional: true,
-  },
   name: {
     type: String,
   },
@@ -20,6 +16,10 @@ Organizations.schema = new SimpleSchema({
     autoform: {
       rows: 3,
     },
+    optional: true,
+  },
+  organizationLogoFileId: {
+    type: String,
     optional: true,
   },
   contactPerson: {
