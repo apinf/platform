5094deb78d5f406bb3a1e95f8f303e16d8279f96
illya <in11092008@gmail.com>
2016-11-29 13:28:12 +0200

    remove singleOrganization route

--- organizations/client/lib/router.js
+++ organizations/client/lib/router.js
@@ -1,13 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
-FlowRouter.route('/organization/:slug', {
-  name: 'singleOrganization',
-  action () {
-    BlazeLayout.render('masterLayout', { main: 'singleOrganization' });
-  },
-});
-
 FlowRouter.route('/organization/new', {
   name: 'addOrganization',
   action () {
