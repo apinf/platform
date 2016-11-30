633456c2f791393e8f629deef22c9acf2aa57a08
Daria Voytova <d.voytova@2glab.com>
2016-11-30 17:15:04 +0300

    Remove the useless methods, create getElasticQuery

--- elasticsearch/server/methods.js
+++ elasticsearch/server/methods.js
@@ -1,49 +1,94 @@
 import { Meteor } from 'meteor/meteor';
 import ElasticSearch from 'elasticsearch';
-// Collection import
-import { Proxies } from '/proxies/collection';
-
 
 Meteor.methods({
-  getElasticSearchData (opts, proxyId) {
-    // Check if user is authorised
-    if (Meteor.user()) {
-      const host = Meteor.call('getElasticsearchUrl', proxyId);
+  getElasticSearchData (proxyData, analyticasData) {
+    let params;
+    // TODO: Add case when we have MQQT protocol
 
-      // Init ES client
-      const esClient = new ElasticSearch.Client({ host });
-
-      // Get elasticsearch data and return
-      return esClient.search(opts).then((res) => {
-        return res;
-      }, (err) => {
-        throw new Meteor.Error(err.message);
-      });
+    // Constructs the Elastic Search query object
+    switch (proxyData.proxyType) {
+      case 'apiUmbrella':
+        params = Meteor.call('getElasticQuery', proxyData.frontendPrefix, analyticasData);
+        break;
+      default:
+        throw new Meteor.Error('Unknow proxy type');
     }
 
-    throw new Meteor.Error('User is not authorised.');
+    // Save the Elastic Search url
+    const host = proxyData.elasticSearchUrl;
+    // Init ES client
+    const esClient = new ElasticSearch.Client({ host });
+
+    // Makes the HTTP request
+    // Get Elastic Search data and return
+    return esClient.search(params).then((res) => res, (err) => {
+      // Throw an error
+      throw new Meteor.Error(err.message);
+    });
   },
-  elasticsearchIsDefined (proxyId) {
-    // Get Proxy instance of ID
-    const proxy = Proxies.findOne(proxyId);
-    // Check of existing this proxy instance
-    if (proxy) {
-      // Get Elastic Search URL
-      const elasticsearch = proxy.apiUmbrella.elasticsearch;
+  // Constructs the Elastic Search query object for apiUmbrella
+  getElasticQuery (frontendPrefix, analyticasData) {
+    // Construct parameters for Elastic Search
+    // TODO: For case "Proxy Admin API" with prefix /api-umbrella/
+    // TODO: Values aggregation associated with granularity
+    const params = {
+      size: 50000,
+      body: {
+        query: {
+          filtered: {
+            query: {
+              bool: {
+                should: [
+                  {
+                    wildcard: {
+                      request_path: {
+                        // Add '*' to partially match the url
+                        value: `${frontendPrefix}*`,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+            filter: {
+              range: {
+                request_at: {},
+              },
+            },
+          },
+        },
+        sort: [
+          {
+            request_at: {
+              order: 'desc',
+            },
+          },
+        ],
+        fields: [
+          'request_at',
+          'response_status',
+          'response_time',
+          'request_ip_country',
+          'request_ip',
+          'request_path',
+          'user_id',
+        ],
+      },
+    };
 
-      // Return true or false, depending on whether elasticsearch is defined
-      return (elasticsearch);
+    // Update query parameters for date range, when provided
+    if (analyticasData.analyticsFrom) {
+      // Set start date (greater than or equal to) for analytics timeframe
+      params.body.query.filtered.filter.range.request_at.gte = analyticasData.analyticsFrom;
     }
 
-    return false;
-  },
-  getElasticsearchUrl (proxyId) {
-    // Check existing of Elastci Search URL
-    if (Meteor.call('elasticsearchIsDefined', proxyId)) {
-      // Return URL
-      return Proxies.findOne(proxyId).apiUmbrella.elasticsearch;
+    // Update query parameters for date range, when provided
+    if (analyticasData.analyticsTo) {
+      // Set end date (less than or equal to) for analytics timeframe
+      params.body.query.filtered.filter.range.request_at.lte = analyticasData.analyticsTo;
     }
 
-    throw new Meteor.Error('Elasticsearch is not defined');
+    return params;
   },
 });
