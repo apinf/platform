3ae421ca66b576a8792a975dbc479c6221afe906
Daria Voytova <d.voytova@2glab.com>
2016-12-01 14:29:31 +0300

    Add case for eMQTT protocol

--- dashboard/server/methods.js
+++ dashboard/server/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
 // Collection import
 import { Proxies } from '/proxies/collection';
 import { ProxyBackends } from '/proxy_backends/collection';
@@ -14,28 +15,39 @@ Meteor.methods({
 
       const response = {};
 
-      // TODO: Change the condition of choose the proxy type when we have MQQT protocol
-      // Check existing of proxy, existing of proxy type, existing of elastic search url
-      if (proxy && proxy._id && proxy.apiUmbrella && proxy.apiUmbrella.elasticsearch) {
-        // save id
-        response.id = proxy._id;
+      // Check existing of proxy, existing of proxy type
+      if (proxy && proxy.type) {
         // save type
-        response.proxyType = 'apiUmbrella';
-        // save fronted prefix
-        response.frontendPrefix = proxyBackend.apiUmbrella.url_matches[0].frontend_prefix;
-        // save url
-        response.elasticSearchUrl = proxy.apiUmbrella.elasticsearch;
+        response.proxyType = proxy.type;
 
-        // return proxy data
+        switch (proxy.type) {
+          case 'apiUmbrella':
+            // save fronted prefix
+            response.frontendPrefix = proxyBackend.apiUmbrella.url_matches[0].frontend_prefix;
+            // save url
+            response.elasticSearchUrl = proxy.apiUmbrella.elasticsearch;
+            break;
+          case 'emqtt':
+            // TODO: Add fronted prefix if eMQTT protocol has it
+            // save url
+            response.elasticSearchUrl = proxy.emqtt.elasticsearch;
+            break;
+          default:
+            // Create & throw message about error
+            const message = TAPi18n.__('dashboard_errorMessage_unknownProxyType');
+            throw new Meteor.Error(message);
+        }
+
+        // Return proxy data
         return response;
       }
 
       // Create & throw message about error
-      const message = `No one proxy with ID ${proxyBackend.proxyId} was found`;
+      const message = TAPi18n.__('dashboard_errorMessage_noProxyFound');
       throw new Meteor.Error(message);
     }
     // Create & throw message about error
-    const message = `No one proxy backend configuration with ID ${proxyBackendId} was found`;
+    const message = TAPi18n.__('dashboard_errorMessage_noProxybackendFound');
     throw new Meteor.Error(message);
   },
 });
--- elasticsearch/server/methods.js
+++ elasticsearch/server/methods.js
@@ -1,18 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
 import ElasticSearch from 'elasticsearch';
 
 Meteor.methods({
-  getElasticSearchData (proxyData, analyticasData) {
+  getElasticSearchData (proxyData, analyticsData) {
     let params;
-    // TODO: Add case when we have MQQT protocol
 
     // Constructs the Elastic Search query object
     switch (proxyData.proxyType) {
       case 'apiUmbrella':
-        params = Meteor.call('getElasticQuery', proxyData.frontendPrefix, analyticasData);
+        params = Meteor.call('getElasticQueryUmbrella', proxyData.frontendPrefix, analyticsData);
+        break;
+      case 'emqtt':
+        // TODO: Add fronted prefix parameter if eMQTT protocol has it
+        params = Meteor.call('getElasticQueryEmqtt');
         break;
       default:
-        throw new Meteor.Error('Unknow proxy type');
+        const message = TAPi18n.__('dashboard_errorMessage_unknownProxyType');
+        throw new Meteor.Error(message);
     }
 
     // Save the Elastic Search url
@@ -28,7 +33,7 @@ Meteor.methods({
     });
   },
   // Constructs the Elastic Search query object for apiUmbrella
-  getElasticQuery (frontendPrefix, analyticasData) {
+  getElasticQueryUmbrella (frontendPrefix, analyticsData) {
     // Construct parameters for Elastic Search
     // TODO: For case "Proxy Admin API" with prefix /api-umbrella/
     // TODO: Values aggregation associated with granularity
@@ -78,17 +83,21 @@ Meteor.methods({
     };
 
     // Update query parameters for date range, when provided
-    if (analyticasData.analyticsFrom) {
+    if (analyticsData.analyticsFrom) {
       // Set start date (greater than or equal to) for analytics timeframe
-      params.body.query.filtered.filter.range.request_at.gte = analyticasData.analyticsFrom;
+      params.body.query.filtered.filter.range.request_at.gte = analyticsData.analyticsFrom;
     }
 
     // Update query parameters for date range, when provided
-    if (analyticasData.analyticsTo) {
+    if (analyticsData.analyticsTo) {
       // Set end date (less than or equal to) for analytics timeframe
-      params.body.query.filtered.filter.range.request_at.lte = analyticasData.analyticsTo;
+      params.body.query.filtered.filter.range.request_at.lte = analyticsData.analyticsTo;
     }
 
     return params;
+  },
+  // TODO: Add right constructor
+  getElasticQueryEmqtt () {
+    return { size: 1000 };
   },
 });
