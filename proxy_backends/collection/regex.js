f15d1ae7b4aa09e1ab282d54cb413f8f4bdf8d97
Santeri Vesalainen <santeri.vesalainen@gmail.com>
2016-12-03 00:46:19 +0200

    Added apiBasePathLooseRegEx
    To match with service call like
    URL:
    https://html-to-json.herokuapp.com
    apiBasePath:
    /jquery?site=https://www.tsr.fi/uudet-hanketiedotteet&selector=find(%27.list-items%20li%27)&forceText=true

--- proxy_backends/collection/regex.js
+++ proxy_backends/collection/regex.js
@@ -1,3 +1,5 @@
 export const proxyBasePathRegEx = new RegExp(/^\/[a-z0-9A-Z_\-\/]+\/$/);
 
 export const apiBasePathRegEx = new RegExp(/^\/[a-z0-9A-Z_\-\/]*$/);
+
+export const apiBasePathLooseRegEx = new RegExp(/^\/[a-z0-9A-Z-?=.&%()_\-\/]*$/);
