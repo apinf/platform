1560596431c78e8a3ce8809609d05d724738b7a8
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-05 16:28:08 +0200

    Update helpers

--- organizations/logo/view/client/view.js
+++ organizations/logo/view/client/view.js
@@ -1,37 +1,39 @@
-import { ApiLogo } from '/apis/logo/collection/collection';
-import { Apis } from '/apis/collection';
+import { Template } from 'meteor/templating';
+import { OrganizationLogo } from '/organizations/logo/collection/collection';
 
-Template.viewApiLogo.onCreated(function () {
+Template.viewOrganizationLogo.onCreated(function () {
   const instance = this;
   // Subscribe to API logo
-  instance.subscribe('allApiLogo');
+  instance.subscribe('allOrganizationLogo');
 });
 
-Template.viewApiLogo.helpers({
-  uploadedApiLogoLink () {
-    // Get API current API Backend from template data
-    const api = Template.currentData().api;
+Template.viewOrganizationLogo.onRendered(function () {
+  // Assign resumable browse to element
+  OrganizationLogo.resumable.assignBrowse(this.$('.organization-file-browse'));
+});
 
-    if (api && api.apiLogoFileId) {
-      const apiLogoFileId = api.apiLogoFileId;
+Template.viewOrganizationLogo.helpers({
+  uploadedOrganizationLogoLink () {
+    // Get API current API Backend from template data
+    const organization = Template.currentData().organization;
+    if (organization && organization.organizationLogoFileId) {
+      const organizationLogoFileId = organization.organizationLogoFileId;
 
       // Convert to Mongo ObjectID
-      const objectId = new Mongo.Collection.ObjectID(apiLogoFileId);
-
+      const objectId = new Mongo.Collection.ObjectID(organizationLogoFileId);
       // Get API logo file Object
-      const apiLogoFile = ApiLogo.findOne(objectId);
-
+      const organizationLogoFile = OrganizationLogo.findOne(objectId);
       // Check if API logo file is available
-      if (apiLogoFile) {
+      if (organizationLogoFile) {
         // Get API logo file URL
-        return Meteor.absoluteUrl().slice(0, -1) + ApiLogo.baseURL + '/md5/' + apiLogoFile.md5;
+        return `${Meteor.absoluteUrl().slice(0, -1) + OrganizationLogo.baseURL}/md5/${organizationLogoFile.md5}`;
       }
     }
   },
-  apiLogoExists () {
-    const api = Template.currentData().api;
+  organizationLogoExists () {
+    const organization = Template.currentData().organization;
 
-    if (api && api.apiLogoFileId) {
+    if (organization && organization.organizationLogoFileId) {
       return true;
     }
   },
