1f2ac1f7eb35efcc277685a53a6a300d42d2cf4a
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-05 16:24:55 +0200

    Remove upload folder & files

--- organizations/logo/upload/client/uploadButton/uploadButton.js
+++
@@ -1,7 +0,0 @@
-import { Template } from 'meteor/templating';
-import { OrganizationLogo } from '/organizations/logo/collection/collection';
-
-Template.uploadOrganizationLogoButton.onRendered(function() {
-  // Assign resumable browse to element
-  OrganizationLogo.resumable.assignBrowse($('.fileBrowse'));
-});
