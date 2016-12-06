03d75d2d1737a7601db4f98c2eca25ea03cc61f4
Daria Voytova <d.voytova@2glab.com>
2016-12-06 14:46:05 +0300

    Publication of organization collection & pagination

--- organizations/collection/server/publications.js
+++ organizations/collection/server/publications.js
@@ -2,3 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { Organizations } from '../';
 
 Meteor.publish('singleOrganization', (slug) => Organizations.find({ slug }));
+
+Meteor.publish('allOrganization', () => {
+  return Organizations.find();
+});
+
+// Publish collection for pagination
+new Meteor.Pagination(Organizations);
