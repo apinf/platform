5bc66a2300de77009c921f1859e0c488e3e09e5f
illya <in11092008@gmail.com>
2016-12-07 16:25:40 +0200

    Move to own if statement

--- catalogue/client/catalogue.js
+++ catalogue/client/catalogue.js
@@ -93,21 +93,26 @@ Template.catalogue.onRendered(function () {
   // Check URL parameter for sorting
   const sortByParameter = FlowRouter.getQueryParam('sortBy');
 
-  if (filterByParameter && sortByParameter) {
+  if (filterByParameter) {
     // Set the filter by UI state from URL parameter
     instance.$(`#filterBy-${filterByParameter}`).button('toggle');
-
-    // Set the sorting by UI state from URL parameter
-    instance.$('#sort-select').val(`${sortByParameter}`).change();
   } else {
     // Get filtering from template
     const filterBy = instance.$('[name=filter-options]:checked').val();
 
+    // Set filtering URL parameter from template value
+    FlowRouter.setQueryParams({ filterBy });
+  }
+
+  if (sortByParameter) {
+    // Set the sorting by UI state from URL parameter
+    instance.$('#sort-select').val(`${sortByParameter}`).change();
+  } else {
     // Get sorting from template
     const sortBy = instance.$('[name=sort-menu]').val();
 
-    // Set filtering and sorting URL parameters from template value
-    FlowRouter.setQueryParams({ filterBy, sortBy });
+    // Set sorting URL parameter from template value
+    FlowRouter.setQueryParams({ sortBy });
   }
 
   // Activate tooltips on all relevant items
