985b6131ff8c231a1f9f0aa3e6f86cf34255e2ab
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-08 10:40:58 +0200

    client side not needed, typo in apiPath

--- rest_api/client/lib/router.js
+++
@@ -1,8 +0,0 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-
-FlowRouter.route('/rest-api/v1/*', {
-  name: 'rest-api',
-  action () {
-    // TODO: find out how to pass through to json-routes
-  },
-});
