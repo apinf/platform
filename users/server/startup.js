0ae18ae00ac9da4f71952a94141d9149ecc15115
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-12 16:13:46 +0200

    configure login expiration to 1 day

--- users/server/startup.js
+++ users/server/startup.js
@@ -3,6 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { loginAttemptVerifier } from './login_verify';
 
 Meteor.startup(() => {
+  // Configure accounts
+  Accounts.config({
+    loginExpirationInDays: 1,
+  });
   // In case of server restart, we need to set MAIL_URL
   Meteor.call('configureSmtpSettings');
   // Toggle attach login attempt verifier function to account login
