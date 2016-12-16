013f932fed7fa77b331d57bec5087ab000f5ea8e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-12-16 14:52:19 +0200

    Add 'editMode' reactive variable

--- apis/client/view/lifecycle_status/lifecycle_status.js
+++ apis/client/view/lifecycle_status/lifecycle_status.js
@@ -1,5 +1,13 @@
 import { Template } from 'meteor/templating';
 
+Template.apiLifecycleStatus.onCreated(function () {
+  // Get reference to template instance
+  const templateInstance = Template.instance();
+
+  // Track edit mode with reactive variable (initially false)
+  templateInstance.editMode = new ReactiveVar(false);
+});
+
 Template.apiLifecycleStatus.helpers({
   lifecycleStatus () {
     // Get reference to template instahce
