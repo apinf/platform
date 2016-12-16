467d244253dbc0ff3ac97d6023c63c14f8db084f
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-12-16 16:56:04 +0200

    Add labelType header

--- apis/client/view/lifecycle_status/lifecycle_status.js
+++ apis/client/view/lifecycle_status/lifecycle_status.js
@@ -21,6 +21,38 @@ Template.apiLifecycleStatus.helpers({
     // Return value of edit mode reactive variable
     return templateInstance.editMode.get();
   },
+  labelType () {
+    // Get reference to template instance
+    const templateInstance = Template.instance();
+
+    // Placeholder for label type
+    let labelType;
+
+    // Get API lifecycle status from template instance
+    const lifecycleStatus = templateInstance.data.api.lifecycleStatus;
+    console.log(lifecycleStatus);
+    // Get label type based on lifecycle status
+    switch (lifecycleStatus) {
+      case 'development':
+        labelType = 'primary';
+        break;
+      case 'production':
+        labelType = 'success';
+        break;
+      case 'design':
+        labelType = 'info';
+        break;
+      case 'testing':
+        labelType = 'warning';
+        break;
+      case 'deprecated':
+        labelType = 'danger';
+        break;
+      default:
+        labelType = 'default';
+    }
+    return labelType;
+  },
   lifecycleStatus () {
     // Get reference to template instahce
     const templateInstance = Template.instance();
