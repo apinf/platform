178830066ff48d8b8440f7a8fc6246e16b0aa82b
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-12-16 14:23:53 +0200

    Use api.lifecycleStatus if available

--- apis/client/view/lifecycle_status/lifecycle_status.js
+++ apis/client/view/lifecycle_status/lifecycle_status.js
@@ -2,6 +2,18 @@ import { Template } from 'meteor/templating';
 
 Template.apiLifecycleStatus.helpers({
   lifecycleStatus () {
-    return 'Unknown';
+    // Get reference to template instahce
+    const instance = Template.instance();
+
+    let statusText;
+
+    if (instance.api && instance.api.lifecycleStatus) {
+      console.log(lifecycleStatus);
+      statusText = instance.api.lifecycleStatus;
+    } else {
+      statusText = 'Unknown';
+    }
+
+    return statusText;
   },
 });
