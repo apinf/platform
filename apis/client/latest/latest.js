7bac0743e5d41cd5b8cd19d6a5d16506e0f9b90a
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-12-22 12:24:14 +0200

    Simplify reactive data handling

--- apis/client/latest/latest.js
+++ apis/client/latest/latest.js
@@ -20,27 +20,14 @@ Template.latestApis.created = function () {
   // Attach cursor function to a template instance
   instance.latestApiBackendsCursor = function () {
     // Get a cursor for API Backends documents limited by provided value and sorted by created date
-    return Apis.find({}, { sort: { created_at: -1}, limit: limit });
+    return ;
   }
 
 };
 
 Template.latestApis.helpers({
   'latestApis': function (limit) {
-
-    // Reference to Template instance
-    var instance = Template.instance();
-
     // Retrieve last API Backends
-    var latestApiBackendsList = instance.latestApiBackendsCursor().fetch();
-
-    // Iterate through all documents
-    _.each(latestApiBackendsList, function (apiBackend) {
-
-      // Return to user human-readable timestamp
-      apiBackend.relative_created_at = moment(apiBackend.created_at).fromNow();
-    });
-
-    return latestApiBackendsList;
+    return Apis.find({}, { sort: { created_at: -1}}).fetch();
   }
 });
