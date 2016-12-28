d4be2d631b0cf7e1b0c61416295e67b97adaf20f
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-28 14:30:15 +0200

    remove orgApis publication, lint

--- organizations/collection/server/publications.js
+++ organizations/collection/server/publications.js
@@ -3,27 +3,20 @@ import { Organizations, OrganizationApis } from '../';
 
 Meteor.publish('singleOrganization', (slug) => Organizations.find({ slug }));
 
-Meteor.publish('allOrganizationBasicDetails', () => {
-  return Organizations.find({},
-    { fields: {
-      _id: 1,
-      name: 1,
-      description: 1,
-      contact: 1,
-    },
-    });
-});
+Meteor.publish('allOrganizationBasicDetails', () => Organizations.find({},
+  { fields: {
+    _id: 1,
+    name: 1,
+    description: 1,
+    contact: 1,
+  },
+  }));
 
 // Publish collection for pagination
 new Meteor.Pagination(Organizations);
 
-Meteor.publish('managedOrganizationsBasicDetails', (userId) => {
-  return Organizations.find(
-    { managerIds: userId },
-    { fields:
+Meteor.publish('managedOrganizationsBasicDetails', (userId) => Organizations.find(
+  { managerIds: userId },
+  { fields:
       { _id: 1, name: 1, description: 1, contact: 1 },
-    });
-});
-
-// TODO: Remove it
-Meteor.publish('orgApis', () => OrganizationApis.find());
+  }));
