b440da371494f746b91cc5a0dcfc1c93b17a0787
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-28 16:59:12 +0200

    fetch organization options, add subscriptions

--- organization_apis/client/form.js
+++ organization_apis/client/form.js
@@ -1,5 +1,15 @@
 import { Template } from 'meteor/templating';
+import { Organizations } from '/organizations/collection';
+import _ from 'lodash';
+
 import { OrganizationApis } from '../collection';
+
+Template.organizationApis.onCreated(function () {
+  const instance = this;
+
+  instance.subscribe('myManagedApis');
+  instance.subscribe('managedOrganizationsBasicDetails');
+});
 
 Template.organizationApis.helpers({
   organizationApisCollection () {
@@ -21,13 +31,14 @@ Template.organizationApis.helpers({
     return organizationApisDoc;
   },
   organizationOptions () {
-    return [
-        { label: '2013', value: 2013 },
-        { label: '2014', value: 2014 },
-        { label: '2015', value: 2015 },
-    ];
+    const organizations = _.map(Organizations.find().fetch(), (organization) => ({
+      label: organization.name,
+      value: organization._id,
+    }));
+    return organizations;
   },
   apiOptions () {
+    // TODO: list myManagedApis
     return [
         { label: '2013', value: 2013 },
         { label: '2014', value: 2014 },
