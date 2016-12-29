23b946e58cd2b5fd39836ff6764cbd476383892e
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-12-29 14:04:35 +0200

    helper & event from apiSettingsDetails

--- organization_apis/client/form.js
+++ organization_apis/client/form.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Organizations } from '/organizations/collection';
+import { ApiMetadata } from '/metadata/collection';
 import _ from 'lodash';
 
 import { OrganizationApis } from '../collection';
@@ -14,6 +15,11 @@ Template.organizationApis.onCreated(function () {
 Template.organizationApis.helpers({
   organizationApisCollection () {
     return OrganizationApis;
+  },
+  userIsOrganizationManager () {
+    const organizationsCount = Organizations.find().count();
+
+    return organizationsCount > 0;
   },
   organizationApisDoc () {
     let organizationApisDoc;
@@ -44,5 +50,52 @@ Template.organizationApis.helpers({
         { label: '2014', value: 2014 },
         { label: '2015', value: 2015 },
     ];
+  },
+});
+
+Template.organizationApis.events({
+  'click #organization-apis-save': (event, templateInstance) => {
+    // Get current API id
+    const apiId = templateInstance.data.apiId;
+
+    // Get the selected organization id
+    const organizationId = templateInstance.$('[name=organizationId]').val();
+
+    // Try to find organization document
+    const organization = Organizations.findOne(organizationId);
+    // Try to find metadata document of current API
+    const metadata = ApiMetadata.findOne({ apiBackendId: apiId });
+    // Get metadata id otherwise it will be empty string
+    const metadataId = metadata ? metadata._id : '';
+
+    // If organization document was found
+    if (organization) {
+      // Fill a object with organization information for metadata
+      const metadataInformation = {
+        organization: {
+          name: organization.name,
+          description: organization.description,
+        },
+        contact: {
+          name: organization.contact.person,
+          phone: organization.contact.phone,
+          email: organization.contact.email,
+        },
+      };
+
+      // If metadata document already exists
+      if (metadata) {
+        // Update information
+        ApiMetadata.update(metadataId, { $set: metadataInformation });
+      } else {
+        // Add information about API
+        metadataInformation.apiBackendId = apiId;
+        // Create a new one metadata
+        ApiMetadata.insert(metadataInformation);
+      }
+    } else {
+      // Was selected the first item in list then delete metadata information
+      ApiMetadata.remove(metadataId);
+    }
   },
 });
