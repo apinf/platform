f8ded561bb66fdb612cb8ec90d90e62855dc5404
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2017-01-02 13:31:38 +0200

    Remove unused helper

--- core/client/helpers.js
+++ core/client/helpers.js
@@ -1,23 +1,4 @@
-import { Meteor } from 'meteor/meteor';
 import { Blaze } from 'meteor/blaze';
 import { Template } from 'meteor/templating';
-
-Template.registerHelper('niceName', (_id) => {
-  let user;
-  if (_id) {
-    user = Meteor.users.findOne(_id);
-  }
-  if (user) {
-    if (user.username) {
-      return user.username;
-    } else if (typeof user.profile !== 'undefined' && user.profile.firstName) {
-      return user.profile.firstName;
-    } else if (user.emails[0].address) {
-      return user.emails[0].address;
-    } else {
-      return 'An user';
-    }
-  }
-});
 
 Template.registerHelper('isTemplate', (templateName) => Blaze.isTemplate(Template[templateName]));
