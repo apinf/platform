ed443b4dd81e904823e33ffa826480f38da7a0da
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2017-01-02 11:00:33 +0200

    Move, rename, update apiBackendForm

---
+++ apis/client/profile/settings/details/autoform.js
@@ -0,0 +1,15 @@
+// Meteor package import
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+import { TAPi18n } from 'meteor/tap:i18n';
+
+AutoForm.hooks({
+  apiBackendForm: {
+    onSuccess () {
+      // User just updated API information
+      const message = TAPi18n.__('apiBackendForm_text_updateInformation');
+      // Show message
+      sAlert.success(message);
+    },
+  },
+});
