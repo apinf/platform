fced32954b0fadf15c470747b6a196f3c4cf8ab0
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-02 14:40:25 +0200

    Fix data/routing for insert and update forms

--- organizations/client/form/autoform.js
+++ organizations/client/form/autoform.js
@@ -1,37 +1,45 @@
-import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
 import { TAPi18n } from 'meteor/tap:i18n';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
 AutoForm.hooks({
   organizationForm: {
-    before: {
-      insert (organization) {
-        // Get current user ID
-        const userId = Meteor.userId();
+    onSuccess (formType) {
+      // Hide organization form modal
+      Modal.hide('organizationForm');
 
-        // Add current user as organization manager
-        organization.managerIds = [userId];
+      if (formType === 'insert') {
+        // Get reference to template instance
+        const instance = this;
 
-        // Submit the form
-        return organization;
-      },
-    },
-    onSuccess () {
-      // Get reference to template instance
-      const instance = this;
+        // Get organization URL slug
+        const slug = instance.insertDoc.slug;
 
-      // Get slug
-      const slug = instance.insertDoc.slug;
+        // Redirect to newly added organization
+        FlowRouter.go('organizationProfile', { slug });
+      } else if (formType === 'update') {
+        // Get reference to template instance
+        const instance = this;
 
-      // Redirect to newly added organization
-      FlowRouter.go('organizationProfile', { slug });
+        if (instance.currentDoc.slug !== instance.updateDoc.$set.slug) {
+          // Get organization URL slug
+          const slug = instance.updateDoc.$set.slug;
 
-      // Create & show message about successfull inserting
-      const message = TAPi18n.__('addOrganizationForm_successText');
-      sAlert.success(message);
+          // Redirect to newly added organization
+          FlowRouter.go('organizationProfile', { slug });
+        }
+      }
 
+      // Show success message once - reactive computation runs multiple times
+      if (Tracker.firstRun) {
+        // Create & show message about successfull inserting
+        const message = TAPi18n.__('addOrganizationForm_successText');
+
+        sAlert.success(message);
+      }
     },
   },
 });
