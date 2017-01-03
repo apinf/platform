93db2ec40a962b776a9f3b16be25e0d87b5bb9c4
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-03 12:09:21 +0200

    Re-work organizationApis publication; lint

--- organization_apis/collection/server/publications.js
+++ organization_apis/collection/server/publications.js
@@ -1,19 +1,40 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { Organizations } from '/organizations/collection';
+import { _ } from 'lodash';
+import { Apis } from '/apis/collection';
 import { OrganizationApis } from '../';
 
 Meteor.publish('organizationApis', (slug) => {
+  // Make sure slug argument is a String
+  check(slug, String);
+
   // Init organizationApis
-  let organizationApis = [];
+  let organizationApis;
+
+  // Initialize apis as empty array, to complete the publication if no results
+  let apis = [];
+
   // Get organization
   const organization = Organizations.findOne({ slug });
+
   // Check organization exist
   if (organization) {
-    // Get cursor to organizationApis
-    organizationApis = OrganizationApis.find({ organizationId: organization._id });
+    // Get all links between this organization and APIs
+    organizationApis = OrganizationApis.find({ organizationId: organization._id }).fetch();
+
+    // Create an array of API IDs
+    const apiIds = _.map(organizationApis, function (organizationApiLink) {
+      // Get API ID from link document
+      return organizationApiLink.apiId;
+    });
+
+    // Get a database cursor of APIs using the API IDs array
+    apis = Apis.find({ _id: { $in: apiIds } });
   }
+
   // Return organizationApis (empty array or cursor)
-  return organizationApis;
+  return apis;
 });
 
 Meteor.publish('organizationApisByApiId', (apiId) =>
