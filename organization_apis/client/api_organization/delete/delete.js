ee0861704f769869de4c10cfd7a0807b6d42c2a9
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-03 15:00:39 +0200

    Initial working delete event handler

---
+++ organization_apis/client/api_organization/delete/delete.js
@@ -0,0 +1,11 @@
+import { OrganizationApis } from '../../../collection';
+
+Template.deleteOrganizationApiConfirmation.events({
+  'click #delete-api-organization': function (event, templateInstance) {
+    // Get Organization API ID from template instance
+    const organizationApiId = templateInstance.data.organizationApi._id;
+
+    // Remove the Organization API link, by ID since code is untrusted
+    OrganizationApis.remove(organizationApiId);
+  },
+});
