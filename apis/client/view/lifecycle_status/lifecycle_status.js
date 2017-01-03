06198deafba04eebfbc6e8a3ceabd9c550f5ddd9
Sridevi <sridevi27@gmail.com>
2017-01-03 10:03:51 +0200

    Added Lifecycle Status to Api Catalogue (gird/table)

--- apis/client/view/lifecycle_status/lifecycle_status.js
+++ apis/client/view/lifecycle_status/lifecycle_status.js
@@ -12,7 +12,6 @@ Template.apiLifecycleStatus.helpers({
     // Get API lifecycle status from template instance
     const lifecycleStatus = templateInstance.data.api.lifecycleStatus;
 
-    console.log(templateInstance.data.api.lifecycleStatus);
     // Get label type based on lifecycle status
     switch (lifecycleStatus) {
       case 'development':
