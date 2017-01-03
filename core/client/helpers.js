7bdcdfc8a0b4b6f67128faca9299ddfc26cb0ab3
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-03 12:59:02 +0200

    Remove unneeded helper (will modularize later)

--- core/client/helpers.js
+++
@@ -1,4 +0,0 @@
-import { Blaze } from 'meteor/blaze';
-import { Template } from 'meteor/templating';
-
-Template.registerHelper('isTemplate', (templateName) => Blaze.isTemplate(Template[templateName]));
