9e746d770071d0d8568d8ac00e90c10d3b97ee0a
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-03 14:11:06 +0200

    Add formType helper; lint; cleanup

--- organization_apis/client/form/form.js
+++ organization_apis/client/form/form.js
@@ -1,39 +1,30 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { Organizations } from '/organizations/collection';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
-import _ from 'lodash';
-
 import { OrganizationApis } from '../../collection';
 
 Template.organizationApisForm.helpers({
+  formtype () {
+    // Placeholder for form type
+    let formType;
+
+    // Get reference to tempplate instance
+    const instance = Template.instance();
+
+    // Get Organization API document from template data context
+    const organizationApi = instance.data.organizationApi;
+
+    // If Organization API doc exists, type is update, otherwise type is insert
+    if (organizationApi) {
+      formType = 'update';
+    } else {
+      formType = 'insert';
+    }
+
+    return formType;
+  },
   organizationApisCollection () {
     return OrganizationApis;
-  },
-});
-
-Template.organizationApisForm.events({
-  'click #organization-apis-disconnect': (event, templateInstance) => {
-    const organizationId = templateInstance.data.api.organization()._id;
-    const apiId = templateInstance.data.api._id;
-
-    // Get current template instance
-    const instance = Template.instance();
-    // Get processing message translation
-    const message = TAPi18n.__('organizationApis_disconnectButton_processing');
-    // Set bootstrap loadingText
-    instance.$('#organization-apis-disconnect').button({ loadingText: message });
-    // Set button to processing state
-    instance.$('#organization-apis-disconnect').button('loading');
-
-    Meteor.call('disconnectOrganizationApi', organizationId, apiId, (error, result) => {
-      if (error) {
-        // Reset button to state
-        instance.$('#organization-apis-disconnect').button('reset');
-        // Show error
-        sAlert.error(error.reason);
-      }
-    });
   },
 });
