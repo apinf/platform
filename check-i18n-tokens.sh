1a959ac72c21b3c6151ae2a8ed9cf622565fdc11
Massimo Rangoni <maxappbr@gmail.com>
2017-01-10 06:31:30 -0300

    Fix names vars

--- check-i18n-tokens.sh
+++ check-i18n-tokens.sh
@@ -5,16 +5,16 @@ fileEnI18n="core/lib/i18n/en.i18n.json"
 echo "Tokens that should be added on en.i18n.json file:"
 
 # FIND TOKENS FROM HTML FILES
-grep -iRoh '{{ *_ .*}}' * --exclude="*.md" | grep -o "['\"].*['\"]" | sed -e "s@['\"]@@g" | while read -r line ; do
-  if ! grep -q "$line" "$fileEnI18n"; then
-    echo "$line"
+grep -iRoh '{{ *_ .*}}' * --exclude="*.md" | grep -o "['\"].*['\"]" | sed -e "s@['\"]@@g" | while read -r token ; do
+  if ! grep -q "$token" "$fileEnI18n"; then
+    echo "$token"
   fi
 done
 
 # FIND TOKENS FROM JS FILES
-grep -iRoh 'TAPi18n\.__(.*)' * --exclude="*.md" | grep -o "['\"].*['\"]" | sed -e "s@['\"]@@g" | while read -r line ; do
-  if ! grep -q "$line" "$fileEnI18n"; then
-    echo "$line"
+grep -iRoh 'TAPi18n\.__(.*)' * --exclude="*.md" | grep -o "['\"].*['\"]" | sed -e "s@['\"]@@g" | while read -r token ; do
+  if ! grep -q "$token" "$fileEnI18n"; then
+    echo "$token"
   fi
 done
 
