d72c2e10b6555640814255fd637a595c27fafbd4
Daria Voytova <d.voytova@2glab.com>
2017-01-16 11:06:18 +0300

    Server method for searsing free apis

---
+++ organizations/server/methods.js
@@ -0,0 +1,45 @@
+// Meteor packages import
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Roles } from 'meteor/alanning:roles';
+
+// APINF collections import
+import { Apis } from '/apis/collection';
+import { OrganizationApis } from '/organization_apis/collection';
+
+Meteor.methods({
+  getNoBookedApis (organizationId) {
+    // Check arguments
+    check(organizationId, String);
+
+    const userId = this.userId;
+
+    // Find result for user with role
+    if (userId) {
+      // Find & group all booked apis
+      const OrganizationApisLink = OrganizationApis.find().fetch();
+
+      const bookedApis = _.map(OrganizationApisLink, (module) => {
+        return module.apiId;
+      });
+
+      // Find apis that no equal booked
+      const queryParams = {
+        _id: { $nin: bookedApis },
+      };
+
+      const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+      // Is user no admin then
+      if (!userIsAdmin) {
+        // limit selection by belong to current user
+        queryParams.managerIds = userId;
+      }
+
+      // Return cursor on no booked apis
+      return Apis.find(queryParams).fetch();
+    }
+
+    // Return empty result
+    return [];
+  },
+});
