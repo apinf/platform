3a0e888403e27e239ee6a57cebeff78f0550f1c5
Daria Voytova <d.voytova@2glab.com>
2017-01-16 11:05:34 +0300

    Add event for appearing module

--- organizations/client/profile/profile.js
+++ organizations/client/profile/profile.js
@@ -1,9 +1,10 @@
-import { Meteor } from 'meteor/meteor';
+// Meteor packages import
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
+// APINF collections import
 import { Organizations } from '/organizations/collection/';
-
 
 Template.organizationProfile.onCreated(function () {
   // Get reference to template instance
@@ -18,15 +19,15 @@ Template.organizationProfile.onCreated(function () {
     if (organizationSlug) {
       // Reactively subscribe to a single Organization
       // Makes sure proper data is available when editing organization name
-      instance.organizationSubscription = instance.subscribe('singleOrganization', organizationSlug);
-
-      // Subscribe to Organization APIs documents
-      instance.subscribe('organizationApis', organizationSlug);
-
-      // Subscribe to Organization APIs documents
-      instance.subscribe('organizationApiLinksByOrganizationSlug', organizationSlug);
+      instance.subscribe('singleOrganization', organizationSlug);
     }
   });
+
+  // Subscribe to Organization APIs documents
+  instance.subscribe('organizationApis', FlowRouter.getParam('slug'));
+
+  // Subscribe to Organization APIs documents
+  instance.subscribe('organizationApiLinksByOrganizationSlug', FlowRouter.getParam('slug'));
 });
 
 Template.organizationProfile.helpers({
@@ -74,5 +75,14 @@ Template.organizationProfile.helpers({
 
     // Return managedApisCount
     return managedApisCount;
+  },
+});
+
+Template.organizationProfile.events({
+  'click #connect-api': () => {
+    const organizationId = Organizations.findOne()._id;
+
+    // Show modal with list of suggested apis and id of current organization
+    Modal.show('connectApiToOrganizationModal', { organizationId });
   },
 });
