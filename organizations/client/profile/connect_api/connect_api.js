5a28e002c80e6cc3293ba4c6a2131aea3c39a625
Daria Voytova <d.voytova@2glab.com>
2017-01-16 11:04:37 +0300

    Add component Connect API

---
+++ organizations/client/profile/connect_api/connect_api.js
@@ -0,0 +1,50 @@
+// Meteor packages import
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+// APINF collections import
+import { OrganizationApis } from '/organization_apis/collection';
+
+Template.connectApiToOrganizationModal.onCreated(function () {
+  const instance = this;
+  // Set status data is not ready
+  instance.dataIsReady = new ReactiveVar(false);
+  // Get organization id
+  const organizationId = Template.currentData().organizationId;
+
+  // Get data about apis without organization
+  Meteor.call('getNoBookedApis', organizationId, (error, apis) => {
+    // Set status data is ready
+    instance.dataIsReady.set(true);
+    // Create & return list of objects for dropdown list
+    instance.noBookedApis =  _.map(apis, (api) => {
+      return {
+        label: api.name,
+        value: api._id,
+      };
+    });
+  });
+});
+
+Template.connectApiToOrganizationModal.helpers({
+  organizationApisCollection () {
+    // Return collection for autoform
+    return OrganizationApis;
+  },
+  noBookedApis () {
+    const instance = Template.instance();
+    // Return list of apis without organization
+    return instance.noBookedApis;
+  },
+  userCanConnectApi () {
+    const instance = Template.instance();
+    // Get count of free apis
+    return instance.noBookedApis.length > 0;
+  },
+  dataIsReady () {
+    const instance = Template.instance();
+    // Get status of data fetch
+    return instance.dataIsReady.get();
+  },
+});
