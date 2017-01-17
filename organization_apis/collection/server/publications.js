899623fe65eef1b1b762b37dcd81a85c7069e0e6
Daria Voytova <d.voytova@2glab.com>
2017-01-17 12:13:36 +0300

    New subscription for APIs on organization profile

--- organization_apis/collection/server/publications.js
+++ organization_apis/collection/server/publications.js
@@ -26,7 +26,7 @@ Meteor.publish('apiOrganizationBasicDetails', (apiId) => {
   return organization;
 });
 
-Meteor.publish('organizationApis', (slug) => {
+Meteor.publish('organizationApis', function(slug) {
   // Make sure slug argument is a String
   check(slug, String);
 
@@ -35,13 +35,40 @@ Meteor.publish('organizationApis', (slug) => {
 
   // Check organization exist
   if (organization) {
-    // Get all links between this organization and APIs
-    const organizationApis = OrganizationApis.find({ organizationId: organization._id }).fetch();
+    // Set cursor to all links between this organization and APIs
+    const cursor = OrganizationApis.find({ organizationId: organization._id });
 
+    // Get data from cursor
+    const organizationApis = cursor.fetch();
     // Create an array of API IDs
     const apiIds = _.map(organizationApis, organizationApiLink => {
       // Get API ID from link document
       return organizationApiLink.apiId;
+    });
+
+    // Get collection name for APIs collection
+    const collectionName = Apis.rawCollection().collectionName;
+    // Follow on changing of organizationAPis DB cursor
+    const handler = cursor.observe({
+      // Describe 'added' event
+      added: (doc) => {
+        // Get id of added API in organizationApi
+        const id = doc.apiId;
+        // Check that current API
+        if (apiIds.indexOf(id) < 0) {
+          // Get related API
+          const apiDocument = Apis.findOne({ _id: id });
+          // Add API to Mongo on client-side
+          this.added(collectionName, id, apiDocument);
+          // Update list of API ids
+          apiIds.push(id);
+        }
+      },
+    });
+
+    // Stop observe when subscription was stopped
+    this.onStop(() => {
+      handler.stop();
     });
 
     // Return a database cursor of APIs using the API IDs array
