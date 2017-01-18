53f2b19b01f7f21d223899fb9a749decc59549ad
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-01-18 08:38:13 -0200

    Remove useless message from script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -7,6 +7,4 @@ then
   docker build -t apinf/platform:$DOCKER_TAG .
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push apinf/platform:$DOCKER_TAG
-else
-  echo "Do not build the docker image from a PULL REQUEST, ok?"
 fi
