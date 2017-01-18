44606a8d9a555cd76efb04b31876cdddb7a41adf
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-18 13:22:09 +0200

    Comment out organizationApis collection

--- organization_apis/collection/server/publications.js
+++ organization_apis/collection/server/publications.js
@@ -26,58 +26,58 @@ Meteor.publish('apiOrganizationBasicDetails', (apiId) => {
   return organization;
 });
 
-Meteor.publish('organizationApis', function(slug) {
-  // Make sure slug argument is a String
-  check(slug, String);
-
-  // Get organization
-  const organization = Organizations.findOne({ slug });
-
-  // Check organization exist
-  if (organization) {
-    // Set cursor to all links between this organization and APIs
-    const cursor = OrganizationApis.find({ organizationId: organization._id });
-
-    // Get data from cursor
-    const organizationApis = cursor.fetch();
-    // Create an array of API IDs
-    const apiIds = _.map(organizationApis, organizationApiLink => {
-      // Get API ID from link document
-      return organizationApiLink.apiId;
-    });
-
-    // Get collection name for APIs collection
-    const collectionName = Apis.rawCollection().collectionName;
-    // Follow on changing of organizationAPis DB cursor
-    const handler = cursor.observe({
-      // Describe 'added' event
-      added: (doc) => {
-        // Get id of added API in organizationApi
-        const id = doc.apiId;
-        // Check that current API
-        if (apiIds.indexOf(id) < 0) {
-          // Get related API
-          const apiDocument = Apis.findOne({ _id: id });
-          // Add API to Mongo on client-side
-          this.added(collectionName, id, apiDocument);
-          // Update list of API ids
-          apiIds.push(id);
-        }
-      },
-    });
-
-    // Stop observe when subscription was stopped
-    this.onStop(() => {
-      handler.stop();
-    });
-
-    // Return a database cursor of APIs using the API IDs array
-    return Apis.find({ _id: { $in: apiIds } });
-  }
-
-  // Otherwise, return an empty array so the publication can be marked as 'ready'
-  return [];
-});
+// Meteor.publish('organizationApis', function(slug) {
+//   // Make sure slug argument is a String
+//   check(slug, String);
+//
+//   // Get organization
+//   const organization = Organizations.findOne({ slug });
+//
+//   // Check organization exist
+//   if (organization) {
+//     // Set cursor to all links between this organization and APIs
+//     const cursor = OrganizationApis.find({ organizationId: organization._id });
+//
+//     // Get data from cursor
+//     const organizationApis = cursor.fetch();
+//     // Create an array of API IDs
+//     const apiIds = _.map(organizationApis, organizationApiLink => {
+//       // Get API ID from link document
+//       return organizationApiLink.apiId;
+//     });
+//
+//     // Get collection name for APIs collection
+//     const collectionName = Apis.rawCollection().collectionName;
+//     // Follow on changing of organizationAPis DB cursor
+//     const handler = cursor.observe({
+//       // Describe 'added' event
+//       added: (doc) => {
+//         // Get id of added API in organizationApi
+//         const id = doc.apiId;
+//         // Check that current API
+//         if (apiIds.indexOf(id) < 0) {
+//           // Get related API
+//           const apiDocument = Apis.findOne({ _id: id });
+//           // Add API to Mongo on client-side
+//           this.added(collectionName, id, apiDocument);
+//           // Update list of API ids
+//           apiIds.push(id);
+//         }
+//       },
+//     });
+//
+//     // Stop observe when subscription was stopped
+//     this.onStop(() => {
+//       handler.stop();
+//     });
+//
+//     // Return a database cursor of APIs using the API IDs array
+//     return Apis.find({ _id: { $in: apiIds } });
+//   }
+//
+//   // Otherwise, return an empty array so the publication can be marked as 'ready'
+//   return [];
+// });
 
 Meteor.publish('organizationApisByApiId', (apiId) => {
   // Make sure API ID is String
