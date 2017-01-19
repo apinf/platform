4f125a6c243d18f245fb2bdce4b299b08e8ff776
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-01-19 13:44:41 +0200

    Add linter ignore rule

--- organizations/collection/server/publications.js
+++ organizations/collection/server/publications.js
@@ -29,6 +29,8 @@ Meteor.publish('allOrganizationBasicDetails', () => {
 });
 
 // Publish collection for pagination
+// TODO: Determine if there is a better way to handle pagination
+// eslint-disable-next-line no-new
 new Meteor.Pagination(Organizations);
 
 Meteor.publish('userManagedOrganizations', function () {
