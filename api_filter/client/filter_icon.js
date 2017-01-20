cf87a974736b77466c85db32a11f9fca39553abb
Daria Voytova <d.voytova@2glab.com>
2017-01-20 12:56:46 +0300

    Lint

--- api_filter/client/filter_icon.js
+++ api_filter/client/filter_icon.js
@@ -5,7 +5,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 Template.apisFilterIcon.events({
   'click #filter-icon': (event, templateInstance) => {
     // Show/hide filter options
-    templateInstance.$('.filter-popup').toggleClass('filter-popup-showed');
+    templateInstance.$('.filter-popup').toggleClass('filter-popup-visible');
   },
   'click [type=reset]': () => {
     // Delete query parameter
--- api_filter/client/form/filter_form.js
+++ api_filter/client/form/filter_form.js
@@ -1,4 +1,4 @@
-
+import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
 Template.apisFilterForm.helpers({
@@ -9,16 +9,14 @@ Template.apisFilterForm.helpers({
       'design',
       'production',
       'testing',
-    ]
+    ];
   },
-})
+});
+
 Template.apisFilterForm.events({
   'change [name=lifecycle]': (event, templateInstance) => {
     // Get & save value of selected item
     templateInstance.lifecycle = event.currentTarget.value;
-    if (event.currentTarget.value) {
-
-    }
   },
   'click #filter-apis': (event, templateInstance) => {
     if (templateInstance.lifecycle) {
@@ -26,15 +24,7 @@ Template.apisFilterForm.events({
       // Save selected filter on query params
       FlowRouter.setQueryParams({ lifecycle: tag });
     }
-
+    // Hide filter form
     $('.filter-popup').toggleClass('filter-popup-visible');
-
   },
-  'click [type=reset]': (event, templateInstance) => {
-    // Delete tag from
-    // templateInstance.lifecycle = undefined;
-
-    FlowRouter.setQueryParams({ lifecycle: null });
-  },
-
 });
