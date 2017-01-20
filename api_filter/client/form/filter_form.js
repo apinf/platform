6153e7ad1bf61238857e06a83b5cf8123429b006
Daria Voytova <d.voytova@2glab.com>
2017-01-20 12:15:51 +0300

    Create filter options template, handlers, css

---
+++ api_filter/client/form/filter_form.js
@@ -0,0 +1,40 @@
+
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.apisFilterForm.helpers({
+  lifeCycleFilter () {
+    return [
+      'development',
+      'deprecated',
+      'design',
+      'production',
+      'testing',
+    ]
+  },
+})
+Template.apisFilterForm.events({
+  'change [name=lifecycle]': (event, templateInstance) => {
+    // Get & save value of selected item
+    templateInstance.lifecycle = event.currentTarget.value;
+    if (event.currentTarget.value) {
+
+    }
+  },
+  'click #filter-apis': (event, templateInstance) => {
+    if (templateInstance.lifecycle) {
+      const tag = templateInstance.lifecycle;
+      // Save selected filter on query params
+      FlowRouter.setQueryParams({ lifecycle: tag });
+    }
+
+    $('.filter-popup').toggleClass('filter-popup-showed');
+
+  },
+  'click [type=reset]': (event, templateInstance) => {
+    // Delete tag from
+    // templateInstance.lifecycle = undefined;
+
+    FlowRouter.setQueryParams({ lifecycle: null });
+  },
+
+});
