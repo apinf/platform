2de9a2deb395d838dafb3c60b7ceb4ff7be62b4f
Daria Voytova <d.voytova@2glab.com>
2017-01-20 12:22:31 +0300

    Filtering APIs, refactor

--- organizations/client/profile/profile.js
+++ organizations/client/profile/profile.js
@@ -1,18 +1,22 @@
 // Meteor packages import
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
-import _ from 'lodash';
 
 // APINF collections import
 import Organizations from '/organizations/collection/';
-import OrganizationApis from '/organization_apis/collection/';
 
 Template.organizationProfile.onCreated(function () {
   // Get reference to template instance
   const instance = this;
+
+  // Create placeholder for storage
+  instance.dbQuery = {};
+  instance.filteredApis = new ReactiveVar();
+  instance.selectedFilterOption = new ReactiveVar();
 
   instance.autorun(() => {
     // Get the Organization slug from the route
@@ -32,19 +36,10 @@ Template.organizationProfile.onCreated(function () {
       const organization = Organizations.findOne({ slug: organizationSlug });
 
       if (organization) {
-        // Get all Organization API links
-        const apiLinks = OrganizationApis.find({ organizationId: organization._id }).fetch();
-
-        // Make sure there is at least one Organization API
-        if (apiLinks.length > 0) {
-          // Get all Organization API IDs
-          const apiIds = _.map(apiLinks, (apiLink) => {
-            return apiLink.apiId;
-          });
-
-          // Subscribe to Organization APIs documents
-          instance.subscribe('apisByIds', apiIds);
-        }
+        // Get IDs of managed APIs via collection helper
+        const managedApiIds = organization.managedApiIds();
+        // Subscribe to Organization APIs documents
+        instance.subscribe('apisByIds', managedApiIds);
       }
     }
   });
@@ -52,30 +47,59 @@ Template.organizationProfile.onCreated(function () {
 
 Template.organizationProfile.helpers({
   organization () {
+    const instance = Template.instance();
+
     // Get the Organization slug from the route
     const slug = FlowRouter.getParam('slug');
+    // Get Organization, based on slug & Save it in template instance variable
+    instance.organization = Organizations.findOne({ slug });
 
-    // Get Organization, based on slug
-    return Organizations.findOne({ slug });
+    // Return organization document
+    return instance.organization;
   },
-  managedApisCount () {
-    // Get the Organization slug from the route
-    const slug = FlowRouter.getParam('slug');
-
-    // Init managedApisCount
-    let managedApisCount = 0;
+  filteredApisCount () {
+    const instance = Template.instance();
 
     // Get Organization, based on slug
-    const organization = Organizations.findOne({ slug });
+    const organization = instance.organization;
 
-    // Check organization exist
-    if (organization) {
-      // Get organization apis count
-      managedApisCount = organization.apisCount();
+    // Placeholder for storaging APIs of current organization
+    let organizationApis;
+
+    // Get query parameter LifeCycle
+    const lifecycleParameter = FlowRouter.getQueryParam('lifecycle');
+    // Save selected option in reactive var
+    instance.selectedFilterOption.set(lifecycleParameter);
+
+
+    if (lifecycleParameter) {
+      // Save parameter in database query options
+      instance.dbQuery.lifecycleStatus = lifecycleParameter;
+
+      // Find APIs with filter options
+      organizationApis = organization.filteredApis(instance.dbQuery);
+    } else {
+      // Otherwise filter isn't set
+      // Find all managed apis
+      organizationApis = organization.apis();
     }
+    // Save APIs in reactive var
+    instance.filteredApis.set(organizationApis);
 
-    // Return managedApisCount
-    return managedApisCount;
+    // Return count of managed/filtered APIs
+    return organizationApis.length;
+  },
+  filteredApis () {
+    const instance = Template.instance();
+
+    // Return array of organization APIs
+    return instance.filteredApis.get();
+  },
+  selectedFilter () {
+    const instance = Template.instance();
+
+    // Return selected filter option
+    return instance.selectedFilterOption.get();
   },
 });
 
