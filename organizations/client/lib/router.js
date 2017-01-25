2140a966386dc9f0d5ace25449af08956b74e1a6
Daria Voytova <d.voytova@2glab.com>
2017-01-25 17:34:47 +0300

    Add comments

--- organizations/client/lib/router.js
+++ organizations/client/lib/router.js
@@ -8,15 +8,20 @@ FlowRouter.route('/organizations', {
   // Get query parameters for Catalog page on Enter
   triggersEnter: [function (context) {
     if (!context.queryParams.sortBy) {
+      // Set query parameter if it doesn't exist
       context.queryParams.sortBy = 'name';
     }
     if (!context.queryParams.sortDirection) {
+      // Set query parameter if it doesn't exist
       context.queryParams.sortDirection = 'ascending';
     }
     if (!context.queryParams.viewMode) {
+      // Set query parameter if it doesn't exist
       context.queryParams.viewMode = 'grid';
     }
+    // filterBy parameter must be available only for registered users
     if (!context.queryParams.filterBy && Meteor.userId()) {
+      // Set query parameter if it doesn't exist
       context.queryParams.filterBy = 'all';
     }
   }],
