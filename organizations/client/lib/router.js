4cfee58bab8f603f26b2c16c9d44b02f6bb39132
Daria Voytova <d.voytova@2glab.com>
2017-01-25 11:22:04 +0300

    Set query parameters on triggers enter

--- organizations/client/lib/router.js
+++ organizations/client/lib/router.js
@@ -1,7 +1,25 @@
+import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
 FlowRouter.route('/organizations', {
+  // Get query parameters for Catalog page on Enter
+  triggersEnter: [function (context) {
+    if (!context.queryParams.sortBy) {
+      context.queryParams.sortBy = 'name';
+    }
+    if (!context.queryParams.sortDirection) {
+      context.queryParams.sortDirection = 'ascending';
+    }
+    if (!context.queryParams.viewMode) {
+      context.queryParams.viewMode = 'grid';
+    }
+    if (!context.queryParams.filterBy && Meteor.userId()) {
+      context.queryParams.filterBy = 'all';
+    }
+  }],
   name: 'organizationCatalog',
   action () {
     BlazeLayout.render('masterLayout', { main: 'organizationCatalog' });
