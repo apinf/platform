9b46d271273b4594b40664ab91a55c7fb71e6862
Daria Voytova <d.voytova@2glab.com>
2017-01-26 16:36:28 +0300

    Condition to showing APIs tab

--- organizations/client/profile/header/header.js
+++ organizations/client/profile/header/header.js
@@ -7,6 +7,25 @@ Template.organizationProfileHeader.onRendered(function () {
   OrganizationLogo.resumable.assignBrowse(this.$('#organization-file-browse'));
 });
 
+Template.organizationProfileHeader.helpers({
+  currentUserCanView () {
+    const userId = Meteor.userId();
+    // Get related organization
+    const organization = Template.currentData().organization;
+
+    // Check user's role on admin
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+    const userIsOrganizationManager = organization.managerIds.indexOf(userId) > -1;
+
+    // Show APIs tab if user is admin or manager of current organization
+    if (userIsAdmin || userIsOrganizationManager) {
+      return true;
+    }
+    // Show APIs tab if public apis are available otherwise don't show
+    return organization.availablePublicApis();
+  },
+});
+
 Template.organizationProfileHeader.events({
   'click #edit-organization': function (event, templateInstance) {
     // Get organization from template instance
