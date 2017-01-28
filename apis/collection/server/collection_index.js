d754d8bb3c9e73efb1e04b5807ac1ae58b66fce3
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-01-28 08:21:15 -0200

    Fix no-underscore-dangle eslint check

--- apis/collection/server/collection_index.js
+++ apis/collection/server/collection_index.js
@@ -1,7 +1,10 @@
 import Apis from '../';
 
 // Create indexes for fields in MongoDB collection (API backends search functionality)
+// TODO: shouldn't it be during Meteor.startup?
+//       ref: http://joshowens.me/how-to-optimize-your-mongo-database-for-meteor-js/
+// eslint-disable-next-line no-underscore-dangle
 Apis._ensureIndex({
-  "name": 1,
-  "backend_host": 1
+  name: 1,
+  backend_host: 1,
 });
