cda069ddf4833e87fd6cb9cded07d506c366910e
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-01-28 20:01:37 -0200

    Lint apis/logo/collection/server/permissions.js

--- apis/logo/collection/server/permissions.js
+++ apis/logo/collection/server/permissions.js
@@ -1,16 +1,23 @@
+import { Roles } from 'meteor/alanning:roles';
+
 import ApiLogo from '../';
 
+
 ApiLogo.allow({
-  insert: function(userId, file) {
-    return Roles.userIsInRole(userId, ['admin','manager']);
+  // eslint-disable-next-line no-unused-vars
+  insert (userId, file) {
+    return Roles.userIsInRole(userId, ['admin', 'manager']);
   },
-  remove: function(userId, file) {
-    return Roles.userIsInRole(userId, ['admin','manager']);
+  // eslint-disable-next-line no-unused-vars
+  remove (userId, file) {
+    return Roles.userIsInRole(userId, ['admin', 'manager']);
   },
-  read: function(userId, file) {
+  // eslint-disable-next-line no-unused-vars
+  read (userId, file) {
     return true;
   },
-  write: function(userId, file, fields) {
+  // eslint-disable-next-line no-unused-vars
+  write (userId, file, fields) {
     return true;
-  }
+  },
 });
