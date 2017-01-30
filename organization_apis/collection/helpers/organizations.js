adf6cb2db955ea6b992e6a33ac3c92218712ff36
Daria Voytova <vdariaclem@gmail.com>
2017-01-30 12:14:36 +0000

    Delete filter from helpers

--- organization_apis/collection/helpers/organizations.js
+++ organization_apis/collection/helpers/organizations.js
@@ -1,5 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
 import { _ } from 'lodash';
 
 // APINF collection imports
@@ -14,45 +12,8 @@ Organizations.helpers({
 
     // Make sure organization has APIs
     if (apiIds.length > 0) {
-      // Get user id
-      const userId = Meteor.userId();
-
-      // Placeholder for filtering
-      let filtering;
-
-      // Set filters
-      // Case: Registered users
-      if (userId) {
-        // Case: user is admin or manager of organization
-
-        // Get all managed organizations fo current user
-        const userIsOrganizationManager = Organizations.find({ managerIds: userId }).count();
-        // Check if user is admin
-        const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
-
-        if (userIsAdmin || userIsOrganizationManager > 0) {
-          // Show all managed apis of current organization
-          filtering = { _id: { $in: apiIds } };
-        } else {
-          // Case: user is manager of APIs or without APIs
-
-          // Select available managed apis of current organization
-          filtering = { _id: { $in: apiIds },
-            $or: [
-              { isPublic: true },
-              { managerIds: userId },
-              { authorizedUserIds: userId },
-            ],
-          };
-        }
-      } else {
-        // Case: Anonymous users
-
-        // Show all public managed apis of current organization
-        filtering = { _id: { $in: apiIds }, isPublic: true };
-      }
       // Return list of APIs
-      return Apis.find(filtering).fetch();
+      return Apis.find({ _id: { $in: apiIds } }).fetch();
     }
     // Return empty array because organization doesn't have APIs
     return [];
@@ -63,7 +24,7 @@ Organizations.helpers({
   },
   hasPublicApis () {
     // Get array of managed apis which available for current user
-    const apis = this.apis();
+    const apis = Apis.find().fetch();
 
     // Return true if organization has at least one public api
     return apis.length > 0;
@@ -82,56 +43,5 @@ Organizations.helpers({
     }
     // Return empty array because organization doesn't have APIs
     return [];
-  },
-  filteredApis (filterQuery) {
-    // Get user id
-    const userId = Meteor.userId();
-
-    // Get IDs of managed APIs
-    const apiIds = this.managedApiIds();
-
-    // Placeholder for filtering
-    let filtering;
-
-    // Set filters
-    // Case: Registered users
-    if (userId) {
-      // Case: user is admin or manager of organization
-
-      // Get all managed organizations fo current user
-      const userIsOrganizationManager = Organizations.find({ managerIds: userId }).count();
-      // Check if user is admin
-      const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
-
-      if (userIsAdmin || userIsOrganizationManager > 0) {
-        // Show all managed apis of current organization
-        filtering = { _id: { $in: apiIds } };
-      } else {
-        // Case: user is manager of APIs or without APIs
-
-        // Select available managed apis of current organization
-        filtering = { _id: { $in: apiIds },
-          $or: [
-            { isPublic: true },
-            { managerIds: userId },
-            { authorizedUserIds: userId },
-          ],
-        };
-      }
-    } else {
-      // Case: Anonymous users
-
-      // Show all public managed apis of current organization
-      filtering = { _id: { $in: apiIds }, isPublic: true };
-    }
-
-    // Add filter options to database query
-    _.forEach(filterQuery, (value, field) => {
-      // Add fields from filter to database query
-      filtering[field] = value;
-    });
-
-    // Get an array of APIs, based on API IDs array and filter
-    return Apis.find(filtering).fetch();
   },
 });
