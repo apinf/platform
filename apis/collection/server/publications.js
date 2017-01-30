ee58ede85e876e6fbe2be17ec87b4ae48d776991
Daria Voytova <vdariaclem@gmail.com>
2017-01-30 12:20:13 +0000

    New publication for public APis

--- apis/collection/server/publications.js
+++ apis/collection/server/publications.js
@@ -27,6 +27,40 @@ Meteor.publish('allApisByIds', function (apiIds) {
 });
 
 // eslint-disable-next-line prefer-arrow-callback
+Meteor.publish('publicApisByIds', function (apiIds) {
+  check(apiIds, Array);
+
+  // Get user id
+  const userId = this.userId;
+
+  // Placeholder for filtering
+  let filteredApis;
+
+  // Case: Registered users
+  if (userId) {
+    // Case: user is manager of APIs or without APIs
+
+    // Select available managed apis of current organization
+    filteredApis = {
+      _id: { $in: apiIds },
+      $or: [
+        { isPublic: true },
+        { managerIds: userId },
+        { authorizedUserIds: userId },
+      ],
+    };
+  } else {
+    // Case: Anonymous users
+
+    // Show all public managed apis of current organization
+    filteredApis = { _id: { $in: apiIds }, isPublic: true };
+  }
+
+  // Find one or more APIs using an array of API IDs
+  return Apis.find(filteredApis);
+});
+
+// eslint-disable-next-line prefer-arrow-callback
 Meteor.publish('latestPublicApis', function (limit) {
   check(limit, Number);
 
