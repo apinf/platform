a553d70a31ef7cdf861aecc8505c86b3ea5b532e
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-01-31 08:39:09 -0200

    Lint apis/logo/upload/client/upload.js

--- apis/logo/upload/client/upload.js
+++ apis/logo/upload/client/upload.js
@@ -1,5 +1,13 @@
-import ApiLogo from '../../collection';
+/* eslint-env browser */
+
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { Template } from 'meteor/templating';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+
 import Apis from '/apis/collection';
+import ApiLogo from '../../collection';
 
 Template.uploadApiLogo.onCreated(function () {
   const instance = this;
@@ -9,15 +17,16 @@ Template.uploadApiLogo.onCreated(function () {
 });
 
 Template.uploadApiLogo.events({
-  'click .delete-apiLogo': function (event, instance) {
+  'click .delete-apiLogo': function (event, templateInstance) {
     // Show confirmation dialog to user
+    // eslint-disable-next-line no-alert
     const confirmation = confirm(TAPi18n.__('uploadApiLogo_confirm_delete'));
 
 
     // Check if user clicked "OK"
     if (confirmation === true) {
       // Get currentApiBackend documentationFileId
-      const apiLogoFileId = instance.data.api.apiLogoFileId;
+      const apiLogoFileId = templateInstance.data.api.apiLogoFileId;
 
       // Convert to Mongo ObjectID
       const objectId = new Mongo.Collection.ObjectID(apiLogoFileId);
@@ -26,7 +35,7 @@ Template.uploadApiLogo.events({
       ApiLogo.remove(objectId);
 
       // Remove API logo file id field
-      Apis.update(instance.data.api._id, { $unset: { apiLogoFileId: '' } });
+      Apis.update(templateInstance.data.api._id, { $unset: { apiLogoFileId: '' } });
 
       // Get deletion success message
       const message = TAPi18n.__('uploadApiLogo_successfully_deleted');
@@ -46,23 +55,25 @@ Template.uploadApiLogo.helpers({
     // Get API logo file Object
     const apiLogoFile = ApiLogo.findOne(objectId);
 
+    let url;
     // Check if API logo file is available
     if (apiLogoFile) {
       // Get API logo file URL
-      return Meteor.absoluteUrl().slice(0, -1) + ApiLogo.baseURL + '/md5/' + apiLogoFile.md5;
+      url = `${Meteor.absoluteUrl().slice(0, -1) + ApiLogo.baseURL}/md5/${apiLogoFile.md5}`;
     }
+    return url;
   },
   uploadedApiLogoFile () {
     const apiLogoFileId = Apis.findOne().apiLogoFileId;
 
+    let apiLogoFile;
     if (apiLogoFileId) {
       // Convert to Mongo ObjectID
       const objectId = new Mongo.Collection.ObjectID(apiLogoFileId);
 
       // Get API logo file Object
-      const apiLogoFile = ApiLogo.findOne(objectId);
-
-      return apiLogoFile;
+      apiLogoFile = ApiLogo.findOne(objectId);
     }
+    return apiLogoFile;
   },
 });
