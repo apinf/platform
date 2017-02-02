3688b637f159739fdff6d75c8eb862c6479b49d5
illya <in11092008@gmail.com>
2017-02-02 11:51:01 +0200

    Move common code to core/client/lib

--- setup/client/lib/router.js
+++ setup/client/lib/router.js
@@ -1,21 +1,5 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-const requireAdminRole = function () {
-  if (Meteor.user()) {
-    // Get user ID
-    const userId = Meteor.user()._id;
-
-    const userIsAdmin = Roles.userIsInRole(userId, 'admin');
-
-    if (!userIsAdmin) {
-      // User is not authorized to access route
-      FlowRouter.go('notAuthorized');
-    }
-  } else {
-    FlowRouter.go('signIn');
-  }
-};
-
 const additionalSetupRequired = function () {
   if (Meteor.user()) {
     // Get user ID
@@ -36,5 +20,3 @@ const additionalSetupRequired = function () {
 
 // check if setup is required before opening any page
 FlowRouter.triggers.enter([additionalSetupRequired], { except: ['settings', 'branding'] });
-
-FlowRouter.triggers.enter([requireAdminRole], { only: ['settings', 'branding'] });
