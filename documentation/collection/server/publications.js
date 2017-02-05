3521b2ade7e1a124933409eae111458d6035b219
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-02-05 05:21:29 -0200

    Remove unused allApiDocumentation method

--- documentation/collection/server/publications.js
+++
@@ -1,9 +0,0 @@
-Meteor.publish('allApiDocumentation', function () {
-  // Check if the user is signed in
-  if (this.userId) {
-    // Return all API Documentation
-    return ApiDocumentation.find();
-  }
-    // Return nothing
-  return null;
-});
