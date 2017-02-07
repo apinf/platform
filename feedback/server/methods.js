5e5aa107bb05760b6bcf14fb8363f1592f89db3c
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-02-07 00:51:34 -0200

    Fix meteor/audit-argument-checks eslint rule

--- feedback/server/methods.js
+++ feedback/server/methods.js
@@ -1,17 +1,28 @@
+import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 import { TAPi18n } from 'meteor/tap:i18n';
-// Collection imports
+
 import FeedbackVotes from '/feedback_votes/collection';
 import Feedback from '../collection';
 
 Meteor.methods({
   deleteFeedback (feedbackItemId) {
+    // Make sure feedbackItemId is a String
+    check(feedbackItemId, String);
+
     // 1. Remove feedback votes
     FeedbackVotes.remove({ feedbackId: feedbackItemId });
+
     // 2. Remove feedback item
     Feedback.remove(feedbackItemId);
   },
   submitVote (feedbackId, vote) {
+    // Make sure feedbackId is a String
+    check(feedbackId, String);
+
+    // Make sure vote is a Number
+    check(vote, Number);
+
     // Get current user ID
     const userId = Meteor.userId();
 
