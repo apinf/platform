280b97ceb064f0eb0152952a79fdad6dd6e0649b
Daria Voytova <d.voytova@2glab.com>
2017-02-08 12:10:59 +0300

    make sure api id exists

--- dashboard/client/dashboard.js
+++ dashboard/client/dashboard.js
@@ -47,20 +47,23 @@ Template.dashboard.onCreated(function () {
         granularity,
       };
 
-      // Check of existing needed proxy data
-      Meteor.call('getProxyData', backendParameter, (error, result) => {
-        // if it was not error
-        if (!error) {
-          // Provide proxy data to elastic search
-          instance.getChartData(result, filterParameters)
-            .then((chartData) => {
-              // Update reactive variable
-              instance.chartData.set(chartData);
-            })
-            // eslint-disable-next-line no-console
-            .catch(err => { return console.error(err); });
-        }
-      });
+      // Make sure backend id exists
+      if (backendParameter) {
+        // Check of existing needed proxy data
+        Meteor.call('getProxyData', backendParameter, (error, result) => {
+          // if it was not error
+          if (!error) {
+            // Provide proxy data to elastic search
+            instance.getChartData(result, filterParameters)
+              .then((chartData) => {
+                // Update reactive variable
+                instance.chartData.set(chartData);
+              })
+              // eslint-disable-next-line no-console
+              .catch(err => { return console.error(err); });
+          }
+        });
+      }
     }
   });
 });
