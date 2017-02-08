7b584553f744d7ccb4249e9f3f03d2625d3f893e
Daria Voytova <d.voytova@2glab.com>
2017-02-08 12:09:08 +0300

    Remove setting query params to router triggers

--- dashboard/client/filtering/granularity/granularity.js
+++ dashboard/client/filtering/granularity/granularity.js
@@ -5,19 +5,13 @@ Template.granularity.onRendered(function () {
   // Get reference to template instance
   const instance = this;
 
-// Check URL parameters for granularity
-  const granularityParameter = FlowRouter.getQueryParam('granularity');
+  instance.autorun(() => {
+    // Get granularity parameters
+    const granularityParameter = FlowRouter.getQueryParam('granularity');
 
-  if (granularityParameter) {
     // Set the granularity UI state from URL parameter
     instance.$(`#${granularityParameter}-granularity`).button('toggle');
-  } else {
-    // Get granularity from template
-    const granularity = instance.$('[name=granularity]:checked').val();
-
-    // Set granularity URL parameter from template value
-    FlowRouter.setQueryParams({ granularity });
-  }
+  });
 });
 
 Template.granularity.events({
--- dashboard/client/lib/routes.js
+++ dashboard/client/lib/routes.js
@@ -4,6 +4,13 @@ import signedIn from '/core/client/lib/router';
 
 // Add route to signedIn group, requires user to sign in
 signedIn.route('/dashboard', {
+  // Get granularity parameter for Dashboard page on Enter
+  triggersEnter: [function (context) {
+    if (!context.queryParams.granularity) {
+      // Set query parameter if it doesn't exist
+      context.queryParams.granularity = 'month';
+    }
+  }],
   name: 'dashboard',
   action () {
     BlazeLayout.render('masterLayout', { main: 'dashboard' });
