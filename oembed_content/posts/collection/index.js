5dd546cdab4e66cc102296a7883106266fbbc0a5
matleppa <matti.leppanen@apinf.io>
2017-02-09 13:38:30 +0200

    Removed posts directory with subdirectories

--- oembed_content/posts/collection/index.js
+++
@@ -1,9 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-const Posts = new Mongo.Collection('posts');
-export { Posts };
-
-// This code only runs on the server
-if (Meteor.isServer) {
-  // Use pagination when posts are requested
-  new Meteor.Pagination(Posts);
-}
