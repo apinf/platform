e60c79d574b41fa04ff4d0d573d91332c2be5603
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-02-11 11:54:26 -0200

    Lint proxies directory

--- proxies/client/form/validation.js
+++ proxies/client/form/validation.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 
-// The structure "requiredFields" describes proxy types and list of required fields for this proxy 
+// The structure "requiredFields" describes proxy types and list of required fields for this proxy
 // Key is proxy_type
 // Value is list of required fields
 const requiredFieldsForProxy = {
   emqtt: ['url', 'elasticsearch'],
   apiUmbrella: ['url', 'apiKey', 'authToken', 'elasticsearch'],
-}
+};
 
-export default function validateSchema(proxyType, proxyFields) {
+export default function validateSchema (proxyType, proxyFields) {
   // Find required fields for current proxy type
   const requiredFields = requiredFieldsForProxy[proxyType];
 
@@ -17,9 +17,8 @@ export default function validateSchema(proxyType, proxyFields) {
     throw new Error(`Not implemented schema validation for "${proxyType}" type`);
   }
 
-  // Check all fields in schema for filling 
-  return _.reduce(requiredFields, (result, field) => 
-    {
-      return result && !!proxyFields[field]
-    }, true)
+  // Check all fields in schema for filling
+  return _.reduce(requiredFields, (result, field) => {
+    return result && !!proxyFields[field];
+  }, true);
 }
