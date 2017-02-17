ae527fb334aba567be891ecd1114de9b390cde2f
jawidahmadi <jawid.ahmadi@apinf.io>
2017-02-17 11:51:01 +0200

    changed permission for update and delete to

--- oembed_content/collection/permissions.js
+++ oembed_content/collection/permissions.js
@@ -1,18 +1,42 @@
 import { Roles } from 'meteor/alanning:roles';
+import _ from 'lodash';
 import Posts from './';
+
 
 Posts.allow({
   insert (userId) {
     return Roles.userIsInRole(userId, ['admin', 'manager']);
   },
-  remove (userId, doc) {
+  remove (userId) {
     // User can only delete own documents
-    const userCanDelete = doc.userId === userId;
-    return userCanDelete;
+    // console.log(doc);
+    // const userCanDelete = doc.userId === userId;
+    // return userCanDelete;
+    if (userId) {
+      // Check if user is manager of this API
+      const isManager = _.includes(this.managerIds, userId);
+      const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+      if (isManager || userIsAdmin) {
+        return true;
+      }
+    }
+    return false;
   },
-  update (userId, doc) {
+  update (userId) {
+    if (userId) {
+      // Check if user is manager of this API
+      const isManager = _.includes(this.managerIds, userId);
+      console.log('manager', userId);
+      const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+      if (isManager || userIsAdmin) {
+        return true;
+      }
+    }
+    // return api.currentUserCanManage;
     // User can only change own documents
-    const userCanModify = doc.userId === userId;
-    return userCanModify;
+    // console.log(doc);
+    // const userCanModify = doc.userId === userId;
+    // return userCanModify;
+    return false;
   },
 });
