a80f81f5df2696c99b035d16c0fc012389968dd5
matleppa <matti.leppanen@apinf.io>
2017-02-22 09:30:21 +0200

    Added localization for time difference from creation

--- core/client/helpers/calculate_date_format.js
+++ core/client/helpers/calculate_date_format.js
@@ -1,8 +1,12 @@
 import moment from 'moment';
 import { Template } from 'meteor/templating';
+import { TAPi18n } from 'meteor/tap:i18n';
+import 'moment/min/locales.min';
 
 // Calculate time passed from create
-Template.registerHelper('calculateFromDate', function(date) {
+Template.registerHelper('calculateFromDate', (date) => {
+  // Get current language
+  const language = TAPi18n.getLanguage();
   // return moment(date).format('YYYY-MM-DD HH:mm');
-  return moment(date).from(moment());
+  return moment(date).locale(language).from(moment());
 });
