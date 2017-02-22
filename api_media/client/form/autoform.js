e05959030c4460f4c0fa89994da46e0c9231110a
jawidahmadi <jawid.ahmadi@apinf.io>
2017-02-22 10:23:49 +0200

    Added context to the form event

--- api_media/client/form/autoform.js
+++ api_media/client/form/autoform.js
@@ -1,9 +1,12 @@
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import { AutoForm } from 'meteor/aldeed:autoform';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 // Close modal after successful insert/update
 AutoForm.addHooks('apiMediaPostsForm', {
   onSuccess () {
     Modal.hide('postInsertModal');
+    const context = FlowRouter.current();
+    FlowRouter.go(context);
   },
 });
