fd77af5c9cb3f0a9f45a4e2feadb2a74c9aaf341
matleppa <matti.leppanen@apinf.io>
2017-02-22 09:51:55 +0200

    Added localization of the creation date

--- core/client/helpers/calculate_date_format.js
+++ core/client/helpers/calculate_date_format.js
@@ -10,3 +10,10 @@ Template.registerHelper('calculateFromDate', (date) => {
   // return moment(date).format('YYYY-MM-DD HH:mm');
   return moment(date).locale(language).from(moment());
 });
+
+Template.registerHelper('localFormatDate', (date) => {
+  // Get current language
+  const language = TAPi18n.getLanguage();
+  // return moment(date).format('YYYY-MM-DD HH:mm');
+  return moment(date).locale(language).format('LLL');
+});
