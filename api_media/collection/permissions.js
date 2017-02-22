479309a83038ac1c819d34eac0658114b894ca40
jawidahmadi <jawid.ahmadi@apinf.io>
2017-02-22 10:51:43 +0200

    Applied generalization to the permissions file

--- api_media/collection/permissions.js
+++ api_media/collection/permissions.js
@@ -1,44 +1,16 @@
-import Apis from '/apis/collection';
 import Posts from './';
 
 Posts.allow({
   insert (userId, post) {
     const postApiId = post.apiId;
-    // Get API owning current post
-    const postApi = Apis.findOne(postApiId);
-    // Check if current user can edit API
-    const currentUserCanEdit = postApi.currentUserCanEdit();
-    if (currentUserCanEdit) {
-      // User is allowed to perform action
-      return true;
-    }
-    // User is not allowded to perform action
-    return false;
+    return post.postsAllow(postApiId);
   },
   remove (userId, post) {
     const postApiId = post.apiId;
-    // Get API owning current post
-    const postApi = Apis.findOne(postApiId);
-    // Check if current user can edit API
-    const currentUserCanEdit = postApi.currentUserCanEdit();
-    if (currentUserCanEdit) {
-      // User is allowed to perform action
-      return true;
-    }
-    // User is not allowded to perform action
-    return false;
+    return post.postsAllow(postApiId);
   },
   update (userId, post) {
     const postApiId = post.apiId;
-    // Get API owning current post
-    const postApi = Apis.findOne(postApiId);
-    // Check if current user can edit API
-    const currentUserCanEdit = postApi.currentUserCanEdit();
-    if (currentUserCanEdit) {
-      // User is allowed to perform action
-      return true;
-    }
-    // User is not allowded to perform action
-    return false;
+    return post.postsAllow(postApiId);
   },
 });
