ea7ae4fb3271181f67d58538003655f5e9fc9704
jawidahmadi <jawid.ahmadi@apinf.io>
2017-02-23 14:35:07 +0200

    temporary solution to update embeded content

--- api_media/client/form/autoform.js
+++ api_media/client/form/autoform.js
@@ -5,8 +5,12 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 // Close modal after successful insert/update
 AutoForm.addHooks('apiMediaPostsForm', {
   onSuccess () {
-    Modal.hide('postInsertModal');
+    Modal.hide('apiMediaPostsForm');
+    // Quick and dirty solution, to be fixed later
+    // Because after updating URL, the oembed does not refresh
+    // Using flow-router, a refresh is called
     const context = FlowRouter.current();
-    FlowRouter.go(context);
+    FlowRouter.go('/');
+    FlowRouter.go(context.path);
   },
 });
