d857dbbaf6b6fbfa21fc2575c9cb76fe4c313a36
jawidahmadi <jawid.ahmadi@apinf.io>
2017-02-23 13:27:50 +0200

    moved the pagination to server/pagination.js

--- api_media/collection/index.js
+++ api_media/collection/index.js
@@ -2,9 +2,3 @@ import { Mongo } from 'meteor/mongo';
 
 const Posts = new Mongo.Collection('posts');
 export default Posts;
-
-// This code only runs on the server
-if (Meteor.isServer) {
-  // Use pagination when posts are requested
-  new Meteor.Pagination(Posts);
-}
