2f88115738d81d5ace14134604558dcc7ae19574
illya <in11092008@gmail.com>
2017-02-28 17:38:54 +0200

    Initial migration code

---
+++ core/migrations/server/3-move-api-docs.js
@@ -0,0 +1,24 @@
+import { Migrations } from 'meteor/percolate:migrations';
+
+import Apis from '/apis/collection';
+import ApiDocs from '/api_docs/collection';
+
+Migrations.add({
+  version: 3,
+  name: 'Move API docs to ApiDocs collection',
+  up () {
+    // Iterate through Apis collection
+    Apis.find().forEach((apis) => {
+      // New apiDoc object
+      const apiDoc = {
+        apiId: apis._id,
+        fileId: apis.documentationFileId,
+        otherUrl: apis.documentation_link,
+        submit_methods: apis.submit_methods,
+      };
+
+      // Insert migrated API docs
+      ApiDocs.insert(apiDoc);
+    });
+  },
+});
