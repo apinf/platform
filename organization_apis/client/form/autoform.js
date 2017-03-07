d37e75141f6c092b9788587a21a669787fe81e20
Daria Voytova <d.voytova@2glab.com>
2017-03-07 14:34:15 +0300

    Add organizationId when API isconnected

--- organization_apis/client/form/autoform.js
+++ organization_apis/client/form/autoform.js
@@ -4,13 +4,33 @@ import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
 
+// Collection imports
+import ApiMetadata from '/metadata/collection';
+import OrganizationApis from '../../collection';
+
 AutoForm.hooks({
   organizationApisForm: {
     onError (formType, error) {
       // Show error message to user
       sAlert.error(error.message);
     },
-    onSuccess () {
+    onSuccess (formType, result) {
+      // Get organizationApis document
+      const organizationApis = OrganizationApis.findOne(result);
+      // Get API ID
+      const apiId = organizationApis.apiId;
+      // Get API metadata document
+      const apiMetadata = ApiMetadata.findOne({ apiId });
+
+      // Make sure API metadata document exists
+      if (apiMetadata) {
+        // Then update document
+        ApiMetadata.update(apiMetadata._id, { $set: { organizationId: '' } });
+      } else {
+        // Otherwise create a new one
+        ApiMetadata.insert({ apiId, organizationId: organizationApis.organizationId });
+      }
+
       // Create & show message on success
       const message = TAPi18n.__('organizationApisForm_successText');
 
