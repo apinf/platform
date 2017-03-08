ef44e912cc36f3b98684ec6e6fabc0cff333933a
matleppa <matti.leppanen@apinf.io>
2017-03-08 10:42:02 +0200

    Added comment lines for suppressing linter error

--- proxy_backends/collection/regex.js
+++ proxy_backends/collection/regex.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-useless-escape
 export const proxyBasePathRegEx = new RegExp(/^\/[\w\-/\.:\?#@!\$&\*\+,;=\'\(\)]+\/$/);
-
+// eslint-disable-next-line no-useless-escape
 export const apiBasePathRegEx = new RegExp(/^\/[\w\-/\.:\?#@!\$&\*\+,;=\'\(\)]*$/);
