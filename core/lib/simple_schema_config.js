14576f1c11c644c575a1fee6cad42027a8fa2b22
illya <in11092008@gmail.com>
2017-03-10 17:10:05 +0200

    Add regex messages for custom regex

--- core/lib/simple_schema_config.js
+++ core/lib/simple_schema_config.js
@@ -1,11 +1,16 @@
 // Meteor packages imports
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
+// APINF imports
+import { proxyBasePathRegEx, apiBasePathRegEx } from '/proxy_backends/collection/regex';
+
 SimpleSchema.messages({
   regEx: [
     { exp: SimpleSchema.RegEx.Url, msg: '[label] must be a valid URL' },
     { exp: SimpleSchema.RegEx.Id, msg: '[label] must be a valid alphanumeric ID' },
     { exp: SimpleSchema.RegEx.Domain, msg: '[label] must be a valid domain' },
     { exp: SimpleSchema.RegEx.Email, msg: '[label] must be a valid e-mail address' },
+    { exp: proxyBasePathRegEx, msg: '[label] allowed characters: A-Z' },
+    { exp: apiBasePathRegEx, msg: '[label] allowed characters: A-Z' },
   ],
 });
