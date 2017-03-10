01479bb87e377f2b4a616fe72c84dbcd37ac1589
illya <in11092008@gmail.com>
2017-03-10 17:29:38 +0200

    Move RegEx

---
+++ organizations/collection/regex.js
@@ -0,0 +1,3 @@
+// all numbers (0-9) , + , - , space ,/ are allowed and can appear anywhere in the phone numbers
+// e.g. 754-3010,(541) 754-3010,1-541-754-3010,1-541-754-3010,191 541 754 3010,(089) / 636-48018
+export const contactPhone = new RegExp(/^[0-9-+()/\s.]+$/);
