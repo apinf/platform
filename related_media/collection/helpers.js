7120c4cada39293f27967f1fd30f08d0bc5fc1b5
matleppa <matti.leppanen@apinf.io>
2017-03-13 10:51:00 +0200

    Added checking of entity type when deciding which collection is used

--- related_media/collection/helpers.js
+++ related_media/collection/helpers.js
@@ -5,20 +5,24 @@ import Posts from './';
 Posts.helpers({
   isPostActionAllow () {
     let currentUserCanEdit = false;
-    const postEntityId = this.apiId;
+    console.log('this=', this);
+    const postEntityId = this.entityId;
     // TODO checking of entity in question
-    // if (api) {
-    // Check API
-    const postApi = Apis.findOne(postEntityId);
-  // Check if current user can edit API
-    currentUserCanEdit = postApi.currentUserCanEdit();
-    // }
-    // else {
-    // Check Organizations
-    const postOrg = Organizatons.findOne(postEntityId);
-  // Check if current user can edit API
-    currentUserCanEdit = postOrg.currentUserCanManage();
-    // }
+    console.log('postEntityId=', postEntityId, ' type=', this.entityType);
+    if (this.entityType === '0') {
+      // Check API
+      const postApi = Apis.findOne(postEntityId);
+      // Check if current user can edit API
+      currentUserCanEdit = postApi.currentUserCanEdit();
+      console.log('api ', postApi);
+    } else if (this.entityType === '1') {
+      // Check Organizations
+      const postOrg = Organizations.findOne(postEntityId);
+      // Check if current user can edit API
+      currentUserCanEdit = postOrg.currentUserCanManage();
+      console.log('org ', postOrg);
+    }
+    console.log('canedit=', currentUserCanEdit);
     if (currentUserCanEdit) {
     // User is allowed to perform action
       return true;
