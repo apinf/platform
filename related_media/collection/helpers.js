6f4c6ab84350c34a0a20542e863b41f84abd6af7
matleppa <matti.leppanen@apinf.io>
2017-03-13 14:19:46 +0200

    Changed currentUserCanEdit to currentUserCanManage

--- related_media/collection/helpers.js
+++ related_media/collection/helpers.js
@@ -4,21 +4,21 @@ import Posts from './';
 
 Posts.helpers({
   isPostActionAllow () {
-    let currentUserCanEdit = false;
+    let currentUserCanManage = false;
     const postEntityId = this.entityId;
     // TODO checking of entity in question
     if (this.entityType === 0) {
       // Check API
       const postApi = Apis.findOne(postEntityId);
       // Check if current user can edit API
-      currentUserCanEdit = postApi.currentUserCanEdit();
+      currentUserCanManage = postApi.currentUserCanManage();
     } else if (this.entityType === 1) {
       // Check Organizations
       const postOrg = Organizations.findOne(postEntityId);
       // Check if current user can edit API
-      currentUserCanEdit = postOrg.currentUserCanManage();
+      currentUserCanManage = postOrg.currentUserCanManage();
     }
-    if (currentUserCanEdit) {
+    if (currentUserCanManage) {
     // User is allowed to perform action
       return true;
     }
