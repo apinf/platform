85e45bc17d08596c832084b9d48da84e9ee23b70
illya <in11092008@gmail.com>
2017-03-16 16:14:52 +0200

    Add copyright text

--- monitoring/collection/schema.js
+++ monitoring/collection/schema.js
@@ -1,3 +1,8 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
 // Meteor packages imports
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
@@ -57,4 +62,3 @@ MonitoringSettings.schema.i18n('schemas.monitoring');
 
 MonitoringSettings.attachSchema(MonitoringSettings.schema);
 MonitoringData.attachSchema(MonitoringData.schema);
-
