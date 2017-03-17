273bb82e238d0f5ace3f0c9c3b4088b1a378af9a
jawidahmadi <jawid.ahmadi@apinf.io>
2017-03-17 14:19:23 +0200

    Added license to the hmtl and js files.

--- related_media/client/Delete/delete_modal.js
+++ related_media/client/Delete/delete_modal.js
@@ -1,3 +1,8 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
 import { Template } from 'meteor/templating';
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import Posts from '/related_media/collection';
