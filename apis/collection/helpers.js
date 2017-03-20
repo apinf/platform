6256207b3ba6416bb012dbad75b281332ab7c8b4
matleppa <matti.leppanen@apinf.io>
2017-03-20 09:52:17 +0200

    Renamed getRelatedMediaIndex to relatedMediaType

--- apis/collection/helpers.js
+++ apis/collection/helpers.js
@@ -146,7 +146,7 @@ Apis.helpers({
     // Otherwise, get average rating
     return this.averageRating;
   },
-  getRelatedMediaIndex () {
+  relatedMediaType () {
     return 'api';
   },
   relativeUpdatedAt () {
