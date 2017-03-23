bc0de15e1f442aa38e5940ccd54d3a1d0a1576fd
Daria Voytova <d.voytova@2glab.com>
2017-03-23 18:07:46 +0300

    Remove apiAuthorizedUsersPublicDetails & user publish method

--- users/collection/server/publications.js
+++ users/collection/server/publications.js
@@ -7,34 +7,6 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
-// Collection imports
-import Apis from '/apis/collection';
-
 Meteor.publish('allUsersUsernamesOnly', () => {
   return Meteor.users.find({}, { fields: { username: 1 } });
-});
-
-// TODO: determine whether this publication is used
-// If it is used, refactor it to be a regular publication
-Meteor.publishComposite('user', () => {
-  return {
-    find () {
-      return Meteor.users.find({
-        _id: this.userId,
-      });
-    },
-  };
-});
-
-Meteor.publish('apiAuthorizedUsersPublicDetails', (apiId) => {
-  // Make sure apiId is a String
-  check(apiId, String);
-
-  // Get API document
-  const api = Apis.findOne(apiId);
-
-  // Return all authorized user documents
-  return Meteor.users.find({ _id: { $in: api.authorizedUserIds } },
-    { fields: { username: 1, emails: 1, _id: 1 } }
-  );
 });
