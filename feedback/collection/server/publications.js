bceb069d2669b8f87731c52fd640fe0d2bd6f7eb
Daria Voytova <d.voytova@2glab.com>
2017-03-23 18:07:20 +0300

    Remove apiBackendFeedback publish method

--- feedback/collection/server/publications.js
+++ feedback/collection/server/publications.js
@@ -9,15 +9,6 @@ import { check } from 'meteor/check';
 
 // Collection imports
 import FeedbackVotes from '/feedback_votes/collection';
-import Feedback from '../';
-
-Meteor.publish('apiBackendFeedback', (apiBackendId) => {
-  // Make sure apiBackendId is a String
-  check(apiBackendId, String);
-
-  // show feedback to specific API
-  return Feedback.find({ apiBackendId });
-});
 
 Meteor.publish('getAllVotesForSingleFeedback', (feedbackId) => {
   // Make sure apiBackendId is a String
