f99a7a14a97bc8eba4a07aea9612dfce6d2c0270
Daria Voytova <d.voytova@2glab.com>
2017-03-27 11:47:45 +0300

    Increment migration step

---
+++ core/migrations/server/6-add-slug-for-apis.js
@@ -0,0 +1,15 @@
+import { Migrations } from 'meteor/percolate:migrations';
+
+import Apis from '/apis/collection';
+
+Migrations.add({
+  version: 6,
+  name: 'Adds the slug field for APIs document if it has not been created yet',
+  up () {
+    // Code to migrate up to version 4
+
+    Apis.find({ slug: { $exists: false } }).forEach((api) => {
+      Apis.update(api._id);
+    });
+  },
+});
