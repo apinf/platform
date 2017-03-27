7da554402ce3b984489d682c9bb6b261d7b2d05a
Daria Voytova <d.voytova@2glab.com>
2017-03-27 11:02:07 +0300

    Rename class

--- related_media/client/item/item.js
+++ related_media/client/item/item.js
@@ -19,13 +19,13 @@ Template.postItem.onRendered(function () {
   postElement.oembed();
 });
 Template.postItem.events({
-  'click .delete': function (event, templateInstance) {
+  'click .delete-post-item': function (event, templateInstance) {
     // Get post from template instance
     const post = templateInstance.data.post;
     // Call modal confirmation dialogue and action
     Modal.show('deletePostConfirmation', { post });
   },
-  'click .edit': function (event, templateInstance) {
+  'click .edit-post-item': function (event, templateInstance) {
     // Get post from template instance
     const post = templateInstance.data.post;
     // Call modal for editing, passing post and modal header as parameters
