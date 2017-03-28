22e2c76215c72f3b10d2daf2d9a8a484f3704fec
jawidahmadi <jawid.ahmadi@apinf.io>
2017-03-28 15:30:43 +0300

    added new folder and removed the code to new structure

---
+++ organizations/client/content_management/autoform.js
@@ -0,0 +1,16 @@
+// Meteor packages imports
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+
+AutoForm.hooks({
+  numberOfMediasPerPageForm: {
+    onSuccess () {
+      // Get success message translation
+      const message = TAPi18n.__('organizationManagerForm_successMessage');
+
+      // Alert user of success
+      sAlert.success(message);
+    },
+  },
+});
