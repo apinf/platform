bf47b7bfbecbf227fe7141ad62648fab3cce34a0
matleppa <matti.leppanen@apinf.io>
2017-03-31 17:56:54 +0300

    Reverted cover removal

--- organizations/client/profile/header/header.js
+++ organizations/client/profile/header/header.js
@@ -11,14 +11,14 @@ import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
 // Collection imports
 import OrganizationLogo from '/organizations/logo/collection/collection';
-import OrganizationCover from '/organizations/cover/collection/collection';
+// import OrganizationCover from '/organizations/cover/collection/collection';
 
 Template.organizationProfileHeader.onRendered(function () {
   // Assign resumable browse to element
   OrganizationLogo.resumable.assignBrowse(this.$('#organization-file-browse'));
 
   // Assign resumable browse to element
-  OrganizationCover.resumable.assignBrowse(this.$('#organization-cover-file-browse'));
+  // OrganizationCover.resumable.assignBrowse(this.$('#organization-cover-file-browse'));
 });
 
 Template.organizationProfileHeader.events({
