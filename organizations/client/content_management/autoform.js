60df34f0463e7b83e6bac705428ea11d082bd480
jawidahmadi <jawid.ahmadi@apinf.io>
2017-04-05 09:45:44 +0300

    added refresh functionality after update is succeed.

--- organizations/client/content_management/autoform.js
+++ organizations/client/content_management/autoform.js
@@ -2,6 +2,7 @@
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 AutoForm.hooks({
   numberOfMediasPerPageForm: {
@@ -11,6 +12,13 @@ AutoForm.hooks({
 
       // Alert user of success
       sAlert.success(message);
+
+      // Quick and dirty solution, to be fixed later
+      // Because after updating URL, the oembed does not refresh
+      // Using flow-router, a refresh is called
+      const context = FlowRouter.current();
+      FlowRouter.go('/');
+      FlowRouter.go(context.path);
     },
   },
 });
