cce9b53f03914a923551d73910f7ffdb5720810f
Daria Voytova <vdariaclem@gmail.com>
2017-04-13 18:41:38 +0300

    Remove part with FlorRouter.wait()
    Check if user is admin usgin the server method
    Add route '/users' as "requireAdminRole"

--- core/client/lib/router.js
+++ core/client/lib/router.js
@@ -1,32 +1,14 @@
 /* Copyright 2017 Apinf Oy
-This file is covered by the EUPL license.
-You may obtain a copy of the licence at
-https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+ This file is covered by the EUPL license.
+ You may obtain a copy of the licence at
+ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
 // Meteor packages imports
 import { Meteor } from 'meteor/meteor';
-import { Tracker } from 'meteor/tracker';
 
 // Meteor contributed packages imports
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Roles } from 'meteor/alanning:roles';
-
-FlowRouter.wait();
-
-Tracker.autorun(() => {
-  // The Roles package actually depends on a subscription.
-  // If the subscription is not ready the Roles.userIsInRole method will always return false.
-  // That is used for checking of does user have the admin role
-
-  // Make sure the roles subscription is ready & FlowRouter hasn't initialized already
-  if (Roles.subscription.ready() && !FlowRouter._initialized) {
-    // Start routing
-    return FlowRouter.initialize();
-  }
-  // Otherwise nothing
-  return undefined;
-});
 
 // Define group for routes that require sign in
 const signedIn = FlowRouter.group({
@@ -66,24 +48,21 @@ const redirectToCatalogue = function () {
 };
 
 const requireAdminRole = function () {
-  // Get user ID
-  const userId = Meteor.userId();
-
-  if (userId) {
-    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
-
-    if (!userIsAdmin) {
+  // Using a server method check if user is admin
+  Meteor.call('userIsAdmin', (error, result) => {
+    if (!result) {
       // User is not authorized to access route
       FlowRouter.go('notAuthorized');
     }
-  } else {
-    FlowRouter.go('signIn');
-  }
+  });
 };
 
 FlowRouter.triggers.enter([redirectToCatalogue], { only: ['forgotPwd'] });
 
 // Routes that require admin role
-FlowRouter.triggers.enter([requireAdminRole], { only: ['settings', 'branding', 'proxies'] });
+FlowRouter.triggers.enter(
+  [requireAdminRole],
+  { only: ['settings', 'branding', 'proxies', 'accountsAdmin'] }
+);
 
 export default signedIn;
