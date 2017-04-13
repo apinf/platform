faa99456dc118c88afc39ddae508778aa9f2b329
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-04-13 16:29:03 +0300

    Fix autoform ID

--- organizations/client/profile/settings/pagination/autoform.js
+++ organizations/client/profile/settings/pagination/autoform.js
@@ -9,10 +9,10 @@ import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
 
 AutoForm.hooks({
-  organizationAPIsMediasNumberPerPage: {
+  organizationSettingsPagination: {
     onSuccess () {
       // Get success message translation
-      const message = TAPi18n.__('organizationAPIsMediasNumberPerPage_successMessage');
+      const message = TAPi18n.__('organizationSettingsPagination_successMessage');
 
       // Alert user of success
       sAlert.success(message);
