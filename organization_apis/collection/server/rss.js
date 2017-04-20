5c0b6cbe3b75e1e91d3400504ad748f007194a98
jawidahmadi <jawid.ahmadi@apinf.io>
2017-04-20 10:32:52 +0300

    Added functionality for feed generator
    <rss>
    <channel>
    <item>
    <title>organization 6</title>
    <description>undefinedis belongs to organization 3 apinf 5</description>
    <link>/apis/organization-1</link>
    <pubDate>Mon, 27 Mar 2017 12:06:36 GMT</pubDate></item>
    </channel>
    </rss>

--- organization_apis/collection/server/rss.js
+++ organization_apis/collection/server/rss.js
@@ -1,20 +1,32 @@
+// importing RssFeed library
 import { RssFeed } from 'meteor/raix:rssfeed';
-// import { FlowRouter } from 'meteor/kadira:flow-router';
-
-// import Organizations from '/organizations/collection';
+// importing organizations schema
+import Organizations from '/organizations/collection';
+// importing api schema
 import Apis from '/apis/collection';
+// importing apis belong to organization schema
 import OrganizationApis from '../';
 
-
+// calling Rss feed publication
+// first argument (apis) will build the url for the feed i.e domain-name/rss/apis
 RssFeed.publish('apis', function () {
+  // initialization of variable feed
   const feed = this;
+  // Look at each entry of OrganizationApis shcema
   OrganizationApis.find().forEach((organizationApi) => {
+    // make a filter key for Apis schema
     const apiOrganizationId = organizationApi.apiId;
+    // make a filter key for Organization schema
+    const OrganizationId = organizationApi.organizationId;
+    // create object from Apis schema according to apiorganizationId
     const api = Apis.findOne({ _id: apiOrganizationId });
+    // create object from Organizations schema according to OrganizationId
+    const organization = Organizations.findOne({ _id: OrganizationId });
+    // append an item to our feed using the .addItem() method
     feed.addItem({
       title: api.name,
-      description: api.description,
-      // link: 'FlowRouter.go(\'/\')apis/api.name',
+      description: `${api.description}is belongs to ${organization.name}`,
+      link: `/apis/${api.slug}`,
       pubDate: api.created_at,
     });
   });
