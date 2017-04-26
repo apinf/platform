63f8a2b21dcc5e592069de64962aeff0d2b43ade
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-04-26 14:29:17 +0300

    Refactor rss generation.
    - Fix comments
    - Removed hardcoded references to Apinf

--- organization_apis/collection/server/rss.js
+++ organization_apis/collection/server/rss.js
@@ -3,6 +3,9 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
+// Meteor packages imports
+import { Meteor } from 'meteor/meteor';
+
 // Meteor contributed packages imports
 import { RssFeed } from 'meteor/raix:rssfeed';
 
@@ -12,42 +15,56 @@ import Organizations from '/organizations/collection';
 import OrganizationApis from '../';
 
 Organizations.find().forEach((organization) => {
-  // Use variable as in feed argument
-  const organizationName = organization.slug;
-  // calling Rss feed publication
-  // first argument (apis) will build the url for the feed i.e domain-name/rss/apis
-  RssFeed.publish(organizationName, function () {
-    // initialization of variable feed
+  // Get organization slug
+  const organizationSlug = organization.slug;
+
+  // Call Rss feed publication
+  // First argument (apis) will build the url for the feed i.e domain-name/rss/apis
+  RssFeed.publish(organizationSlug, function () {
+    // Initialize variable feed
     const feed = this;
-    // RSS header information
-    feed.setValue('title', feed.cdata('Apinf organization\'s News Feed'));
+
+    // Get organization name
+    const organizationName = organization.name;
+
+    // RSS header title
+    feed.setValue('title', feed.cdata(`${organizationName} organization's News Feed`));
+
+    // RSS header description
     feed.setValue('description', feed.cdata('Apis that are connected to organizations.'));
-    feed.setValue('link', 'https://apinf.io');
+
+    // RSS header link
+    const meteorAbsoluteUrl = Meteor.absoluteUrl().slice(0, -1);
+    feed.setValue('link', meteorAbsoluteUrl);
+
     // lastBuildDate: About RSS feed was last built with new information.
     feed.setValue('lastBuildDate', new Date());
+
     // pubDate: About RSS feed publish Date
     feed.setValue('pubDate', new Date());
+
     /* ttl: The length of time (in minutes) RSS channel can be cached
             before refreshing from the source*/
     feed.setValue('ttl', 1);
-    // Look at each entry of OrganizationApis shcema
-    OrganizationApis.find().forEach((organizationApi) => {
-      // make a filter key for Apis schema
+
+    // Get the organizationId
+    const organizationId = organization._id;
+
+    // Iterate over all OrganizationApis of this organization
+    OrganizationApis.find({ organizationId }).forEach((organizationApi) => {
+      // Make a filter key for Apis schema
       const apiOrganizationId = organizationApi.apiId;
-      // create object from Apis schema according to apiorganizationId
+
+      // Get api from apiOrganizationId
       const api = Apis.findOne({ _id: apiOrganizationId });
-      // make a filter key for Organization schema
-      const OrganizationAPiId = organizationApi.organizationId;
-      // Condition to check if organizationApiID and OrganizationsID match
-      if (OrganizationAPiId === organization._id) {
-      // append an item to our feed using the .addItem() method
-        feed.addItem({
-          title: api.name,
-          description: `${api.description}`,
-          link: `https://apinf.io/apis/${api.slug}`,
-          pubDate: api.created_at,
-        });
-      }
+
+      // Append an item to our feed using the .addItem() method
+      feed.addItem({
+        title: api.name,
+        description: `${api.description}`,
+        link: `${meteorAbsoluteUrl}/apis/${api.slug}`,
+        pubDate: api.created_at,
+      });
     });
   });
 });
