f841bbce609b5bd4fad3892884fe122876726f86
jawidahmadi <jawid.ahmadi@apinf.io>
2017-05-02 12:28:37 +0300

    Add a different logic to make base url

--- organization_apis/collection/server/rss.js
+++ organization_apis/collection/server/rss.js
@@ -14,58 +14,59 @@ import Apis from '/apis/collection';
 import Organizations from '/organizations/collection';
 import OrganizationApis from '../';
 
-Organizations.find().forEach((organization) => {
-  // Get organization slug
-  const organizationSlug = organization.slug;
-
   // Call Rss feed publication
   // First argument (apis) will build the url for the feed i.e domain-name/rss/apis
-  RssFeed.publish(organizationSlug, function () {
+RssFeed.publish('organizations', function (query) {
     // Initialize variable feed
-    const feed = this;
+  const feed = this;
+
+   // Create an object with organization's slug.
+  const selector = { slug: query.slug };
+
+   // Get document containg an organization collection
+  const organization = Organizations.findOne(selector);
 
     // Get organization name
-    const organizationName = organization.name;
+  const organizationName = organization.name;
 
     // RSS header title
-    feed.setValue('title', feed.cdata(`${organizationName} organization's News Feed`));
+  feed.setValue('title', feed.cdata(`${organizationName} organization's News Feed`));
 
     // RSS header description
-    feed.setValue('description', feed.cdata(`Apis that are connected to ${organizationName}.`));
+  feed.setValue('description', feed.cdata(`Apis that are connected to ${organizationName}.`));
 
     // RSS header link
-    const meteorAbsoluteUrl = Meteor.absoluteUrl().slice(0, -1);
-    feed.setValue('link', meteorAbsoluteUrl);
+  const meteorAbsoluteUrl = Meteor.absoluteUrl().slice(0, -1);
+  feed.setValue('link', meteorAbsoluteUrl);
 
     // lastBuildDate: About RSS feed was last built with new information.
-    feed.setValue('lastBuildDate', new Date());
+  feed.setValue('lastBuildDate', new Date());
 
     // pubDate: About RSS feed publish Date
-    feed.setValue('pubDate', new Date());
+  feed.setValue('pubDate', new Date());
 
     // ttl: The length of time (in minutes).
     // RSS channel can be cached
     // before refreshing from the source
-    feed.setValue('ttl', 60);
+  feed.setValue('ttl', 60);
 
     // Get the organizationId
-    const organizationId = organization._id;
+  const organizationId = organization._id;
 
     // Iterate over all OrganizationApis of this organization
-    OrganizationApis.find({ organizationId }).forEach((organizationApi) => {
+  OrganizationApis.find({ organizationId }).forEach((organizationApi) => {
       // Make a filter key for Apis schema
-      const apiId = organizationApi.apiId;
+    const apiId = organizationApi.apiId;
 
       // Get api from apiOrganizationId
-      const api = Apis.findOne(apiId);
+    const api = Apis.findOne(apiId);
 
       // Append an item to our feed using the .addItem() method
-      feed.addItem({
-        title: api.name,
-        description: `${api.description}`,
-        link: `${meteorAbsoluteUrl}/apis/${api.slug}`,
-        pubDate: api.created_at,
-      });
+    feed.addItem({
+      title: api.name,
+      description: `${api.description}`,
+      link: `${meteorAbsoluteUrl}/apis/${api.slug}`,
+      pubDate: api.created_at,
     });
   });
 });
