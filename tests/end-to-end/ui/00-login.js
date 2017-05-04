2b6865427f313c6f97d9a86719f17f2f04598991
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-05-04 00:39:04 -0300

    Add e2e test for login page.

---
+++ tests/end-to-end/ui/00-login.js
@@ -0,0 +1,62 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+/* globals browser */
+
+import signInPage from '../../page-objects/signin.page';
+
+describe('login', () => {
+  before(() => {
+    signInPage.open();
+  });
+
+  describe('render', () => {
+    it('should show email / username field', () => {
+      signInPage.emailOrUsernameField.isVisible().should.be.true;
+    });
+
+    it('should show password field', () => {
+      signInPage.passwordField.isVisible().should.be.true;
+    });
+
+    it('should show github button', () => {
+      signInPage.githubButton.isVisible().should.be.true;
+    });
+
+    it('should show submit button', () => {
+      signInPage.submitButton.isVisible().should.be.true;
+    });
+
+    it('should show register link', () => {
+      signInPage.registerLink.isVisible().should.be.true;
+    });
+
+    it('should show forgot password link', () => {
+      signInPage.forgotPasswordLink.isVisible().should.be.true;
+    });
+
+    it('should show forgot resend verification email link', () => {
+      signInPage.resendVerificationEmailLink.isVisible().should.be.true;
+    });
+
+    it('should not show error alert', () => {
+      signInPage.errorAlert.isVisible().should.be.false;
+    });
+  });
+
+  describe('required fields', () => {
+    before(() => {
+      signInPage.submit();
+    });
+
+    describe('email / username and password', () => {
+      it('should be required', () => {
+        signInPage.errorAlert.isVisible().should.be.true;
+      });
+    });
+  });
+});
---
+++ tests/page-objects/signin.page.js
@@ -0,0 +1,50 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+/* globals browser */
+
+import Page from './page';
+
+class SignInPage extends Page {
+  get emailOrUsernameField () { return browser.element('[name=at-field-username_and_email]'); }
+  get passwordField () { return browser.element('[name=at-field-password]'); }
+
+  get submitButton () { return browser.element('button#at-btn.submit'); }
+  get githubButton () { return browser.element('button.github'); }
+
+  get registerLink () { return browser.element('#at-signUp'); }
+  get forgotPasswordLink () { return browser.element('#at-forgotPwd'); }
+  get resendVerificationEmailLink () { return browser.element('#at-resend-verification-email'); }
+
+  get errorAlert () { return browser.element('.at-error'); }
+
+  open () {
+    super.open('sign-in');
+  }
+
+  gotToForgotPassword () {
+    this.forgotPasswordLink.waitForVisible(5000);
+    this.forgotPasswordLink.click();
+  }
+
+  gotToRegister () {
+    this.registerLink.waitForVisible(5000);
+    this.registerLink.click();
+  }
+
+  login ({ email, password }) {
+    this.emailOrUsernameField.waitForVisible(5000);
+    this.emailOrUsernameField.setValue(email);
+    this.passwordField.setValue(password);
+    this.submit();
+  }
+
+  submit () {
+    this.submitButton.waitForVisible(5000);
+    this.submitButton.click();
+  }
+}
+
+module.exports = new SignInPage();
