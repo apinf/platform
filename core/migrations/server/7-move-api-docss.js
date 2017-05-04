a395f84726d9fca68946f1050bc8f9d1ed37f5c7
jawidahmadi <jawid.ahmadi@apinf.io>
2017-05-04 14:05:16 +0300

    migration

--- core/migrations/server/7-move-api-docss.js
+++ core/migrations/server/7-move-api-docss.js
@@ -4,7 +4,7 @@ import Apis from '/apis/collection';
 import ApiDocs from '/api_docs/collection';
 
 Migrations.add({
-  version: 3,
+  version: 7,
   name: 'Move API documentation from Apis to ApiDocs collection',
   up () {
     // Iterate through Apis collection
