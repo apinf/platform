543c316c5ed05f43cb1efa2f2b7837e717c44882
jawidahmadi <jawid.ahmadi@apinf.io>
2017-05-09 11:59:32 +0300

    name and version updated

---
+++ core/migrations/server/8-move-api-docs.js
@@ -0,0 +1,24 @@
+import { Migrations } from 'meteor/percolate:migrations';
+
+import Apis from '/apis/collection';
+import ApiDocs from '/api_docs/collection';
+
+Migrations.add({
+  version: 8,
+  name: 'Move API documentation from Apis to ApiDocs collection',
+  up () {
+    // Iterate through Apis collection
+    Apis.find().forEach((api) => {
+      // Iterate through ApiDocs collection
+      ApiDocs.find().forEach((apiDoc) => {
+        // Check if fields exist
+        if (api.documentationFileId === apiDoc.fileId) {
+          // perform update operation
+          ApiDocs.update(apiDoc._id, {
+            $set: { type: 'file' },
+          });
+        }
+      });
+    });
+  },
+});
