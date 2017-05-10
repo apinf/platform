a40c6dc903cc6166e04817c72513536e5b0cff33
jawidahmadi <jawid.ahmadi@apinf.io>
2017-05-10 13:34:45 +0300

    decompose the users resources

--- core/server/api.js
+++ core/server/api.js
@@ -3,9 +3,6 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// Meteor packages imports
-import { Meteor } from 'meteor/meteor';
-
 // Meteor contributed packages imports
 import { Restivus } from 'meteor/nimble:restivus';
 
@@ -43,53 +40,6 @@ ApiV1.swagger = {
     },
   },
 };
-
-// Enable user endpoints if authentication is enabled
-// eslint-disable-next-line no-underscore-dangle
-if (ApiV1._config.useDefaultAuth) {
-  // Generates: POST on /api/v1/users and GET, DELETE /api/v1/users/:id for
-  // Meteor.users collection
-  ApiV1.addCollection(Meteor.users, {
-    excludedEndpoints: ['getAll', 'put'],
-    routeOptions: {
-      authRequired: true,
-    },
-    endpoints: {
-      get: {
-        swagger: {
-          description: 'Returns user with given ID.',
-          responses: {
-            200: {
-              description: 'One user.',
-            },
-          },
-        },
-      },
-      post: {
-        authRequired: false,
-        swagger: {
-          description: 'Add user.',
-          responses: {
-            200: {
-              description: 'Return user that was added.',
-            },
-          },
-        },
-      },
-      delete: {
-        roleRequired: 'admin',
-        swagger: {
-          description: 'Delete user.',
-          responses: {
-            200: {
-              description: 'Successful delete.',
-            },
-          },
-        },
-      },
-    },
-  });
-}
 
 // Generate Swagger to route /rest-api/v1/swagger.json
 ApiV1.addSwagger('swagger.json');
