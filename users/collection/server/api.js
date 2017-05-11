e4717d7fb0100c6fa2fd6b4a95c3cd5e73060e92
jawidahmadi <jawid.ahmadi@apinf.io>
2017-05-11 16:08:10 +0300

    add implementation for option values

--- users/collection/server/api.js
+++ users/collection/server/api.js
@@ -8,6 +8,7 @@ import { Meteor } from 'meteor/meteor';
 
 // Collection imports
 import ApiV1 from '/core/server/api';
+import Organizations from '/organizations/collection';
 
 // Npm packages imports
 import _ from 'lodash';
@@ -25,68 +26,99 @@ if (ApiV1._config.useDefaultAuth) {
       getAll: {
         swagger: {
           description: 'Returns users',
+          parameters: [
+            ApiV1.swagger.params.optionalSearch,
+            ApiV1.swagger.params.organization_id,
+            ApiV1.swagger.params.skip,
+            ApiV1.swagger.params.limit,
+            ApiV1.swagger.params.sort_by,
+          ],
           responses: {
             200: {
               description: 'users',
             },
+            400: {
+              description: 'Bad query parameters',
+            },
           },
         },
         action () {
-          // Init response object
-          let response = {};
-          // Get queryParams i.e. /users?username=something
           const queryParams = this.queryParams;
+
+          const query = {};
+          const options = {};
           // Handle query params
           if (queryParams && !_.isEmpty(queryParams)) {
-            // Check queryParams for username
-            if (queryParams.username) {
-              // Fetch only Users with given username
-              const findByUsername = Meteor.users.find(
-                { username: queryParams.username }
-              ).fetch();
-              // Construct response
-              response = {
-                statusCode: 200,
-                body: {
-                  status: 'success',
-                  data: findByUsername,
-                },
-              };
-            } else if (queryParams.sort_by) {
-              // Fetch Users and sort by username
-              const findByUsername = Meteor.users.find(
-                { }, { sort: { username: -1 } }
-              ).fetch();
-              // Construct response
-              response = {
-                statusCode: 200,
-                body: {
-                  status: 'success',
-                  data: findByUsername,
-                },
-              };
-            } else {
-              // Error: bad query params
-              response = {
-                statusCode: 400,
-                body: {
-                  status: 'fail',
-                  message: 'Bad query parameters',
-                },
-              };
+            // parse query parameters
+            if (queryParams.organization_id) {
+              // Get organization document with specified ID
+              const organization = Organizations.findOne(queryParams.organization_id);
+
+              // Make sure Organization exists
+              if (organization) {
+                // Get list of managed API IDs
+                query._id = { $in: organization.managerIds() };
+              }
             }
-          } else {
-          // Construct response
-            response = {
+
+            if (queryParams.limit) {
+              options.limit = parseInt(queryParams.limit, 10);
+            }
+
+            if (queryParams.skip) {
+              options.skip = parseInt(queryParams.skip, 10);
+            }
+
+            if (queryParams.sort_by &&
+                (queryParams.sort_by === 'username' ||
+                 queryParams.sort_by === 'created_at' ||
+                 queryParams.sort_by === 'updated_at' ||
+                 queryParams.sort_by === 'organization'
+                )
+               ) {
+              options.sortBy = queryParams.sort_by;
+            }
+
+            // Pass an optional search string for looking up inventory.
+            if (queryParams.q) {
+              query.$or = [
+                {
+                  username: {
+                    $regex: queryParams.q,
+                    $options: 'i', // case-insensitive option
+                  },
+                },
+                {
+                  profile: {
+                    $regex: queryParams.q,
+                    $options: 'i', // case-insensitive option
+                  },
+                },
+                {
+                  emails: {
+                    $regex: queryParams.q,
+                    $options: 'i', // case-insensitive option
+                  },
+                },
+              ];
+            }
+            // Construct response
+            return {
               statusCode: 200,
               body: {
                 status: 'success',
-                data: Meteor.users.find().fetch(),
+                data: Meteor.users.find(query, options).fetch(),
               },
             };
           }
-          // Return constructed response
-          return response;
+          // Construct response
+          return {
+            statusCode: 200,
+            body: {
+              status: 'success',
+              data: Meteor.users.find().fetch(),
+            },
+          };
         },
 
 
