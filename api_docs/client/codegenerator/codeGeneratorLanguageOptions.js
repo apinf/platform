a47699102cd327da75559d963dc73a39025b5c79
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-15 14:01:01 +0300

    Cleanup

--- api_docs/client/codegenerator/codeGeneratorLanguageOptions.js
+++ api_docs/client/codegenerator/codeGeneratorLanguageOptions.js
@@ -3,11 +3,6 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// The most part of list items can convert to user friendly name by standard method
-// The method is to replace dash on space and to do the capital letter of each word
-// Some language names can't be convert by standard method.
-// Variable 'specificLanguageNames' keeps specific names
-
 
 export default codeGeneratorOptions = [
   {
