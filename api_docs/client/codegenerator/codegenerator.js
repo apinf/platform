98a880e5db6c9256d5aa0e3a18e46de000bb5f6e
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-15 16:32:35 +0300

    Cleanup

--- api_docs/client/codegenerator/codegenerator.js
+++ api_docs/client/codegenerator/codegenerator.js
@@ -4,16 +4,7 @@ You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
 // Meteor packages imports
-import { HTTP } from 'meteor/http';
-import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
-
-// Meteor contributed packages imports
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-import { TAPi18n } from 'meteor/tap:i18n';
-
-// Npm packages imports
-import _ from 'lodash';
 
 // APINF imports
 import codeGeneratorSchema from './schema';
@@ -23,19 +14,5 @@ Template.sdkCodeGeneratorModal.helpers({
   codeGeneratorSchema () {
     // Create simple schema for sdk modal
     return codeGeneratorSchema;
-  },
-  // From template AutoForm we don't have access to instance of this template
-  // getTemplateInstance return object that containts the necessary varaibles
-  getTemplateInstance () {
-    // Get reference to template instance
-    const instance = Template.instance();
-
-    // Create object with instance varaibles
-    return {
-      codegenServer: instance.data.host,
-      callRequest: instance.callRequest,
-      documentationFileURL: instance.documentationFileURL,
-      urlParameters: instance.urlParameters,
-    };
   },
 });
