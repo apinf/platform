5a0556dae472eff3615762e74e344cc56b840f94
Damir Mustafin <frenchthebread@gmail.com>
2017-05-16 19:00:14 +0300

    Show notification to a user when message is successfully updated

--- proxy_backends/client/form/autoform.js
+++ proxy_backends/client/form/autoform.js
@@ -275,7 +275,15 @@ AutoForm.hooks({
             proxyBackend.proxyId,
             proxyBackend.emq.settings.acl,
           (err) => {
-            if (err) sAlert.error(err);
+            if (err) {
+              sAlert.error(err);
+            } else {
+              // Get success message translation
+              const message = TAPi18n.__('proxyBackendForm_successMessage');
+
+              // Alert the user of success
+              sAlert.success(message);
+            }
           });
         }
       // Form Type - INSERT
