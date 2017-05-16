2ab0b8d375e39a4aa1aa29740d8ce65ba37d052e
Massimo Rangoni <maxappbr@gmail.com>
2017-05-16 09:56:24 -0300

    Fix OrganizationApis permissions

--- organization_apis/collection/helpers/organizations_apis.js
+++ organization_apis/collection/helpers/organizations_apis.js
@@ -3,12 +3,6 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// Meteor packages imports
-import { Meteor } from 'meteor/meteor';
-
-// Npm packages imports
-import _ from 'lodash';
-
 // Collection imports
 import Apis from '/apis/collection';
 import Organizations from '/organizations/collection';
@@ -28,6 +22,5 @@ OrganizationApis.helpers({
 
     // Return organization
     return organization;
-
   },
 });
--- organization_apis/collection/permissions.js
+++ organization_apis/collection/permissions.js
@@ -3,17 +3,26 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
+// Meteor packages imports
+import { Roles } from 'meteor/alanning:roles';
+
 // Collection imports
 import OrganizationApis from './';
 
 OrganizationApis.allow({
-  insert () {
-    return true;
+  insert (userId) {
+    // Check if current user is admin
+    const userIsAdmin = Roles.userIsInRole(userId, ['admin']);
+
+    // Only admin can insert
+    if (userIsAdmin) {
+      return true;
+    }
+
+    return false;
   },
-  update () {
-    return true;
-  },
-  remove () {
-    return true;
+  remove (userId, organizationApi) {
+    // Check if current user can remove
+    return organizationApi.organization().currentUserCanManage();
   },
 });
