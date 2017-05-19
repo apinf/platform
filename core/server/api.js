a621c3bae717d194cfe853bd59deea51e014e1ec
matleppa <matti.leppanen@apinf.io>
2017-05-19 11:10:07 +0300

    Resolved conflict

--- core/server/api.js
+++ core/server/api.js
@@ -1,13 +1,14 @@
 /* Copyright 2017 Apinf Oy
-This file is covered by the EUPL license.
-You may obtain a copy of the licence at
-https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+ This file is covered by the EUPL license.
+ You may obtain a copy of the licence at
+ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
 // Meteor contributed packages imports
+import { Meteor } from 'meteor/meteor';
 import { Restivus } from 'meteor/nimble:restivus';
 
 const ApiV1 = new Restivus({
-  apiPath: 'api',
+  apiPath: 'rest',
   version: 'v1',
   defaultHeaders: {
     'Content-Type': 'application/json',
@@ -17,8 +18,7 @@ const ApiV1 = new Restivus({
   enableCors: true,
 });
 
-// Add Restivus Swagger configuration
-// - meta, tags, params, definitions
+// Add Restivus Swagger configuration - meta, tags, params, definitions
 ApiV1.swagger = {
   meta: {
     swagger: '2.0',
@@ -26,22 +26,49 @@ ApiV1.swagger = {
       version: '1.0.0',
       title: 'Admin API',
     },
+    securityDefinitions: {
+      userSecurityToken: {
+        in: 'header',
+        name: 'X-Auth-Token',
+        type: 'apiKey',
+      },
+      userId: {
+        in: 'header',
+        name: 'X-User-Id',
+        type: 'apiKey',
+      },
+    },
   },
   tags: {
-    apis: 'Apis',
+    api: 'APIs',
+    organization: 'Organizations',
   },
   params: {
+    apiId: {
+      name: 'id',
+      in: 'path',
+      description: 'ID of API',
+      required: true,
+      type: 'string',
+    },
+    organizationId: {
+      name: 'id',
+      in: 'path',
+      description: 'ID of Organization',
+      required: true,
+      type: 'string',
+    },
     optionalSearch: {
       name: 'q',
       in: 'query',
-      description: 'Pass an optional search string for looking up inventory.',
+      description: 'An optional search string for looking up inventory.',
       required: false,
       type: 'string',
     },
-    organization_id: {
-      name: 'organization ID',
+    organizationApi: {
+      name: 'organization',
       in: 'query',
-      description: 'Users of indicated organization',
+      description: 'An optional organization id will limit results to the given organization.',
       required: false,
       type: 'string',
     },
@@ -64,6 +91,171 @@ ApiV1.swagger = {
       minimum: 0,
       maximum: 50,
     },
+    lifecycle: {
+      name: 'lifecycle',
+      in: 'query',
+      description: 'Limit the listing based on lifecycle status of APIs.',
+      required: false,
+      type: 'string',
+      enum: ['design', 'development', 'testing', 'production', 'deprecated'],
+    },
+    api: {
+      name: 'api',
+      in: 'body',
+      description: 'Data for adding or editing API',
+      schema: {
+        $ref: '#/definitions/api',
+      },
+    },
+    organization: {
+      name: 'organization',
+      in: 'body',
+      description: 'Data for adding or editing Organization',
+      schema: {
+        $ref: '#/definitions/organization',
+      },
+    },
+  },
+  definitions: {
+    // The schema defining the type used for the body parameter.
+    api: {
+      required: ['name', 'url'],
+      properties: {
+        name: {
+          type: 'string',
+          example: 'My REST API',
+        },
+        description: {
+          type: 'string',
+          example: 'My REST API description',
+        },
+        url: {
+          type: 'string',
+          format: 'url',
+          example: 'https://my.rest.api.com/v1',
+        },
+        lifecycleStatus: {
+          type: 'string',
+          enum: ['design', 'development', 'testing', 'production', 'deprecated'],
+        },
+      },
+    },
+    organization: {
+      required: ['name', 'url'],
+      properties: {
+        name: {
+          type: 'string',
+          example: 'Company',
+        },
+        description: {
+          type: 'string',
+          example: 'Description about company',
+        },
+        url: {
+          type: 'string',
+          format: 'url',
+          example: 'https://organization.com',
+        },
+        contact_name: {
+          type: 'string',
+          description: 'Name of company manager',
+          example: 'David Bar',
+        },
+        contact_phone: {
+          type: 'string',
+          description: 'Phone number of company manager',
+          example: '+7 000 000 00 00',
+        },
+        contact_email: {
+          type: 'string',
+          format: 'email',
+          description: 'E-mail address of company manager',
+          example: 'company-mail@gmail.com',
+        },
+        facebook: {
+          type: 'string',
+          format: 'url',
+          description: 'Link to Facebook',
+          example: 'http://url.com',
+        },
+        twitter: {
+          type: 'string',
+          format: 'url',
+          description: 'Link to Twitter',
+          example: 'http://url.com',
+        },
+        instagram: {
+          type: 'string',
+          format: 'url',
+          description: 'Link to Instagram',
+          example: 'http://url.com',
+        },
+        linkedin: {
+          type: 'string',
+          format: 'url',
+          description: 'Link to Linked In',
+          example: 'http://url.com',
+        },
+      },
+    },
+  },
+};
+
+// Enable user endpoints if authentication is enabled
+if (ApiV1._config.useDefaultAuth) {
+  // Meteor.users collection
+  ApiV1.addCollection(Meteor.users, {
+    excludedEndpoints: ['put', 'patch'],
+    routeOptions: {
+      authRequired: true,
+    },
+    endpoints: {
+      // GET /rest/v1/users/:id
+      get: {
+        swagger: {
+          description: 'Returns user with given ID.',
+          responses: {
+            200: {
+              description: 'One user.',
+            },
+          },
+        },
+      },
+      // POST /rest/v1/users/
+      post: {
+        authRequired: false,
+        swagger: {
+          description: 'Add user.',
+          responses: {
+            200: {
+              description: 'Return user that was added.',
+            },
+          },
+        },
+      },
+      // DELETE /rest/v1/users/:id
+      delete: {
+        roleRequired: 'admin',
+        swagger: {
+          description: 'Delete user.',
+          responses: {
+            200: {
+              description: 'Successful delete.',
+            },
+          },
+        },
+      },
+    },
+    limit: {
+      name: 'limit',
+      in: 'query',
+      description: 'Maximum number of records to return in query.',
+      required: false,
+      type: 'integer',
+      format: 'int32',
+      minimum: 0,
+      maximum: 50,
+    },
     sort_by: {
       name: 'sort By',
       in: 'query',
@@ -71,10 +263,10 @@ ApiV1.swagger = {
       required: false,
       type: 'string',
     },
-  },
-};
+  });
+}
 
-// Generate Swagger to route /rest-api/v1/swagger.json
+// Generate Swagger to route /rest/v1/swagger.json
 ApiV1.addSwagger('swagger.json');
 
 export default ApiV1;
