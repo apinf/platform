b0a7967c3e0911aa173601e8b5bc90105d8c1c0c
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-05-24 15:07:41 +0300

    Move schema into template helper (for reactive l10n)

--- api_docs/client/code_generator/modal.js
+++ api_docs/client/code_generator/modal.js
@@ -6,13 +6,40 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 // Meteor packages imports
 import { Template } from 'meteor/templating';
 
+// Meteor contributed packages imports
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { TAPi18n } from 'meteor/tap:i18n';
+
 // APINF imports
-import codeGeneratorSchema from './schema';
+import codeGeneratorLanguageOptions from './code_generator_language_options';
 
 Template.sdkCodeGeneratorModal.helpers({
   // Schema for SDK Code Generator form
   codeGeneratorSchema () {
     // Create simple schema for sdk modal
+    const codeGeneratorSchema = new SimpleSchema({
+      selectLanguage: {
+        label: TAPi18n.__('sdkCodeGeneratorModal_labelText_selectLanguage'),
+        type: String,
+        autoform: {
+          afFieldInput: {
+            firstOption: TAPi18n.__('sdkCodeGeneratorModal_firstOption_language'),
+          },
+          options () {
+            return codeGeneratorLanguageOptions;
+          },
+        },
+      },
+      host: {
+        type: String,
+        optional: false,
+      },
+      documentationFileURL: {
+        type: String,
+        optional: false,
+      },
+    });
+
     return codeGeneratorSchema;
   },
 });
