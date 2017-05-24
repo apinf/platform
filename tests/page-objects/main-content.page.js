e326d5e5424cda30b27945dabe8b1cb6f21ce82d
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-05-24 13:25:53 -0300

    Test main page
    - loginAsOrCreateUser still flaky

--- tests/page-objects/main-content.page.js
+++ tests/page-objects/main-content.page.js
@@ -9,7 +9,31 @@ import Page from './page';
 
 class MainContentPage extends Page {
 
+  get dropdownToggle () { return browser.element('.dropdown-toggle'); }
+  get dropdownMenu () { return browser.element('.dropdown'); }
   get signOutLink () { return browser.element('.sign-out'); }
+  get username () { return browser.element('.username'); }
+
+  get someoneIsLoggedIn () {
+    return this.dropdownMenu.isVisible();
+  }
+
+  get nobodyIsLoggedIn () {
+    return !this.someoneIsLoggedIn;
+  }
+
+  isUserLoggedIn (username) {
+    let loggedIn = false;
+    if (this.someoneIsLoggedIn) {
+      loggedIn = this.username.getText() === username;
+    }
+    return loggedIn;
+  }
+
+  clickOnDropdownToggle () {
+    this.dropdownToggle.waitForVisible(5000);
+    this.dropdownToggle.click();
+  }
 
   logOut () {
     this.signOutLink.waitForVisible(5000);
