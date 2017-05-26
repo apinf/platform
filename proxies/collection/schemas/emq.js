b5e1c51b2f78d7c4ce1d39b2ec75742489a1467d
Damir Mustafin <frenchthebread@gmail.com>
2017-05-26 03:38:27 +0300

    Cleanup package import and regEx

--- proxies/collection/schemas/emq.js
+++ proxies/collection/schemas/emq.js
@@ -3,9 +3,6 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// Meteor packages imports
-import { SimpleSchema } from 'meteor/aldeed:simple-schema';
-
 export default {
   emq: {
     type: Object,
@@ -35,7 +32,6 @@ export default {
   },
   'emq.httpApi': {
     type: String,
-    // regEx: SimpleSchema.RegEx.Url,
     autoform: {
       placeholder: 'https://user:pasword@host[:port][path]',
     },
