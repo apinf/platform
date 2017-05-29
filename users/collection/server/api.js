c5c55d7f89d9033b8ab585f2f678444ed5d14fab
matleppa <matti.leppanen@apinf.io>
2017-05-29 10:38:59 +0300

    Added route for updates

--- users/collection/server/api.js
+++ users/collection/server/api.js
@@ -420,161 +420,105 @@ ApiV1.addCollection(Meteor.users, {
 });
 
 // Request /rest/v1/users/:id for Users collection
-ApiV1.addRoute('Meteor.users/:id', {
-  // Modify the entity with the given :id with the data contained in the request body.
-  // put: {
-  //   authRequired: true,
-  //   swagger: {
-  //     tags: [
-  //       ApiV1.swagger.tags.user,
-  //     ],
-  //     description: 'Update a User',
-  //     parameters: [
-  //       ApiV1.swagger.params.userId,
-  //       ApiV1.swagger.params.user,
-  //     ],
-  //     responses: {
-  //       200: {
-  //         description: 'User successfully edited.',
-  //       },
-  //       401: {
-  //         description: 'Authentication is required',
-  //       },
-  //       403: {
-  //         description: 'User does not have permission',
-  //       },
-  //       404: {
-  //         description: 'User is not found',
-  //       },
-  //     },
-  //     security: [
-  //       {
-  //         userSecurityToken: [],
-  //         userId: [],
-  //       },
-  //     ],
-  //   },
-  //   action () {
-  //     // Get ID of User
-  //     const userId = this.urlParams.id;
-  //     // Get data from body parameters
-  //     const bodyParams = this.bodyParams;
-  //     console.log('bodyParams=', bodyParams);
-  //     let previousPassword;
-  //     let previousUsername;
-  //     let previousCompany;
-  //
-  //     // Try to change password
-  //     if (bodyParams.old_psw && bodyParams.new_psw) {
-  //       // Save previous password in case restore is needed later
-  //       previousPassword = Meteor.users.services.password.bcrypt;
-  //       Accounts.changePassword(bodyParams.old_psw, bodyParams.new_psw, (error) => {
-  //         if (error) {
-  //           return {
-  //             statusCode: 400,
-  //             body: {
-  //               status: 'Invalid old/new password',
-  //             },
-  //           };
-  //         }
-  //       });
-  //     }
-  //     // Try to change username
-  //     if (bodyParams.username) {
-  //       // Save previous username in case restore is needed later
-  //       previousUsername = Meteor.users.username;
-  //       Accounts.setUsername(userId, bodyParams.username, (error) => {
-  //         if (error) {
-  //           // Try to restore old password
-  //           if (previousPassword) {
-  //             Accounts.changePassword(bodyParams.new_psw, previousPassword);
-  //           }
-  //           return {
-  //             statusCode: 400,
-  //             body: {
-  //               status: 'Username update failed!',
-  //             },
-  //           };
-  //         }
-  //       });
-  //     }
-  //
-  //     // Try to change company name
-  //     if (bodyParams.company) {
-  //       // Save previous company name in case restore is needed later (for future use)
-  //       previousCompany = Meteor.users.profile.company;
-  //       Meteor.users.update(userId, { $set: { 'profile.company': bodyParams.company } });
-  //     }
-  //
-  //     return {
-  //       statusCode: 200,
-  //       body: {
-  //         status: 'Success updating',
-  //         data: Meteor.users.findOne(userId),
-  //       },
-  //     };
-  //   },
-  // },
-  // Delete a user
-  // delete: {
-  //   authRequired: true,
-  //   swagger: {
-  //     tags: [
-  //       ApiV1.swagger.tags.user,
-  //     ],
-  //     description: 'Deletes the identified Organization from catalog.',
-  //     parameters: [
-  //       ApiV1.swagger.params.userId,
-  //     ],
-  //     responses: {
-  //       200: {
-  //         description: 'User successfully removed.',
-  //       },
-  //       400: {
-  //         description: 'Invalid input, invalid object',
-  //       },
-  //       401: {
-  //         description: 'Authentication is required',
-  //       },
-  //       403: {
-  //         description: 'User does not have permission',
-  //       },
-  //       404: {
-  //         description: 'User not found',
-  //       },
-  //     },
-  //     security: [
-  //       {
-  //         userSecurityToken: [],
-  //         userId: [],
-  //       },
-  //     ],
-  //   },
-  //   action () {
-  //     // Get ID of Organization
-  //     const userId = this.urlParams.id;
-  //     const user = Meteor.users.findOne(userId).fetch();
-  //     console.log('tuhottava user=', user);
-  //     if (user) {
-  //       // Remove User account
-  //       Meteor.call('deleteAccount', userId);
-  //
-  //       return {
-  //         statusCode: 200,
-  //         body: {
-  //           status: 'User successfully removed',
-  //         },
-  //       };
-  //     }
-  //
-  //     // User doesn't exist
-  //     return {
-  //       statusCode: 404,
-  //       body: {
-  //         status: 'Fail',
-  //         message: 'User is not found with specified ID',
-  //       },
-  //     };
-  //   },
-  // },
+ApiV1.addRoute('Meteor.users/updates', {
+  getAll: {
+    swagger: {
+      tags: [
+        ApiV1.swagger.tags.users,
+      ],
+      description: 'Returns users',
+      parameters: [
+        ApiV1.swagger.params.since,
+        ApiV1.swagger.params.organization_id,
+        ApiV1.swagger.params.skip,
+        ApiV1.swagger.params.limit,
+      ],
+      responses: {
+        200: {
+          description: 'success',
+        },
+        400: {
+          description: 'Bad query parameters',
+        },
+      },
+    },
+    action () {
+      const queryParams = this.queryParams;
+
+      console.log('update q=', queryParams);
+      const query = {};
+      const options = {};
+      const searchCondition = {};
+
+      // parse query parameters
+      if (queryParams.organization_id) {
+        // Get organization document with specified ID
+        const organization = Organizations.findOne(queryParams.organization_id);
+
+        // Make sure Organization exists
+        if (organization) {
+          // Get list of managed API IDs
+          query._id = { $in: organization.managerIds };
+        }
+      }
+
+      if (queryParams.limit) {
+        options.limit = parseInt(queryParams.limit, 10);
+      } else {
+        // By default 100 users is returned
+        options.limit = 100;
+      }
+
+      if (queryParams.skip) {
+        options.skip = parseInt(queryParams.skip, 10);
+      }
+
+      if (queryParams.since) {
+        query.createdAt = {
+          $lt: new Date(),
+          $gte: new Date(new Date().setDate(new Date().getDate() - queryParams.since)),
+        };
+      }
+
+      // Get all users
+      const userList = Meteor.users.find(query, options).fetch();
+      // Get Organization names and ids for every User
+      if (userList) {
+        // Loop through user list one by one
+        userList.forEach((userData) => {
+          // Array for Organization name and id
+          const orgDataList = [];
+          // Get user id
+          const userId = userData._id;
+          // Find all Organizations, where User belongs to
+          const organizations = Organizations.find({
+            managerIds: userId,
+          }).fetch();
+          // If there are Users' Organizations
+          if (organizations.length > 0) {
+            // Loop through Users' Organizations
+            organizations.forEach((organization) => {
+              const orgData = {};
+              // Put Organization name and id into an object
+              orgData.organization_id = organization._id;
+              orgData.organization_name = organization.name;
+              // Add this Organization data into Users' organization data list
+              orgDataList.push(orgData);
+            });
+            // Add Organizations' information to Users' data
+            userData.organization = orgDataList;
+          }
+        });
+      }
+      // Construct response
+      return {
+        statusCode: 200,
+        body: {
+          status: 'success',
+          data: userList,
+        },
+      };
+    },
+
+  },
 });
