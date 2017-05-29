8417124008dea073e8259520ec897fcbe8f0abb9
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-05-29 18:50:15 -0300

    Set admin user always the same
    - admin / password will be the default first user, or at least an admin user on testing environment

--- tests/data/user.js
+++ tests/data/user.js
@@ -3,6 +3,6 @@ export const username = `me_${timestamp}`;
 export const email = `${username}@apinf.io`;
 export const password = 'password';
 
-export const adminUsername = `adm${timestamp}`;
+export const adminUsername = `admin`;
 export const adminEmail = `${adminUsername}@apinf.io`;
 export const adminPassword = password;
