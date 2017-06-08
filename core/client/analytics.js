6beaefb18dd34ec495d35a1c7634151d834ae4a8
illya <in11092008@gmail.com>
2017-06-08 11:31:43 +0300

    Get Google Analytics tracking ID from branding

--- core/client/analytics.js
+++ core/client/analytics.js
@@ -3,10 +3,25 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-import OKGAnalytics from '@okgrow/auto-analytics';
+import { Meteor } from 'meteor/meteor';
 
-const analytics = {
-  'Google Analytics': { trackingId: '' },
-};
-// eslint-disable-next-line
-OKGAnalytics(analytics);
+import { OKGAnalytics as okGAnalytics } from '@okgrow/auto-analytics';
+
+import Branding from '/branding/collection';
+
+Meteor.autorun(() => {
+  // Get branding
+  const branding = Branding.findOne();
+
+  if (branding) {
+    const googleAnalytics = branding.analytics.googleAnalytics;
+
+    if (googleAnalytics) {
+      const settings = {
+        'Google Analytics': { trackingId: googleAnalytics },
+      };
+      // Run OKGrow analytics
+      okGAnalytics(settings);
+    }
+  }
+});
