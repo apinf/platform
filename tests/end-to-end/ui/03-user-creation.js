0c7a1287901613f7d5ed361f02c3d083892a46e3
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-20 12:03:02 -0300

    Make admin user creation test to create settings

--- tests/end-to-end/ui/03-user-creation.js
+++ tests/end-to-end/ui/03-user-creation.js
@@ -10,19 +10,41 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 
 import signUpPage from '../../page-objects/signup.page';
 import mainContent from '../../page-objects/main-content.page';
+import settingsPage from '../../page-objects/settings.page';
 
-import { username, email, password } from '../../data/user';
-
+import {
+  username,
+  email,
+  password,
+  adminUsername,
+  adminEmail,
+  adminPassword,
+} from '../../data/user';
+import { githubClientId, githubClientSecret } from '../../data/settings';
 
 describe('03 user creation', () => {
   before(() => {
     signUpPage.open();
   });
 
-  it('create user', () => {
+  it('create admin user', () => {
+    // Create Admin user
+    signUpPage.registerNewUser({ adminUsername, adminEmail, adminPassword });
+
+    // Setup settings
+    signUpPage.gotModalToSetup();
+    settingsPage.setupGithub({ githubClientId, githubClientSecret });
+
+    mainContent.signOutLink.waitForExist(5000);
+    mainContent.signOutLink.isVisible().should.be.true;
+    mainContent.logOut();
+  });
+
+  it('create normal user', () => {
     signUpPage.registerNewUser({ username, email, password });
 
     mainContent.signOutLink.waitForExist(5000);
     mainContent.signOutLink.isVisible().should.be.true;
+    mainContent.logOut();
   });
 });
