0388954e234257c328b1d6c6074df7eca1a5ffad
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-06-22 11:23:35 -0300

    Add getters for settings page

--- tests/page-objects/settings.page.js
+++ tests/page-objects/settings.page.js
@@ -10,12 +10,26 @@ import Page from './page';
 class SettingsPage extends Page {
 
   // Left Menu
+  get buttonProfileLink () { return browser.element('#button-profile'); }
+  get buttonAccountLink () { return browser.element('#button-account'); }
+  get buttonBrandingLink () { return browser.element('#button-branding'); }
+  get buttonSettingsLink () { return browser.element('#button-settings'); }
+  get buttonProxiesLink () { return browser.element('#button-proxies'); }
 
   // Profile
+  get username () { return browser.element('#username'); }
+  get company () { return browser.element('#company'); }
+  get updateButton () { return browser.element('#update-button'); }
 
   // Account
+  get passwordOld () { return browser.element('#password-old'); }
+  get passwordNew () { return browser.element('#password-new'); }
+  get passwordNewAgain () { return browser.element('#password-new-again'); }
+  get submitPasswordButton () { return browser.element('#submit-password'); }
 
   // Branding
+  get brandingUpdateButton () { return browser.element('#branding-update-button'); }
+  get brandingSaveButton () { return browser.element('#branding-save-button'); }
 
   // Settings
   get githubId () { return browser.element('#github-id'); }
