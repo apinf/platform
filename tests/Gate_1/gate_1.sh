fbcd6aa821e85709cee3ffb093075948bc9b8c32
wuder4 <Joonas.heikkila@samposoftware.com>
2017-07-14 15:02:33 +0300

    changing scripts to run all files in directory instead of hardcoded files

--- tests/Gate_1/gate_1.sh
+++ tests/Gate_1/gate_1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Running gate 1 tests"
-python -m robot 1.registering.robot 2.Login.robot 3.addapi.robot 4.apicat.robot 5.proxy.robot 6.profile.robot
+echo "Running all .robot tests"
+files=$(ls *.robot)
+python -m robot $files
--- tests/Gate_2/gate_2.sh
+++ tests/Gate_2/gate_2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Running gate 1 tests"
-python -m robot 1.registering.robot 2.Login.robot 3.addapi.robot 4.addorg.robot 5.profile_admin.robot 6.apicat.robot 7.orgcat.robot 8.proxy.robot 9.profile.robot
+echo "Running all .robot tests"
+files=$(ls *.robot)
+python -m robot $files
