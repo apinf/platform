f23cd5464e4f328c9e298156bad683d3814592e9
wuder4 <Joonas.heikkila@samposoftware.com>
2017-07-14 13:05:20 +0300

    adding script files that automatically run gate 1 and 2 tests

---
+++ tests/Gate_2/gate_2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Running gate 1 tests"
+python -m robot 1.registering.robot 2.Login.robot 3.addapi.robot 4.addorg.robot 5.profile_admin.robot 6.apicat.robot 7.orgcat.robot 8.proxy.robot 9.profile.robot
