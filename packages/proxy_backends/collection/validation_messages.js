c9ff48e4c3f7a65eafd20aa3ff228b3ca9058bbf
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-08-15 12:52:54 +0300

    Use relative import

--- packages/proxy_backends/collection/validation_messages.js
+++ packages/proxy_backends/collection/validation_messages.js
@@ -8,7 +8,7 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { TAPi18n } from 'meteor/tap:i18n';
 
 // APInf imports
-import { proxyBasePathRegEx, apiBasePathRegEx } from '/packages/proxy_backends/collection/regex';
+import { proxyBasePathRegEx, apiBasePathRegEx } from './regex';
 
 const invalidProxyBasePathMessage = TAPi18n.__('invalidProxyBasePathMessage');
 const invalidApiBasePathMessage = TAPi18n.__('invalidApiBasePathMessage');
