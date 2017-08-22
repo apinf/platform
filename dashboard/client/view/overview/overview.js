567615be3a85f8845d55828aa351f8f3223fe0a9
Daria Voytova <vdariaclem@gmail.com>
2017-08-22 18:34:24 +0300

    Create a new Dashboard module

---
+++ dashboard/client/view/overview/overview.js
@@ -0,0 +1,25 @@
+/* Copyright 2017 Apinf Oy
+ This file is covered by the EUPL license.
+ You may obtain a copy of the licence at
+ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Template } from 'meteor/templating';
+
+// APInf imports
+// eslint-disable-next-line max-len
+import { arrowDirection, percentageValue, summaryComparing } from '/dashboard/client/compare_indicating';
+
+Template.dashboardOverviewStatistic.helpers({
+  arrowDirection (parameter) {
+    // Provide compared data
+    return arrowDirection(parameter, this);
+  },
+  percentages (parameter) {
+    // Provide compared data
+    return percentageValue(parameter, this);
+  },
+  overviewComparing (parameter) {
+    return summaryComparing(parameter, this);
+  },
+});
