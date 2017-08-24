da322716dc328ecf3c34e1d34877a01078a3fa72
matleppa <matti.leppanen@apinf.io>
2017-08-24 12:39:04 +0300

    Removed outcommented lines

--- apinf_packages/users/client/account/password/schema.js
+++ apinf_packages/users/client/account/password/schema.js
@@ -9,17 +9,14 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 const updatePasswordSchema = new SimpleSchema({
   old: {
     type: String,
-    // max: 50,
   },
   new: {
     type: String,
     min: 6,
-    // max: 50,
   },
   confirm: {
     type: String,
     min: 6,
-    // max: 50,
     // TODO: Resolve consistent-return
     // eslint-disable-next-line consistent-return
     custom () {
