d2bb669ffdd4dcc7bf00a058c67398292cae8289
vellames <c.vellames@outlook.com>
2017-08-27 19:34:39 -0300

    imports meteor and template to js file

--- apinf_packages/organizations/client/profile/featured_apis/featured_apis.js
+++ apinf_packages/organizations/client/profile/featured_apis/featured_apis.js
@@ -3,8 +3,15 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
+// Meteor packages imports
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+// Meteor contributed packages imports
 import { Roles } from 'meteor/alanning:roles';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+
+// Models imports
 import Organizations from '/apinf_packages/organizations/collection';
 
 Template.organizationFeaturedApis.onCreated(() => {
