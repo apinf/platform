ee6091f8498e3050d223cea462c38dc006fcbfd0
vellames <c.vellames@outlook.com>
2017-08-27 17:49:01 -0300

    verifies if user is admin

---
+++ apinf_packages/organizations/client/profile/featured_apis/featured_apis.js
@@ -0,0 +1,19 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+import { Roles } from 'meteor/alanning:roles';
+
+Template.organizationFeaturedApis.helpers({
+    isAdminOrOrganizationManager(){
+
+        // Check if user is admin
+        const userIsAdmin = Roles.userIsInRole(Meteor.userId, ['admin']);
+
+        //console.log(organizationManagers);
+        console.log(oi);
+
+        return userIsAdmin;
+    }
+})
