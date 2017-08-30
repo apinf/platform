ca3b18550c8764081bcfd1db790c2c2292ea22b2
Daria Voytova <vdariaclem@gmail.com>
2017-08-30 17:09:22 +0300

    Create case a user has an API
    - Create a new component Date range picker
    - Separate API Analytic page to header, body, date range picker templates
    - Display API Ananlytic header & body if a user has an API is connected to current proxy

---
+++ apinf_packages/dashboard/client/analytic/header/header.js
@@ -0,0 +1,23 @@
+
+/* Copyright 2017 Apinf Oy
+ This file is covered by the EUPL license.
+ You may obtain a copy of the licence at
+ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Template } from 'meteor/templating';
+
+// Collection imports
+import Apis from '/apinf_packages/apis/collection';
+import Proxies from '/apinf_packages/proxies/collection';
+
+Template.apiAnalyticPageHeader.helpers({
+  api () {
+    return Apis.findOne();
+  },
+  proxyName () {
+    const proxy = Proxies.findOne();
+
+    return proxy.name;
+  },
+});
