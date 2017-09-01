0868aa3e8bd8ff07ddceeb5b72810d3e4a6002f5
yannunes <yannunes@gmail.com>
2017-08-31 23:15:16 -0300

    Fix lint

--- tests/endpoints/organization/organization.test.js
+++ tests/endpoints/organization/organization.test.js
@@ -1,5 +1,13 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+
 describe('simple test', () => {
   it('should pass', (done) => {
-    done()
-  })
-})
+    done();
+  });
+});
