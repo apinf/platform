35f9051bf1f8d9032ef6092e0b4b0b8d15e692ad
saransh-dev <saranshdevsingh@gmail.com>
2017-09-01 16:59:48 +0530

    remove extra varibales

--- apinf_packages/apis/client/profile/settings/details/autoform.js
+++ apinf_packages/apis/client/profile/settings/details/autoform.js
@@ -7,10 +7,19 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 AutoForm.hooks({
   apiDetailsForm: {
     onSuccess () {
+      // Getting new slug from this.udateDoc
+      const slug = this.updateDoc.$set.slug;
+      if (slug) {
+      // Redirect to updated API with new slug.
+      // It is use while api's name will update slug have to change
+      // that's mean routing change
+        FlowRouter.go('viewApi', { slug });
+      }
       // Get success message translation
       const message = TAPi18n.__('apiDetailsForm_text_updateInformation');
 
