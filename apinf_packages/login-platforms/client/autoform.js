47dde166c28344c0261d8b22fcd000dc09e05275
yannunes <yannunes@gmail.com>
2017-09-04 21:00:27 -0300

    Add autoform to Login Platforms

---
+++ apinf_packages/login-platforms/client/autoform.js
@@ -0,0 +1,28 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Meteor } from 'meteor/meteor';
+
+// Meteor contributed packages imports
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+
+AutoForm.hooks({
+  settings: {
+    onSuccess () {
+      // Call for configuration updates
+      Meteor.call('updateGithubConfiguration');
+      Meteor.call('updateFiwareConfiguration');
+
+      // Get settings form success message translation
+      const message = TAPi18n.__('settings_successMessage');
+
+      // Alert the user of successful save
+      sAlert.success(message);
+    },
+  },
+});
