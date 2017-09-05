fee1d48f889b40db1ba31e32a220beb7fa59b07c
yannunes <yannunes@gmail.com>
2017-09-04 21:23:47 -0300

    Add server methods for Login Platforms

---
+++ apinf_packages/login-platforms/server/methods.js
@@ -0,0 +1,75 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Meteor } from 'meteor/meteor';
+
+// Meteor contributed packages imports
+import { ServiceConfiguration } from 'meteor/service-configuration';
+
+// APInf imports
+// eslint-disable-next-line max-len
+import { githubSettingsValid, fiwareSettingsValid } from '/apinf_packages/core/helper_functions/validate_settings';
+
+// Collection imports
+import Settings from '../collection';
+
+Meteor.methods({
+  updateGithubConfiguration () {
+    // Try if settings exist
+    try {
+      const settings = Settings.findOne();
+
+      // Check if github settings are valid
+      if (githubSettingsValid(settings)) {
+        // remove existing configuration
+        ServiceConfiguration.configurations.remove({
+          service: 'github',
+        });
+
+        // Insert new service configuration
+        ServiceConfiguration.configurations.insert({
+          service: 'github',
+          clientId: settings.githubConfiguration.clientId,
+          secret: settings.githubConfiguration.secret,
+        });
+      }
+    } catch (error) {
+      // otherwise show an error
+      const message = `Update gitHub configuration: ${error}`;
+
+      // Show an error message
+      throw new Meteor.Error(message);
+    }
+  },
+  updateFiwareConfiguration () {
+    // Try if settings exist
+    try {
+      const settings = Settings.findOne();
+
+      // Check if fiware settings are valid
+      if (fiwareSettingsValid(settings)) {
+        // remove existing configuration
+        ServiceConfiguration.configurations.remove({
+          service: 'fiware',
+        });
+
+        // Insert new service configuration
+        ServiceConfiguration.configurations.insert({
+          service: 'fiware',
+          clientId: settings.githubConfiguration.clientId,
+          rootURL: settings.githubConfiguration.rootURL,
+          secret: settings.githubConfiguration.secret,
+        });
+      }
+    } catch (error) {
+      // otherwise show an error
+      const message = `Update FIWARE configuration: ${error}`;
+
+      // Show an error message
+      throw new Meteor.Error(message);
+    }
+  },
+});
