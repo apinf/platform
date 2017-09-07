04081cf760eb4e5f93fabd1e93b836585b1abf49
Daria Voytova <vdariaclem@gmail.com>
2017-09-07 11:29:44 +0300

    Make Date range feature for Dashboard & API Analytic pages
    - Add handlers for select element
    - Set query parameter "timeframe"
    - Update Query object to Elasticsearch reqeust: it depends on selected timeframe; Rename variables
    - Code refactor

--- apinf_packages/dashboard/client/view/toolbar/toolbar.js
+++ apinf_packages/dashboard/client/view/toolbar/toolbar.js
@@ -9,11 +9,37 @@ import { Template } from 'meteor/templating';
 // Meteor contributed packages imports
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
+Template.dashboardToolbar.onRendered(function () {
+  // Get value of timeframe parameter
+  const timeframeParameter = FlowRouter.getQueryParam('timeframe');
+
+  // Set value
+  this.$('[name="timeframe"]').val(timeframeParameter);
+});
+
 Template.dashboardToolbar.helpers({
-  selectedOption () {
+  selectedProxy () {
     const proxyId = FlowRouter.getQueryParam('proxy_id');
 
     // Select this option if it is equal with query param value
     return proxyId === this._id ? 'selected' : '';
+  },
+});
+
+Template.dashboardToolbar.events({
+  'change .toolbar-select': (event) => {
+    // Placeholder
+    const queryParams = {};
+
+    // Get name & value of select item
+    // These parameters are name and value of query parameter related
+    const paramName = event.currentTarget.name;
+    queryParams[paramName] = event.currentTarget.value;
+
+    // Modifies the current history entry instead of creating a new one
+    FlowRouter.withReplaceState(() => {
+      // Update value
+      FlowRouter.setQueryParams(queryParams);
+    });
   },
 });
--- apinf_packages/dashboard/client/view/view.js
+++ apinf_packages/dashboard/client/view/view.js
@@ -8,6 +8,9 @@ import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Template } from 'meteor/templating';
 
+// Meteor contributed packages imports
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 // Collection imports
 import Proxies from '/apinf_packages/proxies/collection';
 import ProxyBackends from '/apinf_packages/proxy_backends/collection';
@@ -22,36 +25,58 @@ Template.dashboardView.onCreated(function () {
   // Get reference to template instance
   const instance = this;
 
+  // Init variables
   instance.elasticsearchData = new ReactiveVar();
   instance.error = new ReactiveVar();
+  instance.elasticsearchHost = new ReactiveVar();
+  instance.proxyBackendPaths = new ReactiveVar();
 
-  // Fetch Elasticsearch data reactively
+  // Reactive update Elasticsearch host
   instance.autorun(() => {
+    // Get proxy
     const proxy = Proxies.findOne();
 
+    // Get relevant Elasticsearch host
+    const host = _.get(proxy, 'apiUmbrella.elasticsearch', '');
+
+    // Save it in reactive variable
+    instance.elasticsearchHost.set(host);
+  });
+
+  // Reactive update Proxy Backends paths list
+  instance.autorun(() => {
     // Fetch proxy backends
     const proxyBackends = ProxyBackends.find().fetch();
 
-    // Get relevant Elasticsearch host
-    const elasticsearchHost = _.get(proxy, 'apiUmbrella.elasticsearch', '');
-
-    // Make sure main variables are available
-    if (proxyBackends && elasticsearchHost) {
-      // Create a list with requested paths
-      const proxyBackendPaths = _.map(proxyBackends, (backend) => {
-        const requestPath = backend.apiUmbrella.url_matches[0].frontend_prefix;
-        return {
-          wildcard: {
-            request_path: {
-              // Add '*' to partially match the url
-              value: `${requestPath}*`,
-            },
+    // Create a list with requested paths
+    const paths = _.map(proxyBackends, (backend) => {
+      const requestPath = backend.apiUmbrella.url_matches[0].frontend_prefix;
+      return {
+        wildcard: {
+          request_path: {
+            // Add '*' to partially match the url
+            // Remove the last slash
+            value: `${requestPath.slice(0, -1)}*`,
           },
-        };
-      });
+        },
+      };
+    });
 
-      // Make query request
-      const queryParams = queryForDashboardPage(proxyBackendPaths);
+    // Save it in reactive variable
+    instance.proxyBackendPaths.set(paths);
+  });
+
+  instance.autorun(() => {
+    const elasticsearchHost = instance.elasticsearchHost.get();
+    // Make sure Host exists
+    if (elasticsearchHost) {
+      // Get list of proxy backends paths
+      const proxyBackendPaths = instance.proxyBackendPaths.get();
+      // Get timeframe
+      const timeframe = FlowRouter.getQueryParam('timeframe');
+
+      // Make query object
+      const queryParams = queryForDashboardPage(proxyBackendPaths, timeframe);
 
       // Get Elasticsearch data
       Meteor.call('getElasticsearchData', elasticsearchHost, queryParams, (error, result) => {
