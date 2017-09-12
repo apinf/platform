4e37172e98ae3e699b7d1c5bc60c1cff339cf44c
yannunes <yannunes@gmail.com>
2017-09-12 14:57:34 -0300

    Add helper function to promisify Meteor.call function

---
+++ apinf_packages/core/helper_functions/promisify_call.js
@@ -0,0 +1,15 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+import { Meteor } from 'meteor/meteor';
+
+// Validates Promisify Meteor.call, for better response handling.
+export default function promisifyCall (method) {
+  return new Promise((resolve, reject) => {
+    Meteor.call(method, (err, response) => {
+      return err ? reject(err) : resolve(response);
+    });
+  });
+}
