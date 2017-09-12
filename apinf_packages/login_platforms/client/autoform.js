c1d176dcfc293c11092f24df5eee35da7942af1e
yannunes <yannunes@gmail.com>
2017-09-12 14:59:26 -0300

    Promisify method calls
    - Also, handle error/success message correctly

--- apinf_packages/login_platforms/client/autoform.js
+++ apinf_packages/login_platforms/client/autoform.js
@@ -3,26 +3,40 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// Meteor packages imports
-import { Meteor } from 'meteor/meteor';
-
 // Meteor contributed packages imports
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
+import promisifyCall from '/apinf_packages/core/helper_functions/promisify_call';
 
 AutoForm.hooks({
   loginPlatforms: {
     onSuccess () {
-      // Call for configuration updates
-      Meteor.call('updateGithubConfiguration');
-      Meteor.call('updateFiwareConfiguration');
+      // Promise array of all method calls to be executed.
+      const callsArray = [
+        promisifyCall('updateGithubConfiguration'),
+        promisifyCall('updateFiwareConfiguration'),
+      ];
 
-      // Get settings form success message translation
-      const message = TAPi18n.__('settings_successMessage');
+      // Executes all promises and wait for their responses
+      Promise.all(callsArray)
+        .then(result => {
+          if (result) {
+            // Get settings form success message translation
+            const message = TAPi18n.__('settings_successMessage');
 
-      // Alert the user of successful save
-      sAlert.success(message);
+            // Alert the user of successful save
+            sAlert.success(message);
+          }
+        })
+        .catch(err => {
+          if (err) {
+            // Get settings form success message translation
+            const message = TAPi18n.__('settings_errorMessage');
+            // Alert the user of successful save
+            sAlert.error(message);
+          }
+        });
     },
   },
 });
