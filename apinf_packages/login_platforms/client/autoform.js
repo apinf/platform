15c7dc29bfc2cfcf4c791b5cf93d63eadc68229e
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-09-14 16:56:05 -0300

    Add import comment

--- apinf_packages/login_platforms/client/autoform.js
+++ apinf_packages/login_platforms/client/autoform.js
@@ -7,6 +7,8 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 import { AutoForm } from 'meteor/aldeed:autoform';
 import { TAPi18n } from 'meteor/tap:i18n';
 import { sAlert } from 'meteor/juliancwirko:s-alert';
+
+// APInf imports
 import promisifyCall from '/apinf_packages/core/helper_functions/promisify_call';
 
 // Define hooks for the form
