153c96b7ad1569cbc03c52743587f37632ae08e7
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-09-13 23:23:23 -0300

    Add standard comment

--- apinf_packages/core/helper_functions/promisify_call.js
+++ apinf_packages/core/helper_functions/promisify_call.js
@@ -3,6 +3,7 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
+// Meteor packages imports
 import { Meteor } from 'meteor/meteor';
 
 // Validates Promisify Meteor.call, for better response handling.
