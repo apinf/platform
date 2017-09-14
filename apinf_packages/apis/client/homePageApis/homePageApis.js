687535aaf0f05a4da484ef7904e1cb77cfe866ed
saransh-dev <saranshdevsingh@gmail.com>
2017-09-14 17:38:53 +0530

    remove error of Line 31 exceeds the maximum line length of 100  max-len

--- apinf_packages/apis/client/homePageApis/homePageApis.js
+++ apinf_packages/apis/client/homePageApis/homePageApis.js
@@ -16,7 +16,8 @@ Template.homePageApis.onRendered(function () {
   templateInstance.autorun(() => {
     templateInstance.branding = Branding.findOne();
     // check featured apis available or not
-    const haveFeaturedApis = templateInstance.branding.featuredApis && templateInstance.branding.featuredApis.length !== 0;
+    const haveFeaturedApis = templateInstance.branding.featuredApis &&
+      templateInstance.branding.featuredApis.length !== 0;
     if (haveFeaturedApis) {
       templateInstance.subscribe('apiIds', templateInstance.branding.featuredApis);
     }
@@ -28,7 +29,9 @@ Template.homePageApis.helpers({
     const templateInstance = Template.instance();
     const branding = templateInstance.branding;
     // Check whether any APIs have been featured
-    const haveFeaturedApis = branding && branding.featuredApis && branding.featuredApis.length !== 0;
+    const haveFeaturedApis = branding && 
+      branding.featuredApis && 
+      branding.featuredApis.length !== 0;
     if (haveFeaturedApis) {
       // Fetch featured apis from all apis
       const featuredApis = Apis.find().fetch();
