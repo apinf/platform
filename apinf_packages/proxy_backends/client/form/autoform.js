21961ab2777d1e28cc431c3a6f375b695a41ed36
amardeep-deligence <amardeep@deligence.com>
2017-09-14 17:58:00 +0530

    fixed trailing-spaces and indent related errors

--- apinf_packages/proxy_backends/client/form/autoform.js
+++ apinf_packages/proxy_backends/client/form/autoform.js
@@ -85,12 +85,12 @@ AutoForm.hooks({
           });
         } else if (proxyBackend.type === 'emq') {
           // Cancel form if topicPrefix is not present
-          if (!proxyBackend.emq || !proxyBackend.emq.settings || !proxyBackend.emq.settings.topicPrefix) {
-              const errorMessage = TAPi18n.__('proxyBackendForm_topicPrefixRequired');
-              sAlert.error(errorMessage);
-              // Cancel form
-              form.result(false);
-          } 
+          if (!proxyBackend.emq || !proxyBackend.emq.settings.topicPrefix) {
+            const errorMessage = TAPi18n.__('proxyBackendForm_topicPrefixRequired');
+            sAlert.error(errorMessage);
+            // Cancel form
+            form.result(false);
+          }
 
           // Before insert iterate through ACL rules
           if (proxyBackend.emq.settings.acl && proxyBackend.emq.settings.acl.length !== 0) {
