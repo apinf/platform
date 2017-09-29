d450ed9698b76cb2f398c33868b29147c2cc9378
Daria Voytova <vdariaclem@gmail.com>
2017-09-29 17:21:38 +0300

    Global featured APIs in Branding setting
    - Add autoform-select2 package
    - Update schema of Branding collection (additional field "featuredApis")
    - Display field with multiple select
    - Display featured APIs on home page if they exist otherwise display Latest APIs
    - New publish functions (apisForBranding & homePageApis)
    - New template homePageApis & new folder homePageApis under /apis

--- apinf_packages/branding/client/branding.js
+++ apinf_packages/branding/client/branding.js
@@ -6,8 +6,25 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 // Meteor packages imports
 import { Template } from 'meteor/templating';
 
+// Meteor contributed packages imports
+import { TAPi18n } from 'meteor/tap:i18n';
+
 // Collection imports
+import Apis from '/apinf_packages/apis/collection';
 import Branding from '/apinf_packages/branding/collection';
+
+// Npm packages
+import 'select2';
+import 'select2/dist/css/select2.css';
+import 'select2-bootstrap-theme/dist/select2-bootstrap.css';
+
+Template.branding.onCreated(function () {
+  // Get reference to template instance
+  const templateInstance = this;
+
+  // Get public APIs
+  templateInstance.subscribe('apisForBranding');
+});
 
 Template.branding.onRendered(() => {
   $('[data-toggle="popover"]').popover();
@@ -20,5 +37,24 @@ Template.branding.helpers({
   },
   brandingCollection () {
     return Branding;
+  },
+  homepageApisSelect2Options () {
+    // Get featuredApis or an empty array on default
+    const featuredApis = Branding.findOne().featuredApis || [];
+    // Message for placeholder
+    const message = TAPi18n.__('branding_projectFeaturedApisMessage_featuredApiMessage');
+
+    return {
+      placeholder: message,
+      // Data for select box
+      data: Apis.find().map((api) => {
+        return {
+          text: api.name,
+          id: api._id,
+          // If featured APIs contains current one then mark it as "selected"
+          selected: featuredApis.indexOf(api._id) > -1,
+        };
+      }),
+    };
   },
 });
