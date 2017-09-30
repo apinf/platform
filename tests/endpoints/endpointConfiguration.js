21c7b9c4470479380d0d485b0cf3004e70a6ae9c
yannunes <yannunes@gmail.com>
2017-09-30 15:53:51 -0300

    Set endpoint pattern that exports the entire url
    Each module's url is now exported in a single variable.

--- tests/endpoints/endpointConfiguration.js
+++ tests/endpoints/endpointConfiguration.js
@@ -1,14 +1,26 @@
 // Object with data shared by all endpoints
 const common = {
-  baseURL: 'localhost:3000/rest/v1',
+  baseURL: `localhost:3000/rest/v1`,
 };
 
 // Data specific for the /organizations endpoint
 const organizations = {
-  endpoint: '/organizations',
+  endpoint: `${common.baseURL}/organizations`,
+};
+
+// Data specific for the /organizations endpoint
+const users = {
+  endpoint: `${common.baseURL}/users`,
+};
+
+// Data specific for the /organizations endpoint
+const login = {
+  endpoint: `${common.baseURL}/login`,
 };
 
 module.exports = {
   common,
+  users,
+  login,
   organizations,
 };
