db890cc3cc820b1f913e3abe3bcf8ef3a9a43d2e
yannunes <yannunes@gmail.com>
2017-09-30 15:51:04 -0300

    Add login and createUser function to helper class for retrieving user credentials

---
+++ tests/endpoints/testHelper.js
@@ -0,0 +1,37 @@
+const request = require('superagent');
+const { common, users, login } = require('./endpointConfiguration.js');
+
+const createUser = ({ username, email, password }) =>
+  new Promise((resolve, reject) => {
+    // Define new user variable
+    const newUser = { username, email, password }
+
+    // Perform request to register new user
+    request
+      .post(users.endpoint)
+      .send(newUser)
+      .end((err, res) => err? reject(err) : resolve(res));
+  });
+
+
+const performLogin = ({ username, password }) =>
+  new Promise((resolve, reject) => {
+    // Define user variable
+    const user = { username, password }
+
+    // Perform request to retrieve user credentials
+    request
+      .post(login.endpoint)
+      .send(user)
+      .end((err, res) => err? reject(err) : resolve(res));
+  });
+
+
+const getUserCredentials = ({ username, password }) =>
+  new Promise((resolve, reject) => {
+    // First login
+    // If status == 200, resolve data with promise
+    // If status == 401, create new username
+    // Login with new user
+    // Resolve data with promise
+  });
