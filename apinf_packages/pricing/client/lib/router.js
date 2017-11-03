ef9f344a91be7e1b97e37d2d31f66bd11862a612
illya <in11092008@gmail.com>
2017-11-03 13:42:29 +0200

    Add route

---
+++ apinf_packages/pricing/client/lib/router.js
@@ -0,0 +1,15 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+FlowRouter.route('/pricing', {
+  name: 'pricing',
+  action () {
+    BlazeLayout.render('masterLayout', { main: 'pricing' });
+  },
+});
