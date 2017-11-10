d2f9504fd8439b743955a946c0b82bf5709e8479
deligence-dharmendra <dharmendra.deligence@gmail.com>
2017-11-10 12:33:18 +0530

    Fixed issue No notifications of save status in organization #3113

---
+++ apinf_packages/organizations/client/profile/settings/general/autoform.js
@@ -0,0 +1,35 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Meteor } from 'meteor/meteor';
+
+// Meteor contributed packages imports
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { TAPi18n } from 'meteor/tap:i18n';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
+
+AutoForm.hooks({
+  organizationSettingsGeneral: {
+
+    onSuccess (formType) {
+
+      // Get success message translation
+      const message = TAPi18n.__('organizationSettingsGeneral_update_successMessage');
+
+      // Alert success message to user
+      sAlert.success(message);
+    },
+    onError (formType, error) {
+
+      // Get failure message translation
+      const message = TAPi18n.__('organizationSettingsGeneral_update_failedMessage');
+
+      // Alert failure message to user
+      sAlert.warning(message);
+    },
+  },
+});
