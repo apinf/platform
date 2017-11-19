214f9c1a82ac732aa1abf0f1eac811065d5777c4
yannunes <yannunes@gmail.com>
2017-11-19 14:54:09 -0300

    Add tests for collections functions

--- tests/apinf_packages/ratings/ratings.test.js
+++ tests/apinf_packages/ratings/ratings.test.js
@@ -7,3 +7,74 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
 /* eslint-disable max-len */
+
+// Import node packages
+import path from 'path';
+
+// Defining root path of the project
+const currentPath = path.resolve(__dirname);
+const rootPath = currentPath.replace('/tests/apinf_packages/ratings', '');
+const ratingsPath = `${rootPath}/apinf_packages/ratings/functions_index.js`;
+
+// ES5 require had to be used because the 'import' statement is static and cannot
+// depend on runtime information
+const { collection } = require(ratingsPath);
+
+const { insert, update } = collection;
+
+describe('Ratings Package', () => {
+  console.log()
+  describe('Collection module', () => {
+    describe('permission.js functions', () => {
+      describe('insert function', () => {
+        it('should return true when userId is defined', () => {
+          // Define userId variable
+          const userId = 'userId';
+
+          // Flag to define if user and insert rating
+          const canInsert = insert(userId);
+
+          expect(canInsert).toEqual(true);
+        });
+
+        it('should return false when userId is not defined', () => {
+          // Define userId variable
+          const userId = '';
+
+          // Flag to define if user and insert rating
+          const canInsert = insert(userId);
+
+          expect(canInsert).toEqual(false);
+        });
+      });
+
+      describe('update function', () => {
+        it('should return true when userId is equal to the rating userId', () => {
+          // Define userId variable
+          const userId = 'userId';
+
+          // Define rating variable
+          const rating = { userId };
+
+          // Flag to define if user and update rating
+          const canUpdate = update(userId, rating);
+
+          expect(canUpdate).toEqual(true);
+        });
+
+        it('should return false when userId is not defined', () => {
+          // Define userId variable
+          const userId = 'userId';
+
+          // Define rating variable
+          const rating = { userId: 'otherUserId' };
+
+          // Flag to define if user and update rating
+          const canUpdate = update(userId, rating);
+
+          expect(canUpdate).toEqual(false);
+        });
+      });
+    });
+  });
+});
