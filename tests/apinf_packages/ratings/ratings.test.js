15a16453fd9ffb08e4bbbaf985daa4012f3489b4
yannunes <yannunes@gmail.com>
2017-11-19 13:44:22 -0300

    Add eslint config

---
+++ tests/apinf_packages/ratings/ratings.test.js
@@ -0,0 +1,9 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
