53188f9425e662a3df875c0ad0cb6c39ac2359bb
yannunes <yannunes@gmail.com>
2017-11-19 14:53:53 -0300

    Folder restructure to allow unit testing
    ### What changed:
    
    - Each folder now has a funtion.js file, where all functions should be placed
    - All apinf_package folders have a functions_index.js file, that should import and expose all functions.js files

--- apinf_packages/ratings/collection/permissions.js
+++ apinf_packages/ratings/collection/permissions.js
@@ -6,12 +6,7 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 // Collection imports
 import ApiBackendRatings from './';
 
-ApiBackendRatings.allow({
-  insert (userId) {
-    // User must be logged in to vote
-    return !!(userId);
-  },
-  update (userId, rating) {
-    return (userId === rating.userId);
-  },
-});
+// Import functions
+import { insert, update } from './functions.js';
+
+ApiBackendRatings.allow({ insert, update });
---
+++ apinf_packages/ratings/functions_index.js
@@ -0,0 +1,16 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Import functions from package
+import collection from './collection/functions.js';
+import server from './collection/server/functions.js';
+import client from './client/functions.js';
+
+// Export all functions
+export {
+  collection,
+  server,
+  client,
+};
