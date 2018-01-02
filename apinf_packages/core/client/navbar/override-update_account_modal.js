ca321946d7a57517238cc97cbb5edc91af574451
Krashna <krashna@deligence.com>
2018-01-02 13:27:29 +0530

    resolved click-on-add-role-admin-leads-to-homepage-in-Internet-explorer
    Removed empty parameter required from usersUpdate
    
    Reactive update the subscription for feedback replies #3273
    
    Multilinks to remote API documentation (no Swagger specification):
    
    - Change a type field otherUrl to Array of String
    - Create a mew migration step (number 14) for that
    - Provide UI to allow a user adds multi links (less 8)
    
    	# Please enter the commit message for your changes. Lines starting
    
    travis error resolved
    
    travis error resolved tab spaced
    
    blank line added
    
    use arrow instead function

---
+++ apinf_packages/core/client/navbar/override-update_account_modal.js
@@ -0,0 +1,15 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Template } from 'meteor/templating';
+
+Template.overrideUpdateAccountModalInner.events({
+  'click .add-role': (event) => {
+    event.preventDefault();
+  },
+});
+
+Template.updateAccountModalInner.inheritsEventsFrom('overrideUpdateAccountModalInner');
