f8264d69113059c052fdd216b7533be2269be1ea
deligence-dharmendra <dharmendra.deligence@gmail.com>
2018-01-03 18:29:08 +0530

    Fixed Revoke my API key

--- apinf_packages/api_keys/server/methods.js
+++ apinf_packages/api_keys/server/methods.js
@@ -74,4 +74,68 @@ Meteor.methods({
       );
     }
   },
+  revokeApiKey (apiId, apiKey) {
+    // Make sure apiId is a string
+    check(apiId, String);
+    // Make sure apiKey is a string
+    check(apiKey, String);
+    // Get logged in user
+    const currentUser = Meteor.user();
+
+    // Check currentUser exists
+    if (currentUser) {
+      const proxyBackend = ProxyBackends.findOne({ apiId });
+
+      // Check proxyBackend is defined, and it has proxyId
+      if (proxyBackend && proxyBackend.proxyId) {
+        // Get Proxy by proxyId of proxyBackend
+        const proxyId = proxyBackend.proxyId;
+        const proxy = Proxies.findOne({ _id: proxyId });
+
+        // Check type & call appropriate function
+        if (proxy && proxy.type === 'apiUmbrella') {
+          // Call Umbrella method to create user with API key
+          Meteor.call('createApiUmbrellaUser', currentUser, proxyId, (error, umbrellaUser) => {
+            if (error) {
+              // Throw apiumbrellauser error for client
+              throw new Meteor.Error(
+                'apinf-apiumbrellauser-error', TAPi18n.__('apinf_apiumbrellauser_error')
+              );
+            } else {
+              // Construct apiKey object
+              const newApiKey = {
+                apiUmbrella: {
+                  id: umbrellaUser.id,
+                  apiKey: umbrellaUser.api_key,
+                },
+                userId: currentUser._id,
+                proxyId: proxy._id,
+              };
+
+              // Insert apiKey
+              ApiKeys.insert(newApiKey);
+
+              // Remove collection
+              ApiKeys.remove({'apiUmbrella.apiKey': apiKey});
+            }
+          });
+        } else {
+          // Throw no proxy error for client
+          throw new Meteor.Error(
+            'apinf-noproxy-error', TAPi18n.__('apinf_noproxy_error')
+          );
+        }
+      } else {
+        // Throw no proxybackend error for client
+        throw new Meteor.Error(
+          'apinf-noproxybackend-error', TAPi18n.__('apinf_noproxybackend_error')
+        );
+      }
+    } else {
+      // Throw usernotloggedin error for client
+      throw new Meteor.Error(
+        'apinf-usernotloggedin-error', TAPi18n.__('apinf_usernotloggedin_error')
+      );
+    }
+  },
 });
