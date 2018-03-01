6f4351dcb486026c880ca5d4343655957223f812
matleppa <matti.leppanen@apinf.io>
2018-03-01 11:50:16 +0200

    Corrections in guidance texts

--- apinf_packages/rest_apis/lib/descriptions/analytics_texts.js
+++ apinf_packages/rest_apis/lib/descriptions/analytics_texts.js
@@ -166,19 +166,18 @@ You can have multiple proxies under one management.
   ### Request parameters
 
   APIs are listed depending on value given to parameter **apisBy**
-  - **organization** = all APIs, *that belong to Organizations*,
-  which are managed by User (default value)
+  - **organization** = all APIs, *that belong to Organizations*, which are managed by User (default value)
   - **owner** = all APIs, which are managed by User
 
-  By using parameter **organizationId** the User can limit the search
-  in one Organization in both cases.
+  In both cases the User can limit the search in one Organization by using parameter **organizationId**.
 
 
-  With parameter **period** the User can select the one of three periods,
-  over which the data is returned. Periods are calculated by server time.
-  - **today** is todays values (default value)
-  - **week** is for past week (7 days back beginning from yesterday)
-  - **month** (30 days back beginning from yesterday).
+  With parameter **period** the User can select the period (up to 30 days), over which the data is returned. Periods are calculated by server time.
+  - **0** | **period not given**, data contains todays values, i.e. from midnight to moment, when request was sent (default value)
+  - **1** to **30**, is period length in full days. Period either ends yesterday (when startDate not given) or begins on startDate.
+
+  Suitable values for parameter *period* are 7 (last week) or 28 (four last weeks).
+  Note! Todays response dataset will never be complete 24 hours
 
   ### Response content
   The response contains meta data
@@ -192,9 +191,7 @@ You can have multiple proxies under one management.
   - median response time
   - number of unique users
 
-  Note! When period is set to 'Today' the current day data from midnight
-  (on server time) to current moment is returned, not a complete 24 hour period.
-  `,
+`,
   // --------------------------------------------
   getAnalyticsApiId: `
   ## List API with detailed information
@@ -203,7 +200,7 @@ You can have multiple proxies under one management.
   ## Approaches
 
   Select either
-  - a predefined period (today, week, month)
+  - a period (1 - 30)
   - or a specific date with granularity interval.
 
   If the parameter *period* is used, the latter two parameters are obsolete.
@@ -212,16 +209,13 @@ You can have multiple proxies under one management.
 
   API is identified with parameter **apiId**.
 
-  With parameter **period** the User can select the one of three periods,
-  over which the data is returned. Periods are calculated by server time.
-  - **Today** is todays values (default value)
-  - **Week** is for past week (7 days back beginning from yesterday)
-  - **month** (30 days back beginning from yesterday).
+  With parameter **period** the User can select length of the period, over which the data is returned. Period is calculated based on server time.
+  - **0** is todays data (default value)
+  - **1** - **30** is length of period, ending yesterday (yesterday included)
 
   With parameter **date** the User can identify a date of which more granular details are fetched.
   The form of value for parameter is given according to ISO 8601, YYYY-MM-DD.
-  With parameter user can pick one of days between yesterday to 7 days back from today
-  (which, by the way, is same as the range indicated with value *week* of parameter *period*).
+  With parameter user can pick one of days between yesterday to 30 days back from today.
 
   Parameter **date** is obsolete in case parameter *period* was given.
 
@@ -275,6 +269,9 @@ You can have multiple proxies under one management.
   managed by user (value 'organization') or managed by user (value 'owner').
   *Default value is 'organization'*.
   `,
+  apiId: `
+  Mandatory parameter to limit the search to one API only.
+  `,
   date: `
   Date of which detailed information is retrieved. Format: yyyy-mm-dd (Standard: ISO 8601).
   **Note!** If _period_ parameter is used, then _date_ parameter is obsolete.
@@ -283,41 +280,45 @@ You can have multiple proxies under one management.
   Optional parameter to limit the search to one Organization only.
   `,
   period: `
-  Predefined periods for detailed API performance data.
-  Periods are calculated by server time.
-  - **today** is current day values from midnight to current moment. (Default)
-  - **week** is past 7 days (does not include current day).
-  - **month** is 30 days (does not include current day).
+  Optional parameter to indicate the period (in days) for detailed API performance data.
+  Periods are calculated **based on server time**.
+  There are two ways to use the parameter
+  - **0** | **not given**, period contains only current day from midnight to current moment.
+  (Default value, startDate is obsolete)
+  - **1** - **30**, period length is given number of days.
+
+  On latter alternative the beginning and ending of period depends on
+  whether parameter *startDate* is given.
+  - without *startDate* the period ends yesterday (also including it)
+  - with *startDate* the period begins on given date (also including it),
+  but it must not exceed yesterday.
+`,
+  periodApiId: `
+  Predefined periods for detailed API performance data.  Periods are calculated based on server time.
+  - **0** is current day data from midnight to current moment.
+  - **1** - **30** period length is given number of days (yesterday as last included day)
   `,
   startDate: `
-  Provide start date value from which the analytics is provided from.
-  Date is expected in following format: yyyy-mm-dd, example 2017-11-30
-  (Standard: ISO 8601). With *days* parameter you define the period length.
+  Optional parameter to indicate the *date* from which the provided analytics period begins.
+  Date value is expected in following format: yyyy-mm-dd, example 2017-11-30 (Standard: ISO 8601).
 
-  **Note!** Keep in mind that our platform can provide fast access to
-  data only 30 days backwards. Any request going further down the history
-  will most likely cause slower API response time.
+  **Note!** Keep in mind that our platform can provide fast access to data only 30 days backwards.
+  Any request going further down the history will most likely cause slower API response time.
 
-  **Note!** If you use combination of *startDate* and *days* parameters,
-  you can not use *period* parameter.
-  `,
-  days: `
-  Provide number (integer) of days you want to include
-  in the result set. The start date defined with *startDate* parameter is
-  included in the count.
+  **Note!** Parameter *period* must be used to define the period length.
 
-  **Note!** If you use combination of *startDate* and *days* parameters,
-  you can not use *period* parameter.
+  **Note!** In case parameter *period* has value 0 or it is not given, parameter StartDate
+  is obsolete.
+
+  **Note!** Period formed with parameters startDate and Period can not contain current day.
   `,
   interval: `
   Granularity (in minutes) of the data within a day. Available options are:
-  - 5
-  - 10
-  - 15
   - 30
   - 60
 
-  If granularity interval is not given, default value is 24 hours (1440 minutes), which is by the way the same as to use _today_ option in period parameter.
+  If granularity interval is not given, default value is 24 hours (1440 minutes),
+  which is by the way the same as to use (1 - 30) option in parameter _period_.
   **Note!** If _period_ parameter is used, then _interval_ parameter is obsolete.
   `,
 };
