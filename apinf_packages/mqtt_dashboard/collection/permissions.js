c6a308f34a0099e9e645b613a9296dde94b50d3e
Daria Voytova <vdariaclem@gmail.com>
2018-03-20 17:35:24 +0300

    Create the StoredTopics collection
    - Index
    - Schema
    - Permissions
    - Publications

---
+++ apinf_packages/mqtt_dashboard/collection/permissions.js
@@ -0,0 +1,25 @@
+/* Copyright 2017 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { Roles } from 'meteor/alanning:roles';
+
+// Collection imports
+import StoredTopics from './';
+
+StoredTopics.allow({
+  insert (userId) {
+    // Check if user has admin role
+    return Roles.userIsInRole(userId, ['admin']);
+  },
+  update (userId) {
+    // Check if user has admin role
+    return Roles.userIsInRole(userId, ['admin']);
+  },
+  remove (userId) {
+    // Check if user has admin role
+    return Roles.userIsInRole(userId, ['admin']);
+  },
+});
