7b29eab6fc454bafac836f6d4924f3c2aeedeff8
illya <in11092008@gmail.com>
2018-04-24 15:09:16 +0300

    wip

--- apinf_packages/dashboard/client/analytic/header/header.js
+++ apinf_packages/dashboard/client/analytic/header/header.js
@@ -1,7 +1,8 @@
+
 /* Copyright 2017 Apinf Oy
-This file is covered by the EUPL license.
-You may obtain a copy of the licence at
-https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+ This file is covered by the EUPL license.
+ You may obtain a copy of the licence at
+ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
 // Meteor packages imports
 import { Meteor } from 'meteor/meteor';
@@ -11,6 +12,22 @@ import { Template } from 'meteor/templating';
 // Collection imports
 import Apis from '/apinf_packages/apis/collection';
 import Proxies from '/apinf_packages/proxies/collection';
+import ProxyBackends from '/apinf_packages/proxy_backends/collection';
+
+Template.apiAnalyticPageHeader.onCreated(function () {
+  const instance = this;
+
+  // Get ID of current proxy backend
+  const proxyBackendId = instance.data.proxyBackendId;
+  // Get instance of Proxy Backend
+  const proxyBackend = ProxyBackends.findOne(proxyBackendId);
+
+  // make sure proxy backend exists and
+  // Get IDs of relevant API and Proxy
+  instance.apiId = proxyBackend && proxyBackend.apiId;
+  instance.proxyId = proxyBackend && proxyBackend.proxyId;
+
+});
 
 Template.apiAnalyticPageHeader.helpers({
   api () {
