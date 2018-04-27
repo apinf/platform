66fc932ab72ef2d68e4b5a958d36b81628490bbd
illya <in11092008@gmail.com>
2018-04-27 15:02:59 +0300

    WIP

--- apinf_packages/apis/client/profile/header/header.js
+++ apinf_packages/apis/client/profile/header/header.js
@@ -1,3 +1,8 @@
+/* Copyright 2018 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
 // Meteor packages imports
 import { Template } from 'meteor/templating';
 
