7aa935dac04fc158bc9756fbdd8dad80a8caa7ed
Matti Leppänen <matti.leppanen@apinf.io>
2018-05-25 16:19:54 +0300

    Added documentation fo PUT /apis/:id/proxyBackend

--- apinf_packages/rest_apis/lib/descriptions/apis_texts.js
+++ apinf_packages/rest_apis/lib/descriptions/apis_texts.js
@@ -123,20 +123,54 @@ const descriptionApis = {
   Parameters
   * *:id* is API id, mandatory (in URL)
   `,
-// --------------------------------------------
-  postProxyBackend: `
-  ### Connects an API to a Proxy ###
+  // --------------------------------------------
+    postProxyBackend: `
+    ### Connects an API to a Proxy ###
 
-  Adds API connection to an identified Proxy.
-  The given parameter set depends on the type of selected proxy.
-  On success, returns the updated API object.
+    Adds API connection to an identified Proxy.
+    The given parameter set depends on the type of selected proxy.
+    On success, returns the updated Proxy connection object.
+
+
+    Common parameters (M = mandatory)
+    * *:id* is API id (in URL), (M)
+    * *proxyId* is id of the Proxy, to which the API is to be connected, (M)
+
+    #### In case type of selected proxy is **apiUmbrella**, the following parameters are needed ####
+    * *frontendPrefix* is a unique identification for
+    summarizing requests and responses done via this proxy connection, (M)
+    * *backendPrefix* is an identification of API on server, (M)
+    * *apiPort* is port used on API server, default value for https is 443, http is 80.
+    * *disableApiKey*, [false | true], true = skip API key requirement in Proxy, default false
+    * *rateLimitMode*, [unlimited | custom], default 'unlimited'
+
+    When parameter *rateLimitMode* has value 'custom', following parameters are needed
+    * *duration*, set request duration in milliseconds
+    * *limitBy*, [apiKey | ip],
+    * *limit*, set number of request
+    * *showLimitInResponseHeaders*, [true | false], is limit shown in response headers or not
+
+    #### In case type of proxy is **emq**, the following Access Control Rules (ACL)
+    parameters are needed ####
+    * *allow*, values [0 | 1], 0 = deny, 1 = access
+    * *access*, values [1 | 2 | 3], 1 = subscribe, 2 = publish, 3 = both
+    * *topic*, value is a string
+    * *fromType*, values [clientid | username | ipaddr]
+    * *fromValue*, value is a string
+    `,
+// --------------------------------------------
+  putProxyBackend: `
+  ### Modifies Proxy connection parameters of an API ###
+
+  When an API has a Proxy connection, the connection parameters can be modified.
+  The given parameter set depends on the type of connected proxy.
+  On success, returns the updated Proxy connection object.
 
 
   Common parameters (M = mandatory)
   * *:id* is API id (in URL), (M)
-  * *proxyId* is id of the Proxy, to which the API is to be connected, (M)
 
-  #### In case type of selected proxy is **apiUmbrella**, the following parameters are needed ####
+  #### In case type of selected proxy is **apiUmbrella**, at least one of following parameters must be given ####
   * *frontendPrefix* is a unique identification for
   summarizing requests and responses done via this proxy connection, (M)
   * *backendPrefix* is an identification of API on server, (M)
@@ -144,14 +178,14 @@ const descriptionApis = {
   * *disableApiKey*, [false | true], true = skip API key requirement in Proxy, default false
   * *rateLimitMode*, [unlimited | custom], default 'unlimited'
 
-  When parameter *rateLimitMode* has value 'custom', following parameters are needed
+  When parameter *rateLimitMode* has value 'custom', at least one of following parameters must be given
   * *duration*, set request duration in milliseconds
   * *limitBy*, [apiKey | ip],
   * *limit*, set number of request
   * *showLimitInResponseHeaders*, [true | false], is limit shown in response headers or not
 
-  #### In case type of proxy is **emq**, the following Access Control Rules (ACL)
-  parameters are needed ####
+  #### In case type of proxy is **emq**, at least one of following Access Control Rules (ACL)
+  parameters must be given ####
   * *allow*, values [0 | 1], 0 = deny, 1 = access
   * *access*, values [1 | 2 | 3], 1 = subscribe, 2 = publish, 3 = both
   * *topic*, value is a string
