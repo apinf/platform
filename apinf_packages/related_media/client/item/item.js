08b210b131610202fdb5d8caf1aedc42df496e84
illya <in11092008@gmail.com>
2018-05-31 14:41:05 +0300

    WIP

--- apinf_packages/related_media/client/item/item.js
+++ apinf_packages/related_media/client/item/item.js
@@ -6,18 +6,6 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 import { Template } from 'meteor/templating';
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 
-Template.postItem.onRendered(function () {
-  // Get reference to template instance
-  const instance = this;
-
-  // Get Post ID from template instance
-  const postId = instance.data.post._id;
-  // Get reference to post DOM element
-  const postElement = instance.$(`#${postId}`);
-
-  // Render OEmbed content
-  postElement.oembed();
-});
 Template.postItem.events({
   'click .delete-post-item': function (event, templateInstance) {
     // Get post from template instance
