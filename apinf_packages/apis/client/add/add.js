0a458df490a69d95701d8385f65f7e19be28634b
Nazarah <nazia@Omistaja-MBP.lan>
2018-06-14 16:23:43 +0300

    field descriptions and placeholders in Add API fields
    1. i18n.json: added templates addApi_nameFieldInfo, addApi_descriptionFieldInfo and addApi_urlFieldInfo which describe purpose of name, description and URL fields (respectively) in Add API page.
    
    2. i18n.json: added template addApi_ownerHelpText that describes where an added API appears with what visibility.
    
    3. add.html: added placeholders for fields name, description and url
    
    4. add.html: added templates texts describing purpose of name, description and URL fields.
    
    5. add.html: added template text describing where added API appears with what visibility.
    
    6. add.less: added new css class field-description-text to style field description texts.
    
    7. add.js: added constant variables to compare placeholder values from name, description and URL fields in add API page.
    
    8. Added event listeners for click and blur events in name, description and url fields in Add API page.

--- apinf_packages/apis/client/add/add.js
+++ apinf_packages/apis/client/add/add.js
@@ -14,6 +14,11 @@ import { TAPi18n } from 'meteor/tap:i18n';
 import Branding from '/apinf_packages/branding/collection';
 import Apis from '../../collection';
 
+// variables to check placeholder values from Add API fieldset
+const apiNameFieldValue = "e.g. Tampere Bus Service";
+const apiDescriptionFieldValue = "e.g. Plan your journey across Tampere city by getting up-to-date bus information, including real-time vehicle location detection and inter zone routing. You can lookup bus timing by line and stoppage.";
+const apiUrlFieldValue = "e.g. https://tampere-bus-service.fi";
+
 Template.addApi.onCreated(function () {
   const instance = this;
 
@@ -33,5 +38,50 @@ Template.addApi.helpers({
   ApisCollection () {
     // Make APIs collection available to template (i.e. autoform)
     return Apis;
+  },
+});
+
+Template.addApi.events({
+  'click #api-name': function(event) {
+    // on clicking field, placeholder value is removed.
+    const field = document.getElementById("api-name");
+    if (field.placeholder === apiNameFieldValue){
+      field.placeholder = '';
+    }
+  },
+  'blur #api-name': function(event) {
+    // If no field value is provided, placeholder value is restored on blur event
+    const field = document.getElementById("api-name");
+    if (field.placeholder === '') {
+      field.placeholder = apiNameFieldValue;
+    }
+  },
+  'click #api-description': function(event) {
+    // on clicking field, placeholder value is removed.
+    const field = document.getElementById("api-description");
+    if (field.placeholder === apiDescriptionFieldValue){
+      field.placeholder = '';
+    }
+  },
+  'blur #api-description': function(event) {
+    // If no field value is provided, placeholder value is restored on blur event
+    const field = document.getElementById("api-description");
+    if (field.placeholder === '') {
+      field.placeholder = apiDescriptionFieldValue;
+    }
+  },
+  'click #api-url': function(event) {
+    // on clicking field, placeholder value is removed.
+    const field = document.getElementById("api-url");
+    if (field.placeholder === apiUrlFieldValue){
+      field.placeholder = '';
+    }
+  },
+  'blur #api-url': function(event) {
+    // If no field value is provided, placeholder value is restored on blur event
+    const field = document.getElementById("api-url");
+    if (field.placeholder === '') {
+      field.placeholder = apiUrlFieldValue;
+    }
   },
 });
