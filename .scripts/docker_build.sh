a32a6a290251511a72261f9cdacb46dca34d1114
Vesa Härkönen <coyotronics@gmail.com>
2018-07-12 15:48:43 +0300

    Update docker_build.sh

--- .scripts/docker_build.sh
+++ .scripts/docker_build.sh
@@ -12,6 +12,7 @@ echo "docker_build.sh - kissa1"
 sudo apt-get install -y --no-install-recommends bsdtar
 sudo -i export tar='bsdtar'
 tar --version
+bsdtar --version
 
 echo "df -h starts, woow!"
 df -h
