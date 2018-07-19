16cd1c36ed8882c8209aef101698b59e0b08426a
illya <in11092008@gmail.com>
2018-07-19 18:21:26 +0300

    initial commit

---
+++ apinf_packages/overview/client/lib/router.js
@@ -0,0 +1,15 @@
+/* Copyright 2018 Apinf Oy
+This file is covered by the EUPL license.
+You may obtain a copy of the licence at
+https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
+
+// Meteor packages imports
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+FlowRouter.route('/overview', {
+  name: 'overview',
+  action () {
+    BlazeLayout.render('masterLayout', { main: 'overview' });
+  },
+});
