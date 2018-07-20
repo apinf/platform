0b5cfc8fbf64f7f239bc9e71d5e1b700ab632fde
Vinay Sanker <vinaytech09@gmail.com>
2018-07-20 14:19:50 +0530

    changes for display version story

--- apinf_packages/about/server/methods.js
+++ apinf_packages/about/server/methods.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
   fetchVersionData () {
+    /* global Assets */
     const myFile = JSON.parse(Assets.getText('versions.json'));
     return myFile;
   },
