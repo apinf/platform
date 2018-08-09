9a168bffeddce88c045904d5e20b2e17a1d43513
illya <in11092008@gmail.com>
2018-08-09 18:53:24 +0300

    add example chart

--- apinf_packages/overview/client/plans/plans.js
+++ apinf_packages/overview/client/plans/plans.js
@@ -7,6 +7,33 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+Template.plans.onRendered(() => {
+  const ctx = document.getElementById('test');
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['One', 'Two', 'Three', 'Four', 'Five', 'Six'],
+      datasets: [{
+        data: [
+          12, 19, 3, 5, 2, 3,
+        ],
+      }],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+    },
+  });
+});
+
 Template.plans.helpers({
   evaluationPlan () {
     return Meteor.user().subscriptionPlan;
