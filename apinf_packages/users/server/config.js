16282018be422035bd1e62bd82d17bda732c103e
Nazarah <nazia@Omistaja-MBP.lan>
2018-08-13 16:30:39 +0300

    Alternative Login Platform - Modifications
    Fixed errors causing Travis Build Failure.

--- apinf_packages/users/server/config.js
+++ apinf_packages/users/server/config.js
@@ -10,11 +10,11 @@ const settings = LoginPlatforms.findOne();
 
 // Check if github settings are valid
 if (githubSettingsValid(settings)) {
-  ServiceConfiguration.configurations.upsert({ service: 'github'}, {
+  ServiceConfiguration.configurations.upsert({ service: 'github' }, {
     $set: {
       clientId: settings.githubConfiguration.clientId,
       secret: settings.githubConfiguration.secret,
       loginStyle: 'popup',
-    }
+    },
   });
 }
