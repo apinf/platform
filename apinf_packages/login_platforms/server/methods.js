e273038db4c7e7d17b011e4a630ed84cad728617
matleppa <matti.leppanen@apinf.io>
2018-09-28 09:35:59 +0300

    Added updating service configuration values to loginPlatform

--- apinf_packages/login_platforms/server/methods.js
+++ apinf_packages/login_platforms/server/methods.js
@@ -9,14 +9,8 @@ import { Meteor } from 'meteor/meteor';
 // Meteor contributed packages imports
 import { ServiceConfiguration } from 'meteor/service-configuration';
 
-// APInf imports
-import {
-  githubSettingsValid,
-  fiwareSettingsValid,
-  oidcSettingsValid,
-} from '/apinf_packages/core/helper_functions/validate_settings';
-
 // Collection imports
+import Settings from '/apinf_packages/settings/collection';
 import LoginPlatforms from '../collection';
 
 // Helper object to organize save functions for each service configuration
@@ -68,13 +62,13 @@ const updateFunctions = {
     if (settings.oidcConfiguration) {
       ServiceConfiguration.configurations.insert({
         service: 'oidc',
-        loginStyle: 'popup',
         clientId: settings.oidcConfiguration.clientId,
         secret: settings.oidcConfiguration.secret,
         serverUrl: settings.oidcConfiguration.serverUrl,
         authorizationEndpoint: settings.oidcConfiguration.authorizationEndpoint,
         tokenEndpoint: settings.oidcConfiguration.tokenEndpoint,
         userinfoEndpoint: settings.oidcConfiguration.userinfoEndpoint,
+        idTokenWhitelistFields: settings.oidcConfiguration.idTokenWhitelistFields || [],
       });
     }
 
@@ -85,6 +79,8 @@ const updateFunctions = {
 
 Meteor.methods({
   updateLoginPlatformsConfiguration () {
+    // DB ServiceConfiguration is updated to be same as LoginPlatforms
+
     // Status variable returned to client
     let status = null;
 
@@ -109,24 +105,29 @@ Meteor.methods({
 
 Meteor.methods({
   updateLoginPlatformsFromConfiguration () {
+    // DB LoginPlatforms is updated to be same as ServiceConfiguration
+
     // Variable to be returned to client
-    let parameters = {};
     let loginParameterId;
     let changesFound = false;
 
     // Try if settings exist
     try {
+      // Check if some methods are not allowed
+      const notAllowedMethods = Settings.findOne();
+
+      // Get loginPlatform data
       const loginParameters = LoginPlatforms.findOne() || {};
       if (loginParameters._id) {
         loginParameterId = loginParameters._id;
         delete loginParameters._id;
       }
+      // If data in dbs is different, modify loginPlatforms according to
+      // ServiceConfiguration
+
       // Read Github parameters from ServiceConfiguration
       const configGithubParameters = ServiceConfiguration.configurations
       .findOne({ service: 'github' });
-
-      // If data in dbs is different, modify loginPlatforms according to
-      // ServiceConfiguration
 
       // Check Github
       if (!configGithubParameters) {
@@ -138,10 +139,9 @@ Meteor.methods({
       } else if (!loginParameters ||
                 !loginParameters.githubConfiguration ||
                 loginParameters.githubConfiguration.clientId !==
-                  configGithubParameters.clientId   ||
+                  configGithubParameters.clientId ||
                 loginParameters.githubConfiguration.secret !==
-                  configGithubParameters.secret      ) {
-
+                  configGithubParameters.secret) {
         // values from configuration
         const githubConfiguration = {
           clientId: configGithubParameters.clientId,
@@ -152,6 +152,7 @@ Meteor.methods({
         changesFound = true;
       }
 
+      // Get parameters for fiware from configuration DB
       const configFiwareParameters = ServiceConfiguration.configurations
       .findOne({ service: 'fiware' });
 
@@ -165,12 +166,11 @@ Meteor.methods({
       } else if (!loginParameters ||
                 !loginParameters.fiwareConfiguration ||
                 loginParameters.fiwareConfiguration.clientId !==
-                  configFiwareParameters.clientId   ||
+                  configFiwareParameters.clientId ||
                 loginParameters.fiwareConfiguration.secret !==
-                  configFiwareParameters.secret     ||
+                  configFiwareParameters.secret ||
                 loginParameters.fiwareConfiguration.rootURL !==
-                  configFiwareParameters.rootURL ) {
-
+                  configFiwareParameters.rootURL) {
         // values from configuration
         const fiwareConfiguration = {
           clientId: configFiwareParameters.clientId,
@@ -181,7 +181,7 @@ Meteor.methods({
         loginParameters.fiwareConfiguration = fiwareConfiguration;
         changesFound = true;
       }
-
+      // Get parameters for OIDC from configuration DB
       const configOidcParameters = ServiceConfiguration.configurations
       .findOne({ service: 'oidc' });
 
@@ -195,18 +195,19 @@ Meteor.methods({
       } else if (!loginParameters ||
                 !loginParameters.oidcConfiguration ||
                 loginParameters.oidcConfiguration.clientId !==
-                  configOidcParameters.clientId   ||
+                  configOidcParameters.clientId ||
                 loginParameters.oidcConfiguration.secret !==
-                  configOidcParameters.secret     ||
+                  configOidcParameters.secret ||
                 loginParameters.oidcConfiguration.serverUrl !==
                   configOidcParameters.serverUrl ||
                 loginParameters.oidcConfiguration.authorizationEndpoint !==
                   configOidcParameters.authorizationEndpoint ||
                 loginParameters.oidcConfiguration.tokenEndpoint !==
-                  configOidcParameters.tokenEndpoint  ||
+                  configOidcParameters.tokenEndpoint ||
                 loginParameters.oidcConfiguration.userinfoEndpoint !==
-                  configOidcParameters.userinfoEndpoint  ) {
-
+                  configOidcParameters.userinfoEndpoint ||
+                loginParameters.oidcConfiguration.idTokenWhitelistFields !==
+                  configOidcParameters.idTokenWhitelistFields) {
         // values from configuration
         const oidcConfiguration = {
           clientId: configOidcParameters.clientId,
@@ -215,6 +216,7 @@ Meteor.methods({
           authorizationEndpoint: configOidcParameters.authorizationEndpoint,
           tokenEndpoint: configOidcParameters.tokenEndpoint,
           userinfoEndpoint: configOidcParameters.userinfoEndpoint,
+          idTokenWhitelistFields: configOidcParameters.idTokenWhitelistFields || [],
         };
         // store OIDC values
         loginParameters.oidcConfiguration = oidcConfiguration;
@@ -228,7 +230,6 @@ Meteor.methods({
         }
         LoginPlatforms.insert(loginParameters);
       }
-
     } catch (error) {
       // otherwise show an error
       const message = `Update gitHub configuration: ${error}`;
@@ -238,6 +239,14 @@ Meteor.methods({
     }
 
     // Return method status to the client
-    return parameters;
+    return true;
   },
 });
+/*
+Meteor.loginWithOIDC({
+  requestPermissions: ['email']
+}, function (err) {
+  if (err)
+      console.log(err);
+});
+*/
