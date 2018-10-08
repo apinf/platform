2ffa8f3b06d55858d8c34ea6ba054a44bba61167
Matti Leppänen <matti.leppanen@apinf.io>
2018-10-08 12:58:25 +0300

    Removed not necessary import of HTTP

--- apinf_packages/oauth_hsl/server/hsl_server.js
+++ apinf_packages/oauth_hsl/server/hsl_server.js
@@ -3,8 +3,6 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-import { HTTP } from 'meteor/http';
-
 Hsl = {};
 
 OAuth.registerService('hsl', 2, null, function (query) {
