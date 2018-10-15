719435f459438b145716be101031809f757dd5b9
Nazarah <nhasan1987@gmail.com>
2018-10-15 14:57:02 +0300

    reactivated code to show pricing info in Next
    1. branding.html: added tab link for custom html block and markup to show the custom html block editor and save branding button.
    
    2. price.html and price.js: commented out contents in these files to resolve naming conflict of packages.
    
    3. htmlBlock.html: created template to show custom HTML Block editor in Branding menu
    
    4. htmlBlock.js: created code to make call to the custom HTML Block template.
    
    i18n.json: created new label "branding_homeCustomBlock_title": "Custom HTML Block"

--- apinf_packages/branding/client/custom_html_block/htmlBlock.js
+++ apinf_packages/branding/client/custom_html_block/htmlBlock.js
@@ -3,9 +3,7 @@ This file is covered by the EUPL license.
 You may obtain a copy of the licence at
 https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11 */
 
-// commenting this out for the time being to avoid package name conflict
 // Meteor packages imports
-/*
 import { Template } from 'meteor/templating';
 
 // Collection imports
@@ -17,4 +15,3 @@ Template.pricing.helpers({
     return Branding.findOne();
   },
 });
-*/
