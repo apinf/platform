768dd45eace9653b7e4e61d503e5bc5022d85596
Matti Leppänen <matti.leppanen@apinf.io>
2019-03-25 16:40:44 +0200

    Show Edit and Remove buttons only for tenant owner

--- apinf_packages/tenant/client/catalog/table/table.js
+++ apinf_packages/tenant/client/catalog/table/table.js
@@ -5,31 +5,19 @@
  */
 
 // Meteor packages imports
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-Template.organizationCatalogTable.onCreated(function () {
-  this.autorun(() => {
-    // Get organizations
-   // const organizations = Template.currentData().organizations;
-    const organizations = [
-      { name: 'testinimi 1' },
-      { name: 'testinimi 2' },
-      { name: 'testinimi 3' },
-      { name: 'testinimi 4' },
-      { name: 'testinimi 5' },
-      { name: 'testinimi 6' },
-    ];
+Template.tenantCatalogTable.helpers({
+  tenantOwner (tenantOwnerId) {
+    // Get user id
+    const userId = Meteor.userId();
+    const user = Meteor.users.findOne(userId);
 
-    const organizationLogoIds = [];
-
-    // Get list of logo IDs
-    organizations.forEach((organization) => {
-      if (organization.organizationLogoFileId) {
-        organizationLogoIds.push(organization.organizationLogoFileId);
-      }
-    });
-
-    // Subscribe to Organization logo collection by logo IDs
-    this.subscribe('organizationLogosByIds', organizationLogoIds);
-  });
+    // Only Tenant owner is able to modify and remove
+    if (user && user.services && user.services.fiware && user.services.fiware.id) {
+      return user.services.fiware.id === tenantOwnerId;
+    }
+    return false;
+  },
 });
