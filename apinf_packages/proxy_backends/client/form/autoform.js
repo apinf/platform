162669814846901eb294574434549201e991aefd
Matti Leppänen <matti.leppanen@apinf.io>
2019-04-11 16:29:56 +0300

    db schema fixes

--- apinf_packages/proxy_backends/client/form/autoform.js
+++ apinf_packages/proxy_backends/client/form/autoform.js
@@ -24,6 +24,7 @@ import requiredFieldsFilled from './required_fields';
 AutoForm.hooks({
   proxyBackendForm: {
     formToModifier: (doc) => {
+      console.log('form-to-modifier');
       if (doc.$set['apiUmbrella.sub_settings']) {
         doc.$set['apiUmbrella.sub_settings'] = _.compact(doc.$set['apiUmbrella.sub_settings']);
       }
@@ -45,6 +46,7 @@ AutoForm.hooks({
     },
     before: {
       insert (proxyBackend) {
+        console.log('inserttiä puskee');
         // TODO: Refactor this method. It is too long and complex
 
         // Get reference to autoform instance, for form submission callback
@@ -129,11 +131,16 @@ AutoForm.hooks({
 
           // Save proxy backend
           form.result(proxyBackend);
+        } else if (proxyBackend.type === 'proxy42') {
+          console.log('proxy 42 talteen=', proxyBackend);
+          // Save proxy backend
+          form.result(proxyBackend);
         }
         // Autoform does not expect anything to be returned
         return undefined;
       },
       update (updateDoc) {
+        console.log('updatea tehtään');
         // TODO: Refactor this method. It is too long and complex
 
         // Get reference to autoform instance, for form submission callback
