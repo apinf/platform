40c787cde51e416367d0e5df896febb9d0114079
illya <in11092008@gmail.com>
2019-07-09 13:43:58 +0300

    Show token

--- apinf_packages/tenant/client/authorization/authorization.js
+++ apinf_packages/tenant/client/authorization/authorization.js
@@ -6,19 +6,20 @@ https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence
 // Meteor packages imports
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 
 // Meteor contributed packages imports
 import { TAPi18n } from 'meteor/tap:i18n';
 import moment from 'moment';
+import { sAlert } from 'meteor/juliancwirko:s-alert';
 
 Template.authorizationForm.helpers({
   tenantToken () {
-    // Get user id
-    const userId = Meteor.userId();
-    const user = Meteor.users.findOne(userId);
+    const tenantTokenObj = Session.get('tenantTokenObj');
 
-    if (user && user.services && user.services.fiware) {
-      return user.services.fiware.accessToken;
+    if (tenantTokenObj) {
+      const str = JSON.stringify(tenantTokenObj, null, 4);
+      return str;
     }
     return false;
   },
@@ -57,6 +58,53 @@ Template.authorizationForm.helpers({
 
 Template.authorizationForm.events({
   'click #refreshTenantAuthorization': function () {
-    // TODO Here the refresh functionality, if needed
+    // Get processing message translation
+    const message = TAPi18n.__('apiKeys_getApiKeyButton_processing');
+
+    const tenantPassword = $('#tenantPasswordInput').val();
+
+    if ($('#tenantPasswordInput').val() === '') {
+      sAlert.error('Enter password', { timeout: 'none' });
+    } else {
+      // Set bootstrap loadingText
+      $('#refreshTenantAuthorization').button({ loadingText: message });
+
+      // Set button to processing state
+      $('#refreshTenantAuthorization').button('loading');
+
+      Meteor.call('getTenantTokenObj', tenantPassword, (error, result) => {
+        if (error) {
+          sAlert.error(error);
+        } else if (result.response && result.response.data.error === 'invalid_grant') {
+          // Show error
+          sAlert.error('Incorrect password.');
+
+          // Reset processing button
+          $('#refreshTenantAuthorization').button('reset');
+        } else {
+          // Get success message translation
+          const successMessage = 'We have successfully got you a new token!';
+
+          // Alert the user of success
+          sAlert.success(successMessage);
+
+          // Reset processing button
+          $('#refreshTenantAuthorization').button('reset');
+
+          Session.set('tenantTokenObj', result.data);
+        }
+      });
+
+      $('#tenantPasswordInput').val('');
+    }
+  },
+  'click .toggle-password': function () {
+    // Show and hide password
+    $('.toggle-password').toggleClass('mdi-eye mdi-eye-off');
+
+    const input = $('#tenantPasswordInput');
+
+    return input.attr('type') === 'password' ?
+    input.attr('type', 'text') : input.attr('type', 'password');
   },
 });
